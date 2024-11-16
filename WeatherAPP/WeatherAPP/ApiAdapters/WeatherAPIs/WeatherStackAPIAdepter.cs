71daf181c5f5ca66ffd88656e012d440468b3870
sakhadib <adibsakhawat@iut-dhaka.edu>
2024-11-16 14:44:28 +0600

    Weather Stack done

--- WeatherAPP/WeatherAPP/ApiAdapters/WeatherAPIs/WeatherStackAPIAdepter.cs
+++ WeatherAPP/WeatherAPP/ApiAdapters/WeatherAPIs/WeatherStackAPIAdepter.cs
@@ -4,13 +4,28 @@ public class WeatherStackAPIAdapter : IWeatherAdapter
 {
     private string _city;
 
+    private readonly string _url =
+        "http://api.weatherstack.com/current?access_key=abad40a649a54808aaaaeed9be38e6e7&query=";
+
     public WeatherStackAPIAdapter(string city)
     {
         _city = city;
+        _url = _url + city;
     }
     
     public string GetWeather()
     {
-        
+        try
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                return client.GetStringAsync(_url).Result;
+            }
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine("Error Fetching Weather from Weather Stack" + exception.Message);
+            throw;
+        }
     }
 }
