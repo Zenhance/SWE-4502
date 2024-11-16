ffea89db021c4c552e7ef1bfa5554814081b6078
sakhadib <adibsakhawat@iut-dhaka.edu>
2024-11-16 16:09:38 +0600

    Is location Available

--- WeatherAPP/WeatherAPP/Service/IsLocationAvailableService.cs
+++ WeatherAPP/WeatherAPP/Service/IsLocationAvailableService.cs
@@ -1,4 +1,5 @@
-﻿using WeatherAPP.Environment;
+﻿using System.Text.Json;
+using WeatherAPP.Environment;
 
 namespace WeatherAPP.Service;
 
@@ -13,8 +14,32 @@ public class IsLocationAvailableService
         _location = WEnvironment.GetLocationFilePath();
     }
 
-    public void Execute()
+    public bool Execute(string ip)
     {
+        try
+        {
+            JsonDocument locationJson = new JSONReaderService().ReadJSON(_location);
+
+            JsonElement root = locationJson.RootElement;
+
+            if (root.ValueKind == JsonValueKind.Array)
+            {
+                foreach (JsonElement item in root.EnumerateArray())
+                {
+                    if (item.TryGetProperty("ip", out JsonElement ipElement) && ipElement.GetString() == ip)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error while checking IP: {ex.Message}");
+            return false;
+        }
         
     }
 }
