a509cfb9fd87a2b3d66a219654aaa697a367c169
Ashik150 <sskhan4409@gmail.com>
2024-11-19 22:14:22 +0600

    weather info fetch for city

--- FindWeather/Lab08-210042150/Model/WeatherService.java
+++ FindWeather/Lab08-210042150/Model/WeatherService.java
@@ -2,8 +2,20 @@ package Model;
 
 import Interfaces.IWeatherService;
 
+import WeatherAPI.weatherAPIUtil;
+
 import java.io.IOException;
 
 public class WeatherService implements IWeatherService {
     private static final String WEATHERSTACK_API_KEY = "0d65452eba6c09f62cc24ff0381ab83e";
+
+    @Override
+    public void getWeatherByCity(String city) throws IOException {
+        System.out.println("Getting weather data for city: " + city);
+
+        String weatherResponse = weatherAPIUtil.getResponse(
+                "http://api.weatherstack.com/current?access_key=" + WEATHERSTACK_API_KEY + "&query=" + city
+        );
+
+    }
 }
