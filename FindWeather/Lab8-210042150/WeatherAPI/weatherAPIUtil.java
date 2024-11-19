0b2db96d2320a879ba916409653b4c663e417a52
Ashik150 <sskhan4409@gmail.com>
2024-11-19 23:24:07 +0600

    API URL object set

---
+++ FindWeather/Lab8-210042150/WeatherAPI/weatherAPIUtil.java
@@ -0,0 +1,10 @@
+package WeatherAPI;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class weatherAPIUtil {
+    public static String getResponse(String urlString) throws IOException {
+        URL url = new URL(urlString);
+    }
+}
