0915fc37f982a8fafe27accb2d54f1d88e09fa46
Ashik150 <sskhan4409@gmail.com>
2024-11-19 23:36:48 +0600

    Get ip address fom ipify and location info from ipstack

--- FindWeather/Lab8-210042150/Model/LocationService.java
+++ FindWeather/Lab8-210042150/Model/LocationService.java
@@ -1,9 +1,19 @@
 package Model;
 
 import Interfaces.ILocationService;
+import WeatherAPI.weatherAPIUtil;
+
+import java.io.IOException;
 
 public class LocationService implements ILocationService {
     private static final String IPSTACK_API_KEY = "4a075c3610c3cba539c3fd2aca4d76ab";
     private String latitude;
     private String longitude;
+
+    @Override
+    public void getLocationByIP() throws IOException {
+        String ip = weatherAPIUtil.getResponse("https://api.ipify.org");
+        String locationResponse = weatherAPIUtil.getResponse(
+                "http://api.ipstack.com/" + ip + "?access_key=" + IPSTACK_API_KEY
+    }
 }
