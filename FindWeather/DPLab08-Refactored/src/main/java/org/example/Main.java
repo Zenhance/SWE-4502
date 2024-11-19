0f57994b776340ee3a70e91f3bc8fd3109a927e0
nstuli <nusratsiddiquetuli@gmail.com>
2024-11-20 01:10:27 +0600

    ok

--- FindWeather/DPLab08-Refactored/src/main/java/org/example/Main.java
+++ FindWeather/DPLab08-Refactored/src/main/java/org/example/Main.java
@@ -1,11 +1,9 @@
 package org.example;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+
 
 public class Main {
     public static void main(String[] args) {
-        // Print Hello, World! to the console
+        
         System.out.println("Hello, World!");
     }
 }
