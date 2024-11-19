3fe848e3125d8458258a7f0430ba6a8924e40142
sheona001 <sirexcc001@gmail.com>
2024-11-20 00:13:59 +0600

    WeatherCache ful

--- FindWeather/210042111_Lab08/WeatherCache.cs
+++ FindWeather/210042111_Lab08/WeatherCache.cs
@@ -1,6 +1,15 @@
-﻿namespace _210042111_Lab08
+﻿using System;
+
+namespace _210042111_Lab08
 {
-    internal class WeatherCache
-    {
+    public class WeatherCache
+    {   public WeatherData WeatherData { get; }
+        public DateTime Timestamp { get; }
+
+        public WeatherCache(WeatherData weatherData, DateTime timestamp)
+        {
+            WeatherData = weatherData;
+            Timestamp = timestamp;
+        }
     }
 }
