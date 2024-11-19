4499e7c6696b9b17788a7e74fc46994cc9cdc024
Ashik150 <sskhan4409@gmail.com>
2024-11-19 22:07:11 +0600

    WeatherStack APIKEY for weather info fetch

--- FindWeather/Lab08-210042150/Model/WeatherService.java
+++ FindWeather/Lab08-210042150/Model/WeatherService.java
@@ -5,5 +5,5 @@ import Interfaces.IWeatherService;
 import java.io.IOException;
 
 public class WeatherService implements IWeatherService {
-
+    private static final String WEATHERSTACK_API_KEY = "0d65452eba6c09f62cc24ff0381ab83e";
 }
