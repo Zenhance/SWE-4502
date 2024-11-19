60545ec24c4fa8edd0db94ff54c9316f31d69a02
Ashik150 <sskhan4409@gmail.com>
2024-11-20 00:35:04 +0600

    Client code WeatherConsoleApp declared

---
+++ FindWeather/Lab8-210042150/Main/WeatherConsoleApp.java
@@ -0,0 +1,10 @@
+package Main;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class WeatherConsoleApp {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
