9690fc715ed8b19bb404c04a961845ecec8eef9b
Ashik150 <sskhan4409@gmail.com>
2024-11-19 20:02:47 +0600

    LocationService Interface Done

--- FindWeather/Lab08-210042150/Interfaces/ILocationService.java
+++ FindWeather/Lab08-210042150/Interfaces/ILocationService.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 public interface ILocationService {
     void getLocationByIP() throws IOException;
-
     String getCity() throws IOException;
+    String getLatitude();
+    String getLongitude();
 }
