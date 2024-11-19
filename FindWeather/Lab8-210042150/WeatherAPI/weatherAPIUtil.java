9126c8d10170f572f33286b2048b8c6f9aa7844b
Ashik150 <sskhan4409@gmail.com>
2024-11-19 23:26:36 +0600

    Initialize a scanner and create a stringbuilder

--- FindWeather/Lab8-210042150/WeatherAPI/weatherAPIUtil.java
+++ FindWeather/Lab8-210042150/WeatherAPI/weatherAPIUtil.java
@@ -3,6 +3,7 @@ package WeatherAPI;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Scanner;
 
 public class weatherAPIUtil {
     public static String getResponse(String urlString) throws IOException {
@@ -15,5 +16,9 @@ public class weatherAPIUtil {
         if (responseCode != 200) {
             throw new IOException("HTTP response code: " + responseCode);
         }
+
+        Scanner scanner = new Scanner(url.openStream());
+        StringBuilder response = new StringBuilder();
+
     }
 }
