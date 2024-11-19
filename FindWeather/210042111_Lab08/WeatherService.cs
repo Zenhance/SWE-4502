94f697da27de78904a0f82b19ffe007ab0fc62fe
sheona001 <sirexcc001@gmail.com>
2024-11-19 23:15:33 +0600

    interface class create

--- FindWeather/210042111_Lab08/WeatherService.cs
+++ FindWeather/210042111_Lab08/WeatherService.cs
@@ -3,5 +3,6 @@ namespace _210042111_Lab08
 {public class WeatherService
     { WeatherFacade weatherFacade = new WeatherFacade();
         bool exit = false;
+        public void Go() { while(!exit) { } }
     }
 }
