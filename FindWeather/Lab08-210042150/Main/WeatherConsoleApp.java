b3fad57b6fba9c6c9978c9a8ae38621ee655d87a
Ashik150 <sskhan4409@gmail.com>
2024-11-19 22:32:40 +0600

    object creation

--- FindWeather/Lab08-210042150/Main/WeatherConsoleApp.java
+++ FindWeather/Lab08-210042150/Main/WeatherConsoleApp.java
@@ -1,4 +1,18 @@
 package Main;
 
+import java.io.IOException;
+import java.util.Scanner;
+import Interfaces.IWeatherService;
+import Interfaces.ILocationService;
+import Model.LocationService;
+import Model.WeatherService;
+
+
 public class WeatherConsoleApp {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        ILocationService locationService = new LocationService();
+        IWeatherService weatherService = new WeatherService();
+    }
 }
