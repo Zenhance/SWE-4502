f6c3a8f6f99f124ffa35385abe75b6c34a953588
Ashik150 <sskhan4409@gmail.com>
2024-11-19 20:01:14 +0600

    LocationService Interface Declared

---
+++ FindWeather/Lab08-210042150/Interfaces/ILocationService.java
@@ -0,0 +1,9 @@
+package Interfaces;
+
+import java.io.IOException;
+
+public interface ILocationService {
+    void getLocationByIP() throws IOException;
+
+    String getCity() throws IOException;
+}
