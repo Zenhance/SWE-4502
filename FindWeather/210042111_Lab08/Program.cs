3391e9548418dcf0b556ae33a5a03b422f1972b0
sheona001 <sirexcc001@gmail.com>
2024-11-20 00:18:30 +0600

    main class fix

--- FindWeather/210042111_Lab08/Program.cs
+++ FindWeather/210042111_Lab08/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using lab08_210042111;
 namespace _210042111_Lab08
 {
     internal class Program
