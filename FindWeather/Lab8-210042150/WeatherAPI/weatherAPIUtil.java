bf5050e24b3c54680718862abf0fbd28de5087e8
Ashik150 <sskhan4409@gmail.com>
2024-11-19 23:27:30 +0600

    scan each line and return the response through getresponse method

--- FindWeather/Lab8-210042150/WeatherAPI/weatherAPIUtil.java
+++ FindWeather/Lab8-210042150/WeatherAPI/weatherAPIUtil.java
@@ -19,6 +19,12 @@ public class weatherAPIUtil {
 
         Scanner scanner = new Scanner(url.openStream());
         StringBuilder response = new StringBuilder();
+        while (scanner.hasNext()) {
+            response.append(scanner.nextLine());
+        }
+        scanner.close();
+
+        return response.toString();
 
     }
 }
