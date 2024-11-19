ca4fbab3607dfc0bd207347aaf8b2754c622b4c4
sheona001 <sirexcc001@gmail.com>
2024-11-20 00:21:57 +0600

    proxy class instantitation

--- FindWeather/210042111_Lab08/WeatherServiceProxy.cs
+++ FindWeather/210042111_Lab08/WeatherServiceProxy.cs
@@ -1,7 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace _210042111_Lab08
 {
-    public class WeatherServiceProxy
+    public class WeatherServiceProxy: IWeatherService
     {
+        private readonly IWeatherService weatherService;
+        private readonly Dictionary<string, WeatherCache> cache = new Dictionary<string, WeatherCache>();
+        private static readonly TimeSpan CacheDuration = TimeSpan.FromMinutes(10);
+        private static readonly TimeSpan RateLimitDuration = TimeSpan.FromSeconds(30);
     }
 }
