08b6830bb1fb473bb52a21741b48fe27d7638db2
Ashik150 <sskhan4409@gmail.com>
2024-11-19 23:24:42 +0600

    connection set Req method set up

--- FindWeather/Lab8-210042150/WeatherAPI/weatherAPIUtil.java
+++ FindWeather/Lab8-210042150/WeatherAPI/weatherAPIUtil.java
@@ -1,10 +1,14 @@
 package WeatherAPI;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class weatherAPIUtil {
     public static String getResponse(String urlString) throws IOException {
         URL url = new URL(urlString);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.connect();
     }
 }
