c58fa756c31ff82c02d949a086a2f617fbaf74c9
Tashfia <tashfiahassan@iut-dhaka.edu>
2024-11-21 04:28:37 +0600

    new

--- FindWeather/lab08/main.java
+++ FindWeather/lab08/main.java
@@ -21,4 +21,7 @@ public class main {
     public static void main(String[] args) throws IOException, JSONException {
         Scanner scanner = new Scanner(System.in);
         RealWeatherService weatherService = new RealWeatherService();
-}
+        while (true) {
+            System.out.println("Select an option:");
+            System.out.println("1. Choose Weather Service");
+
