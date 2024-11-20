fd5a92faebc4ba44360e77d5bb2d1d3682df2b85
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2024-11-20 11:07:17 +0600

    feat: properties for abstract weather service

--- FindWeather/src/abstracts/AbstractWeatherService.java
+++ FindWeather/src/abstracts/AbstractWeatherService.java
@@ -1,11 +1,16 @@
 package abstracts;
 
+import java.net.http.HttpClient;
 import java.util.concurrent.CompletableFuture;
 
 import behaviours.IweatherService;
 import models.WeatherData;
+import services.LocationService;
 
 public class AbstractWeatherService implements IweatherService {
+    protected final String API_Key;
+    protected LocationService locationService;
+    protected HttpClient httpClient;
 
     @Override
     public CompletableFuture<WeatherData> getWeatherDataByIP() throws Exception {
