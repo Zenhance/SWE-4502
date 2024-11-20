d840cf5a920ca664a654ac3e4c33dca6683123bd
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2024-11-20 10:22:13 +0600

    feature: WeatherData properties

---
+++ FindWeather/src/models/WeatherData.java
@@ -0,0 +1,9 @@
+package models;
+
+public class WeatherData {
+    private double temperature;
+    private String conditions;
+    private String location;
+    private String source;
+    private long timestamp;
+}
