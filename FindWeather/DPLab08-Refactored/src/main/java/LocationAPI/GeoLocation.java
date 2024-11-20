fcf2c30e7fb028a49165972092d52eea9ceeec2c
nstuli <nusratsiddiquetuli@gmail.com>
2024-11-20 22:16:06 +0600

    Update GeoLocation.java

--- FindWeather/DPLab08-Refactored/src/main/java/LocationAPI/GeoLocation.java
+++ FindWeather/DPLab08-Refactored/src/main/java/LocationAPI/GeoLocation.java
@@ -1,4 +1,5 @@
 package LocationAPI;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -7,5 +8,21 @@ import java.net.URL;
 public class GeoLocation {
     private static final String GET_URL = "http://api.weatherstack.com/current?access_key=";
     private static final String WEATHERSTACK_API_KEY = "23be6ddf45a59d7958e26dd3e1fb3e77";
+
+    String urlString = GET_URL + WEATHERSTACK_API_KEY + "&query=" + city;
+    URL url = new URL(urlString);
+    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+
+    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+    StringBuilder response = new StringBuilder();
+
+    String line;
+        while ((line = reader.readLine()) != null) {
+        response.append(line);
+    }   reader.close();
+
+
+
 
 }
