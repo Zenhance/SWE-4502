dae0210c1dcb1420d73babeccdb7ef30f5a41bd3
nstuli <nusratsiddiquetuli@gmail.com>
2024-11-20 21:53:08 +0600

    added weather model

--- FindWeather/DPLab08-Refactored/src/main/java/Model/Weather.java
+++ FindWeather/DPLab08-Refactored/src/main/java/Model/Weather.java
@@ -1,4 +1,20 @@
 package Model;
 
 public class Weather {
+    public Location location;
+    public double temperature;
+    public String weather_condition;
+    public String WeatherAPI_name;
+
+    public Weather(double temperature, String weather_condition, Location location, String WeatherAPI_name) {
+        this.temperature = temperature;
+        this.weather_condition = weather_condition;
+        this.location = location;
+        this.WeatherAPI_name = WeatherAPI_name;
+    }
+
+    public void DisplayWeather() {
+        System.out.println("Weather in " + location.getCity() + " from " + WeatherAPI_name + ": "
+                + temperature + "°C, " + weather_condition);
+    }
 }
