9fb52e2a5895bf8190caa54ec962872e104f52e9
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2024-11-21 00:28:22 +0600

    update WeatherCache: added CacheEntry

--- FindWeather/WeatherApp/src/Models/WeatherData.java
+++ FindWeather/WeatherApp/src/Models/WeatherData.java
@@ -1,17 +1,20 @@
 package Models;
 
 public class WeatherData {
-   public String temperature;
-   public  String conditions;
-   public  String locationInformation;
-   public  String source;
+   public Double temperature;
+   public String conditions;
+   public String locationInformation;
+   public String source;
 
-    public WeatherData(String temperature, String conditions, String locationInformation, String source){
+    public WeatherData(Double temperature, String conditions, String locationInformation, String source){
         this.temperature = temperature;
         this.conditions = conditions;
         this.locationInformation = locationInformation;
         this.source = source;
     }
 
-    
+    public void displayData() {
+        System.out.println("Data retrieved from " + source);
+        System.out.printf("Weather in %s from %s: %.2f°C, %s%n", locationInformation, source, temperature, conditions);
+    }
 }
