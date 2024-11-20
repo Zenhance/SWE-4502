3e5979552fe53bf38914ef2359e4995c573fd6d4
sheona001 <sirexcc001@gmail.com>
2024-11-21 00:22:28 +0600

    weatherstack3

--- FindWeather/210042111_Lab08/WeatherStackAdapters.cs
+++ FindWeather/210042111_Lab08/WeatherStackAdapters.cs
@@ -4,12 +4,15 @@ using System.Net;
 using System.Threading.Tasks;
 namespace _210042111_Lab08
 {
-    public class WeatherStackAdapters
+    public class WeatherStackAdapters:IWeatherService
     {
         string ApiKey = "309d469af3c87de70e7d58bfa498d1e9"; 
         public async Task<WeatherData> GetWeather(string cityName)
         {
             string apiUrl = $"http://api.weatherstack.com/current?access_key={ApiKey}&query={cityName}";
             string response = await SendGetRequestAsync(apiUrl);
+            JObject json = JObject.Parse(response);
+            string weatherCondition = json["current"]["weather_descriptions"][0].ToString();
+            double temperature = (double)json["current"]["temperature"];
         }
 }
