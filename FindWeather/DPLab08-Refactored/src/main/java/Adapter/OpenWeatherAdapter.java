1d8bb2e7001e6fdaec807c7abd2af27aedc0b073
nstuli <nusratsiddiquetuli@gmail.com>
2024-11-20 22:39:02 +0600

    ok

--- FindWeather/DPLab08-Refactored/src/main/java/Adapter/OpenWeatherAdapter.java
+++ FindWeather/DPLab08-Refactored/src/main/java/Adapter/OpenWeatherAdapter.java
@@ -1,4 +1,12 @@
 package Adapter;
 
+
+import Model.Location;
+import Model.Weather;
+import WeatherAPI.OpenWeatherAPI;
+import org.json.JSONException;
+
+import java.io.IOException;
+
 public class OpenWeatherAdapter {
 }
