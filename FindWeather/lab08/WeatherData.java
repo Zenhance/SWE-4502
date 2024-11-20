d401aa83eb452bb0d5dcd13461d92e1526fea876
Tashfia <tashfiahassan@iut-dhaka.edu>
2024-11-21 02:37:45 +0600

    h

--- FindWeather/lab08/WeatherData.java
+++ FindWeather/lab08/WeatherData.java
@@ -3,4 +3,11 @@ public class WeatherData {
      String condition;
      String source;
 
+    public WeatherData(String temperature, String condition, String source) {
+        this.temperature = temperature;
+        this.condition = condition;
+        this.source = source;
+    }
+
+    @Override
 }
