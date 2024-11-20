3eca4466dce88666dbbb4521615f968b1c98a53e
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2024-11-20 23:16:02 +0600

    access change: public attributes

--- FindWeather/WeatherApp/src/Models/WeatherData.java
+++ FindWeather/WeatherApp/src/Models/WeatherData.java
@@ -1,15 +1,17 @@
 package Models;
 
 public class WeatherData {
-    String temperature;
-     String conditions;
-     String locationInformation;
-     String source;
+   public String temperature;
+   public  String conditions;
+   public  String locationInformation;
+   public  String source;
 
-     public WeatherData(String temperature, String conditions, String locationInformation, String source){
+    public WeatherData(String temperature, String conditions, String locationInformation, String source){
         this.temperature = temperature;
         this.conditions = conditions;
         this.locationInformation = locationInformation;
         this.source = source;
-     }
+    }
+
+    
 }
