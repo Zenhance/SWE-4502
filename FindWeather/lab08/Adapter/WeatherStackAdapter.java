3212fb46ed115077065fb010808892cf23506cc7
Tashfia <tashfiahassan@iut-dhaka.edu>
2024-11-21 04:11:29 +0600

    new

--- FindWeather/lab08/Adapter/WeatherStackAdapter.java
+++ FindWeather/lab08/Adapter/WeatherStackAdapter.java
@@ -5,5 +5,10 @@ public class WeatherStackAdapter {
     public Weather getWeather(Location location) throws IOException {
         try {
             return WeatherStackAPI.Get_Weather(location);
+        }catch (org.json.JSONException e) {
+            System.err.println("Error parsing JSON in WeatherStackAPI: " + e.getMessage());
+
+            return null;
         }
+    }
     }
