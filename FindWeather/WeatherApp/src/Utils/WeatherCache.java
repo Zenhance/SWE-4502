5f308af21391d44e2240293c67bc5d812931678d
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2024-11-21 00:28:47 +0600

    update WeatherCache: added CacheEntry

--- FindWeather/WeatherApp/src/Utils/WeatherCache.java
+++ FindWeather/WeatherApp/src/Utils/WeatherCache.java
@@ -1,9 +1,20 @@
 package Utils;
 
+import java.time.Instant;
 import java.util.HashMap;
 import Models.WeatherData;
 
 public class WeatherCache {
     private static final long CACHE_DURATION = 10 * 60; // 600 seconds
-    private final HashMap<String, WeatherData> cache = new HashMap<>();
+    private final HashMap<String, CacheEntry> cache = new HashMap<>();
+
+    private  class CacheEntry {
+        WeatherData data;
+        Instant timestamp;
+
+        CacheEntry(WeatherData data, Instant timestamp) {
+            this.data = data;
+            this.timestamp = timestamp;
+        }
+    }
 }
