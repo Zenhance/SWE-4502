20861379eb80386c1aa2d9ce333ede710b1e8aef
Tashfia <tashfiahassan@iut-dhaka.edu>
2024-11-21 04:19:15 +0600

    new

---
+++ FindWeather/lab08/API/OpenWeatherAPI.java
@@ -0,0 +1,14 @@
+package API;
+import Model.Location;
+import Model.Weather;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class OpenWeatherAPI {
+}
