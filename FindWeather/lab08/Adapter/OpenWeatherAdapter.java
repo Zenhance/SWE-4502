5aab84e9189bd6ab32432977fbae720ffadcff19
Tashfia <tashfiahassan@iut-dhaka.edu>
2024-11-21 04:09:52 +0600

    new

--- FindWeather/lab08/Adapter/OpenWeatherAdapter.java
+++ FindWeather/lab08/Adapter/OpenWeatherAdapter.java
@@ -1,4 +1,11 @@
 package Adapter;
+import Model.Location;
+import Model.Weather;
+import WeatherAPI.OpenWeatherAPI;
+import org.json.JSONException;
+
+import java.io.IOException;
+
 
 public class OpenWeatherAdapter implements IWeatherAdapter{
     @Override
