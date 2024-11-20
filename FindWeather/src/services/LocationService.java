050517f4c93776d40600f67635bc0f35751fb835
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2024-11-20 10:30:29 +0600

    feat: Implement ILocationService to concrete LocationService

---
+++ FindWeather/src/services/LocationService.java
@@ -0,0 +1,14 @@
+package services;
+
+import java.util.concurrent.CompletableFuture;
+
+import behaviours.ILocationService;
+import models.LocationData;
+
+public class LocationService implements ILocationService {
+
+    @Override
+    public CompletableFuture<LocationData> getLocationData() {
+
+    }
+}
