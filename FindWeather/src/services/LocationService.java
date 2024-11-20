1f9862985a91bbdd140271364b9c31566a23e2ff
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2024-11-20 10:34:44 +0600

    feat: Added API urls

--- FindWeather/src/services/LocationService.java
+++ FindWeather/src/services/LocationService.java
@@ -1,11 +1,15 @@
 package services;
 
+import java.net.http.HttpClient;
 import java.util.concurrent.CompletableFuture;
 
 import behaviours.ILocationService;
 import models.LocationData;
 
 public class LocationService implements ILocationService {
+    private final String IPIFY_URL = "https://api.ipify.org/";
+    private final String GEOLOCATION_URL = "http://ip-api.com/json/";
+    private HttpClient httpClient;
 
     @Override
     public CompletableFuture<LocationData> getLocationData() {
