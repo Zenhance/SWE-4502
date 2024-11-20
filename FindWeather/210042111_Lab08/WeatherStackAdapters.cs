3350a8b289cf6c246c7b778a5eed0d7d98f246d2
sheona001 <sirexcc001@gmail.com>
2024-11-21 00:21:33 +0600

    weatherstack3

--- FindWeather/210042111_Lab08/WeatherStackAdapters.cs
+++ FindWeather/210042111_Lab08/WeatherStackAdapters.cs
@@ -6,6 +6,8 @@ namespace _210042111_Lab08
 {
     public class WeatherStackAdapters
     {
-        string ApiKey = "309d469af3c87de70e7d58bfa498d1e9";
-    }
+        string ApiKey = "309d469af3c87de70e7d58bfa498d1e9"; 
+        public async Task<WeatherData> GetWeather(string cityName)
+        { 
+        }
 }
