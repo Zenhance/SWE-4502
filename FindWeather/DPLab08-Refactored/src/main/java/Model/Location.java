8975be29ff7f7c54c527077c36b723783542c926
nstuli <nusratsiddiquetuli@gmail.com>
2024-11-20 20:58:39 +0600

    loaction model

--- FindWeather/DPLab08-Refactored/src/main/java/Model/Location.java
+++ FindWeather/DPLab08-Refactored/src/main/java/Model/Location.java
@@ -2,4 +2,19 @@ package Model;
 
 
 public class Location {
+
+    private String city;
+    private double latitude;
+    private double longitude;
+
+    public Location(String city, double latitude, double longitude) {
+        this.city = city;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
 }
