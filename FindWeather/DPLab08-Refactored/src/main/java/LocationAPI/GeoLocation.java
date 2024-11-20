fba436d5a73e7ab68d92d53ce6bb0e411e9bb8d8
nstuli <nusratsiddiquetuli@gmail.com>
2024-11-20 22:06:28 +0600

    Create GeoLocation.java

---
+++ FindWeather/DPLab08-Refactored/src/main/java/LocationAPI/GeoLocation.java
@@ -0,0 +1,11 @@
+package LocationAPI;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class GeoLocation {
+    private static final String GET_URL = "http://api.weatherstack.com/current?access_key=";
+    private static final String WEATHERSTACK_API_KEY = "23be6ddf45a59d7958e26dd3e1fb3e77";
+
+}
