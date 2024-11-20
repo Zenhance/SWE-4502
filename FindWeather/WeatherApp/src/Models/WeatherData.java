e248886e4c799b2c4c72ed45a2926620897a266a
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2024-11-20 21:26:50 +0600

    model added: WeatherData

---
+++ FindWeather/WeatherApp/src/Models/WeatherData.java
@@ -0,0 +1,15 @@
+package Models;
+
+public class WeatherData {
+    String temperature;
+     String conditions;
+     String locationInformation;
+     String source;
+
+     public WeatherData(String temperature, String conditions, String locationInformation, String source){
+        this.temperature = temperature;
+        this.conditions = conditions;
+        this.locationInformation = locationInformation;
+        this.source = source;
+     }
+}
