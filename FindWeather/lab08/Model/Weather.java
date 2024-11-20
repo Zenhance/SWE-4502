c3e03c77e37705ddf63c00a3b664e0507411e9c5
Tashfia <tashfiahassan@iut-dhaka.edu>
2024-11-21 03:19:40 +0600

    jjj

--- FindWeather/lab08/Model/Weather.java
+++ FindWeather/lab08/Model/Weather.java
@@ -4,4 +4,8 @@ public class Weather {
     public Location location;
     public double temperature;
     public String weather_condition;
+    public String WeatherAPI_name;
+
+    public Weather(double temperature, String weather_condition, Location location, String WeatherAPI_name) {
+        this.temperature = temperature;
 }
