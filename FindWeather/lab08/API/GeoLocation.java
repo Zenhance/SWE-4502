2a98bd7205efed251e3f7a4e29097fbe8571bb68
Tashfia <tashfiahassan@iut-dhaka.edu>
2024-11-21 04:15:26 +0600

    new

---
+++ FindWeather/lab08/API/GeoLocation.java
@@ -0,0 +1,7 @@
+package API;
+
+public class GeoLocation {
+    private static final String GET_URL = "http://api.weatherstack.com/current?access_key=";
+    private static final String WEATHERSTACK_API_KEY = "23be6ddf45a59d7958e26dd3e1fb3e77";
+
+}
