9b4408bc0303affda14274f75822f92e84e12fcd
Tashfia <tashfiahassan@iut-dhaka.edu>
2024-11-21 04:07:00 +0600

    new

--- FindWeather/lab08/Adapter/IWeatherAdapter.java
+++ FindWeather/lab08/Adapter/IWeatherAdapter.java
@@ -1,4 +1,9 @@
 package Adapter;
+import Model.Location;
+import Model.Weather;
+import org.json.JSONException;
+
+import java.io.IOException;
 
 public interface IWeatherAdapter {
     Weather getWeather(Location location) throws IOException, JSONException;
