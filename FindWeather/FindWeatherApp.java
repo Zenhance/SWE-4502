6bfc27b312e19ba4b3a1410cdd0eeb4db5628fb0
AASani29 <alfey2001sani@gmail.com>
2024-11-20 20:29:27 +0600

    Main class created

---
+++ FindWeather/FindWeatherApp.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindWeatherApp {
+    public static void main(String[] args) throws Exception {
+
+    }
+}
