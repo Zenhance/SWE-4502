955c56113eee57225960a9ea985bc9860032f684
Mainul <mainulhasan@iut-dhaka.edu>
2024-11-20 21:18:37 +0600

    Commit number - 22

--- FindWeather/FindWeather - Mainul/src/concreteClasses/WeatherData.java
+++ FindWeather/FindWeather - Mainul/src/concreteClasses/WeatherData.java
@@ -5,4 +5,15 @@ public class WeatherData {
     private double latitude;
     private double longitude;
     private double temperature;
+    private String weatherCondition;
+    private String dataSource;
+
+    public WeatherData(String cityName, double lattitude, double longitude, double temperature, String weatherCondition, String dataSource){
+        this.cityName = cityName;
+        this.latitude = lattitude;
+        this. longitude = longitude;
+        this.temperature = temperature;
+        this.weatherCondition = weatherCondition;
+        this.dataSource = dataSource;
+    }
 }
