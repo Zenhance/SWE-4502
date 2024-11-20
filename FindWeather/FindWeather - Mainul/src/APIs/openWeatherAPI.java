081bfeb31580053e1fbe6e228ba0f2e0bd8cf67f
Mainul <mainulhasan@iut-dhaka.edu>
2024-11-20 22:17:51 +0600

    Commit number - 39

--- FindWeather/FindWeather - Mainul/src/APIs/openWeatherAPI.java
+++ FindWeather/FindWeather - Mainul/src/APIs/openWeatherAPI.java
@@ -8,5 +8,9 @@ class openWeatherAPI {
     private JSONObject allInfo;
     public openWeatherAPI(){
         api_key = "e3dfd49f7917fe082f153b64f7ab41b9";
+        base_url = "https://api.openweathermap.org/data/2.5/weather";
+    }
+    private void fetchData(String location){
+
     }
 }
