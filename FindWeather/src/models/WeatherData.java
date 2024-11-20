0ce0c56a8342899807afeb63469fadacb8ef609e
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2024-11-20 10:22:37 +0600

    feature: WeatherData constructor

--- FindWeather/src/models/WeatherData.java
+++ FindWeather/src/models/WeatherData.java
@@ -6,4 +6,12 @@ public class WeatherData {
     private String location;
     private String source;
     private long timestamp;
+
+    public WeatherData(double temperature, String conditions, String location, String source) {
+        this.temperature = temperature;
+        this.conditions = conditions;
+        this.location = location;
+        this.source = source;
+        this.timestamp = System.currentTimeMillis();
+    }
 }
