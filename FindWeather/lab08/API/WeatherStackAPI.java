b8c86320d7684ea85b11d1342a01b47bbb5f372f
Tashfia <tashfiahassan@iut-dhaka.edu>
2024-11-21 04:24:40 +0600

    new

---
+++ FindWeather/lab08/API/WeatherStackAPI.java
@@ -0,0 +1,17 @@
+package API;
+import Model.Location;
+import Model.Weather;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+public class WeatherStackAPI {
+    private static final String WeatherStack_API_KEY = "2e980aab13ac4d92aa70e13237f89212";
+
+    public static Weather Get_Weather(Location location) throws IOException, JSONException {
+        String urlString = "http://api.weatherstack.com/current?access_key=" + WeatherStack_API_KEY + "&query=" + location.getCity();
+
+}
