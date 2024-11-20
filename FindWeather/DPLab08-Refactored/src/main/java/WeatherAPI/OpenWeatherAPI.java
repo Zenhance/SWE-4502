b5c351853e246a416a64b7af5d818e9bb91a8edb
nstuli <nusratsiddiquetuli@gmail.com>
2024-11-20 22:22:14 +0600

    update ok

--- FindWeather/DPLab08-Refactored/src/main/java/WeatherAPI/OpenWeatherAPI.java
+++ FindWeather/DPLab08-Refactored/src/main/java/WeatherAPI/OpenWeatherAPI.java
@@ -12,4 +12,15 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class OpenWeatherAPI {
+
+    private static final String OpenWeather_API_KEY = "23be6ddf45a59d7958e26dd3e1fb3e77";
+
+    public static Weather Get_Weather(Location location) throws IOException, JSONException {
+        String urlString = "http://api.openweathermap.org/data/2.5/weather?q=" + location.getCity() + "&appid=" + OpenWeather_API_KEY + "&units=metric";
+        URL url = new URL(urlString);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        StringBuilder response = new StringBuilder();
 }
