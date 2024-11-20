786176a21b181d0196b9118de0771424746a6715
sheona001 <sirexcc001@gmail.com>
2024-11-21 00:04:42 +0600

    renew

---
+++ FindWeather/210042111_Lab08/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _210042111_Lab08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WeatherService weatherService = new WeatherService();
+
+            weatherService.Go();
+        }
+    }
+}
