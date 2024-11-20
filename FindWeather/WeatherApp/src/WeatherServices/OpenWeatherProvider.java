322443525267e47beb1ff0bafc8348f4144d7adf
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2024-11-20 23:16:43 +0600

    getWeatherData: api call and response handling

--- FindWeather/WeatherApp/src/WeatherServices/OpenWeatherProvider.java
+++ FindWeather/WeatherApp/src/WeatherServices/OpenWeatherProvider.java
@@ -1,6 +1,15 @@
 package WeatherServices;
 
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.io.IOException;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import Models.WeatherData;
+import org.json.simple.JSONArray;
 
 public class OpenWeatherProvider implements IWeatherProvider {
     String ApiKey = "8b2131d2c1d531dd655dde21ebfc3984";
@@ -10,9 +19,44 @@ public class OpenWeatherProvider implements IWeatherProvider {
     public WeatherData getWeatherData(String location){
         String Url = BaseUrl + location + "&appid=" + ApiKey;
         // need to call api and get response
-        // create weatherdata using the response body    
-    
-        WeatherData weatherData = new WeatherData("32", "Sunny", "Dhaka", "OpenWeather");
-        return weatherData;
+        HttpRequest request = HttpRequest.newBuilder()
+				.uri(URI.create(Url))
+				.method("GET", HttpRequest.BodyPublishers.noBody())
+				.build();
+		HttpResponse<String> response = null;
+		
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            WeatherData weatherData = null;
+
+            JSONParser parser = new JSONParser();
+            JSONObject jsonResponse = (JSONObject) parser.parse(response.body());
+
+            if (jsonResponse.containsKey("main") && jsonResponse.containsKey("weather")) {
+                
+                JSONObject main = (JSONObject) jsonResponse.get("main");
+                double temperature = (double) main.get("temp");
+                // Kelvin to Celcius
+                temperature -= 273.15;
+
+                
+                JSONArray weatherArray = (JSONArray) jsonResponse.get("weather");
+                String conditions = ((JSONObject) weatherArray.get(0)).get("description").toString();
+                String locationInformation = jsonResponse.get("name").toString(); // Location name
+
+            
+                weatherData = new WeatherData(String.valueOf(temperature), conditions, locationInformation, "OpenWeather");
+                
+                return weatherData;
+            } else {
+                System.out.println("Invalid response structure: " + jsonResponse);
+            }
+        } catch (IOException | InterruptedException | ParseException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
