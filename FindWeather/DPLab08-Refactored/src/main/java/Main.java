868c1c649b8317f9c2e083a83c310a28656b6914
nstuli <nusratsiddiquetuli@gmail.com>
2024-11-22 05:42:57 +0600

    main

--- FindWeather/DPLab08-Refactored/src/main/java/Main.java
+++ FindWeather/DPLab08-Refactored/src/main/java/Main.java
@@ -1,2 +1,51 @@
+import LocationAPI.GeoLocation;
+import Model.Location;
+import Model.Weather;
+import WeatherService.IWeatherService;
+import WeatherService.RealWeatherService;
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) throws IOException, JSONException {
+        Scanner scanner = new Scanner(System.in);
+        RealWeatherService weatherService = new RealWeatherService();
+    }
+     while (true) {
+        System.out.println("Select an option:");
+        System.out.println("1. Choose Weather Service");
+        System.out.println("2. Get Weather by City");
+        System.out.println("3. Exit");
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+
+        if (choice == 3) {
+            break;
+        }
+
+        switch (choice) {
+            case 1 -> {
+                System.out.println("Select Weather API: 1. WeatherStack, 2. OpenWeather");
+                int apiChoice = scanner.nextInt();
+                scanner.nextLine();
+
+                if (apiChoice == 1) {
+                    RealWeatherService.useWeatherStack = true;
+                    System.out.println("WeatherStack selected as the API.");
+                } else if (apiChoice == 2) {
+                    RealWeatherService.useWeatherStack = false;
+                    System.out.println("OpenWeather selected as the API.");
+                } else {
+                    System.out.println("Invalid choice. Using default API.");
+                }
+            }
+           
+        }
+    }
+
+        scanner.close();
 }
+
