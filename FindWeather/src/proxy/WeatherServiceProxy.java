743ada3e135f1c84fd9c97cabfbdc5bcd2858195
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2024-11-21 22:43:51 +0600

    Proxy get weather data

--- FindWeather/src/proxy/WeatherServiceProxy.java
+++ FindWeather/src/proxy/WeatherServiceProxy.java
@@ -2,8 +2,10 @@ package proxy;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import behaviours.IWeatherService;
+import models.WeatherData;
 import services.OpenWeatherService;
 import services.WeatherStackService;
 
@@ -19,5 +21,24 @@ public class WeatherServiceProxy {
 
     public void addWeatherService(IWeatherService service) {
         weatherServices.add(service);
+    }
+
+    public WeatherData getWeatherData() throws Exception {
+        // Check from cache
+
+        for (IWeatherService service : weatherServices) {
+            try {
+                CompletableFuture<WeatherData> dataFuture = service.getWeatherDataByIP();
+                WeatherData weatherData = dataFuture.get();
+
+                // Cache the data
+
+                return weatherData;
+            } catch (Exception e) {
+                System.out.println("Service failed: " + service.getClass().getSimpleName());
+            }
+        }
+
+        throw new Exception("All weather services failed");
     }
 }
