9d10e23165d713c7dada883f07d11153ea7bd1f5
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2024-11-21 19:46:43 +0600

    added WeatherStack: ApiKey and Url

---
+++ FindWeather/WeatherApp/src/WeatherServices/WeatherStackProvider.java
@@ -0,0 +1,8 @@
+package WeatherServices;
+
+public class WeatherStackProvider {
+    private final String ApiKey = "ce4b6d132e608ff12c4bd2724a3e6c64";
+    private final String BaseUrl = "http://api.weatherstack.com/current?access_key=";
+
+
+}
