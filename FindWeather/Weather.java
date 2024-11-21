3859037af81bff88047340de54467be2e302034f
tauhid-taaha <tauhidtaaha7@gmail.com>
2024-11-21 22:42:02 +0600

    weather class updated

---
+++ FindWeather/Weather.java
@@ -0,0 +1,19 @@
+public class Weather {
+    public Location location;
+    public double temperature;
+    public String weather_condition;
+    public String WeatherAPI_name;
+
+    public Weather(double temperature, String weather_condition, Location location, String WeatherAPI_name) {
+        this.temperature = temperature;
+        this.weather_condition = weather_condition;
+        this.location = location;
+        this.WeatherAPI_name = WeatherAPI_name;
+    }
+    public void getWeather() {
+        System.out.println("Weather in " + location.getCity() + " from " + WeatherAPI_name + ": "
+                + temperature +  + weather_condition);
+    }
+
+
+}
