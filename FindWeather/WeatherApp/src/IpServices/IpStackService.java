62c483a50fedef5f7e7e1508f62fd8531a5869d5
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2024-11-21 21:04:19 +0600

    updated: added getLocationByIp

--- FindWeather/WeatherApp/src/IpServices/IpStackService.java
+++ FindWeather/WeatherApp/src/IpServices/IpStackService.java
@@ -9,6 +9,8 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import Models.LocationData;
+
 public class IpStackService {
     private final String ApiKey = "6f97c01d7812a64550fb69c59a001387";
     private final String BaseUrl = "http://api.ipstack.com/";
@@ -33,6 +35,40 @@ public class IpStackService {
                 return jsonResponse.get("ip").toString();
             } else {
                 System.out.println("Invalid IP response structure: " + jsonResponse);
+            }
+        } catch (IOException | InterruptedException | ParseException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public LocationData getLocationByIP(String ipAddress) {
+        String Url = BaseUrl + ipAddress + "?access_key=" + ApiKey;
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(Url))
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            // Parse JSON response
+            JSONParser parser = new JSONParser();
+            JSONObject jsonResponse = (JSONObject) parser.parse(response.body());
+
+            if (jsonResponse.containsKey("latitude") && jsonResponse.containsKey("longitude")) {
+                double latitude = ((Number) jsonResponse.get("latitude")).doubleValue();
+                double longitude = ((Number) jsonResponse.get("longitude")).doubleValue();
+                String city = jsonResponse.get("city").toString();
+                String country = jsonResponse.get("country_name").toString();
+
+                // Return LocationData object
+                return new LocationData(city, country, latitude, longitude);
+            } else {
+                System.out.println("Invalid response structure: " + jsonResponse);
             }
         } catch (IOException | InterruptedException | ParseException e) {
             e.printStackTrace();
