43909565afeb4b8f50b1f02ade343681a7d6efe0
Mainul <mainulhasan@iut-dhaka.edu>
2024-11-21 16:55:13 +0600

    Commit number - 56

--- FindWeather/FindWeather - Mainul/src/concreteClasses/WeatherService.java
+++ FindWeather/FindWeather - Mainul/src/concreteClasses/WeatherService.java
@@ -1,21 +1,22 @@
 package concreteClasses;
+import java.util.*;
 
 import APIs.WeatherStackAPI;
 import adapters.OpenWeatherAdapter;
 import adapters.WeatherStackAdapter;
 
-import java.util.Dictionary;
 import java.util.HashMap;
 
 public class WeatherService {
     private WeatherStackAdapter weatherStack;
     private OpenWeatherAdapter openWeather;
-    private Dictionary<String, WeatherData> cache = new Hashtable<>();
+    private Dictionary<String, WeatherData> cache;
 
 
     public WeatherService(){
         weatherStack = new WeatherStackAdapter();
         openWeather = new OpenWeatherAdapter();
+        cache = new Hashtable<>();
 
     }
 
