a88a09d6553871ab4961476d4c900867e6e7ee80
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2024-11-21 21:56:12 +0600

    GetWeatherDataByIP method

--- FindWeather/src/abstracts/AbstractWeatherService.java
+++ FindWeather/src/abstracts/AbstractWeatherService.java
@@ -1,6 +1,9 @@
 package abstracts;
 
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.concurrent.CompletableFuture;
 
 import behaviours.IweatherService;
@@ -21,6 +24,23 @@ public abstract class AbstractWeatherService implements IweatherService {
 
     @Override
     public CompletableFuture<WeatherData> getWeatherDataByIP() throws Exception {
+        CompletableFuture<LocationData> locationFuture = locationService.getLocationData();
+        LocationData locationData = locationFuture.get();
+
+        String requestUrl = buildRequestUrl(locationData);
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create(requestUrl))
+                .build();
+
+        try {
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            return CompletableFuture.completedFuture(parseWeatherResponse(response.body()));
+        } catch (Exception e) {
+            CompletableFuture<WeatherData> future = new CompletableFuture<>();
+            future.completeExceptionally(e);
+            return future;
+        }
     }
 
     @Override
