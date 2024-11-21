2a56eca0e7f9eb1adbbbade089939dc6dc600fec
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2024-11-21 21:43:21 +0600

    Constrcutor and Abstract methods for weather service

--- FindWeather/src/abstracts/AbstractWeatherService.java
+++ FindWeather/src/abstracts/AbstractWeatherService.java
@@ -4,20 +4,30 @@ import java.net.http.HttpClient;
 import java.util.concurrent.CompletableFuture;
 
 import behaviours.IweatherService;
+import models.LocationData;
 import models.WeatherData;
 import services.LocationService;
 
-public class AbstractWeatherService implements IweatherService {
+public abstract class AbstractWeatherService implements IweatherService {
     protected final String API_Key;
     protected LocationService locationService;
     protected HttpClient httpClient;
+
+    public AbstractWeatherService(String API_Key) {
+        this.API_Key = API_Key;
+        this.locationService = new LocationService();
+        this.httpClient = HttpClient.newHttpClient();
+    }
 
     @Override
     public CompletableFuture<WeatherData> getWeatherDataByIP() throws Exception {
     }
 
     @Override
-    CompletableFuture<WeatherData> getWeatherDataByCity() throws Exception {
+    public CompletableFuture<WeatherData> getWeatherDataByCity() throws Exception {
     }
 
+    protected abstract String buildRequestUrl(LocationData locationData);
+
+    protected abstract WeatherData parseWeatherResponse(String jsonBody);
 }
