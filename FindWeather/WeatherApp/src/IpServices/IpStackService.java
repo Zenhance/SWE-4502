53ac3303ddb851c256ebe30c4075fc701212d1af
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2024-11-21 20:34:38 +0600

    added IpStackService: ApiKey and Url

---
+++ FindWeather/WeatherApp/src/IpServices/IpStackService.java
@@ -0,0 +1,8 @@
+package IpServices;
+
+public class IpStackService {
+    private final String ApiKey = "6f97c01d7812a64550fb69c59a001387";
+    private final String BaseUrl = "http://api.ipstack.com/";
+
+    
+}
