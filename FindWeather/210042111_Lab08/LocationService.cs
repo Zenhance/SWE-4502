04824cb7cbab638858db41baa7353b3d8ad56cc0
sheona001 <sirexcc001@gmail.com>
2024-11-22 22:59:34 +0600

    locationservice

---
+++ FindWeather/210042111_Lab08/LocationService.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _210042111_Lab08
+{
+    public class LocationService
+    {
+        private const string IpApiUrl = "http://ip-api.com/json/";
+
+        public async Task<(double Latitude, double Longitude)> GetLocationFromIpAsync()
+        {
+            string response = await SendGetRequestAsync(IpApiUrl);
+            JObject json = JObject.Parse(response);
+
+            if (json["status"]?.ToString() == "success")
+            {
+                double latitude = (double)json["lat"];
+                double longitude = (double)json["lon"];
+                return (latitude, longitude);
+            }
+
+            throw new Exception("Unable to retrieve location information from IP.");
+        }
+
+        private async Task<string> SendGetRequestAsync(string apiUrl)
+        {
+            using (WebClient client = new WebClient())
+            {
+                return await client.DownloadStringTaskAsync(new Uri(apiUrl));
+            }
+        }
+    }
+}
--- FindWeather/210042111_Lab08/OpenWeatherMapApi.cs
+++ FindWeather/210042111_Lab08/OpenWeatherMapApi.cs
@@ -1,6 +1,43 @@
-﻿namespace _210042111_Lab08
+﻿using Newtonsoft.Json.Linq;
+using System.Net;
+using System;
+using System.Threading.Tasks;
+
+namespace _210042111_Lab08
 {
-    internal class OpenWeatherMapApi
+    public class OpenWeatherMapApi
     {
+        private readonly string ApiKey = "dac1719e24399fbaddf4ad41b384611b";
+
+        public async Task<WeatherData> GetWeatherByCityAsync(string cityName)
+        {
+            string apiUrl = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={ApiKey}&units=metric";
+            return await FetchWeatherData(apiUrl);
+        }
+
+        public async Task<WeatherData> GetWeatherByCoordinatesAsync(double latitude, double longitude)
+        {
+            string apiUrl = $"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={ApiKey}&units=metric";
+            return await FetchWeatherData(apiUrl);
+        }
+        private async Task<WeatherData> FetchWeatherData(string apiUrl)
+        {
+            string response = await SendGetRequestAsync(apiUrl);
+            JObject json = JObject.Parse(response);
+            string cityName = json["name"].ToString();
+            double temperature = (double)json["main"]["temp"];
+            string weatherCondition = json["weather"][0]["description"].ToString();
+
+            return new WeatherData(cityName, weatherCondition, temperature);
+        }
+
+        private async Task<string> SendGetRequestAsync(string apiUrl)
+        {
+            using (WebClient client = new WebClient())
+            {
+                return await client.DownloadStringTaskAsync(new Uri(apiUrl));
+            }
+        }
+        }
     }
 }
