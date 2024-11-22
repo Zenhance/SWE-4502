7ae0477afa48c7441080b92bb813acc5a1e0db34
Namisa Najah <namisa.najah.raisa@gmail.com>
2024-11-22 19:07:50 +0600

    location ( lat long cache check)

--- FindWeather/FindWeather v112/FindWeather v112/Facades/WeatherFacade.cs
+++ FindWeather/FindWeather v112/FindWeather v112/Facades/WeatherFacade.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace FindWeather_v112
@@ -17,6 +19,15 @@ namespace FindWeather_v112
             var openWeather = new OpenWeatherAdapter();
             weatherStackProxy = new WeatherProxy(weatherStack);
             openWeatherProxy = new WeatherProxy(openWeather);
+        }
+        public async Task<(double Latitude, double Longitude)> GetLocation()
+        {
+            if (cachedLocation.HasValue)
+            {
+                Console.WriteLine("Using cached location.");
+                return cachedLocation.Value;
+            }
+            
         }
     }
 }
