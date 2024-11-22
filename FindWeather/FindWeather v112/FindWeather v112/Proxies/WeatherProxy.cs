5d5a83f5d90266ff036fdd3fa680ff04d02b9a34
Namisa Najah <namisa.najah.raisa@gmail.com>
2024-11-22 18:47:54 +0600

    proxy creation

---
+++ FindWeather/FindWeather v112/FindWeather v112/Proxies/WeatherProxy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindWeather_v112
+{
+    internal class WeatherProxy:IWeatherProvider
+    {
+        public IWeatherProvider provider;
+
+    }
+}
