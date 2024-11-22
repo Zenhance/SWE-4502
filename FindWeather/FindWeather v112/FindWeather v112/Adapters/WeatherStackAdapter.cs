5aa68a63bc67815c57bf9e733e2c2f83fb859291
Namisa Najah <namisa.najah.raisa@gmail.com>
2024-11-22 18:44:27 +0600

    ws getweather lat long

--- FindWeather/FindWeather v112/FindWeather v112/Adapters/WeatherStackAdapter.cs
+++ FindWeather/FindWeather v112/FindWeather v112/Adapters/WeatherStackAdapter.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace FindWeather_v112
@@ -12,6 +13,11 @@ namespace FindWeather_v112
         public async Task<WeatherData> GetWeatherByCityAsync(string city)
         {
             throw new NotImplementedException("This adapter does not support city-based lookups.");
+        }
+        public async Task<WeatherData> GetWeatherAsync(double latitude, double longitude)
+        {
+            var response = await baseClient.FetchWeatherData(latitude, longitude);
+            var json = JsonDocument.Parse(response);
         }
     }
 }
