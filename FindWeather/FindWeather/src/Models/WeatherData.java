9328a02baecbcd56bcc0f895dcc824e55d26b102
Hasibul Islam Nirjhar <hasibulislam@iut-dhaka.edu>
2024-11-22 19:33:05 +0600

    WeatherData Initialized

--- FindWeather/FindWeather/src/Models/WeatherData.java
+++ FindWeather/FindWeather/src/Models/WeatherData.java
@@ -1,4 +1,32 @@
 package Models;
 
 public class WeatherData {
+    private final String location;
+    private final String source;
+    private final double temperature;
+    private final String conditions;
+
+    public WeatherData(String location, String source, double temperature, String conditions) {
+        this.location = location;
+        this.source = source;
+        this.temperature = temperature;
+        this.conditions = conditions;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public String getConditions() {
+        return conditions;
+    }
+
 }
