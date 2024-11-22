0f5646aba5323882d9546252a11d6ea61eec2e22
Namisa Najah <namisa.najah.raisa@gmail.com>
2024-11-22 19:19:50 +0600

    case 1 : ip : weatherStack

--- FindWeather/FindWeather v112/FindWeather v112/Program.cs
+++ FindWeather/FindWeather v112/FindWeather v112/Program.cs
@@ -15,8 +15,13 @@ namespace FindWeather_v112
             int choice = int.Parse(Console.ReadLine());
             try
             {
-                
-            }
+                switch(choice)
+                {
+                    case 1: // By IP (WeatherStack)
+                        var weatherByIP = await facade.GetWeatherByIP();
+                        Console.WriteLine($"Weather in {weatherByIP.City} from {weatherByIP.Source}: {weatherByIP.Temperature}°C, {weatherByIP.Condition}");
+                        break;
+                }
             catch (Exception ex)
             {
                 Console.WriteLine($"Error: {ex.Message}");
