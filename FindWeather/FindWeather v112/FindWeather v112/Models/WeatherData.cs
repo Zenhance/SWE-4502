edc7cca0a16ff8f5d6426fd48b572ede687138ec
Namisa Najah <namisa.najah.raisa@gmail.com>
2024-11-22 13:10:23 +0600

    weather data model

---
+++ FindWeather/FindWeather v112/FindWeather v112/Models/WeatherData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindWeather_v112.Models
+{
+    internal class WeatherData
+    {
+        public string City { get; set; }
+        public double Temperature { get; set; }
+        public string Condition { get; set; }
+        public string Source { get; set; }
+    }
+}
