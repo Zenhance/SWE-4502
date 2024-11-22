e4ba9bfe0d69fbf08cdfc032db7124036a6ccd5f
Shefwef <shefayatadib@iut-dhaka.edu>
2024-11-22 12:07:51 +0600

    Location schema created

---
+++ FindWeather/WeatherApp/src/models/Location.java
@@ -0,0 +1,15 @@
+package models;
+
+public class Location 
+{
+    public String city;
+    public String latitude;
+    public String longitude;
+
+    public Location(String city, String latitude, String longitude)
+    {
+        this.city = city;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
