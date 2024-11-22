3b2f4feebcc507cae49214347ca20e9e63071e0e
Namisa Najah <namisa.najah.raisa@gmail.com>
2024-11-22 13:21:41 +0600

    weather stack api create

---
+++ FindWeather/FindWeather v112/FindWeather v112/APIs/WeatherStackAPI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindWeather_v112.APIs
+{
+    internal class WeatherStackAPI
+    {
+        private readonly string apiKey = "1d5774fb918f8d1301086452e73b086b";
+        private readonly HttpClient client = new HttpClient();
+    }
+}
