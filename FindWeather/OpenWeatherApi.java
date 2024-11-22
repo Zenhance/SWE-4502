e549ed4d799eed14906fdb422282c82d0bedab84
tauhid-taaha <tauhidtaaha7@gmail.com>
2024-11-22 11:38:43 +0600

    openweatherapi

---
+++ FindWeather/OpenWeatherApi.java
@@ -0,0 +1,23 @@
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class OpenWeatherAPI {
+
+        private static final String OpenWeather_API_KEY = "23be6ddf45a59d7958e26dd3e1fb3e77";
+
+        public static Weather Get_Weather(Location location) throws IOException, JSONException {
+            String urlString = "http://api.openweathermap.org/data/2.5/weather?q=" + location.getCity() + "&appid=" + OpenWeather_API_KEY + "&units=metric";
+            URL url = new URL(urlString);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+
+           
+        }
+    }
