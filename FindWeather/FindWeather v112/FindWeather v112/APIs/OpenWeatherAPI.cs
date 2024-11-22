2af3a7db852cef00b0e3bfbf8c19d12dd23d44c9
Namisa Najah <namisa.najah.raisa@gmail.com>
2024-11-22 13:17:07 +0600

    openweather api done

--- FindWeather/FindWeather v112/FindWeather v112/APIs/OpenWeatherAPI.cs
+++ FindWeather/FindWeather v112/FindWeather v112/APIs/OpenWeatherAPI.cs
@@ -11,6 +11,11 @@ namespace FindWeather_v112.APIs
     {
         private readonly string apiKey = "6478730e0f11b84d687c7b57123b8fbf";
         private readonly HttpClient client = new HttpClient();
+        public async Task<string> FetchWeatherDataByCoordinatesAsync(double latitude, double longitude)
+        {
+            string url = $"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&units=metric&appid={apiKey}";
+            return await client.GetStringAsync(url);
+        }
 
     }
 }
