e662291fd2e9dcb8f655a9f2d507c66c9cd36813
Hasibul Islam Nirjhar <hasibulislam@iut-dhaka.edu>
2024-11-22 19:30:08 +0600

    Models Initialized

---
+++ FindWeather/FindWeather/src/Models/Location.java
@@ -0,0 +1,28 @@
+package Models;
+
+public class Location {
+    private String city;
+
+    private double latitude;
+    private double longitude;
+
+    public Location(String city, double latitude, double longitude) {
+        this.city = city;
+
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getCity() {
+        return city;
+    }
+    
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+}
