7842bcfbf0cc5848e7aad82278235d3df5f2008c
Namisa Najah <namisa.najah.raisa@gmail.com>
2024-11-22 13:24:16 +0600

    fetch weather by city

--- FindWeather/FindWeather v112/FindWeather v112/APIs/OpenWeatherAPI.cs
+++ FindWeather/FindWeather v112/FindWeather v112/APIs/OpenWeatherAPI.cs
@@ -5,7 +5,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FindWeather_v112.APIs
+namespace FindWeather_v112
 {
     internal class OpenWeatherAPI
     {
--- FindWeather/FindWeather v112/FindWeather v112/APIs/WeatherStackAPI.cs
+++ FindWeather/FindWeather v112/FindWeather v112/APIs/WeatherStackAPI.cs
@@ -5,11 +5,16 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FindWeather_v112.APIs
+namespace FindWeather_v112
 {
     internal class WeatherStackAPI
     {
         private readonly string apiKey = "1d5774fb918f8d1301086452e73b086b";
         private readonly HttpClient client = new HttpClient();
+        public async Task<string> FetchWeatherData(double latitude, double longitude)
+        {
+            string url = $"http://api.weatherstack.com/current?access_key={apiKey}&query={latitude},{longitude}";
+            return await client.GetStringAsync(url);
+        }
     }
 }
