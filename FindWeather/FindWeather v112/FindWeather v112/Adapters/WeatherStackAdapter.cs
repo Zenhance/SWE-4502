93ba24e744de168d06de109285aa1e0c64933faf
Namisa Najah <namisa.najah.raisa@gmail.com>
2024-11-22 18:42:44 +0600

    weather stack weather by city implementation

--- FindWeather/FindWeather v112/FindWeather v112/Adapters/WeatherStackAdapter.cs
+++ FindWeather/FindWeather v112/FindWeather v112/Adapters/WeatherStackAdapter.cs
@@ -8,5 +8,10 @@ namespace FindWeather_v112
 {
     internal class WeatherStackAdapter:IWeatherProvider
     {
+        public WeatherStackAPI baseClient = new WeatherStackAPI();
+        public async Task<WeatherData> GetWeatherByCityAsync(string city)
+        {
+            throw new NotImplementedException("This adapter does not support city-based lookups.");
+        }
     }
 }
