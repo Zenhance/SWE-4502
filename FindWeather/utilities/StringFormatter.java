0df11036ff5526c81c46e09f16c6473630b05635
tasniaanwer <tasniaanwer@gmail.com>
2024-11-23 23:59:53 +0600

    u-sf2

--- FindWeather/utilities/StringFormatter.java
+++ FindWeather/utilities/StringFormatter.java
@@ -3,3 +3,9 @@ package utilities;
 public class StringFormatter {
     public String replaceSpaces(String inputString) {
         String formattedString = "";
+        for (char character : inputString.toCharArray()) {
+            formattedString += (character == ' ') ? "%20" : character;
+        }
+        return formattedString;
+    }
+}
