b87319c2e60030c591834e6aff8584e2856d70f8
tasniaanwer <tasniaanwer@gmail.com>
2024-11-23 23:33:14 +0600

    wapi2

--- FindWeather/WeatherAPI.java
+++ FindWeather/WeatherAPI.java
@@ -7,3 +7,8 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+
+public class WeatherAPI {
+    private String apiKey;
+    private String endpointUrl;
+    private JSONObject apiResponse;
