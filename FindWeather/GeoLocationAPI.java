14466cea9371c83ef84080dfe636fee5a9924b18
tasniaanwer <tasniaanwer@gmail.com>
2024-11-23 23:30:11 +0600

    l3

--- FindWeather/GeoLocationAPI.java
+++ FindWeather/GeoLocationAPI.java
@@ -11,3 +11,8 @@ import java.net.http.HttpResponse;
 public class GeoLocationAPI {
     private String apiKey;
     private String baseUrl;
+
+    public GeoLocationAPI(String key) {
+        apiKey = key; // Example key: "355dbc2e06a2a5956bc522f5a7d1ca21"
+        baseUrl = "https://api.ipstack.com/";
+    }
