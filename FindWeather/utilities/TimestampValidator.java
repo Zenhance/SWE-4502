9fe58c347ad43d47fc8a9653cfa499e0de19fd1e
tasniaanwer <tasniaanwer@gmail.com>
2024-11-24 00:01:34 +0600

    u-ts2

--- FindWeather/utilities/TimestampValidator.java
+++ FindWeather/utilities/TimestampValidator.java
@@ -3,3 +3,6 @@ package utilities;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+
+public class TimestampValidator {
+    public boolean isWithinLastTenMinutes(String timestamp) {
