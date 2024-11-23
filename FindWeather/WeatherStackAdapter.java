6d938a56e176c6739a9c2d6b1f72aac9b016f552
tasniaanwer <tasniaanwer@gmail.com>
2024-11-23 23:01:32 +0600

    WSA2

--- FindWeather/WeatherStackAdapter.java
+++ FindWeather/WeatherStackAdapter.java
@@ -5,3 +5,10 @@ import concreteClasses.utility.StringManager;
 import concreteClasses.WeatherData;
 import interfaces.IweatherInfoProvider;
 import org.json.JSONObject;
+
+public class WeatherStackAdapter implements IweatherInfoProvider {
+    private JSONObject allInfo;
+    private WeatherStackAPI api;
+    public  WeatherStackAdapter(){
+        api = new WeatherStackAPI("4c31a3c95ae754203e58d51a39643e4b");
+    }
