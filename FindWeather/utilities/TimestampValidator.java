ed6acb654def351be434b96ce9d9565a7ccf760f
tasniaanwer <tasniaanwer@gmail.com>
2024-11-24 00:02:06 +0600

    u-ts3

--- FindWeather/utilities/TimestampValidator.java
+++ FindWeather/utilities/TimestampValidator.java
@@ -6,3 +6,7 @@ import java.time.format.DateTimeFormatter;
 
 public class TimestampValidator {
     public boolean isWithinLastTenMinutes(String timestamp) {
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime parsedTime = LocalDateTime.parse(timestamp, dateFormatter);
+        LocalDateTime currentTime = LocalDateTime.now();
+        Duration timeDifference = Duration.between(parsedTime, currentTime);
