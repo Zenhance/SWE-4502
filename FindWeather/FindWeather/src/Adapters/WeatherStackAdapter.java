b9bcc4dc39d8529ae62fa3aa50f18cb315f129b5
Hasibul Islam Nirjhar <hasibulislam@iut-dhaka.edu>
2024-11-27 11:17:58 +0600

    Added WeatherStack adapter

--- FindWeather/FindWeather/src/Adapters/WeatherStackAdapter.java
+++ FindWeather/FindWeather/src/Adapters/WeatherStackAdapter.java
@@ -2,23 +2,38 @@ package Adapters;
 
 import Interface.WeatherProvider;
 import Models.WeatherData;
+import Services.LocationService;
+import org.json.JSONObject;
 
 public class WeatherStackAdapter implements WeatherProvider {
 
-    private static final String API_Key="dfa082b6e772662f7041fc5f8370e72d";
+    private static final String API_Key = "dfa082b6e772662f7041fc5f8370e72d";
 
     @Override
     public WeatherData getWeather(String city, double latitude, double longitude) {
 
-        try{
+        try {
             String url = String.format(
                     "http://api.weatherstack.com/current?access_key=%s&query=%s",
                     API_Key, city);
 
-            String response=
+
+
+            String response = new LocationService().fetchFromUrl(url);
+
+            JSONObject json = new JSONObject(response);
+            double temp= json.getJSONObject("current").getDouble("temperature");
+            String condition= json.getJSONObject("current").getJSONArray("weather_descriptions").getString(0);
+
+            return new WeatherData(city, "WeatherStack", temp, condition);
+
+        } catch (Exception e) {
+            System.out.println("Error in WeatherStackAdapter.getWeather()");
+            return null;
         }
 
 
     }
+
 
 }
