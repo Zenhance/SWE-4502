530be302bfd1310e1213284760ed971341164079
tasniaanwer <tasniaanwer@gmail.com>
2024-11-27 23:30:37 +0600

    IPAPI5

--- FindWeather/IPStackAPI.java
+++ FindWeather/IPStackAPI.java
@@ -13,3 +13,8 @@ import static java.net.http.HttpClient.newHttpClient;
 public class IPStackAPI {
     private String api_key;
     private String base_url;
+    
+    public IPStackAPI(String _api_key){
+        api_key = _api_key; //355dbc2e06a2a5956bc522f5a7d1ca21
+        base_url = "https://api.ipstack.com/";
+    }
