72d5b056d4f59d111d42d72ddb0fbdabf33dfbb3
tasniaanwer <tasniaanwer@gmail.com>
2024-11-28 15:25:38 +0600

    owAPI3

--- FindWeather/openWeatherAPI.java
+++ FindWeather/openWeatherAPI.java
@@ -18,3 +18,21 @@ public class openWeatherAPI {
         api_key = _api_key;
         base_url = "https://api.openweathermap.org/data/2.5/weather";
     }
+    public JSONObject fetchData(String location){
+        String url = base_url + "?q=" + location + "&appid=" + api_key;
+
+        try {
+            HttpClient client = newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .build();
+            HttpResponse<String> response =
+                    client.send(request, HttpResponse.BodyHandlers.ofString());
+            allInfo = new JSONObject(response.body());
+            return allInfo;
+        } catch (IOException | InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+}
