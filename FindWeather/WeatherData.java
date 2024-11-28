bbabad1aff5c7c6b289e3eb1079432cdab326e6d
tasniaanwer <tasniaanwer@gmail.com>
2024-11-28 15:37:09 +0600

    wd1

---
+++ FindWeather/WeatherData.java
@@ -0,0 +1,10 @@
+import org.json.JSONObject;
+
+public class WeatherData {
+    private String cityName;
+    private double latitude;
+    private double longitude;
+    private double temperature;
+    private String weatherCondition;
+    private String dataSource;
+    private String fetchedTime;
