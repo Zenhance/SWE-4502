dd792241ce8b72c5bdcb4844784f76671d4a3831
tasniaanwer <tasniaanwer@gmail.com>
2024-11-29 01:01:02 +0600

    ws1

---
+++ FindWeather/WeatherServoce.java
@@ -0,0 +1,10 @@
+package concreteClasses;
+
+import java.util.*;
+
+import APIs.WeatherStackAPI;
+import adapters.OpenWeatherAdapter;
+import adapters.WeatherStackAdapter;
+import concreteClasses.utility.TimeManager;
+
+import java.util.HashMap;
