27bf19a090973ef568064f5c9fee576c8d9e9008
tasniaanwer <tasniaanwer@gmail.com>
2024-11-28 15:27:26 +0600

    wsAPI1

---
+++ FindWeather/WeatherStackAPI.java
@@ -0,0 +1,11 @@
+package APIs;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static java.net.http.HttpClient.newHttpClient;
