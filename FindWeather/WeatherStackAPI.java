d79b47f4c37a5374df5581213d9d71b1723991c7
tasniaanwer <tasniaanwer@gmail.com>
2024-11-28 15:27:54 +0600

    wsAPI2

--- FindWeather/WeatherStackAPI.java
+++ FindWeather/WeatherStackAPI.java
@@ -9,3 +9,13 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 import static java.net.http.HttpClient.newHttpClient;
+
+public class WeatherStackAPI {
+    private String api_key;
+    private String base_url;
+    private JSONObject allInfo;
+
+    public WeatherStackAPI(String _api_key) {
+        api_key = _api_key;
+        base_url = "http://api.weatherstack.com/current";
+    }
