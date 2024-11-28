218706fa51672ed340bcaee02d92e3ac2b31f890
tasniaanwer <tasniaanwer@gmail.com>
2024-11-28 15:25:12 +0600

    owAPI2

--- FindWeather/openWeatherAPI.java
+++ FindWeather/openWeatherAPI.java
@@ -9,3 +9,12 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 import static java.net.http.HttpClient.newHttpClient;
+
+public class openWeatherAPI {
+    private String api_key;
+    private String base_url;
+    private JSONObject allInfo;
+    public openWeatherAPI(String _api_key){
+        api_key = _api_key;
+        base_url = "https://api.openweathermap.org/data/2.5/weather";
+    }
