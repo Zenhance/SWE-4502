d15a8e242667b2b26f53a8b2003b4184832619c5
bhuiyan.minhajul@gmail.com <bhuiyan.minhajul@gmail.com>
2025-03-04 22:09:09 +0600

    Adding Weather.cs

--- FindWeather/Lab-8_FindWeather/Location.cs
+++ FindWeather/Lab-8_FindWeather/Location.cs
@@ -11,5 +11,34 @@ namespace Lab_8_FindWeather
         public string City { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
+
+        public Location(string city, double latitude, double longitude)
+        {
+            City = city;
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        public string GetCity()
+        {
+            return City;
+        }
+
+
+        public double GetLatitude()
+        {
+            return Latitude;
+        }
+
+        public double GetLongitude()
+        {
+            return Longitude;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"City: {City}, Latitude: {Latitude}, Longitude: {Longitude}");
+        }
+
     }
 }
---
+++ FindWeather/Lab-8_FindWeather/Weather.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_8_FindWeather
+{
+    public class Weather
+    {
+        public double Temperature { get; set; }
+        public string Condition { get; set; }
+        public Location Location { get; set; }
+        public string Source { get; set; } // kottheke nibo tar jonno
+                                           // "WeatherStack" or "OpenWeather"
+
+
+        public Weather(double temperature, string condition, Location location, string source)
+        {
+            Temperature = temperature;
+            Condition = condition;
+            Location = location;
+            Source = source;
+        }
+
+
+        public Weather(Location location)
+        {
+            Location = location;
+        }
+
+        public double GetTemperature()
+        {
+            return Temperature;
+        }
+
+        public string GetCondition()
+        {
+            return Condition;
+        }
+
+        public string GetSource()
+        {
+            return Source;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Weather in {Location.GetCity()} from {Source}: {Temperature}°C, {Condition}");
+        }
+    }
+}
