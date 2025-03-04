021c9b857b896daa9fbacb628dc888d9df303f49
bhuiyan.minhajul@gmail.com <bhuiyan.minhajul@gmail.com>
2025-03-04 21:57:23 +0600

    First Commit

---
+++ FindWeather/Lab-8_FindWeather/Location.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_8_FindWeather
+{
+    public class Location
+    {
+        public string City { get; set; }
+        public double Latitude { get; set; }
+    }
+}
---
+++ FindWeather/Lab-8_FindWeather/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_8_FindWeather
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
