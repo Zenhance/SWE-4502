4386761b7318ed062c34a2d8b920d811e4583d2a
Ayonspoiler <ayon4052@gmail.com>
2025-03-05 12:16:23 +0600

    IWeatherService implemented

---
+++ FindWeather/Lab8_210042166/API_Class/weatherAPI.java
@@ -0,0 +1,15 @@
+package Lab8_210042166.API_Class;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class weatherAPI {
+    public static String getResponse(String urlString)throws IOException{
+        URL url=new URL(urlString);
+        HttpURLConnection connection=(HttpURLConnection).url.openConnecction();
+        connection.setRequestMethod("GET");
+        connection.connect();
+    }
+
+}
