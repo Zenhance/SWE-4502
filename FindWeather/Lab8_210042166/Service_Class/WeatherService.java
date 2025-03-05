bd2b1f9198f14a0bdfa2b945a5d54a340aba22bb
Ayonspoiler <ayon4052@gmail.com>
2025-03-05 12:49:44 +0600

    Service Class (WeatherService) implementing;

--- FindWeather/Lab8_210042166/Service_Class/WeatherService.java
+++ FindWeather/Lab8_210042166/Service_Class/WeatherService.java
@@ -2,7 +2,21 @@ package Lab8_210042166.Service_Class;
 
 import Lab8_210042166.Interface.IWeatherService;
 
+import java.io.IOException;
+import Lab8_210042166.API_Class.weatherAPI
+
 public class WeatherService implements IWeatherService {
 
     private static final String weatherStack_Key="76e91d82542ba4d15bf0ef04c7955f93";
+
+    @Override
+    public void getWeatherThroughCity(String city) throws IOException {
+        System.out.println("Weather data for city: " + city);
+
+        String weatherResponse = weatherAPI.getResponse(
+                "http://api.weatherstack.com/current?access_key=" + weatherStack_Key + "&query=" + city
+        );
+
+    }
+
 }
