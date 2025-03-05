3dfbf766c4ff29e48bbb8bcc1527014bfb6a0d05
Ayonspoiler <ayon4052@gmail.com>
2025-03-05 12:19:36 +0600

    API Class updated

--- FindWeather/Lab8_210042166/API_Class/weatherAPI.java
+++ FindWeather/Lab8_210042166/API_Class/weatherAPI.java
@@ -3,6 +3,7 @@ package Lab8_210042166.API_Class;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Scanner;
 
 public class weatherAPI {
     public static String getResponse(String urlString)throws IOException{
@@ -11,7 +12,18 @@ public class weatherAPI {
         connection.setRequestMethod("GET");
         connection.connect();
 
-        int response=connection.getResponseCode();
+        int responseCode=connection.getResponseCode();
+        if (responseCode != 200) {
+            throw new IOException("HTTP response: " + responseCode);
+        }
+        Scanner scanner=new Scanner(url.openStream());
+       StringBuilder response = new StringBuilder();
+        while (scanner.hasNext()) {
+            response.append(scanner.nextLine());
+        }
+        scanner.close();
+
+        return response.toString();
     }
 
 }
