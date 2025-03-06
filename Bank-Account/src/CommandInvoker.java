9c2bef83a425c9c63c011b313efa59ce0d1af7f9
Hasibul Islam Nirjhar <hasibulislam@iut-dhaka.edu>
2025-03-07 00:11:02 +0600

    Create Account added

--- Bank-Account/src/CommandInvoker.java
+++ Bank-Account/src/CommandInvoker.java
@@ -4,4 +4,6 @@ public class CommandInvoker {
         command.execute();
     }
 
+
+
 }
--- Bank-Account/src/CreateAccountCommand.java
+++ Bank-Account/src/CreateAccountCommand.java
@@ -1,4 +1,25 @@
-public class CreateAccountCommand {
+import java.util.List;
+import java.util.Map;
+
+public class CreateAccountCommand implements Command{
+
+    private Map<String, BankAccount> accounts;
+    private String accountNumber;
+    private String customerName;
+    private double initialDeposit;
+
+    public CreateAccountCommand(Map<String, BankAccount> accounts, String accountNumber, String customerName, double initialDeposit){
+        this.accounts = accounts;
+        this.accountNumber = accountNumber;
+        this.customerName = customerName;
+        this.initialDeposit = initialDeposit;
+    }
+
+    @Override
+    public void execute() {
+        BankAccount account = new BankAccount(accountNumber, customerName, initialDeposit);
+        accounts.put(accountNumber, account);
+    }
 
 
 
