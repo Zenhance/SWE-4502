2a3059461157b9d861d3759a2673d85c07901be5
whiteflags26 <whiteflags26@gmail.com>
2025-03-07 01:03:06 +0600

    feat: add Location and WeatherData models for weather application

---
+++ FindWeather/FindWeatherMySolution/models/WeatherData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindWeatherMySolution.models
+{
+    public class WeatherData
+    {
+        public Location Location { get; set; }
+        public double Temperature { get; set; }
+        public string Conditions { get; set; }
+        public string Source { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
