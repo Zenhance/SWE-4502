ffcb637ddd4349955b693c8597fbf7fa7a37ef80
Ekramul Esham <112435061+EshaAam@users.noreply.github.com>
2025-03-07 02:45:11 +0600

    in progress

--- FindWeather/lab8_210042164/Services/WeatherService.java
+++ FindWeather/lab8_210042164/Services/WeatherService.java
@@ -7,6 +7,11 @@ import java.io.IOException;
 
 public class WeatherService implements IWeatherService {
     private static final String WeatherStackApiKey = "498c9df3f684ab09c3a5bd832507110a";
+    private void displayWeatherData(String city, String weatherResponse) {
+        String temperature = WeatherApi.extractValue(weatherResponse, "\"temperature\":", ",");
+        String weatherDescription = WeatherApi.extractValue(weatherResponse, "\"weather_descriptions\":[\"", "\"]");
+        System.out.println("Weather in " + city + ": " + temperature + "°C, " + weatherDescription);
+    }
 
     @Override
     public void getWeatherByCity(String city) throws IOException {
@@ -15,11 +20,19 @@ public class WeatherService implements IWeatherService {
         String weatherResponse = WeatherApi.getResponse(
                 "http://api.weatherstack.com/current?access_key=" + WeatherStackApiKey + "&query=" + city
         );
+        displayWeatherData(city, weatherResponse);
+
     }
 
     @Override
-    public void getWeatherByLocation(String latitude, String longitude, String city) {
+    public void getWeatherByLocation(String latitude, String longitude, String city) throws IOException {
         System.out.println("Getting weather by location: " + latitude + ", " + longitude + ", " + city);
+
+        String weatherResonse = WeatherApi.getResponse(
+                "http://api.weatherstack.com/current?access_key=" + WeatherStackApiKey + "&query=" + latitude + "," + longitude
+        );
+        displayWeatherData(city, weatherResponse);
     }
+
 
 }
