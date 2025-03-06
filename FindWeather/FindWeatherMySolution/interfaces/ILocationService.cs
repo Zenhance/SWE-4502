71ff86da8a797d2d8d5544602fc0192a8cdf202b
whiteflags26 <whiteflags26@gmail.com>
2025-03-07 01:24:17 +0600

    feat: add ILocationService and IWeatherProvider interfaces for weather application

---
+++ FindWeather/FindWeatherMySolution/interfaces/ILocationService.cs
@@ -0,0 +1,14 @@
+﻿using FindWeatherMySolution.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindWeatherMySolution.interfaces
+{
+    public interface ILocationService
+    {
+        Task<Location> GetLocationByIp();
+    }
+}
---
+++ FindWeather/FindWeatherMySolution/interfaces/IWeatherProvider.cs
@@ -0,0 +1,16 @@
+﻿using FindWeatherMySolution.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindWeatherMySolution.interfaces
+{
+    public interface IWeatherProvider
+    {
+        string Name { get; }
+        Task<WeatherData> GetWeatherData(Location location);
+        DateTime LastRequestTime { get; set; }
+    }
+}
