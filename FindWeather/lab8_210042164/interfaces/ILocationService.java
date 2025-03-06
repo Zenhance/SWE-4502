b0c7a90c427246112599e984f2002baed4fc18fd
Ekramul Esham <112435061+EshaAam@users.noreply.github.com>
2025-03-06 23:24:31 +0600

    Interface ILocationService in progress

--- FindWeather/lab8_210042164/interfaces/ILocationService.java
+++ FindWeather/lab8_210042164/interfaces/ILocationService.java
@@ -1,4 +1,10 @@
 package lab8_210042164.interfaces;
 
+import java.io.IOException;
+
 public interface ILocationService {
+    void getLocationByIP() throws IOException;
+    void getCity() throws IOException;
+    String getLatitude();
+    String getLongitude();
 }
