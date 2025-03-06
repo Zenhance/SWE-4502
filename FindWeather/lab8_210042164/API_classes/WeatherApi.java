790d681ef835c4b62b95eef83c7ab076c7bfb512
Ekramul Esham <112435061+EshaAam@users.noreply.github.com>
2025-03-07 02:28:49 +0600

    progress

--- FindWeather/lab8_210042164/API_classes/WeatherApi.java
+++ FindWeather/lab8_210042164/API_classes/WeatherApi.java
@@ -1,4 +1,10 @@
 package lab8_210042164.API_classes;
 
+import java.io.IOException;
+import java.net.URL;
+
 public class WeatherApi {
+    public static String getResponse(String urlString) throws IOException {
+        URL url = new URL(urlString);
+    }
 }
--- FindWeather/lab8_210042164/Services/WeatherService.java
+++ FindWeather/lab8_210042164/Services/WeatherService.java
@@ -3,11 +3,13 @@ package lab8_210042164.Services;
 import lab8_210042164.interfaces.IWeatherService;
 
 public class WeatherService implements IWeatherService {
-    private static final String WeatherApiKey = "498c9df3f684ab09c3a5bd832507110a";
+    private static final String WeatherStackApiKey = "498c9df3f684ab09c3a5bd832507110a";
 
     @Override
     public void getWeatherByCity(String city) {
+
         System.out.println("Getting weather by city: " + city);
+
     }
 
     @Override
