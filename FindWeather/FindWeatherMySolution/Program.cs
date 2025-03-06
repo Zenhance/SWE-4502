b52310ec60104275201ab4b3b480b6f4b57dd905
whiteflags26 <whiteflags26@gmail.com>
2025-03-07 03:21:43 +0600

    feat: integrate WeatherFacade for weather retrieval by IP and city name in Program.cs

--- FindWeather/FindWeatherMySolution/Program.cs
+++ FindWeather/FindWeatherMySolution/Program.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FindWeatherMySolution.adopters;
+using FindWeatherMySolution.facade;
 
 namespace FindWeatherMySolution
 {
@@ -13,20 +14,37 @@ namespace FindWeatherMySolution
     {
         static async Task Main(string[] args)
         {
-            LocationService locationService = new LocationService();
-            Location location = await locationService.GetLocationByIp();
-            location.ShowDetails();
+            var weatherFacade = new WeatherFacade();
 
-            //OpenWeatherMapAdapter adapter = new OpenWeatherMapAdapter();
-            //var weatherData = await adapter.GetWeatherData(location);
-            //weatherData.Location = location;
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("Select location method: 1. By IP, 2. By City, 3. Exit");
+                var input = Console.ReadLine();
 
-            WeatherStackAdapter stackAdapter = new WeatherStackAdapter();
-            var weatherData2 = await stackAdapter.GetWeatherData(location);
-            weatherData2.Location = location;
-            weatherData2.ShowDetails();
+                switch (input)
+                {
+                    case "1":
+                        await weatherFacade.GetWeatherByIp();
+                        break;
+                    case "2":
+                        Console.Write("Enter city name: ");
+                        var city = Console.ReadLine();
+                        if (!string.IsNullOrWhiteSpace(city))
+                        {
+                            await weatherFacade.GetWeatherByCity(city);
+                        }
+                        break;
+                    case "3":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.");
+                        break;
+                }
 
-            Console.ReadKey();
+                Console.WriteLine();
+            }
         }
     }
 }
