97231cd82f86b624bfbf3439ac1ab817e8b32ee1
Hasibul Islam Nirjhar <hasibulislam@iut-dhaka.edu>
2025-03-07 01:00:20 +0600

    WithdrawCommand Added

--- Bank-Account/src/WithdrawCommand.java
+++ Bank-Account/src/WithdrawCommand.java
@@ -1,3 +1,19 @@
-public class WithdrawCommand {
+public class WithdrawCommand implements  Command {
+
+    private BankAccount account;
+    private double amount;
+    private String reference;
+
+    public WithdrawCommand(BankAccount account, double amount, String reference){
+        this.account = account;
+        this.amount = amount;
+        this.reference = reference;
+    }
+
+    @Override
+    public void execute() {
+        account.withdraw(amount);
+        System.out.println("Withdrawal of "+amount+" made from account "+account.getAccountNumber()+" "+reference);
+    }
 
 }
