0794b2fdb06db4fe844d25b288bfd7a07e3fda0d
Ekramul Esham <112435061+EshaAam@users.noreply.github.com>
2025-03-07 02:32:18 +0600

    Api calling

--- FindWeather/lab8_210042164/API_classes/WeatherApi.java
+++ FindWeather/lab8_210042164/API_classes/WeatherApi.java
@@ -1,10 +1,28 @@
 package lab8_210042164.API_classes;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Scanner;
 
 public class WeatherApi {
     public static String getResponse(String urlString) throws IOException {
         URL url = new URL(urlString);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.connect();
+
+        int responseCode = connection.getResponseCode();
+        if (responseCode != 200) {
+            throw new RuntimeException("HttpResponseCode: " + responseCode);
+        }
+        Scanner scanner = new Scanner(url.openStream());
+        StringBuilder response = new StringBuilder();
+        while (scanner.hasNext()) {
+            response.append(scanner.nextLine());
+        }
+        scanner.close();
+
+        return response.toString();
     }
 }
