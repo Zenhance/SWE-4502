bd36466a3a3543896bbfacccab794416758b9081
mdhralif <hasiburrahman999.alif@gmail.com>
2025-03-07 23:54:49 +0600

    Little work on Issue #1: Core System Framework

--- HomeAutomation/HomeAutomation/UnitTest/src/TestMain.java
+++ HomeAutomation/HomeAutomation/UnitTest/src/TestMain.java
@@ -1,14 +1,5 @@
 import org.junit.jupiter.api.Test;
 public class TestMain {
-    @Test
-    public void testAddition() {
-        Calculator calculator = new Calculator(10, 5);
-        assert calculator.add() == 15;
-    }
 
-    @Test
-    public void testSubtraction() {
-        Calculator calculator = new Calculator(10, 5);
-        assert calculator.subtract() == 5;
-    }
+
 }
