5f4af3b4ad154ed1d04f90ae3db79d1466b770c5
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2025-03-07 23:15:13 +0600

    #10: Data Structure (to hold environmental measurements and states)

---
+++ HomeAutomation/home-automation/src/main/models/EnvironmentState.java
@@ -0,0 +1,7 @@
+public class EnvironmentState {
+    private boolean motionDetected;
+    private double temperature;
+    private double lightLevel;
+    private double powerUsage;
+    private String voiceCommand;
+}
