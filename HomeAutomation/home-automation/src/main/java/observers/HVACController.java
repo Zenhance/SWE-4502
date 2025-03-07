c45b18e2f75356acd38d9023e83e61df74c24696
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2025-03-08 00:55:27 +0600

    #13: HVACController Added (Concrete Observer) (For Temperature Controls)

---
+++ HomeAutomation/home-automation/src/main/java/observers/HVACController.java
@@ -0,0 +1,44 @@
+package observers;
+
+import models.EnvironmentState;
+
+public class HVACController implements Observer {
+    private boolean heatingOn;
+    private boolean coolingOn;
+    private double targetTemperature;
+    private double hysteresisRange = 1.5;
+
+    public HVACController(double targetTemperature) {
+        this.targetTemperature = targetTemperature;
+        this.heatingOn = false;
+        this.coolingOn = false;
+    }
+    
+    @Override
+    public void update(EnvironmentState environmentState) {
+        double currentTemperature = environmentState.getTemperature();
+
+        if (currentTemperature < targetTemperature - hysteresisRange) {
+            heatingOn = true;
+            coolingOn = false;
+        } else if (currentTemperature > targetTemperature + hysteresisRange) {
+            heatingOn = false;
+            coolingOn = true;
+        } else {
+            heatingOn = false;
+            coolingOn = false;
+        }
+    } 
+
+    public boolean isHeatingOn() {
+        return heatingOn;
+    }
+
+    public boolean isCoolingOn() {
+        return coolingOn;
+    }
+
+    public void setTargetTemperature(double targetTemperature) {
+        this.targetTemperature = targetTemperature;
+    }
+}
