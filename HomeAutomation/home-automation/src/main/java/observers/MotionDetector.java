9f3bc5b3aed6397ada980f5dae21fc0b49cbf363
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2025-03-07 23:44:07 +0600

    #11: Added MotionDetector (Concrete Observer)

---
+++ HomeAutomation/home-automation/src/main/java/observers/MotionDetector.java
@@ -0,0 +1,11 @@
+package observers;
+
+import models.EnvironmentState;
+
+public class MotionDetector implements Observer {
+    private boolean motionDetected;
+
+    @Override
+    public void update(EnvironmentState environmentState){
+    }
+}
