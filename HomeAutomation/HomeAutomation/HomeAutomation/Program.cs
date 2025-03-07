b773873a4842de8c91dbab888510825a9ac89c97
Mohammad Istiaq Prodhan <istiaqprodhan02@gmail.com>
2025-03-07 15:11:30 +0600

    project-test-fixed

--- HomeAutomation/HomeAutomation/HomeAutomation/Program.cs
+++ HomeAutomation/HomeAutomation/HomeAutomation/Program.cs
@@ -1,15 +1,2 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HomeAutomation
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
