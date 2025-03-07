af44ace2404bbbdaaa8f18132dc8ca4ea188c561
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2025-03-07 23:47:10 +0600

    #10: Added getter setter for states

--- HomeAutomation/home-automation/src/main/java/models/EnvironmentState.java
+++ HomeAutomation/home-automation/src/main/java/models/EnvironmentState.java
@@ -6,4 +6,44 @@ public class EnvironmentState {
     private double lightLevel;
     private double powerUsage;
     private String voiceCommand;
+
+    public void setMotionDetected(boolean motionDetected) {
+        this.motionDetected = motionDetected;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public void setLightLevel(double lightLevel) {
+        this.lightLevel = lightLevel;
+    }
+
+    public void setPowerUsage(double powerUsage) {
+        this.powerUsage = powerUsage;
+    }
+
+    public void setVoiceCommand(String voiceCommand) {
+        this.voiceCommand = voiceCommand;
+    }
+
+    public boolean isMotionDetected() {
+        return motionDetected;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public double getLightLevel() {
+        return lightLevel;
+    }
+
+    public double getPowerUsage() {
+        return powerUsage;
+    }
+
+    public String getVoiceCommand() {
+        return voiceCommand;
+    }
 }
