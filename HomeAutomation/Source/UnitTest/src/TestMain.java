e18e20cdb43587a6175b3789ef89356062156ef6
HamimSaadAlRaji <hamimsaad.raji@gmail.com>
2025-03-07 15:32:09 +0600

    Added unit test

---
+++ HomeAutomation/Source/UnitTest/src/TestMain.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class TestMain {
+    @Test
+    public void testMain() {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+    }
+}
