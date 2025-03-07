d585d74a551ceb929d8d827e71360ce20eb995f7
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2025-03-07 23:48:48 +0600

    #11: Added Logic for Motion Detection Update

--- HomeAutomation/home-automation/src/main/java/observers/MotionDetector.java
+++ HomeAutomation/home-automation/src/main/java/observers/MotionDetector.java
@@ -7,5 +7,11 @@ public class MotionDetector implements Observer {
 
     @Override
     public void update(EnvironmentState environmentState){
+        this.motionDetected = environmentState.isMotionDetected();
+        if (motionDetected) {
+            System.out.println("Motion detected!");
+        } else {
+            System.out.println("No motion detected.");
+        }
     }
 }
