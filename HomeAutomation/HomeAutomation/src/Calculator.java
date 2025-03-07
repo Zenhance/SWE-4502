1a547d14ab25fc6efe4533ac24663279b4fe0dee
mdhralif <hasiburrahman999.alif@gmail.com>
2025-03-07 14:58:16 +0600

    Initial setup Done!

---
+++ HomeAutomation/HomeAutomation/src/Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator
+{
+    int x;
+    int y;
+    public Calculator(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int add()
+    {
+        return x + y;
+    }
+    public int subtract()
+    {
+        return x - y;
+    }
+}
