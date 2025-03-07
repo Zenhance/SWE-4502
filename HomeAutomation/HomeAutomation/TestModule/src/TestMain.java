cd2f685a5d304b1444a53080ce30e67d8f42766d
tauhid-taaha <tauhidtaaha7@gmail.com>
2025-03-07 15:25:38 +0600

    initialized

---
+++ HomeAutomation/HomeAutomation/TestModule/src/TestMain.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class TestMain {
+    @Test
+    public void test() {
+
+        }
+    }
