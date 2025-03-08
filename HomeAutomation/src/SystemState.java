8daba456f16c28b2187e3c33fe0a351ff8307042
Nafisa Nawal <nafisanawal@iut-dhaka.edu>
2025-03-09 02:11:37 +0600

    #12: Created LightManager class for controlling light level according to scene

--- HomeAutomation/src/SystemState.java
+++ HomeAutomation/src/SystemState.java
@@ -1,6 +1,7 @@
 public class SystemState {
     public boolean motionDetected;
     public int lightLevel;
+    public boolean roomOccupied;
     public int temperature;
     public int energyUsage;
     public String voiceCommand;
