159ef573852b0471ac0f9010bc05212a4aa00a51
Nafisa Nawal <nafisanawal@iut-dhaka.edu>
2025-03-09 05:23:35 +0600

    #13: Modified logic in Temperature Controller

---
+++ HomeAutomation/src/TemperatureController.java
@@ -0,0 +1,36 @@
+public class TemperatureController implements IObserver {
+    public static final int hysteresis = 2;
+
+    public boolean isHeating = false;
+    public boolean isCooling = false;
+    public int targetTemperature;
+
+    @Override
+    public void update(SystemState state) {
+        int currentTemp = state.temperature;
+        controlTemperature(currentTemp);
+    }
+
+    public void setTargetTemperature(int targetTemperature)
+    {
+        this.targetTemperature = targetTemperature;
+    }
+
+    private void controlTemperature(int currentTemp) {
+        if (currentTemp < targetTemperature - hysteresis) {
+            isHeating = true;
+            isCooling = false;
+            System.out.println("Heating activated.");
+        } else if (currentTemp > targetTemperature + hysteresis) {
+            isHeating = false;
+            isCooling = true;
+            System.out.println("Cooling activated.");
+        }
+        else {
+            isHeating = false;
+            isCooling = false;
+            System.out.println("Temperature is normal.");
+        }
+    }
+
+}
