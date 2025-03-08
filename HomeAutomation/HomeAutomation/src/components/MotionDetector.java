1abd78ea7ad9b1c8b0278e55d64b89c664b63e6d
mdhralif <hasiburrahman999.alif@gmail.com>
2025-03-09 01:48:16 +0600

    A structure have been made on Issue #2: Motion Detection.

---
+++ HomeAutomation/HomeAutomation/src/components/MotionDetector.java
@@ -0,0 +1,37 @@
+package components;
+import core.Sensor;
+
+import java.util.List;
+
+public class MotionDetector implements Sensor
+{
+    private List<String> motionLog; //list of motion logs
+    private int suspiciousActivityThreshold;
+
+    @Override
+    public void onStateChange(String newState) //when the state changes (e.g. motion detected)
+    {
+        if(newState.contains("motion"))
+        {
+            logActivity(newState); //log the activity
+            analyzePattern(); //analyze the pattern of the motion (eg. if the motion is detected more than 5 times, it is considered suspicious)
+        }
+    }
+
+    private void logActivity(String motionData)
+    {
+        motionLog.add(motionData);
+        System.out.println("Motion detected! Logged: " + motionData);
+    }
+
+    private void analyzePattern()
+    {
+        if (motionLog.size() > suspiciousActivityThreshold) {
+            System.out.println("Suspicious activity detected! Alerting security.");
+        }
+        
+    }
+
+
+
+}
