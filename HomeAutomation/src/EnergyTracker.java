7decae5ca2232522af5be7b464a32d0695c43271
Nafisa Nawal <nafisanawal@iut-dhaka.edu>
2025-03-09 05:40:41 +0600

    #14: Added statistics for Energy Tracker

--- HomeAutomation/src/EnergyTracker.java
+++ HomeAutomation/src/EnergyTracker.java
@@ -2,13 +2,23 @@ import java.util.*;
 
 public class EnergyTracker implements IObserver{
     public List<Double> history = new ArrayList<>();
+    public double totalUsage = 0;
+    public int count = 0;
+    public double averageUsage = 0;
 
     @Override
     public void update(SystemState state)
     {
         double currentUsage = state.energyUsage;
+        usageStatistics(currentUsage);
     }
 
+    private void usageStatistics(double usage) {
+        history.add(usage);
+        totalUsage += usage;
+        count++;
+        averageUsage = totalUsage / count;
+    }
 
 
 }
