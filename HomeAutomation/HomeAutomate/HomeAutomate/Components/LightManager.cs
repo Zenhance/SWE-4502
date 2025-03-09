29733cde213d8b4ec97466ee30ebf7d289df0405
sakhadib <adibsakhawat@iut-dhaka.edu>
2025-03-10 02:53:08 +0600

    #3 Light Management

---
+++ HomeAutomation/HomeAutomate/HomeAutomate/Components/LightManager.cs
@@ -0,0 +1,33 @@
+﻿using HomeAutomate.Enum;
+using HomeAutomate.Factory;
+using HomeAutomate.Interfaces;
+
+namespace HomeAutomate.Components;
+
+public class LightManager : ISmartHomeComponent
+{
+    private LIGHTMODE _currentMode;
+    private float _currentLightLevel;
+    
+    public LightManager()
+    {
+        _currentMode = LIGHTMODE.DAYLIGHT;
+        _currentLightLevel = 1.0f;
+    }
+    
+    public void update(SENSOR sensor, float value)
+    {
+        if (sensor == SENSOR.LIGHT)
+        {
+            // Value = ambient light level
+            _currentMode = LightModeFactory.getMode(value);
+        }
+        
+        if (sensor == SENSOR.MOTION)
+        {
+            // Value = motion level
+            _currentLightLevel = LightModeFactory.getLevel(value);
+        }
+        
+    }
+}
