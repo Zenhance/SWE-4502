b14b7637bc0e06214d8918aa1c3fa6fd7f1ebf5e
Hasibul Islam Nirjhar <hasibulislam@iut-dhaka.edu>
2025-03-10 02:54:24 +0600

    Issue #1 Built Test Component

---
+++ HomeAutomation/Test/Issue1Test.java
@@ -0,0 +1,51 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Issue1Test {
+
+    private StateManager stateManager;
+
+
+    private TestComponent component;
+
+    @Before
+    public void setUp() {
+
+        stateManager = new StateManager();
+
+        component = new TestComponent();
+
+        };
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+    }
+
+
+
+    @Test
+    public void testObserver() {
+
+        IComponent component = new
+
+
+    }
+}
---
+++ HomeAutomation/src/TestComponent.java
@@ -0,0 +1,11 @@
+public class TestComponent implements IComponent {
+
+    private String lastUpdatedKey;
+    private Object lastUpdatedValue;
+
+    @Override
+    public void onStateChange(String key, Object newValue) {
+        lastUpdatedKey = key;
+        lastUpdatedValue = newValue;
+    }
+}
