2066e8d39bf53fd18f85cced92beae41a98fbe7b
sakhadib <adibsakhawat@iut-dhaka.edu>
2025-03-10 03:21:13 +0600

    #5 Power Consumption

---
+++ HomeAutomation/HomeAutomate/HomeAutomate/Models/PowerConsumptionData.cs
@@ -0,0 +1,23 @@
+﻿namespace HomeAutomate.Models;
+
+public class PowerConsumptionData
+{
+    private float _consumption;
+    private DateTime _timestamp;
+    
+    public PowerConsumptionData(float consumption)
+    {
+        _consumption = consumption;
+        _timestamp = DateTime.Now;
+    }
+    
+    public float getConsumption()
+    {
+        return _consumption;
+    }
+    
+    public DateTime getTimestamp()
+    {
+        return _timestamp;
+    }
+}
