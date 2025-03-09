88df43344447c18279f9f75d44076297ac879666
sakhadib <adibsakhawat@iut-dhaka.edu>
2025-03-10 03:02:14 +0600

    #4 Temperature Control

---
+++ HomeAutomation/HomeAutomate/HomeAutomate/Components/TemperatureManager.cs
@@ -0,0 +1,26 @@
+﻿using HomeAutomate.Enum;
+using HomeAutomate.Factory;
+using HomeAutomate.Interfaces;
+
+namespace HomeAutomate.Components;
+
+public class TemperatureManager : ISmartHomeComponent
+{
+    private TEMPSTATE _state;
+    private int _temperature;
+    
+    public TemperatureManager()
+    {
+        _state = TEMPSTATE.IDLE;
+        _temperature = 25;
+    }
+
+    public void update(SENSOR sensor, float value)
+    {
+        if (sensor == SENSOR.TEMPERATURE)
+        {
+            _state = TemperatureFactory.GetState(value);
+            _temperature = TemperatureFactory.GetTemperature(value);
+        }
+    }
+}
---
+++ HomeAutomation/HomeAutomate/HomeAutomate/Factory/TemperatureFactory.cs
@@ -0,0 +1,38 @@
+﻿using HomeAutomate.Enum;
+
+namespace HomeAutomate.Factory;
+
+public class TemperatureFactory
+{
+    public static TEMPSTATE GetState(float outsideTemperature)
+    {
+        if(outsideTemperature < 15)
+        {
+            return TEMPSTATE.HEATING;
+        }
+        else if(outsideTemperature > 30)
+        {
+            return TEMPSTATE.COOLING;
+        }
+        else
+        {
+            return TEMPSTATE.IDLE;
+        }
+    }
+    
+    public static int GetTemperature(float outsideTemperature)
+    {
+        if(outsideTemperature < 15)
+        {
+            return 25;
+        }
+        else if(outsideTemperature > 30)
+        {
+            return 20;
+        }
+        else
+        {
+            return 22;
+        }
+    }
+}
