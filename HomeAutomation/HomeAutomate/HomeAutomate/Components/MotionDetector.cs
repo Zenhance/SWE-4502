82bca09199b7b0126890b9a4486963e94ce37436
sakhadib <adibsakhawat@iut-dhaka.edu>
2025-03-10 02:37:40 +0600

    #2 Motion Detector and History Keeping

---
+++ HomeAutomation/HomeAutomate/HomeAutomate/Components/MotionDetector.cs
@@ -0,0 +1,32 @@
+﻿using HomeAutomate.Enum;
+using HomeAutomate.Interfaces;
+using HomeAutomate.Models;
+
+namespace HomeAutomate.Components;
+
+public class MotionDetector : ISmartHomeComponent
+{
+    private List<MotionData> _motionData;
+    private float _threshold;
+    
+    public MotionDetector(float threshold)
+    {
+        _threshold = threshold;
+        _motionData = new List<MotionData>();
+    }
+    
+    public void update(SENSOR sensor, float value)
+    {
+        if (sensor == SENSOR.MOTION)
+        {
+            if(value > _threshold)
+            {
+                _motionData.Add(new MotionData(value, MOTIONBEHAVIOR.SUSPICIOUSBEHAVIOR));
+            }
+            else
+            {
+                _motionData.Add(new MotionData(value, MOTIONBEHAVIOR.NORMALBEHAVIOR));
+            }
+        }
+    }
+}
