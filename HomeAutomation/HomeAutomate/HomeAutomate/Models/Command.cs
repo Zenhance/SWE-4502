f14cfede8f626eb57090c9cf68a6ebcfb7e92ece
sakhadib <adibsakhawat@iut-dhaka.edu>
2025-03-10 14:49:19 +0600

    #10 Added Command Class for better Encapsulation

---
+++ HomeAutomation/HomeAutomate/HomeAutomate/Models/Command.cs
@@ -0,0 +1,35 @@
+﻿namespace HomeAutomate.Models;
+
+public class Command
+{
+    private string _command;
+    private float _value;
+    
+    public Command(string command, float value)
+    {
+        _command = command;
+        _value = value;
+    }
+    
+    public Command(string command)
+    {
+        _command = command;
+        _value = 0;
+    }
+    
+    public Command(float value)
+    {
+        _command = "";
+        _value = value;
+    }
+    
+    public string getCommand()
+    {
+        return _command;
+    }
+    
+    public float getValue()
+    {
+        return _value;
+    }
+}
