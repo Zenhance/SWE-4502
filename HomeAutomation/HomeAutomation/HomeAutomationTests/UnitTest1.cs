d2904487e33bfafdcff7ebcced15784b227161d5
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-10 21:59:06 +0600

    project >

---
+++ HomeAutomation/HomeAutomation/HomeAutomationTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace HomeAutomationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
