d5d09d45a7eb468d54dabeaef9c8da4b586e43f3
sakhadib <adibsakhawat@iut-dhaka.edu>
2025-03-10 20:58:19 +0600

    #17 Added Functionality to LightManagementTest.cs

--- HomeAutomation/HomeAutomate/HomeAutomationTest/LightManagementTest.cs
+++ HomeAutomation/HomeAutomate/HomeAutomationTest/LightManagementTest.cs
@@ -88,4 +88,62 @@ public class LightManagementTest
         
         Assert.That(lightMode, Is.EqualTo(LIGHTMODE.DAYLIGHT));
     }
+    
+    
+    [Test]
+    public void LightLevelUpdateTestFor0()
+    {
+        LightManager lightManager = new LightManager();
+        Command command = new Command(0.05f);
+        lightManager.update(SENSOR.MOTION, command);
+        float lightLevel = lightManager.getCurrentLightLevel();
+        
+        Assert.That(lightLevel, Is.EqualTo(0.0f));
+    }
+    
+    [Test]
+    public void LightLevelUpdateTestFor0_3()
+    {
+        LightManager lightManager = new LightManager();
+        Command command = new Command(0.2f);
+        lightManager.update(SENSOR.MOTION, command);
+        float lightLevel = lightManager.getCurrentLightLevel();
+        
+        Assert.That(lightLevel, Is.EqualTo(0.3f));
+    }
+    
+    
+    [Test]
+    public void LightLevelUpdateTestFor0_5()
+    {
+        LightManager lightManager = new LightManager();
+        Command command = new Command(0.4f);
+        lightManager.update(SENSOR.MOTION, command);
+        float lightLevel = lightManager.getCurrentLightLevel();
+        
+        Assert.That(lightLevel, Is.EqualTo(0.5f));
+    }
+    
+    [Test]
+    public void LightLevelUpdateTestFor0_7()
+    {
+        LightManager lightManager = new LightManager();
+        Command command = new Command(0.6f);
+        lightManager.update(SENSOR.MOTION, command);
+        float lightLevel = lightManager.getCurrentLightLevel();
+        
+        Assert.That(lightLevel, Is.EqualTo(0.7f));
+    }
+    
+    
+    [Test]
+    public void LightLevelUpdateTestFor1()
+    {
+        LightManager lightManager = new LightManager();
+        Command command = new Command(0.8f);
+        lightManager.update(SENSOR.MOTION, command);
+        float lightLevel = lightManager.getCurrentLightLevel();
+        
+        Assert.That(lightLevel, Is.EqualTo(1.0f));
+    }
 }
