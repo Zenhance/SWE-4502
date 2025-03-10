dad5b2d8a47d2f8939c67bd09f4d80dfed8ed84c
sakhadib <adibsakhawat@iut-dhaka.edu>
2025-03-10 21:05:34 +0600

    #17 Added Tests for TemperatureManagementTest.cs

---
+++ HomeAutomation/HomeAutomate/HomeAutomationTest/TemperatureManagementTest.cs
@@ -0,0 +1,49 @@
+﻿using HomeAutomate.Core;
+using HomeAutomate.Models;
+using HomeAutomate.Components;
+using HomeAutomate.Enum;
+
+namespace HomeAutomationTest;
+
+public class TemperatureManagementTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+    
+    [Test]
+    public void TemperatureManagerInitializeTest()
+    {
+        TemperatureManager temperatureManager = new TemperatureManager();
+        Assert.That(temperatureManager.GetCurrentState(), Is.EqualTo(TEMPSTATE.IDLE));
+        Assert.That(temperatureManager.GetCurrentTemperatureGoal(), Is.EqualTo(25));
+    }
+    
+    [Test]
+    public void TemperatureManagerUpdateTestForHeating()
+    {
+        TemperatureManager temperatureManager = new TemperatureManager();
+        temperatureManager.update(SENSOR.TEMPERATURE, new Command(10));
+        Assert.That(temperatureManager.GetCurrentState(), Is.EqualTo(TEMPSTATE.HEATING));
+        Assert.That(temperatureManager.GetCurrentTemperatureGoal(), Is.EqualTo(25));
+    }
+    
+    [Test]
+    public void TemperatureManagerUpdateTestForCooling()
+    {
+        TemperatureManager temperatureManager = new TemperatureManager();
+        temperatureManager.update(SENSOR.TEMPERATURE, new Command(35));
+        Assert.That(temperatureManager.GetCurrentState(), Is.EqualTo(TEMPSTATE.COOLING));
+        Assert.That(temperatureManager.GetCurrentTemperatureGoal(), Is.EqualTo(20));
+    }
+    
+    [Test]
+    public void TemperatureManagerUpdateTestForIdle()
+    {
+        TemperatureManager temperatureManager = new TemperatureManager();
+        temperatureManager.update(SENSOR.TEMPERATURE, new Command(25));
+        Assert.That(temperatureManager.GetCurrentState(), Is.EqualTo(TEMPSTATE.IDLE));
+        Assert.That(temperatureManager.GetCurrentTemperatureGoal(), Is.EqualTo(22));
+    }
+}
