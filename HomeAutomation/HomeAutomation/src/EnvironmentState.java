84b20077a0e3440ebde1f5c28eb228b0afaf0b16
Shefayat E- Shams Adib <shefayatadib@iut-dhaka.edu>
2025-03-11 13:03:10 +0600

    #10: Added Environment State and Getters and Setters

---
+++ HomeAutomation/HomeAutomation/src/EnvironmentState.java
@@ -0,0 +1,50 @@
+public class EnvironmentState {
+    private boolean motionDetected;
+    private double temperature;
+    private double lightLevel;
+    private double powerUsage;
+    private String voiceCommand;
+
+
+    public boolean isMotionDetected() {
+        return motionDetected;
+    }
+
+    public void setMotionDetected(boolean motionDetected) {
+        this.motionDetected = motionDetected;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    // Getter and Setter for lightLevel
+    public double getLightLevel() {
+        return lightLevel;
+    }
+
+    public void setLightLevel(double lightLevel) {
+        this.lightLevel = lightLevel;
+    }
+
+
+    public double getPowerUsage() {
+        return powerUsage;
+    }
+
+    public void setPowerUsage(double powerUsage) {
+        this.powerUsage = powerUsage;
+    }
+
+    public String getVoiceCommand() {
+        return voiceCommand;
+    }
+
+    public void setVoiceCommand(String voiceCommand) {
+        this.voiceCommand = voiceCommand;
+    }
+}
