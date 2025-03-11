c9d928e7d372e2018f6433a86e1042bf4fdd4867
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-11 21:23:51 +0600

    Core Framework #10:notification purposes + initiate home environment

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Environment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation
+{
+    public class Environment
+    {
+        public List<IObserver> subscribers = new List<IObserver> ();
+        public bool isMotionDetected { get; set; }
+        public double Temperature { get; set; }
+
+    }
+}
--- HomeAutomation/HomeAutomation/HomeAutomation/IObserver.cs
+++ HomeAutomation/HomeAutomation/HomeAutomation/IObserver.cs
@@ -8,6 +8,6 @@ namespace HomeAutomation
 {
     public interface IObserver
     {
-        void Update();
+        void Notify();
     }
 }
