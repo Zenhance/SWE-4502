f8a94245faad209202c2c09396748d283efe83fe
takiafarhin <takiafarhin@iut-dhaka.edu>
2025-03-12 02:20:43 +0600

    #11 MotionDetection class added

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Components/MotionDetection.cs
@@ -0,0 +1,19 @@
+﻿using HomeAutomation.Core_Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation.Components
+{
+    public class MotionDetection : IObserver
+    {
+
+        public List<bool> MotionLog { get; set; } = new List<bool>();
+        public void Update(CentralState state)
+        {
+            MotionLog.Add(state.MotionDetected);
+        }
+    }
+}
