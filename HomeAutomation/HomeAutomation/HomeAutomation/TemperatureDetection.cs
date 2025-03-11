ff0eccca193726774e4b90961c5207b8c3ac7509
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-11 21:35:38 +0600

    Core Framework #10  : Observing Temperature detection

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/TemperatureDetection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation
+{
+    public class TemperatureDetection:IObserver
+    {
+        public void Notify(Environment env)
+        {
+            Console.WriteLine($"Current Temperature is: {env.Temperature}");
+        }
+    }
+}
