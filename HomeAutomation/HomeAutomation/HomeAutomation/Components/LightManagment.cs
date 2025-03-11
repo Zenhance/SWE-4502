253e3b1fb295c5cbc5dfe2a6bd6a6fdb160a2276
takiafarhin <takiafarhin@iut-dhaka.edu>
2025-03-12 03:15:46 +0600

    #12 update logic updated

--- HomeAutomation/HomeAutomation/HomeAutomation/Components/LightManagment.cs
+++ HomeAutomation/HomeAutomation/HomeAutomation/Components/LightManagment.cs
@@ -9,11 +9,40 @@ namespace HomeAutomation.Components
 {
      public class LightManagment : IObserver
     {
-        public bool LightOn { get; set; } = false;
+        public int LightLevel { get; set; }
+        public int ChangeLightLevel { get; set; }
+        public bool RoomOccupied { get; set; }
+        public String Scene { get; set; }
 
         public void Update(CentralState state)
         {
-            LightOn = state.LightLevel< 50;
+            LightLevel = state.LightLevel;
+            RoomOccupied = state.RoomOccupied;
+
+            if(!RoomOccupied)
+            {
+                ChangeLightLevel = 0;
+                Scene = "Off";
+            }
+            else
+            {
+                if(LightLevel < 50)
+                {
+                    ChangeLightLevel = 50;
+                    Scene = "Low";
+                }
+                else if (LightLevel >= 50 && LightLevel < 100)
+                {
+                    ChangeLightLevel = 100;
+                    Scene = "Medium";
+                }
+                else
+                {
+                    ChangeLightLevel = 100;
+                    Scene = "High";
+                }
+            }
+            Console.WriteLine($"Light Level: {LightLevel} Change Light Level: {ChangeLightLevel} Scene: {Scene}");
         }
     }
 }
