30025d853eee38b485904d85a20e82d5f31e33d9
takiafarhin <takiafarhin@iut-dhaka.edu>
2025-03-12 00:29:53 +0600

    centralStateManager class created

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Core_Model/CentralStateManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation.Core_Model
+{
+    class CentralStateManager
+    {
+    }
+}
