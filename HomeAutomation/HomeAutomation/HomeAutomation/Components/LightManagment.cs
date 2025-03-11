9657fb88d1e0885629fd7568acd7b0eeb06bb980
takiafarhin <takiafarhin@iut-dhaka.edu>
2025-03-12 02:51:07 +0600

    #12  LightManagment  concrete class created

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Components/LightManagment.cs
@@ -0,0 +1,19 @@
+﻿using HomeAutomation.Core_Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation.Components
+{
+     public class LightManagment : IObserver
+    {
+        public bool LightOn { get; set; } = false;
+
+        public void Update(CentralState state)
+        {
+            LightOn = state.LightLevel< 50;
+        }
+    }
+}
