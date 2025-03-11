180c1883c0053ece9c86f7d8a50446a05e007b92
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-11 21:32:45 +0600

    Core Framework #10 : Observing motion detection

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/MotionDetector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation
+{
+    public class MotionDetector:IObserver
+    {
+        public void Notify(Environment env)
+        {
+            if (env.isMotionDetected)
+            {
+                Console.WriteLine("Motion Detected!");
+            }
+            else
+            {
+                Console.WriteLine("No motion detected.");
+            }
+        }
+
+    }
+}
