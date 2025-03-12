124ce292343af711ed144f5d938bb78f89893ff4
AntaraArifa <antara5643@gmail.com>
2025-03-13 05:28:40 +0600

    Update TemperatureControlSystem to notify observers with temperature and room information

--- HomeAutomation/src/TemperatureControlSystem.java
+++ HomeAutomation/src/TemperatureControlSystem.java
@@ -1,5 +1,5 @@
 public class TemperatureControlSystem extends Subject {
-    public void changeTemperature(String temperature, String room) {
+    public void changeTemperature(int temperature, String room) {
         String message = temperature + "," + room;
         notifyObservers(message);
     }
