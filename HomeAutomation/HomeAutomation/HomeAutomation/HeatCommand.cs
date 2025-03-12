d11cf8430d6fa7579aae95dbfd1c9e43e13eaa45
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-12 17:31:26 +0600

    Temperature Control #13 : cooling and heating coommands

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/HeatCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation
+{
+    public class HeatCommand : ITemp
+    {
+        public void Do()
+        {
+            Console.WriteLine("Activating heating...");
+
+        }
+    }
+}
