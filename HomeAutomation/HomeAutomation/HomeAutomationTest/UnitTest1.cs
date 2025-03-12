abe82e4973560ec31678670af4c920099000d678
rakins <shahriar.rakin99@gmail.com>
2025-03-13 00:54:59 +0600

    init

--- HomeAutomation/HomeAutomation/HomeAutomationTest/UnitTest1.cs
+++ HomeAutomation/HomeAutomation/HomeAutomationTest/UnitTest1.cs
@@ -1,9 +1,15 @@
 namespace HomeAutomationTest;
 
-public class UnitTest1
+public class Tests
 {
-    [Fact]
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
     public void Test1()
     {
+        Assert.Pass();
     }
 }
