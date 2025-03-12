a0c75416e12d283554bc69bb76f1d9601f9cf558
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2025-03-13 03:34:18 +0600

    #10 temperature state change executor implemented

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Executor/TemperatureStateChangeExecutor.cs
@@ -0,0 +1,18 @@
+﻿using HomeAutomation.Core;
+using HomeAutomation.Models;
+
+namespace HomeAutomation.Executor;
+
+public class TemperatureStateChangeExecutor: StateChangeExecutor
+{
+    public TemperatureStateChangeExecutor(EnvironmentState environmentState, Command command)
+    {
+        this.environmentState = environmentState;
+        this._command = command;
+    }
+    
+    public override void execute()
+    {
+        environmentState.SetTemperature(_command);
+    }
+}
