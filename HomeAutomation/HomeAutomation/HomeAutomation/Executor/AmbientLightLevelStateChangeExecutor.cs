6a0b74052d45bf075bb778da88dd6c38362f0b46
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2025-03-13 03:32:29 +0600

    #10 Ambient Light Level State Change Executor implemented

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Executor/AmbientLightLevelStateChangeExecutor.cs
@@ -0,0 +1,18 @@
+﻿using HomeAutomation.Core;
+using HomeAutomation.Models;
+
+namespace HomeAutomation.Executor;
+
+public class AmbientLightLevelStateChangeExecutor : StateChangeExecutor
+{
+    public AmbientLightLevelStateChangeExecutor(EnvironmentState environmentState, Command command)
+    {
+        this.environmentState = environmentState;
+        this._command = command;
+    }
+    
+    public override void execute()
+    {
+        environmentState.SetAmbientLightLevel(_command);
+    }
+}
