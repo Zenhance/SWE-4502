009d5b49a41b569f4777fbfdec09192b8c54bc97
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2025-03-13 01:22:21 +0600

    #10 interface completed

--- HomeAutomation/HomeAutomation/HomeAutomation/Interface/ISmartHomeComponent.cs
+++ HomeAutomation/HomeAutomation/HomeAutomation/Interface/ISmartHomeComponent.cs
@@ -1,6 +1,9 @@
-﻿namespace HomeAutomation.Interface;
+﻿using HomeAutomation.ENUM;
+using HomeAutomation.Models;
+
+namespace HomeAutomation.Interface;
 
 public class ISmartHomeComponent
 {
-    
+    void update(SENSOR sensor, Command command);
 }
