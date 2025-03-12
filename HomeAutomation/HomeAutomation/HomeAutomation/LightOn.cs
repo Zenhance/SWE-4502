f2bcc508b603ac416cc2d624f25eac6e51ce3061
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-12 18:36:23 +0600

    Voice Command System #15 : light on voice command

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/LightOn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation
+{
+    public class LightOn:IVoice
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Voice Command: Turning the light on.");
+        }
+    }
+}
