ed233481da6e01b46040ded0860ca2e629d0ca4a
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-12 18:08:38 +0600

    Energy Consumption Tracking #14 : initiate the base chain

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/EnergyChain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation
+{
+    public abstract class EnergyChain
+    {
+        protected EnergyChain chain;
+        public void SetEnergyChain(EnergyChain energychain)
+        {
+            chain = energychain;
+        }
+        public abstract void Manager(HomeEnvironment env);
+    }
+}
