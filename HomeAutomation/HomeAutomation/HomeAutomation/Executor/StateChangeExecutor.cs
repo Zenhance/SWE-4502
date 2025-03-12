45c42e5370d9a94dc3453cec28d8b54265edb1ba
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2025-03-13 03:25:52 +0600

    #10 StateChangeExecutor class implemented

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Executor/StateChangeExecutor.cs
@@ -0,0 +1,28 @@
+﻿using HomeAutomation.Core;
+using HomeAutomation.Models;
+
+namespace HomeAutomation.Executor;
+
+public abstract class StateChangeExecutor
+{
+    protected EnvironmentState environmentState;
+    protected Command _command;
+
+    public StateChangeExecutor(EnvironmentState environmentState, Command command)
+    {
+        this.environmentState = environmentState;
+        this._command = command;
+    }
+
+    public StateChangeExecutor()
+    {
+        
+    }
+    
+    public abstract void execute();
+
+    public Command getCommand()
+    {
+        return _command;
+    }
+}
