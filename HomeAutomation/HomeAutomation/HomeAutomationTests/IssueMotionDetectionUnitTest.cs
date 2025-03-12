1bc69c3844b8b8e084cc1f6183c592c1baff5f86
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-12 16:13:05 +0600

    Unit tests #17 : motion detection test : no motion detected

---
+++ HomeAutomation/HomeAutomation/HomeAutomationTests/IssueMotionDetectionUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using HomeAutomation;
+
+namespace HomeAutomationTests
+{
+    public class IssueMotionDetectionUnitTest
+    {
+        [Fact]
+        public void ShouldLogMotionWhenMotionIsDetected()
+        {
+            // Arrange
+            var motionDetector = new MotionDetector();
+            var homeEnvironment = new HomeEnvironment();
+            homeEnvironment.Subscribe(motionDetector);
+
+            // Act
+            homeEnvironment.UpdateHomeEnvironment(true, 12.12, "Basement");
+
+            // Assert
+            Assert.Single(motionDetector.detectionLogs);  
+            Assert.Contains("Motion detected at", motionDetector.detectionLogs[0]);
+            Assert.Contains("Basement", motionDetector.detectionLogs[0]); 
+
+        }
+    }
+}
