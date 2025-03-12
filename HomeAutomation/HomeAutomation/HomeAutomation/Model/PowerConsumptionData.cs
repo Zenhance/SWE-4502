45a5f668f41318d6c55f9fe959fa605def00e293
rakins <shahriar.rakin99@gmail.com>
2025-03-13 03:15:38 +0600

    #14 PowerConsumptionData class with constructor

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Model/PowerConsumptionData.cs
@@ -0,0 +1,13 @@
+﻿namespace HomeAutomation.Model;
+
+public class PowerConsumptionData
+{
+    private float _consumption;
+    private DateTime _timestamp;
+    
+    public PowerConsumptionData(float consumption)
+    {
+        _consumption = consumption;
+        _timestamp = DateTime.Now;
+    }
+}
