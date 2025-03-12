857f2da19552654e6fb15c91dcec54c851120ad6
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-12 16:58:28 +0600

    Light Management #12 : breaking lightwork in ambientlight handler

--- HomeAutomation/HomeAutomation/HomeAutomation/AmbientLight.cs
+++ HomeAutomation/HomeAutomation/HomeAutomation/AmbientLight.cs
@@ -6,21 +6,17 @@ using System.Threading.Tasks;
 
 namespace HomeAutomation
 {
-    public class LightWork:Light
+    public class AmbientLight:Light
     {
         public override void Manage(LightState state)
         {
-            if (state.IsRoomTaken)
-            {
-                Console.WriteLine("Adjusting light for occupied room.");
-            }
-            else if (state.AmbientLight < 50)
+            if (state.AmbientLight < 50)
             {
                 Console.WriteLine("Ambient light too low : increasing light.");
             }
             else
             {
-                Console.WriteLine("No adjustent needed : perfect.");
+                Console.WriteLine("No adjustment needed : perfect");
             }
 
             light?.Manage(state);
