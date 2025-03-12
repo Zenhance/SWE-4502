233beee2d4d94271b7f66cdd1baf0c851b12da0f
Shefayat E- Shams Adib <shefayatadib@iut-dhaka.edu>
2025-03-12 14:13:17 +0600

    #12: Added a light controller class

---
+++ HomeAutomation/HomeAutomation/src/LightController.java
@@ -0,0 +1,42 @@
+public class LightController implements Observer
+{
+    private int lightLevel;
+
+    @Override
+    public void update(EnvironmentState state)
+    {
+        this.lightLevel = (int) state.getLightLevel();
+        if (lightLevel < 50)
+        {
+            turnOnLights();
+        }
+        else
+        {
+            turnOffLights();
+        }
+    }
+
+    @Override
+    public void update(SystemState state)
+    {
+        this.lightLevel = state.lightLevel;
+        if (lightLevel < 50)
+        {
+            turnOnLights();
+        }
+        else
+        {
+            turnOffLights();
+        }
+    }
+
+    public void turnOnLights()
+    {
+        System.out.println("Turning on the lights.");
+    }
+
+    public void turnOffLights()
+    {
+        System.out.println("Turning off the lights.");
+    }
+}
