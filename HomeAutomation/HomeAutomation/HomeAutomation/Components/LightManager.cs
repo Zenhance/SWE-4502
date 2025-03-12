b2bf241c783a501f63de73a92ec3afb4332c977d
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2025-03-13 02:10:36 +0600

    #12 LightManager.cs constructor created

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Components/LightManager.cs
@@ -0,0 +1,22 @@
+﻿using HomeAutomation.ENUM;
+using HomeAutomation.Interface;
+using HomeAutomation.Models;
+
+namespace HomeAutomation.Components;
+
+public class LightManager:ISmartHomeComponent
+{
+    private LIGHTMODE currentMode;
+    private float currentLightLevel;
+    
+    public LightManager()
+    {
+        currentMode = LIGHTMODE.DAYLIGHT;
+        currentLightLevel = 1.0f;
+    }
+    
+    public void update(SENSOR sensor, Command command)
+    {
+        
+    }
+}
