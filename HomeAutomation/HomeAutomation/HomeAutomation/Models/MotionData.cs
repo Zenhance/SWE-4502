85d5965571a2a87db80963efb2835aec73780ed4
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2025-03-13 01:43:34 +0600

    #11 MotionData model created

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Models/MotionData.cs
@@ -0,0 +1,17 @@
+﻿using HomeAutomation.ENUM;
+
+namespace HomeAutomation.Models;
+
+public class MotionData
+{
+    private float value;
+    private DateTime time;
+    private MOTIONBEHAVIOR behavior;
+    
+    public MotionData(float value, DateTime time, MOTIONBEHAVIOR behavior)
+    {
+        this.value = value;
+        this.time = time;
+        this.behavior = behavior;
+    }
+}
