4693d4061c9d2f8b9eb05fa11a97609c3bdd78de
whiteflags26 <whiteflags26@gmail.com>
2025-03-13 00:37:12 +0600

    feat(core): implement CoreSystem and IObserver for state management
    Closes #1
    
    - Added CoreSystem to manage state changes and notify observers
    - Implemented IObserver for components to register interest in state updates
    - Introduced a data structure to store environmental measurements

---
+++ HomeAutomation/MyHomeAutomation/CoreSystem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHomeAutomation
+{
+    public class CoreSystem
+    {
+        private List<IObserver> observers;
+        private Dictionary<string, string> state;
+
+        CoreSystem() 
+        {
+            observers = new List<IObserver>();
+            state = new Dictionary<string, string>();
+        }
+
+        public void RegisterObserver(IObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void UnregisterObserver(IObserver observer)
+        {
+            observers.Remove(observer);
+        }
+
+        public void NotifyObservers(string key)
+        {
+            foreach (var observer in observers)
+            {
+                observer.Update(key, state[key]);
+            }
+        }
+
+        public void SetState(string key, string value)
+        {
+            state[key] = value;
+            NotifyObservers(key);
+        }
+
+        public string GetState(string key)
+        {
+            if(state.ContainsKey(key)) return state[key];
+            return null;
+        }
+    }
+}
