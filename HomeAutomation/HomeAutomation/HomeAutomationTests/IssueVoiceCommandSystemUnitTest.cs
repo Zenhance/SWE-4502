b94a3256bce90dc539d17be0619640e94bdab11b
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-12 18:42:46 +0600

    Unit tests #17 : Voice command unit tests - lights turn on and off

---
+++ HomeAutomation/HomeAutomation/HomeAutomationTests/IssueVoiceCommandSystemUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using HomeAutomation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomationTests
+{
+    public class IssueVoiceCommandSystemUnitTest
+    {
+        private readonly StringWriter stringWriter;
+        public IssueVoiceCommandSystemUnitTest()
+        {
+            stringWriter = new StringWriter();
+        }
+
+        [Fact]
+        public void ShouldTurnOnLightVoiceCommand()
+        {
+            var voiceCommandSystem = new VoiceCommand();
+            var homeEnvironment = new HomeEnvironment();
+            homeEnvironment.Subscribe(voiceCommandSystem);
+
+            Console.SetOut(stringWriter);
+            homeEnvironment.UpdateHomeEnvironment(true, 12.12, "Hallway");
+
+
+            Assert.Contains("Voice Command: Turning the light on.", stringWriter.ToString());
+        }
+
+    }
+}
