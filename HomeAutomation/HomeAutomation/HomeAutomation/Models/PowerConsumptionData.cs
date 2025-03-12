b0091470491902e0685bad2433bc1501aecdfc7d
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2025-03-13 02:47:44 +0600

    #14 PowerConsumptionData model implemented

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Models/PowerConsumptionData.cs
@@ -0,0 +1,23 @@
+﻿namespace HomeAutomation.Models;
+
+public class PowerConsumptionData
+{
+    private float consumption;
+    private DateTime time;
+    
+    public PowerConsumptionData(float consumption, DateTime time)
+    {
+        this.consumption = consumption;
+        this.time = DateTime.Now;
+    }
+    
+    public float getConsumption()
+    {
+        return consumption;
+    }
+    
+    public DateTime getTimestamp()
+    {
+        return time;
+    }
+}
