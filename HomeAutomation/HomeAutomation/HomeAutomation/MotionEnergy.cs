46eac780117863cc086c8178b66fbd9f91055515
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-12 18:10:27 +0600

    Energy Consumption Tracking #14 : motion energy chain responsibility

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/MotionEnergy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation
+{
+    public class MotionEnergy : EnergyChain
+    {
+        public double totalConsumption = 0;
+
+        public override void Manager(HomeEnvironment env)
+        {
+            if (env.isMotionDetected)
+            {
+                double consumption = 12.12;
+                totalConsumption += consumption;
+                Console.WriteLine($"Energy Consumption Updated: {consumption} watts. Total: {totalConsumption} watts.");
+
+                if (totalConsumption > 1212.12)
+                {
+                    Console.WriteLine("Warning: Unusual energy consumption detected!");
+                }
+            }
+
+            chain?.Manager(env);
+        }
+    }
+}
