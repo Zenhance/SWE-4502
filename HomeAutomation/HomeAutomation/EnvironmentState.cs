5f7a975a1b5a0ec69f7b5b69e7545638beb65635
Md. Abid Hasan <emonff89@gmail.com>
2025-03-12 23:17:02 +0600

    Issue #1: Core System Framework

---
+++ HomeAutomation/HomeAutomation/EnvironmentState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation
+{
+    // Core/EnvironmentState.cs
+    public class EnvironmentState
+    {
+        
+        public Dictionary<string, object> State { get; set; } = new Dictionary<string, object>();
+
+      
+        public void PrintState()
+        {
+            Console.WriteLine("Environment State:");
+            foreach (var key in State.Keys)
+            {
+                Console.WriteLine($"{key}: {State[key]}");
+            }
+        }
+    }
+}
---
+++ HomeAutomation/HomeAutomation/IStateChangeListener.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation
+{
+    public interface IStateChangeListener
+    {
+        void OnStateChange(EnvironmentState state);
+    }
+}
