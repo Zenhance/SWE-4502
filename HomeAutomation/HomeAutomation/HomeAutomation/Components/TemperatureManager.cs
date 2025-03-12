b38ed3030525a1af13033a587dffe3460f822924
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2025-03-13 02:34:42 +0600

    #13 TemperatureManager.cs implemented with update function and getter functions

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Components/TemperatureManager.cs
@@ -0,0 +1,38 @@
+﻿using HomeAutomation.ENUM;
+using HomeAutomation.Factory;
+using HomeAutomation.Interface;
+using HomeAutomation.Models;
+
+namespace HomeAutomation.Components;
+
+public class TemperatureManager: ISmartHomeComponent
+{
+    private TEMPSTATE state;
+    private int temperature;
+    
+    public TemperatureManager()
+    {
+        state = TEMPSTATE.IDLE;
+        temperature = 25;
+    }
+    
+    public void update(SENSOR sensor, Command command)
+    {
+        float value = command.getValue();
+        if (sensor == SENSOR.TEMPERATURE)
+        {
+            state = TemperatureFactory.GetState(value);
+            temperature = TemperatureFactory.GetTemperature(value);
+        }
+    }
+    
+    public TEMPSTATE GetState()
+    {
+        return state;
+    }
+    
+    public int GetTemperature()
+    {
+        return temperature;
+    }
+}
