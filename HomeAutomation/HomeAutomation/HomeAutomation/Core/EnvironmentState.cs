740371a03730503375f0cc25345a851242d5ba47
rakins <shahriar.rakin99@gmail.com>
2025-03-13 01:29:20 +0600

    #10 EnvironmentState() constructor

--- HomeAutomation/HomeAutomation/HomeAutomation/Core/EnvironmentState.cs
+++ HomeAutomation/HomeAutomation/HomeAutomation/Core/EnvironmentState.cs
@@ -1,12 +1,26 @@
-﻿namespace HomeAutomation.Core;
+﻿using HomeAutomation.Interface;
+
+namespace HomeAutomation.Core;
 
 public class EnvironmentState
 {
     private float _motionLevel;
     private float _temperature;
-    private float ambientLightLevel;
+    private float _ambientLightLevel;
     private float _energyConsumption;
-    private float _lastVoiceCommand;
+    private string _lastVoiceCommand;
+    
+    private readonly List<ISmartHomeComponent> _components;
+    
+    public EnvironmentState()
+    {
+        _components = new List<ISmartHomeComponent>();
+        _motionLevel = 0;
+        _ambientLightLevel = 0;
+        _temperature = 0;
+        _energyConsumption = 0;
+        _lastVoiceCommand = "";
+    }
     
     
 }
