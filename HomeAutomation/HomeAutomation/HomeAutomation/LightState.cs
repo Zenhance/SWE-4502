3df7e60115abcf46d4e9a966f377a88cc80957c8
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-12 16:31:36 +0600

    Light Management #12 : room and ambientlight initiate

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/LightState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation
+{
+    public class LightState
+    {
+        public bool IsRoomTaken {get;set;}
+        public int AmbientLight { get; set; }
+
+        public LightState(bool istaken,int ambientlight) 
+        {
+            IsRoomTaken = istaken;
+            AmbientLight = ambientlight;
+        }
+    }
+}
