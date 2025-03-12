1541a7f1989f0397ed3daf3b06cadf10f34b5380
Md. Abid Hasan <emonff89@gmail.com>
2025-03-12 21:43:17 +0600

    Made Light Management
    ILightStrategy:Defines a contract for light adjustment strategies.
    DefaultLightStrategy:A default strategy for adjusting lights.
    LightController Strategy Pattern to adjust lighting.

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/DefaultLightStrategy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation
+{
+   
+    public class DefaultLightStrategy : ILightStrategy
+    {
+        public void AdjustLighting(int ambientLightLevel, bool isOccupied)
+        {
+            string lightStatus = isOccupied ? "On" : "Off";
+            Console.WriteLine($"Default Strategy: Lights {lightStatus}");
+        }
+    }
+}
