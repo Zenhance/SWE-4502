75f058506d516e89d4052d537d5f376ad6747955
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-12 16:32:35 +0600

    Light Management #12 : abstract light management class because I want it to be protected.

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Light.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation
+{
+    public abstract class Light
+    {
+        protected Light light;
+        public void Setlight(Light lightt)
+        {
+            light = lightt;
+        }
+        public abstract void Manage(LightState state);
+    }
+}
