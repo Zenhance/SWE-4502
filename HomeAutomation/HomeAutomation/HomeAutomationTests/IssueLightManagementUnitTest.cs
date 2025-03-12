8f099e0971a7af6ca66e3fbc4674cf2e0f7664df
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-12 17:13:58 +0600

    Unit tests #17 : light management room occupancy and ambient light level tests passed

---
+++ HomeAutomation/HomeAutomation/HomeAutomationTests/IssueLightManagementUnitTest.cs
@@ -0,0 +1,41 @@
+﻿using HomeAutomation;
+using System.IO;
+
+namespace HomeAutomationTests
+{
+    public class IssueLightManagementUnitTest
+    {
+        [Fact]
+        public void ShouldTurnOnLightsIfRoomIsTaken()
+        {
+            var lightControl = new RoomLightControl();
+            var state = new LightState(true, 50);
+
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            // Act
+            lightControl.AdjustLight(state);
+
+            // Assert
+            Assert.Contains("Turning lights on.", stringWriter.ToString());
+        }
+        [Fact]
+        public void ShouldIncreaseIfAmbientLightIsLow()
+        {
+            // Arrange
+            var lightControl = new RoomLightControl();
+            var state = new LightState(true, 12);
+
+            
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            // Act
+            lightControl.AdjustLight(state);
+
+            // Assert
+            Assert.Contains("Ambient light too low : increasing light.", stringWriter.ToString());
+        }
+    }
+}
