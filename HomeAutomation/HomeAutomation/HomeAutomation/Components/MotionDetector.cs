e4bcd293e2134a969679e3e398d866d24a854c92
rakins <shahriar.rakin99@gmail.com>
2025-03-13 01:50:32 +0600

    #11 MotionDetector class constructor and update function creaded

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Components/MotionDetector.cs
@@ -0,0 +1,33 @@
+﻿using HomeAutomation.Enum;
+using HomeAutomation.Interface;
+using HomeAutomation.Model;
+
+namespace HomeAutomation.Components;
+
+public class MotionDetector : ISmartHomeComponent
+{
+    private List<MotionData> _motionData;
+    private float _threshold;
+    
+    public MotionDetector(float threshold)
+    {
+        _threshold = threshold;
+        _motionData = new List<MotionData>();
+    }
+    
+    public void update(SENSOR sensor, Command command)
+    {
+        float value = command.getValue();
+        if (sensor == SENSOR.MOTION)
+        {
+            if(value > _threshold)
+            {
+                _motionData.Add(new MotionData(value, MOTIONBEHAVIOR.SUSPICIOUSBEHAVIOR));
+            }
+            else
+            {
+                _motionData.Add(new MotionData(value, MOTIONBEHAVIOR.NORMALBEHAVIOR));
+            }
+        }
+    }
+}
