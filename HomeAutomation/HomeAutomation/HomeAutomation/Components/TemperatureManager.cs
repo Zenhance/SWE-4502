59f285d18998fb4fb013524c3dca34a263a42678
rakins <shahriar.rakin99@gmail.com>
2025-03-13 03:01:35 +0600

    #13 TemperatureManager class and constructor

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Components/TemperatureManager.cs
@@ -0,0 +1,17 @@
+﻿using HomeAutomation.Enum;
+using HomeAutomation.Interface;
+
+namespace HomeAutomation.Components;
+
+public class TemperatureManager : ISmartHomeComponent
+{
+    private TEMPSTATE _state;
+    private int _temperature;
+    
+    public TemperatureManager()
+    {
+        _state = TEMPSTATE.IDLE;
+        _temperature = 25;
+    }
+    
+}
