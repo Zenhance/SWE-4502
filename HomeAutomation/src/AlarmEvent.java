908bf2a57d4b07f9b27c4a18a1efbd71ad5067f2
Hasibul Islam Nirjhar <hasibulislam@iut-dhaka.edu>
2025-03-13 05:04:05 +0600

    Issue #2 added test component of notifying

---
+++ HomeAutomation/src/AlarmEvent.java
@@ -0,0 +1,17 @@
+public class AlarmEvent {
+    private final String message;
+    private final long timestamp;
+
+    public AlarmEvent(String message, long timestamp) {
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+}
---
+++ HomeAutomation/src/AlarmListener.java
@@ -0,0 +1,14 @@
+public class AlarmListener implements IComponent {
+    private AlarmEvent lastAlarm;
+
+    @Override
+    public void onStateChange(String key, Object newValue) {
+        if ("alarm".equals(key) && newValue instanceof AlarmEvent) {
+            this.lastAlarm = (AlarmEvent) newValue;
+        }
+    }
+
+    public AlarmEvent getLastAlarm() {
+        return lastAlarm;
+    }
+}
