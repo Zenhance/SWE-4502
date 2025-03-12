be776ae984538a994c83a7e1ee7f114fae1ac6da
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2025-03-13 01:51:47 +0600

    #11 update function implemented at MotionDetector.cs

--- HomeAutomation/HomeAutomation/HomeAutomation/Components/MotionDetector.cs
+++ HomeAutomation/HomeAutomation/HomeAutomation/Components/MotionDetector.cs
@@ -1,4 +1,5 @@
-﻿using HomeAutomation.Interface;
+﻿using HomeAutomation.ENUM;
+using HomeAutomation.Interface;
 using HomeAutomation.Models;
 
 namespace HomeAutomation.Components;
@@ -13,4 +14,34 @@ public class MotionDetector: ISmartHomeComponent
         motionDataList = new List<MotionData>();
         this.threshold = threshold;
     }
+    
+    public void update(SENSOR sensor, Command command)
+    {
+        float value = command.getValue();
+        if (sensor == SENSOR.MOTION)
+        {
+            if (value > threshold)
+            {
+                motionDataList.Add(new MotionData(value, DateTime.Now, MOTIONBEHAVIOR.SUSPICIOUS));
+            }
+            else
+            {
+                motionDataList.Add(new MotionData(value, DateTime.Now, MOTIONBEHAVIOR.NORMAL));
+            }
+        }
+    }
+    
+    public List<MotionData> GetMotionDataList()
+    {
+        return motionDataList;
+    }
+    
+    public MotionData GetLatestMotionData()
+    {
+        return motionDataList.Last();
+    }
+    
+    
+    
+    
 }
