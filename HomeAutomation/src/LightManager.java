99c361746ec2941f5f6d145c0ef92e6707ab664c
Faiza Maliat <faiza.maliat20@gmail.com>
2025-03-13 02:44:59 +0600

    #12: Update LightManager class with allw controlss

--- HomeAutomation/src/LightManager.java
+++ HomeAutomation/src/LightManager.java
@@ -10,15 +10,51 @@ public class LightManager implements IObserver {
         homeState.registerObserver(this);
     }
 
+    @Override
+    public void update(String stateName, Object value) {
+        if (stateName.equals("light-" + room) && value instanceof Integer) {
+            int lightLevel = (Integer) value;
+            if (lightLevel < 63) {
+
+                //mathhaaaa amar mathhaaaaa 63
+                lightsOn = true;
+            } else {
+                lightsOn = false;
+            }
+            updateLights();
+        }
+        if (stateName.equals("occupied-" + room) && value instanceof Boolean) {
+            boolean occupied = (Boolean) value;
+            if (!occupied) {
+                lightsOn = false;
+            }
+            updateLights();
+        }
+    }
 
     public void setLightLevel(int lightLevel) {
         homeState.setState("light-" + room, lightLevel);
     }
 
-
-
-    public boolean isLightsOn() {
-        return lightsOn;
+    public void setOccupancy(boolean occupied) {
+        homeState.setState("occupied-" + room, occupied);
     }
 
+    public void setScene(String scene) {
+        if (scene.equals("reading")) {
+            lightsOn = true;
+        } else if (scene.equals("movie")) {
+            lightsOn = true;
+        }
+        updateLights();
+    }
+
+    private void updateLights() {
+        homeState.setState("lights-" + room, lightsOn);
+    }
+
+    public boolean isLightsOn() {
+        Object status = homeState.getState("lights-" + room);
+        return status != null && (Boolean) status;
+    }
 }
