ac40ec667d70070a54c9d0aade2fbd682d764f6d
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-12 18:19:09 +0600

    Energy Consumption Tracking #14 : Building the chain

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/EnergyTracker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation
+{
+    public class EnergyTracker : IObserver
+    {
+        public EnergyChain energychain;
+        public EnergyTracker()
+        {
+            energychain = new MotionEnergy();
+        }
+        public void Notify(HomeEnvironment env)
+        {
+            energychain.Manager(env);
+        }
+    }
+}
