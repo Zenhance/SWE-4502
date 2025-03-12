eb92f00bfbea38320f61a3f3e4c4a0bba425c5a6
AntaraArifa <antara5643@gmail.com>
2025-03-13 03:38:31 +0600

    Enhance LightObserver to handle invalid message formats and adjust lighting levels based on room occupancy and light levels (Reading, Normal, Movie scenes).

--- HomeAutomation/src/LightControlSystem.java
+++ HomeAutomation/src/LightControlSystem.java
@@ -1,6 +1,6 @@
 public class LightControlSystem extends Subject {
-    public void changeLightStatus(String lightStatus, String room) {
-        String message = lightStatus + "," + room;
+    public void changeLightStatus(int lightLevel, boolean roomOccupied, String room) {
+        String message = lightLevel + "," + roomOccupied + "," + room;
         notifyObservers(message);
     }
 }
--- HomeAutomation/src/LightObserver.java
+++ HomeAutomation/src/LightObserver.java
@@ -1,17 +1,50 @@
 public class LightObserver implements Observer {
-    private String lastLightStatus;
+    private int changedLightLevel;
+    private String scene;
     private String lastRoom;
 
     @Override
     public void update(String message) {
+
         String[] parts = message.split(",");
-        this.lastLightStatus = parts[0];
-        this.lastRoom = parts[1];
-        System.out.println("Light status in " + lastRoom + ": " + lastLightStatus);
+
+        // Ensure message is in correct format
+        if (parts.length != 3) {
+            System.err.println("Invalid message format");
+            return;
+        }
+
+        int lightLevel = Integer.parseInt(parts[0]);
+        boolean roomOccupied = Boolean.parseBoolean(parts[1]);
+        this.lastRoom = parts[2];
+
+        // We arw adjusting light level based on room occupancy and light level
+        if (!roomOccupied) {
+            changedLightLevel = 0;
+            scene = "Off";
+        } else {
+            if (lightLevel < 30) {
+                changedLightLevel = 70;
+                scene = "Reading";
+            } else if (lightLevel >= 30 && lightLevel <= 70) {
+                changedLightLevel = 60;
+                scene = "Normal";
+            } else {
+                changedLightLevel = 30;
+                scene = "Movie";
+            }
+        }
+
+
+        System.out.println("Lighting Adjusted: Scene - " + scene + ", changedLight Level - " + changedLightLevel + ", Last Room - " + lastRoom);
     }
 
-    public String getLastLightStatus() {
-        return lastLightStatus;
+    public int getChangedLightLevel() {
+        return changedLightLevel;
+    }
+
+    public String getScene() {
+        return scene;
     }
 
     public String getLastRoom() {
