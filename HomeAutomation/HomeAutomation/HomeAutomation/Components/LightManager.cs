5164ca72c58cc46b823eb7af93b3c6f2cc4cb66f
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2025-03-13 02:19:34 +0600

    #12 update and getter functions in LightManager.cs

--- HomeAutomation/HomeAutomation/HomeAutomation/Components/LightManager.cs
+++ HomeAutomation/HomeAutomation/HomeAutomation/Components/LightManager.cs
@@ -1,4 +1,5 @@
 ﻿using HomeAutomation.ENUM;
+using HomeAutomation.Factory;
 using HomeAutomation.Interface;
 using HomeAutomation.Models;
 
@@ -17,6 +18,24 @@ public class LightManager:ISmartHomeComponent
     
     public void update(SENSOR sensor, Command command)
     {
-        
+        float value = command.getValue();
+        if (sensor == SENSOR.MOTION)
+        {
+            currentLightLevel = LightModeFactory.getLevel(value);
+        }
+        if (sensor == SENSOR.LIGHT)
+        {
+            currentMode = LightModeFactory.getMode(value);
+        }
+    }
+    
+    public LIGHTMODE GetCurrentMode()
+    {
+        return currentMode;
+    }
+    
+    public float GetCurrentLightLevel()
+    {
+        return currentLightLevel;
     }
 }
