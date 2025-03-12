9f2f5f2a2edc2a1e78b6ac55adf66856eca4a988
rakins <shahriar.rakin99@gmail.com>
2025-03-13 03:23:35 +0600

    #14 EnergyConsumptionManager class with constructor

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Components/EnergyConsumptionManager.cs
@@ -0,0 +1,16 @@
+﻿using HomeAutomation.Interface;
+using HomeAutomation.Model;
+
+namespace HomeAutomation.Components;
+
+public class EnergyConsumptionManager : ISmartHomeComponent
+{
+    private List<PowerConsumptionData> _consumptionData;
+    private float _threshold;
+    
+    public EnergyConsumptionManager(float threshold)
+    {
+        _consumptionData = new List<PowerConsumptionData>();
+        _threshold = threshold;
+    }
+}
