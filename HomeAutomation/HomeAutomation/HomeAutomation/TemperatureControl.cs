51c8d46e7e2117b6c31c110ad5959f1b5cd5feaa
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-12 17:34:47 +0600

    Temperature Control #13 : temp control for heat and cool

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/TemperatureControl.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation
+{
+    public class TemperatureControl:IObserver
+    {
+        public double TargetTemp;
+        public ITemp heat;
+        public ITemp cool;
+
+        public TemperatureControl(double targetTemperature)
+        {
+            this.TargetTemp = targetTemperature;
+            this.heat = new HeatCommand();
+            this.cool = new CoolCommand();
+        }
+        public void Notify(HomeEnvironment env)
+        {
+            if (env.Temperature < TargetTemp)
+            {
+                heat.Do();
+            }
+            else if (env.Temperature > TargetTemp)
+            {
+                cool.Do();
+            }
+            else
+            {
+                Console.WriteLine("Temperature is stable.");
+            }
+        }
+    }
+}
