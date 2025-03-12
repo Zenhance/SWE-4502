8522e0ebb96f26cf32cb0cfdeaef252ac650cd58
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-12 18:46:13 +0600

    Remote Access App #16 : Remote Access App initiate

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/RemoteAccess.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation
+{
+    public class RemoteAccess:IObserver
+    {
+        public HomeEnvironment homeenv;
+        public RemoteAccess(HomeEnvironment env)
+        {
+            this.homeenv = env;
+        }
+        public void Notify(HomeEnvironment env)
+        {
+            Console.WriteLine($"Motion Detected : {env.isMotionDetected}, {env.Temperature}, in {env.Location}");
+        }
+
+        public void ControlSystem(bool motionDetected)
+        {
+            homeenv.UpdateHomeEnvironment(motionDetected, homeenv.Temperature, homeenv.Location);
+        }
+    }
+}
