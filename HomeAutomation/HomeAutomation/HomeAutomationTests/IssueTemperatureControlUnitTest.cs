857d8b27f5aae8f2e2d25b926d254d8c72fe0a86
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-12 17:56:55 +0600

    Unit tests #17 : temp control unit test + string writer isolation

---
+++ HomeAutomation/HomeAutomation/HomeAutomationTests/IssueTemperatureControlUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using HomeAutomation;
+using System.IO;
+
+namespace HomeAutomationTests
+{
+    public class IssueTemperatureControlUnitTest
+    {
+        private readonly StringWriter stringWriter;
+        public IssueTemperatureControlUnitTest()
+        {
+            stringWriter = new StringWriter();
+        }
+        [Fact]
+        public void ShouldActivateHeatingWhenTemperatureIsLow()
+        {
+            // Arrange
+            var tempControl = new TemperatureControl(22.0);
+            var homeEnvironment = new HomeEnvironment();
+            homeEnvironment.Subscribe(tempControl);
+            homeEnvironment.UpdateHomeEnvironment(false, 20.0, "Living Room");
+
+            Console.SetOut(stringWriter); 
+
+            // Act
+            homeEnvironment.UpdateHomeEnvironment(false, 20.0, "Living Room");
+
+            // Assert
+            Assert.Contains("Activating heating...", stringWriter.ToString());
+        }
+
+        
+    }
+}
