c0f64930fd03b3235c24eda1f9061c93280de3c1
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2025-03-13 01:20:25 +0600

    #10 Command model created

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Models/Command.cs
@@ -0,0 +1,26 @@
+﻿namespace HomeAutomation.Models;
+
+public class Command
+{
+    private string command;
+    private float value;
+
+    public Command(string command, float value)
+    {
+        this.command = command;
+        this.value = value;
+    }
+
+    public Command(string command)
+    {
+        this.command = command;
+        this.value = 0;
+    }
+
+    public Command(float value)
+    {
+        this.command = "";
+        this.value = value;
+    }
+    
+}
