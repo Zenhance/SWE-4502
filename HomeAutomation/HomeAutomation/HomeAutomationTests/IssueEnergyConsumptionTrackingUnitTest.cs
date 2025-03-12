c211117aa84459edc524463edc2f43c40cb20637
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-12 18:28:52 +0600

    Unit tests #17 : Energy consumption test

---
+++ HomeAutomation/HomeAutomation/HomeAutomationTests/IssueEnergyConsumptionTrackingUnitTest.cs
@@ -0,0 +1,26 @@
+﻿using HomeAutomation;
+
+namespace HomeAutomationTests
+{
+    public class IssueEnergyConsumptionTrackingUnitTest
+    {
+        private readonly StringWriter stringWriter;
+        public IssueEnergyConsumptionTrackingUnitTest()
+        {
+            stringWriter = new StringWriter();
+        }
+        [Fact]
+        public void ShouldTrackEnergyConsumptionWhenMotionIsDetected()
+        {
+            var energyTracker = new EnergyTracker();
+            var homeEnvironment = new HomeEnvironment();
+            homeEnvironment.Subscribe(energyTracker);
+
+            Console.SetOut(stringWriter);
+            homeEnvironment.UpdateHomeEnvironment(true, 12.12, "Backyard");
+
+
+            Assert.Contains("Energy Consumption Updated", stringWriter.ToString());
+        }
+    }
+}
