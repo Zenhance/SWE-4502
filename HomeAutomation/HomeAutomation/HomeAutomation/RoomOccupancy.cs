b8bd5ee95eaf1f1aa31750a606678485bd67f463
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-12 16:55:03 +0600

    Light Management #12 : Breking light management into room occupancy management

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/RoomOccupancy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation
+{
+    public class RoomOccupancy:Light
+    {
+        public override void Manage(LightState state)
+        {
+            if (state.IsRoomTaken)
+            {
+                Console.WriteLine("Turning lights on.");
+            }
+            else
+            {
+                Console.WriteLine("Room is empty, lights off.");
+            }
+
+            light?.Manage(state);
+        }
+    }
+}
