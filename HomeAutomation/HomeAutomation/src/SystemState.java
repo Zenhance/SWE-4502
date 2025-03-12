8b2ec176eae6dabed921caa82b661c591e70caeb
Shefayat E- Shams Adib <shefayatadib@iut-dhaka.edu>
2025-03-12 13:23:07 +0600

    #10: Added System State to hold System Data

---
+++ HomeAutomation/HomeAutomation/src/SystemState.java
@@ -0,0 +1,7 @@
+public class SystemState {
+    public boolean motionDetected;
+    public int lightLevel;
+    public int temperature;
+    public int energyUsage;
+    public String voiceCommand;
+}
