10431a6e7e1fce730a15a1a0af94fb6715d883c3
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2025-03-13 01:48:01 +0600

    #11 MotionDetector.cs created

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Components/MotionDetector.cs
@@ -0,0 +1,16 @@
+﻿using HomeAutomation.Interface;
+using HomeAutomation.Models;
+
+namespace HomeAutomation.Components;
+
+public class MotionDetector: ISmartHomeComponent
+{
+    private List<MotionData> motionDataList;
+    private float threshold;
+    
+    public MotionDetector(float threshold)
+    {
+        motionDataList = new List<MotionData>();
+        this.threshold = threshold;
+    }
+}
