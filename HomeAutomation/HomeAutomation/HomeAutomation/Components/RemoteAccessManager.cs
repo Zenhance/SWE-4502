73469bfdf7c71b342707b5bf9f926292e699e04e
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2025-03-13 03:09:28 +0600

    #16 RemoteAccessManager created with constructor

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Components/RemoteAccessManager.cs
@@ -0,0 +1,21 @@
+﻿using HomeAutomation.Core;
+using HomeAutomation.ENUM;
+using HomeAutomation.Interface;
+using HomeAutomation.Models;
+
+namespace HomeAutomation.Components;
+
+public class RemoteAccessManager:ISmartHomeComponent
+{
+    private EnvironmentState environmentState;
+    
+    public RemoteAccessManager(EnvironmentState environmentState)
+    {
+        this.environmentState = environmentState;
+    }
+    
+    public void update(SENSOR sensor, Command command)
+    {
+        
+    }
+}
