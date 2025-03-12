cfd1f1352ba485c306f2e50d7dd727d61c573dc6
labibahhoque <hoquelabibah@gmail.com>
2025-03-13 01:48:23 +0600

    Created HomeAutomation class

--- HomeAutomation/HomeAutomation/src/EnvironmentState.java
+++ HomeAutomation/HomeAutomation/src/EnvironmentState.java
@@ -1,2 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class EnvironmentState {
+    private final Map<String,Object>state=new HashMap<>();
+
+    public void updateState(String key,Object value){
+        state.put(key,value);
+    }
+
+    public Object getState(String key){
+        return state.get(key);
+    }
 }
---
+++ HomeAutomation/HomeAutomation/src/HomeAutomatiom.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeAutomatiom implements Subject{
+    private List<Observer>observers=new ArrayList<>();
+    private final EnvironmentState state=new EnvironmentState();
+
+    public void addObserver(Observer observer){
+        observers.add(observer);
+    }
+
+    public void removeObserver(Observer observer){
+        observers.remove(observer);
+    }
+
+    public void updateState(String key,Object value){
+        state.updateState(key,value);
+        notifyObservers();
+    }
+
+    public void notifyObservers(){
+        for (Observer observer:observers){
+            observer.update(state);
+        }
+    }
+}
