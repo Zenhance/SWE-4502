56eab9e45066cf875422b7b154dac95f1e5fe4ac
rakins <shahriar.rakin99@gmail.com>
2025-03-13 03:36:01 +0600

    #16 RemoteAccessManager created constructor update function done

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Components/RemoteAccessManager.cs
@@ -0,0 +1,21 @@
+﻿using HomeAutomation.Core;
+using HomeAutomation.Enum;
+using HomeAutomation.Interface;
+using HomeAutomation.Model;
+
+namespace HomeAutomation.Components;
+
+public class RemoteAccessManager : ISmartHomeComponent
+{
+    private EnvironmentState _environmentState;
+    
+    public RemoteAccessManager(EnvironmentState environmentState)
+    {
+        _environmentState = environmentState;
+    }
+    
+    public void update(SENSOR sensor, Command command)
+    {
+        
+    }
+}
