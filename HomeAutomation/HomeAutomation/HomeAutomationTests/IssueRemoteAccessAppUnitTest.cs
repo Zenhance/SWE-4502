923e00c702235f08b4fa1a800bfebcd4b3891dda
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-12 18:48:20 +0600

    Unit tests #17 : Remote Access App Unit test

---
+++ HomeAutomation/HomeAutomation/HomeAutomationTests/IssueRemoteAccessAppUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using HomeAutomation;
+
+namespace HomeAutomationTests
+{
+    public class IssueRemoteAccessAppUnitTest
+    {
+        private readonly StringWriter stringWriter;
+        public IssueRemoteAccessAppUnitTest()
+        {
+            stringWriter = new StringWriter();
+        }
+        [Fact]
+        public void ShouldControlSystemSuccessfully()
+        {
+            var homeEnvironment = new HomeEnvironment();
+            var remoteAccessApp = new RemoteAccess(homeEnvironment);
+
+            remoteAccessApp.ControlSystem(true);
+
+            Assert.True(homeEnvironment.isMotionDetected);
+        }
+
+    }
+}
