08011636754105b41a6f0ff41a53379572e5efbc
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2025-03-13 03:42:43 +0600

    #15 voice command manager implemented

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Components/VoiceCommandManager.cs
@@ -0,0 +1,33 @@
+﻿using HomeAutomation.Core;
+using HomeAutomation.ENUM;
+using HomeAutomation.Executor;
+using HomeAutomation.Factory;
+using HomeAutomation.Interface;
+using HomeAutomation.Models;
+
+namespace HomeAutomation.Components;
+
+public class VoiceCommandManager:ISmartHomeComponent
+{
+    private EnvironmentState environmentState;
+
+    public VoiceCommandManager(EnvironmentState environmentState)
+    {
+        this.environmentState = environmentState;
+    }
+    
+    public void update(SENSOR sensor, Command command)
+    {
+        if (sensor == SENSOR.VOICE)
+        {
+            StateChangeExecutor sce = ExecutorFactory.getExecutor(command, environmentState);
+            sce.execute();
+        }
+    }
+
+    public EnvironmentState getEnvironment()
+    {
+        return environmentState;
+    }
+    
+}
