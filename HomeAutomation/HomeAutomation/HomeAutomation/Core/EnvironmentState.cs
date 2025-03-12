33d2168c68019c6d0c1b2631265fe542288a246d
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2025-03-13 01:03:05 +0600

    #10 created EnvironmentState.cs

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Core/EnvironmentState.cs
@@ -0,0 +1,13 @@
+﻿namespace HomeAutomation.Core;
+
+public class EnvironmentState
+{
+    private float motionLevel;
+    private float temperature;
+    private float ambientLightLevel;
+    private float energyConsumption;
+    private string lastVoiceCommand;
+
+    
+
+}
