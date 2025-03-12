6c339d3549abc43ed440c6afec9de4c97e9e842b
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-12 17:00:59 +0600

    Light Management #12 : chain of responsibility or light stuff

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/RoomLightControl.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation
+{
+    public class RoomLightControl
+    {
+        public Light handler;
+        public RoomLightControl()
+        {
+            handler = new RoomOccupancy();
+            var ambientlight = new AmbientLight();
+            handler.Setlight(ambientlight);
+        }
+
+        public void AdjustLight(LightState state)
+        {
+            handler.Manage(state);
+        }
+    }
+}
