f0d1da5456b59d10b63e06b856fb11842960346e
takiafarhin <takiafarhin@iut-dhaka.edu>
2025-03-13 02:08:47 +0600

    #15 update function

--- HomeAutomation/HomeAutomation/HomeAutomation/Components/VoiceCommandSystem.cs
+++ HomeAutomation/HomeAutomation/HomeAutomation/Components/VoiceCommandSystem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HomeAutomation.Core_Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,21 @@ using System.Threading.Tasks;
 
 namespace HomeAutomation.Components
 {
-    class VoiceCommandSystem
+    public class VoiceCommandSystem : IObserver
     {
+        public List<string> CommandHistory { get; private set; } = new List<string>();
+        public string LastCommand { get; private set; } = "";
+
+        public void Update (CentralState state)
+        {
+            if(!string.IsNullOrEmpty(state.VoiceCommand))
+            {
+                LastCommand = state.VoiceCommand;
+                CommandHistory.Add(state.VoiceCommand);
+                Console.WriteLine($"Voice Command Received: {state.VoiceCommand}");
+            }
+        }
+
+
     }
 }
