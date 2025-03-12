a08c2123420c1ff804b59740c5e7598cd2950200
rakins <shahriar.rakin99@gmail.com>
2025-03-13 01:20:56 +0600

    #10 Model folder command class constructor create

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Model/Command.cs
@@ -0,0 +1,25 @@
+﻿namespace HomeAutomation.Model;
+
+public class Command
+{
+    private string _command;
+    private float _value;
+    
+    public Command(string command, float value)
+    {
+        _command = command;
+        _value = value;
+    }
+    
+    public Command(string command)
+    {
+        _command = command;
+        _value =0;
+    }
+    
+    public Command(float value)
+    {
+        _command = "";
+        _value = value;
+    }
+}
