6fd22842bece2a16511b3830f88b207e63f62e35
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2025-03-13 03:35:59 +0600

    #10 Energy Consumption State Change Executor implemented

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Executor/EnergyConsumptionStateChangeExecutor.cs
@@ -0,0 +1,18 @@
+﻿using HomeAutomation.Core;
+using HomeAutomation.Models;
+
+namespace HomeAutomation.Executor;
+
+public class EnergyConsumptionStateChangeExecutor: StateChangeExecutor
+{
+    public EnergyConsumptionStateChangeExecutor(EnvironmentState environmentState, Command command)
+    {
+        this.environmentState = environmentState;
+        this._command = command;
+    }
+    
+    public override void execute()
+    {
+        environmentState.SetEnergyConsumption(_command);
+    }
+}
