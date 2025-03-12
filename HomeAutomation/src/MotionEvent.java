49363a2aff302e6abdfffec90df9b8950d9deb7b
Hasibul Islam Nirjhar <hasibulislam@iut-dhaka.edu>
2025-03-13 01:46:35 +0600

    Issue #2 Concrete Motion Event class created

---
+++ HomeAutomation/src/MotionEvent.java
@@ -0,0 +1,24 @@
+
+public class MotionEvent {
+    private final boolean detected;
+    private final String location;
+    private final long timestamp;
+
+    public MotionEvent(boolean detected, String location, long timestamp) {
+        this.detected = detected;
+        this.location = location;
+        this.timestamp = timestamp;
+    }
+
+    public boolean isDetected() {
+        return detected;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+}
