a74318a7b55d0338df31da3e11c6b8b33dacb5f1
rakins <shahriar.rakin99@gmail.com>
2025-03-13 01:03:39 +0600

    #10created EnvironmentState class

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Core/EnvironmentState.cs
@@ -0,0 +1,12 @@
+﻿namespace HomeAutomation.Core;
+
+public class EnvironmentState
+{
+    private float _motionLevel;
+    private float _temperature;
+    private float ambientLightLevel;
+    private float _energyConsumption;
+    private float _lastVoiceCommand;
+    
+    
+}
