1405c79a6cdf0c51212797cc191a06dd891055b8
rakins <shahriar.rakin99@gmail.com>
2025-03-13 01:23:29 +0600

    #10 ISmartHomeComponent done

--- HomeAutomation/HomeAutomation/HomeAutomation/Interface/ISmartHomeComponent.cs
+++ HomeAutomation/HomeAutomation/HomeAutomation/Interface/ISmartHomeComponent.cs
@@ -1,6 +1,9 @@
-﻿namespace HomeAutomation.Interface;
+﻿using HomeAutomation.Enum;
+using HomeAutomation.Model;
+
+namespace HomeAutomation.Interface;
 
 public interface ISmartHomeComponent
 {
-    
+    void update(SENSOR sensor, Command command);
 }
