46cac98547047f1dfa2789d6389749d349461d64
Md. Abid Hasan <emonff89@gmail.com>
2025-03-12 21:26:29 +0600

    Made motion detector
    Detects motion and notifies the system.
    A Factory to create motion detectors.

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/MotionDetectorFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation
+{
+
+    public static class MotionDetectorFactory
+    {
+        public static IMotionDetector CreateMotionDetector()
+        {
+            return new MotionDetector();
+        }
+    }
+}
