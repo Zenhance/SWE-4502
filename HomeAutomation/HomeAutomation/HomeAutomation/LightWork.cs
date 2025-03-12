5abf626eceec3cdfdca6f22ea149608cba8196b1
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-12 16:33:57 +0600

    Light Management #12 : Managing the necessities for the lightstuff

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/LightWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation
+{
+    public class LightWork:Light
+    {
+        public override void Manage(LightState state)
+        {
+            if (state.IsRoomTaken)
+            {
+                Console.WriteLine("Adjusting light for occupied room.");
+            }
+            else if (state.AmbientLight < 50)
+            {
+                Console.WriteLine("Ambient light too low : increasing light.");
+            }
+            else
+            {
+                Console.WriteLine("No adjustent needed : perfect.");
+            }
+
+            light?.Manage(state);
+        }
+    }
+}
