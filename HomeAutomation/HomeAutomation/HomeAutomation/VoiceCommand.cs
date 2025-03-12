56f97c6f424496b5b20fc817232f13643fa97863
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-12 18:36:50 +0600

    Voice Command System #15 : Voice command centre

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/VoiceCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation
+{
+    public class VoiceCommand : IObserver
+    {
+        public IVoice ivoice;
+        public void Notify(HomeEnvironment env)
+        {
+            if (env.isMotionDetected)
+            {
+                ivoice = new LightOn();
+            }
+            else
+            {
+                ivoice = new LightOff();
+            }
+
+            ivoice.Execute();
+        }
+    }
+}
