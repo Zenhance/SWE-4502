0571eb6fbdb2abed6e94c9f56a4f7f4771c4c366
AntaraArifa <antara5643@gmail.com>
2025-03-13 05:27:16 +0600

    Update TemperatureObserver class to manage heating, cooling, and hysteresis for temperature control

--- HomeAutomation/src/TemperatureObserver.java
+++ HomeAutomation/src/TemperatureObserver.java
@@ -1,20 +1,38 @@
 public class TemperatureObserver implements Observer {
-    private String lastTemperature;
-    private String lastRoom;
+    public boolean isHeating;
+    public boolean isCooling;
+    public String lastRoom;
 
     @Override
     public void update(String message) {
         String[] parts = message.split(",");
-        this.lastTemperature = parts[0];
+
+        if (parts.length != 2) {
+            System.err.println("Invalid message format");
+            return;
+        }
+
+        int currentTemp = Integer.parseInt(parts[0]);
         this.lastRoom = parts[1];
-        System.out.println("Temperature in " + lastRoom + ": " + lastTemperature);
+        controlTemperature(currentTemp);
     }
 
-    public String getLastTemperature() {
-        return lastTemperature;
-    }
+    private void controlTemperature(int currentTemp) {
+        int targetTemperature = 22;
+        int hysteresis = 2;
 
-    public String getLastRoom() {
-        return lastRoom;
+        if (currentTemp < targetTemperature - hysteresis) {
+            isHeating = true;
+            isCooling = false;
+            System.out.println("Heating activated in " + lastRoom);
+        } else if (currentTemp > targetTemperature + hysteresis) {
+            isHeating = false;
+            isCooling = true;
+            System.out.println("Cooling activated in " + lastRoom);
+        } else {
+            isHeating = false;
+            isCooling = false;
+            System.out.println("Temperature is normal in " + lastRoom);
+        }
     }
 }
