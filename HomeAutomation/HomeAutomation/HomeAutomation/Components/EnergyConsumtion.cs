30061996eee5a68b696d6069eab21e60075e94c6
takiafarhin <takiafarhin@iut-dhaka.edu>
2025-03-13 00:39:10 +0600

    #14  created EnergyConsumption class

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Components/EnergyConsumtion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomeAutomation.Core_Model;
+
+namespace HomeAutomation.Components
+{
+    class EnergyConsumtion
+    {
+    }
+}
