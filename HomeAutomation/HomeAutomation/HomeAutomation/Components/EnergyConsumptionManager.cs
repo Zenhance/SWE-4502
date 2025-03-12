53b6b3eaff911845787029c8a2883b8c10d03047
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2025-03-13 02:51:46 +0600

    #14 EnergyConsumptionManager.cs created with constructor

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Components/EnergyConsumptionManager.cs
@@ -0,0 +1,23 @@
+﻿using HomeAutomation.ENUM;
+using HomeAutomation.Interface;
+using HomeAutomation.Models;
+
+namespace HomeAutomation.Components;
+
+public class EnergyConsumptionManager: ISmartHomeComponent
+{
+    
+    private List<PowerConsumptionData> powerConsumptionData;
+    private float threshold;
+    
+    public EnergyConsumptionManager(float threshold)
+    {
+        powerConsumptionData = new List<PowerConsumptionData>();
+        this.threshold = threshold;
+    }
+    
+    public void update(SENSOR sensor, Command command)
+    {
+        
+    }
+}
