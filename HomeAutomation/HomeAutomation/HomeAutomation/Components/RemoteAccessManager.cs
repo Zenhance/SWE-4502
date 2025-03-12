97ced542d059a85cb91e432e82af8b5bf8bfc7cf
rakins <shahriar.rakin99@gmail.com>
2025-03-13 03:45:31 +0600

    #16 RemoteAccessManager with getter and setter

--- HomeAutomation/HomeAutomation/HomeAutomation/Components/RemoteAccessManager.cs
+++ HomeAutomation/HomeAutomation/HomeAutomation/Components/RemoteAccessManager.cs
@@ -18,4 +18,55 @@ public class RemoteAccessManager : ISmartHomeComponent
     {
         
     }
+    
+    public float getTemperature()
+    {
+        return _environmentState.GetTemperature();
+    }
+    
+    public float getMotionLevel()
+    {
+        return _environmentState.GetMotionLevel();
+    }
+    
+    public float getAmbientLightLevel()
+    {
+        return _environmentState.GetAmbientLightLevel();
+    }
+    
+    public float getEnergyConsumption()
+    {
+        return _environmentState.GetEnergyConsumption();
+    }
+    
+    public void setVoiceCommand(Command command)
+    {
+        _environmentState.SetVoiceCommand(command);
+    }
+    
+    public string getLastVoiceCommand()
+    {
+        return _environmentState.GetLastVoiceCommand();
+    }
+    
+    public void setTemperature(Command command)
+    {
+        _environmentState.SetTemperature(command);
+    }
+    
+    public void setMotionLevel(Command command)
+    {
+        _environmentState.SetMotionLevel(command);
+    }
+    
+    public void setAmbientLightLevel(Command command)
+    {
+        _environmentState.SetAmbientLightLevel(command);
+    }
+    
+    public void setEnergyConsumption(Command command)
+    {
+        _environmentState.SetEnergyConsumption(command);
+    }
+
 }
