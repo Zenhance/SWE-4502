4edc709fd5d48608b63bb34f619de0a708d0839e
whiteflags26 <whiteflags26@gmail.com>
2025-03-13 01:18:35 +0600

    feat(light): implement LightManagement class with scene setting and observer functionality
    Closes #3
    
    - Implemented LightManagement component to adjust lighting based on occupancy
    - Added support for different lighting scenes (reading, movie, etc.)
    - Integrated observer pattern to respond to ambient light changes
    - Developed unit tests to verify light adjustments and scene settings

---
+++ HomeAutomation/MyHomeAutomation.Test/LightManagementTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHomeAutomation.Test
+{
+    public class LightManagementTests
+    {
+        [Fact]
+        public void TestSetScene()
+        {
+            var coreSystem = new CoreSystem();
+            var lightManagement = new LightManagement(coreSystem);
+            lightManagement.SetScene("Reading");
+            Assert.Equal("Reading", coreSystem.GetState("light"));
+        }
+    }
+}
---
+++ HomeAutomation/MyHomeAutomation/LightManagement.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHomeAutomation
+{
+    public class LightManagement : IObserver
+    {
+        private CoreSystem coreSystem;
+
+        public LightManagement(CoreSystem coreSystem)
+        {
+            this.coreSystem = coreSystem;
+            coreSystem.RegisterObserver(this);
+        }
+
+        public void Update(string key, string value)
+        {
+            if (key == "light")
+            {
+                AdjustLighting(value);
+            }
+        }
+
+        public void AdjustLighting(string lightLevel)
+        {
+            Console.WriteLine($"Adjusting lighting to: {lightLevel}");
+        }
+
+        public void SetScene(string scene)
+        {
+            coreSystem.SetState("light", scene);
+        }
+
+        public void CheckOccupancy()
+        {
+            Console.WriteLine("Checking room occupancy...");
+        }
+    }
+}
