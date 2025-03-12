63a3127a0354a6372b1899d1dd0418b468c4fbfb
Md. Abid Hasan <emonff89@gmail.com>
2025-03-12 23:29:24 +0600

    #11 MotionDetectorFactory Static Class Added

---
+++ HomeAutomation/HomeAutomation/MotionDetectorFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation
+{
+    public static class MotionDetectorFactory
+    {
+        public static IMotionDetector CreateMotionDetector()
+        {
+            return new MotionDetector();
+        }
+    }
+}
