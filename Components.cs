8b6383d9e7426ee82c7fc8eefb11c2804c6a7e84
IQRA109 <127471680+IQRA109@users.noreply.github.com>
2025-03-14 00:17:29 +0600

    #issue16 remote access class created

---
+++ Components.cs
@@ -0,0 +1,21 @@
+﻿﻿using System;
+ using System.Collections.Generic;
+ using System.Linq;
+ using System.Text;
+ using System.Threading.Tasks;
+ using HomeAutomation.Core_Model;
+ 
+ namespace HomeAutomation.Components
+ {
+     public class RemoteAccessApp : IObserver
+     {
+         public List<string> Notifications { get; private set; } = new List<string>();
+         public CentralState CurrentState { get; private set; } = new CentralState();
+ 
+         public void Update(CentralState state)
+         {
+             CurrentState = state;
+             Notifications.Add($"State updated: Motion: {state.MotionDetected}, Light: {state.LightLevel}, Temp: {state.Temperature}, Energy: {state.EnergyUsage}, VoiceCmd: {state.VoiceCommand}");
+         }
+     }
+ }
