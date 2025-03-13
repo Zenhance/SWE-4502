bc61ca546e69d82bc12831c00c6b858aa7576f4c
rarefox21 <abrarmubeen@iut-dhaka.edu>
2025-03-13 22:56:11 +0600

    #15 done

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Components/VoiceCommandManager.cs
@@ -0,0 +1,32 @@
+﻿using HomeAutomate.Core;
+using HomeAutomate.Enum;
+using HomeAutomate.Executor;
+using HomeAutomate.Factory;
+using HomeAutomate.Interfaces;
+using HomeAutomate.Models;
+
+namespace HomeAutomate.Components;
+
+public class VoiceCommandManager : ISmartHomeComponent
+{
+    private EnvironmentState _environment;
+    
+    public VoiceCommandManager(EnvironmentState environment)
+    {
+        _environment = environment;
+    }
+    
+    public void update(SENSOR sensor, Command command)
+    {
+        if (sensor == SENSOR.SOUND)
+        {
+            StateChangeExecutor sce = ExecutorFactory.getExecutor(command, _environment);
+            sce.execute();
+        }
+    }
+    
+    public EnvironmentState getEnvironment()
+    {
+        return _environment;
+    }
+}
