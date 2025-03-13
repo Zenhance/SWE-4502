8a651594a3ba9514a6d5c0527af1adb92bb3093e
irfanAbir1231 <irfahakim@iut-dhaka.edu>
2025-03-13 23:05:52 +0600

    added motion detector

---
+++ HomeAutomation/HomeAutomation/src/main/Code/components/MotionDetector.java
@@ -0,0 +1,15 @@
+package components;
+
+import core.EventType;
+import core.HomeSystem;
+import core.HomeSystemObserver;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+public interface MotionDetector extends HomeSystemObserver {
+    void detectMotion(String location, LocalDateTime timestamp);
+    boolean isMotionDetected(String location);
+    List<Map<String, Object>> getMotionHistory();
+    boolean isActivitySuspicious(String location);
+}
