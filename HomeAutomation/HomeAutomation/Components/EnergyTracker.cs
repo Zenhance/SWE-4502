52f5ca57e79dac1d18129a8262f9148bbc9892bb
sheona001 <sirexcc001@gmail.com>
2025-03-13 15:36:55 +0600

    #14: Issue #5: Energy Consumption Tracking

---
+++ HomeAutomation/HomeAutomation/Components/EnergyTracker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomeAutomation.Core;
+
+namespace HomeAutomation.Components
+{
+    public class EnergyTracker
+    {
+        public double CurrentPowerUsage { get; private set; }
+        public double TotalEnergyConsumed { get; private set; }
+
+      
+    }
+}
