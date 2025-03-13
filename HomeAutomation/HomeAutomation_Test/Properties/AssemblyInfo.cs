05a49e279b38356c026df82bd155aab840bd709c
bhuiyan.minhajul@gmail.com <bhuiyan.minhajul@gmail.com>
2025-03-13 21:30:40 +0600

    #17 Adding LightingControllerTests

--- HomeAutomation/HomeAutomation_Test/Properties/AssemblyInfo.cs
+++ HomeAutomation/HomeAutomation_Test/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyTitle("HomeAutomation_Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyProduct("HomeAutomation_Test")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("d42cccd9-8098-4df6-94ce-7e33a28c0dec")]
+[assembly: Guid("6e39a267-1097-4d21-b9aa-7203c0c39b08")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
