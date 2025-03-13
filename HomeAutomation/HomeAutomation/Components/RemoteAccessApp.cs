96c4418bb61d407553544acb20f4b21256128fa8
sheona001 <sirexcc001@gmail.com>
2025-03-13 20:07:32 +0600

    #16 : Issue #7: Remote Access App

---
+++ HomeAutomation/HomeAutomation/Components/RemoteAccessApp.cs
@@ -0,0 +1,30 @@
+﻿using HomeAutomation.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HomeAutomation.Components
+{
+    public class RemoteAccessApp : IComponent
+    {
+        public EnvironmentalState CurrentState { get; private set; }
+        public List<string> Notifications { get; private set; } = new List<string>();
+
+        public void OnStateChanged(EnvironmentalState state)
+        {
+            CurrentState = state;
+            if (state.MotionDetected)
+            {
+                Notifications.Add("Motion detected!");
+            }
+        }
+
+        public string GetDisplay()
+        {
+            return $"Temp: {CurrentState?.Temperature ?? 0}, Light: {CurrentState?.AmbientLight ?? 0}";
+        }
+    }
+}
