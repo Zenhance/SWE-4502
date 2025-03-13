9f21bb973b459df03d7f3c1c20fa07842d7bf845
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2025-03-13 22:49:12 +0600

    #17 Light mode init passed

---
+++ HomeAutomation/HomeAutomation/HomeAutomationTest/LightManagementTest.cs
@@ -0,0 +1,21 @@
+﻿using HomeAutomation.Components;
+using HomeAutomation.ENUM;
+
+namespace HomeAutomationTest;
+
+public class LightManagementTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+    
+    [Test]
+    public void TestLightModeInitialization()
+    {
+        LightManager lightManagement = new LightManager();
+        LIGHTMODE lightmode = lightManagement.GetCurrentMode();
+        
+        Assert.That(lightmode, Is.EqualTo(LIGHTMODE.DAYLIGHT));
+    }
+}
