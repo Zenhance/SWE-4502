8e7fc3ebe6e22b073a4b729edd462a90857e1456
Ahmed Sadman Labib <aslabib44@gmail.com>
2025-03-13 17:46:34 +0600

    #17 Add unit tests for LightManagement class Ref: #12

---
+++ HomeAutomation/src/UnitTest/LightManagementTests.cs
@@ -0,0 +1,21 @@
+﻿using src;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    public class LightManagementTests
+    {
+        [Fact]
+        public void TestSetScene()
+        {
+            var coreComponent = new CoreComponent();
+            var lightManagement = new LightManagement(coreComponent);
+            lightManagement.SetScene("Reading");
+            Assert.Equal("Reading", coreComponent.GetState("light"));
+        }
+    }
+}
