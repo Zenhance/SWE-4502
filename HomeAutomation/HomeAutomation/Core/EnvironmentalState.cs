f25a405f6efff1d4cbecb63ca3b08673384d32e3
sheona001 <sirexcc001@gmail.com>
2025-03-13 14:24:50 +0600

    #10 issue one environment setup

---
+++ HomeAutomation/HomeAutomation/Core/EnvironmentalState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation.Core
+{
+    public class EnvironmentalState
+    {
+        public double Temperature { get; set; }
+        public double AmbientLight { get; set; }
+        public bool MotionDetected { get; set; }
+    }
+}
