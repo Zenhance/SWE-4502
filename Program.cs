fc51bec675b15ceed555dfc3fe7e72e5cbf37085
IQRA109 <127471680+IQRA109@users.noreply.github.com>
2025-03-14 00:00:06 +0600

    #issue10 central state manager updated

--- Program.cs
+++ Program.cs
@@ -19,7 +19,21 @@
      {
      }
 
-     class CentralStateManager
+      public class CentralStateManager
      {
+         private List<IObserver> _observers = new List<IObserver>();
+         private CentralState centralState = new CentralState();
+ 
+ 
+         public void RegisterObserver(IObserver observer)
+         {
+             _observers.Add(observer);
+         }
+ 
+         public void RemoveObserver(IObserver observer)
+         {
+             _observers.Remove(observer);
+         }
      }
  }
+ 
