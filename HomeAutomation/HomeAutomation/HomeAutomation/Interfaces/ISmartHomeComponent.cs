1453733d0b3f97fedee4b807384fe3905fe71f52
rarefox21 <abrarmubeen@iut-dhaka.edu>
2025-03-13 22:05:43 +0600

    #10 interface

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Interfaces/ISmartHomeComponent.cs
@@ -0,0 +1,9 @@
+﻿using HomeAutomate.Enum;
+using HomeAutomate.Models;
+
+namespace HomeAutomate.Interfaces;
+
+public interface ISmartHomeComponent
+{
+    void update(SENSOR sensor, Command command);
+}
