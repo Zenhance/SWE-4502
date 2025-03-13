48d873240436010d83eae79e6faa4e6560cc476e
Nafisa Kibria <nafisa21@iut-dhaka.edu>
2025-03-13 20:34:54 +0600

    #14: Created functions looging and tracking energy uses in EnergyTracker class

--- HomeAutpmation/src/EnergyTracker.java
+++ HomeAutpmation/src/EnergyTracker.java
@@ -1,6 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class EnergyTracker implements IObserver{
+
+    private Map<String, Integer> energyConsumption = new HashMap<>();
+
     @Override
     public void update(SystemState state) {
 
+    }
+
+    private void logEnergyConsumption(int consumption) {
+        System.out.println("Logging energy consumption: " + consumption + " kWh.");
+    }
+
+
+    private void trackUsageStats(int consumption) {
+        energyConsumption.put("total", energyConsumption.getOrDefault("total", 0) + consumption);
+
+        int totalConsumption = energyConsumption.get("total");
+
+        if (totalConsumption > 500) {
+            System.out.println("Unusual consumption pattern detected: " + totalConsumption + " kWh.");
+        } else {
+            System.out.println("Energy consumption is within normal range.");
+        }
     }
 }
