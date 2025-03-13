59d45e542cb95509a1eb1076227a493766a7f580
Md. Abid Hasan <emonff89@gmail.com>
2025-03-13 22:16:58 +0600

    #17 Motiondetection Unit test Updated

---
+++ HomeAutomation/HomeAutomation/HomeAutomation.Test/MotionDetectionTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation.Test
+{
+    public class MotionDetectionTests
+    {
+        [Fact]
+        public void TestDetectMotion()
+        {
+            var coreSystem = new CoreSystem();
+            var motionDetection = new MotionDetection(coreSystem);
+            motionDetection.DetectMotion("Living Room", "12:00 PM");
+            Assert.Equal("Living Room at 12:00 PM", coreSystem.GetState("motion"));
+        }
+    }
+}
