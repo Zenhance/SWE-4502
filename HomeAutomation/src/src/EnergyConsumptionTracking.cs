10c0bb5849facd63945fdda1fe003fd6d390e536
Ahmed Sadman Labib <aslabib44@gmail.com>
2025-03-13 17:14:07 +0600

    #14 Add EnergyConsumptionTracking class for energy usage monitoring and observer integration

---
+++ HomeAutomation/src/src/EnergyConsumptionTracking.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src
+{
+    public class EnergyConsumptionTracking : IObserver
+    {
+        private CoreComponent coreComponent;
+
+        public EnergyConsumptionTracking(CoreComponent coreComponent)
+        {
+            this.coreComponent = coreComponent;
+            coreComponent.RegisterObserver(this);
+        }
+
+        public void Update(string key, string value)
+        {
+            if (key == "energy")
+            {
+                LogEnergyUsage(value);
+            }
+        }
+
+        public void LogEnergyUsage(string usage)
+        {
+            coreComponent.SetState("energyConsumption", usage);
+        }
+
+        public void CalculateStatistics()
+        {
+            Console.WriteLine("Calculating energy usage statistics...");
+        }
+
+        public void IdentifyUnusualPatterns()
+        {
+            Console.WriteLine("Identifying unusual consumption patterns...");
+        }
+    }
+}
