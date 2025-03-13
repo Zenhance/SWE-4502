39146662dc0d9490b66d63cc0c8d0c7cf16b4082
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2025-03-13 23:47:33 +0600

    #17 Voice Command all test passed

--- HomeAutomation/HomeAutomation/HomeAutomationTest/VoiceCommandManagerTest.cs
+++ HomeAutomation/HomeAutomation/HomeAutomationTest/VoiceCommandManagerTest.cs
@@ -1,5 +1,7 @@
 ﻿using HomeAutomation.Components;
 using HomeAutomation.Core;
+using HomeAutomation.ENUM;
+using HomeAutomation.Models;
 
 namespace HomeAutomationTest;
 
@@ -18,4 +20,67 @@ public class VoiceCommandManagerTest
         
         Assert.That(voiceCommandManager.getEnvironment(), Is.EqualTo(environmentState));
     }
+    
+    [Test]
+    public void TestVoiceCommandEnvironmentStateChange()
+    {
+        EnvironmentState env = new EnvironmentState();
+        VoiceCommandManager voiceCommandManager = new VoiceCommandManager(env);
+        
+        env.AddComponent(voiceCommandManager);
+        Command command = new Command("energy value 127 kWh");
+        
+        env.SetVoiceCommand(command);
+        
+        Assert.That(env.GetEnergyConsumption(), Is.EqualTo(127));
+    }
+    
+    [Test]
+    public void TestVoiceCommandImpactOnTemperatureComponent()
+    {
+        EnvironmentState env = new EnvironmentState();
+        VoiceCommandManager voiceCommandManager = new VoiceCommandManager(env);
+        TemperatureManager temperatureManager = new TemperatureManager();
+        
+        env.AddComponent(voiceCommandManager);
+        env.AddComponent(temperatureManager);
+        Command command = new Command("temperature value 40 degrees");
+        
+        env.SetVoiceCommand(command);
+        
+        Assert.That(temperatureManager.GetState(), Is.EqualTo(TEMPSTATE.COOLING));
+    }
+    
+    [Test]
+    public void TestVoiceCommandImpactOnMotionComponent()
+    {
+        EnvironmentState env = new EnvironmentState();
+        VoiceCommandManager voiceCommandManager = new VoiceCommandManager(env);
+        MotionDetector motionDetector = new MotionDetector(0.5f);
+        
+        env.AddComponent(voiceCommandManager);
+        env.AddComponent(motionDetector);
+        Command command = new Command("motion value 0.6 level");
+        
+        env.SetVoiceCommand(command);
+        
+        Assert.That(motionDetector.overall(), Is.EqualTo(MOTIONBEHAVIOR.SUSPICIOUS));
+    }
+    
+    [Test]
+    public void TestVoiceCommandImpactOnLightComponent()
+    {
+        EnvironmentState env = new EnvironmentState();
+        VoiceCommandManager voiceCommandManager = new VoiceCommandManager(env);
+        LightManager lightManager = new LightManager();
+        
+        env.AddComponent(voiceCommandManager);
+        env.AddComponent(lightManager);
+        Command command = new Command("light value 0.6 level");
+        
+        env.SetVoiceCommand(command);
+        
+        Assert.That(lightManager.GetCurrentMode(), Is.EqualTo(LIGHTMODE.PARTY));
+    }
+
 }
