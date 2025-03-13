82501afd3461a5bf428ef508a61370630ef83f3d
sheona001 <sirexcc001@gmail.com>
2025-03-13 14:47:56 +0600

    #13 : Issue #4: Temperature Control

--- HomeAutomation/HomeAutomation/Components/TemperatureController.cs
+++ HomeAutomation/HomeAutomation/Components/TemperatureController.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,5 +9,35 @@ namespace HomeAutomation.Components
 {
     public class TemperatureController : IComponent
     {
+        public double CurrentTemperature { get; private set; }
+        public double TargetTemperature { get; private set; }
+        public bool HeatingOn { get; private set; }
+        public bool CoolingOn { get; private set; }
+        private const double Hysteresis = 0.5;
+
+        public TemperatureController(double targetTemperature)
+        {
+            TargetTemperature = targetTemperature;
+        }
+
+        public void OnStateChanged(EnvironmentalState state)
+        {
+            CurrentTemperature = state.Temperature;
+            if (CurrentTemperature < TargetTemperature - Hysteresis)
+            {
+                HeatingOn = true;
+                CoolingOn = false;
+            }
+            else if (CurrentTemperature > TargetTemperature + Hysteresis)
+            {
+                CoolingOn = true;
+                HeatingOn = false;
+            }
+            else
+            {
+                HeatingOn = false;
+                CoolingOn = false;
+            }
+        }
     }
 }
