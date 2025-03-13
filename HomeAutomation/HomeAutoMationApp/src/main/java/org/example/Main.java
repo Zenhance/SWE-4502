b59b21b11adc8e405aa4cf55747d76e5d417627a
Atik Shahriar Ovi <atikshahriar@iut-dhaka.edu>
2025-03-13 21:10:55 +0600

    Core framework #10 : add main class and .gitignore

---
+++ HomeAutomation/HomeAutoMationApp/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+        StateManager centralState = new StateManager();
+
+
+
+
+    }
+}
