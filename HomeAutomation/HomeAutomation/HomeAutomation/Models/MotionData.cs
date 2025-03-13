7ce660389b772ccccf0eb3972808472289d1fa28
rarefox21 <abrarmubeen@iut-dhaka.edu>
2025-03-13 22:17:32 +0600

    #11 MData

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Models/MotionData.cs
@@ -0,0 +1,32 @@
+﻿using HomeAutomate.Enum;
+
+namespace HomeAutomate.Models;
+
+public class MotionData
+{
+    private float _value;
+    private DateTime _time;
+    private MOTIONBEHAVIOR _motionbehavior;
+    
+    public MotionData(float value, MOTIONBEHAVIOR motionbehavior)
+    {
+        _value = value;
+        _time = DateTime.Now;
+        _motionbehavior = motionbehavior;
+    }
+    
+    public float getValue()
+    {
+        return _value;
+    }
+    
+    public DateTime getTime()
+    {
+        return _time;
+    }
+    
+    public MOTIONBEHAVIOR getMotionBehavior()
+    {
+        return _motionbehavior;
+    }
+}
