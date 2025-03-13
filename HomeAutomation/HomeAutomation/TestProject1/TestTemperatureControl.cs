1d97b15b462d9b4d0decd814486b76f47f415f20
takiafarhin <takiafarhin@iut-dhaka.edu>
2025-03-13 21:31:29 +0600

    #17  Test_TemperatureControl_RoomUnoccupied & Test_TemperatureControl_HeatingMode added

--- HomeAutomation/HomeAutomation/TestProject1/TestTemperatureControl.cs
+++ HomeAutomation/HomeAutomation/TestProject1/TestTemperatureControl.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
+using HomeAutomation.Components;
+using HomeAutomation.Core_Model;
 
 namespace TestProject1
 {
-    class TestTemperatureControl
+    public class TestTemperatureControl
     {
-    }
+        
+            [Fact]
+            public void Test_TemperatureControl_RoomUnoccupied()
+            {
+                TemperatureControl tempControl = new TemperatureControl();
+                CentralState state = new CentralState { Temperature = 20, RoomOccupied = false };
+
+                tempControl.Update(state);
+
+                Assert.Equal(25.0, tempControl.GetDesiredTemperature());
+                Assert.Equal("Idle", tempControl.GetMode());
+            }
+
+            [Fact]
+            public void Test_TemperatureControl_HeatingMode()
+            {
+                TemperatureControl tempControl = new TemperatureControl();
+                CentralState state = new CentralState { Temperature = 16, RoomOccupied = true };
+
+                tempControl.Update(state);
+
+                Assert.Equal(22.0, tempControl.GetDesiredTemperature());
+                Assert.Equal("Heating", tempControl.GetMode());
+            }
+        }
 }
