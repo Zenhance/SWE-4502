a2a899254089fb0ab8b452839c0837e3b0b920b6
mdhralif <hasiburrahman999.alif@gmail.com>
2025-03-13 23:56:54 +0600

    #11 Motion detector

---
+++ HomeAuto/HomeAutomation/src/components/MotionDetector.java
@@ -0,0 +1,48 @@
+package components;
+import core.Sensor;
+
+import java.util.List;
+
+public class MotionDetector implements Sensor
+{
+    private List<String> motionLog; //list of motion logs
+    private int suspiciousActivityThreshold;
+
+    @Override
+    public void onStateChange(String newState) //when the state changes (e.g. motion detected)
+    {
+        if(newState.contains("motion"))
+        {
+            logActivity(newState); //log the activity
+            analyzePattern(); //analyze the pattern of the motion (eg. if the motion is detected more than 5 times, it is considered suspicious)
+        }
+    }
+
+    public void setSuspiciousActivityThreshold(int threshold)
+    {
+        suspiciousActivityThreshold = threshold;
+    }
+
+    public void logActivity(String motionData)
+    {
+        motionLog.add(motionData);
+        //System.out.println("Motion detected! Logged: " + motionData);
+    }
+
+    public void analyzePattern()
+    {
+        if (motionLog.size() > suspiciousActivityThreshold) {
+            System.out.println("Suspicious activity detected! Alerting security.");
+        }
+        
+    }
+
+    //Tracks location and time of detected movements
+    public void trackMovement(String location, String time)
+    {
+        System.out.println("Movement detected at " + location + " at " + time);
+    }
+
+
+
+}
