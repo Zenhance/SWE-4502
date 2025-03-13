942595639dd08b7068d823198a3ed598467cba30
rakins <shahriar.rakin99@gmail.com>
2025-03-13 22:01:10 +0600

    #10 StateChangeExecutor class with constructor done

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Executor/StateChangeExecutor.cs
@@ -0,0 +1,25 @@
+﻿using HomeAutomation.Core;
+using HomeAutomation.Model;
+
+namespace HomeAutomation.Executor;
+
+public abstract class StateChangeExecutor
+{
+    protected EnvironmentState _environment;
+    protected Command _command;
+    
+    public StateChangeExecutor(EnvironmentState environment, Command command)
+    {
+        _environment = environment;
+        _command = command;
+    }
+    
+    public StateChangeExecutor() { }
+    
+    public abstract void execute();
+    
+    public Command getCommand()
+    {
+        return _command;
+    }
+}
