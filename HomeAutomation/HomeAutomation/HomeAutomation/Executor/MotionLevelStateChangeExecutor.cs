5e6ca5823ad5bc983613f525bcee7b84b8a384b0
Tauhidur Rahman Tauhid <tauhid04023@gmail.com>
2025-03-13 23:57:31 +0600

    #10 created MotionLevelStateChangeExecutor

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Executor/MotionLevelStateChangeExecutor.cs
@@ -0,0 +1,19 @@
+﻿using HomeAutomate.Core;
+using HomeAutomate.Models;
+
+namespace HomeAutomate.Executor;
+
+public class MotionLevelStateChangeExecutor : StateChangeExecutor
+{
+    public MotionLevelStateChangeExecutor(EnvironmentState environmentState, Command command)
+    {
+        _environment = environmentState;
+        _command = command;
+    }
+
+    public override void execute()
+    {
+        _environment.SetMotionLevel(_command);
+    }
+    
+}
