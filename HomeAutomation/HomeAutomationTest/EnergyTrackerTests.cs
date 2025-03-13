ad7c89fed79d8de1820104b025d7db4044f1d56a
sheona001 <sirexcc001@gmail.com>
2025-03-13 16:19:36 +0600

    #17: Issue #5: Energy Consumption Tracking function implemented

--- HomeAutomation/HomeAutomationTest/EnergyTrackerTests.cs
+++ HomeAutomation/HomeAutomationTest/EnergyTrackerTests.cs
@@ -13,7 +13,19 @@ namespace HomeAutomationTest
         [Fact]
         public void OnStateChanged_UpdatesPowerUsageAndTotalEnergy()
         {
-          
+            var tracker = new EnergyTracker();
+            var state = new EnvironmentalState { AmbientLight = 60 };
+
+            tracker.OnStateChanged(state);
+            double expectedUsage = 60 * 0.7;
+
+            Assert.Equal(expectedUsage, tracker.CurrentPowerUsage);
+            Assert.Equal(expectedUsage, tracker.TotalEnergyConsumed);
+
+         
+            tracker.OnStateChanged(state);
+            Assert.Equal(expectedUsage, tracker.CurrentPowerUsage);
+            Assert.Equal(expectedUsage * 2, tracker.TotalEnergyConsumed);
         }
     }
 }
