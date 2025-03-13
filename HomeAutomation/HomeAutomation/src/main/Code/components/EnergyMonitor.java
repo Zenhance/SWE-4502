e418a8e410fa1bfa8443d6042140e8acf84f2922
irfanAbir1231 <irfahakim@iut-dhaka.edu>
2025-03-13 23:09:12 +0600

    added EnergyMonitor

---
+++ HomeAutomation/HomeAutomation/src/main/Code/components/EnergyMonitor.java
@@ -0,0 +1,15 @@
+package components;
+
+import core.EventType;
+import core.HomeSystem;
+import core.HomeSystemObserver;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+public interface EnergyMonitor extends HomeSystemObserver {
+    void recordConsumption(String device, double watts, LocalDateTime timestamp);
+    double getCurrentConsumption();
+    Map<String, Double> getDeviceConsumption();
+    boolean isConsumptionUnusual();
+    Map<String, Object> getUsageStatistics();
+}
