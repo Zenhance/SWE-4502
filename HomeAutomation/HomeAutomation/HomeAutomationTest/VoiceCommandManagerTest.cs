7ef70882012acff853d1b5de2f1958c75380db06
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2025-03-13 23:25:06 +0600

    #17 intial test passed at Voice command

---
+++ HomeAutomation/HomeAutomation/HomeAutomationTest/VoiceCommandManagerTest.cs
@@ -0,0 +1,21 @@
+﻿using HomeAutomation.Components;
+using HomeAutomation.Core;
+
+namespace HomeAutomationTest;
+
+public class VoiceCommandManagerTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void TestVoiceCommandInitiate()
+    {
+        EnvironmentState environmentState = new EnvironmentState();
+        VoiceCommandManager voiceCommandManager = new VoiceCommandManager(environmentState);
+        
+        Assert.That(voiceCommandManager.getEnvironment(), Is.EqualTo(environmentState));
+    }
+}
