a65c45be1de443fc127b5f27de5ef25784ba70c7
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2025-03-13 22:36:49 +0600

    #17 Motiontest Set motion suspicious test passed

---
+++ HomeAutomation/HomeAutomation/HomeAutomationTest/MotionTest.cs
@@ -0,0 +1,25 @@
+﻿using HomeAutomation.Components;
+using HomeAutomation.Core;
+using HomeAutomation.ENUM;
+using HomeAutomation.Models;
+
+namespace HomeAutomationTest;
+
+public class MotionTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void TestSetMotionLevelSuspicious()
+    {
+        MotionDetector motionDetector = new MotionDetector(0.5f);
+        EnvironmentState environmentState = new EnvironmentState();
+        environmentState.AddComponent(motionDetector);
+        environmentState.SetMotionLevel(new Command(0.6f));
+        MOTIONBEHAVIOR motionBehavior = motionDetector.overall();
+        Assert.That(motionBehavior, Is.EqualTo(MOTIONBEHAVIOR.SUSPICIOUS));
+    }
+}
