c48be749af242335473e353586626c4aea5b9877
Ahmed Sadman Labib <aslabib44@gmail.com>
2025-03-13 17:55:50 +0600

    #17 Add unit tests for TemperatureControl class. Ref: #13

---
+++ HomeAutomation/src/UnitTest/TemperatureControlTests.cs
@@ -0,0 +1,22 @@
+﻿using src;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    public class TemperatureControlTests
+    {
+        [Fact]
+        public void TestSetTargetTemperature()
+        {
+            var coreComponent = new CoreComponent();
+
+            var temperatureControl = new TemperatureControl(coreComponent);
+            temperatureControl.SetTargetTemperature("72°F");
+            Assert.Equal("72°F", coreComponent.GetState("temperature"));
+        }
+    }
+}
