ac94d14d010dbe403bc81c9055b335a6ea9f0279
Nafisa Kibria <nafisa21@iut-dhaka.edu>
2025-03-13 23:41:19 +0600

    #17: creation and setup of  RemoteAccess Test

---
+++ HomeAutpmation/UnitTest/RemoteAccessTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RemoteAccessTest {
+
+    private RemoteAccess remoteAccessApp;
+    private SystemState systemState;
+
+    @BeforeEach
+    public void setUp() {
+        remoteAccessApp = new RemoteAccess();
+        systemState = new SystemState();
+    }
+
+
+
+
+
+}
