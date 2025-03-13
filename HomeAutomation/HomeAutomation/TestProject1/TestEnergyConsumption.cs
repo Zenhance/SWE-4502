1791953a899a86a6dd81762ae61b779986ecca98
takiafarhin <takiafarhin@iut-dhaka.edu>
2025-03-13 14:27:33 +0600

    #17 Test_EnergyConsumption_InitialUsage, Test_EnergyConsumption_AverageCalculation,Test_EnergyConsumption_UnusualHighUsage test case created.

--- HomeAutomation/HomeAutomation/TestProject1/TestEnergyConsumption.cs
+++ HomeAutomation/HomeAutomation/TestProject1/TestEnergyConsumption.cs
@@ -3,10 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
+using HomeAutomation.Components;
+using HomeAutomation.Core_Model;
+using System.Collections.Generic;
+
 
 namespace TestProject1
 {
-    class TestEnergyConsumption
+    public class TestEnergyConsumption
     {
+        [Fact]
+        public void Test_EnergyConsumption_InitialUsage()
+        {
+            EnergyConsumtion energyMonitor = new EnergyConsumtion();
+            CentralState state = new CentralState { EnergyUsage = 10.0 };
+
+            energyMonitor.Update(state);
+
+            Assert.Single(energyMonitor.GetUsageHistory());
+            Assert.Equal(10.0, energyMonitor.GetAverageUsuage());
+        }
+
+        [Fact]
+        public void Test_EnergyConsumption_AverageCalculation()
+        {
+            EnergyConsumtion energyMonitor = new EnergyConsumtion();
+            energyMonitor.TrackUsagesStatistics(5.0);
+            energyMonitor.TrackUsagesStatistics(15.0);
+            energyMonitor.TrackUsagesStatistics(25.0);
+
+            Assert.Equal(15.0, energyMonitor.GetAverageUsuage());
+        }
+
+        [Fact]
+        public void Test_EnergyConsumption_UnusualHighUsage()
+        {
+            EnergyConsumtion energyMonitor = new EnergyConsumtion();
+            energyMonitor.TrackUsagesStatistics(10.0);
+            energyMonitor.TrackUsagesStatistics(12.0);
+            energyMonitor.TrackUsagesStatistics(50.0);
+
+            List<double> history = energyMonitor.GetUsageHistory();
+            Assert.Contains(50.0, history);
+        }
     }
 }
