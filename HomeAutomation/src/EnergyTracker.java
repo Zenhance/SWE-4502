0b14da421ec87c834addb9bd5fc688adf23a3866
Faiza Maliat <faiza.maliat20@gmail.com>
2025-03-13 14:35:30 +0600

    #14 : Update basic power usage tracking and unusual pattern detection.

--- HomeAutomation/src/EnergyTracker.java
+++ HomeAutomation/src/EnergyTracker.java
@@ -1,12 +1,13 @@
 public class EnergyTracker implements IObserver {
     private HomeState homeState;
     private int totalUsage;
-
+    private boolean isUnusual;
 
     public EnergyTracker(HomeState homeState) {
         this.homeState = homeState;
         this.totalUsage = 0;
-
+        this.isUnusual = false;
+        homeState.registerObserver(this);
     }
 
     @Override
@@ -14,18 +15,29 @@ public class EnergyTracker implements IObserver {
         if (stateName.equals("power-usage") && value instanceof Integer) {
             int usage = (Integer) value;
             totalUsage += usage;
-
+            checkUnusual(usage);
         }
     }
 
-
     public void setPowerUsage(int usage) {
         homeState.setState("power-usage", usage);
+    }
+
+    private void checkUnusual(int usage) {
+        if (usage > 1000) { // set korlam arki ekta nije nijee valuee
+            isUnusual = true;
+        } else {
+            isUnusual = false;
+        }
+        homeState.setState("unusual-usage", isUnusual);
     }
 
     public int getTotalUsage() {
         return totalUsage;
     }
 
-
+    public boolean isUnusual() {
+        Object status = homeState.getState("unusual-usage");
+        return status != null && (Boolean) status;
+    }
 }
