62ebe3e1715b491102d886e7cfb25460709603d5
Nafisa Kibria <nafisa21@iut-dhaka.edu>
2025-03-13 22:39:02 +0600

    #12: creaed functions for setting lighting scene in LightManager

--- HomeAutpmation/src/LightManager.java
+++ HomeAutpmation/src/LightManager.java
@@ -2,7 +2,6 @@ public class LightManager implements IObserver {
 
     private String lastAction;
 
-    @Override
     public void update(SystemState state) {
         String description = state.getStateDescription();
 
@@ -10,9 +9,25 @@ public class LightManager implements IObserver {
             lastAction = "It's dark, turning on the lights...";
         } else if (description.equals("Bright")) {
             lastAction = "It's bright, turning off the lights...";
-        }
-         else if (description.equals("Room Unoccupied")) {
+        } else if (description.equals("Room Occupied")) {
+            setLightingScene("Reading");
+        } else if (description.equals("Room Unoccupied")) {
             turnOffLights();
+        }
+    }
+
+
+    private void setLightingScene(String scene) {
+        switch (scene) {
+            case "Reading":
+                lastAction = "Setting lighting scene to Reading.";
+                break;
+            case "Movie":
+                lastAction = "Setting lighting scene to Movie.";
+                break;
+            default:
+                lastAction = "Setting default lighting scene.";
+                break;
         }
     }
 
