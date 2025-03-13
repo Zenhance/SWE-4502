cdd64f0d4751ce7f34e32d0d21aeacea3e145cf5
Nafisa Kibria <nafisa21@iut-dhaka.edu>
2025-03-13 21:38:50 +0600

    #14: updated energy tracker

--- HomeAutpmation/src/EnergyTracker.java
+++ HomeAutpmation/src/EnergyTracker.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class EnergyTracker implements IObserver{
+public class EnergyTracker implements IObserver {
 
     private Map<String, Integer> energyConsumption = new HashMap<>();
 
@@ -18,12 +18,11 @@ public class EnergyTracker implements IObserver{
         }
     }
 
-    private void logEnergyConsumption(int consumption) {
+    public void logEnergyConsumption(int consumption) {
         System.out.println("Logging energy consumption: " + consumption + " kWh.");
     }
 
-
-    private void trackUsageStats(int consumption) {
+    public void trackUsageStats(int consumption) {
         energyConsumption.put("total", energyConsumption.getOrDefault("total", 0) + consumption);
 
         int totalConsumption = energyConsumption.get("total");
@@ -33,5 +32,16 @@ public class EnergyTracker implements IObserver{
         } else {
             System.out.println("Energy consumption is within normal range.");
         }
+    }
+
+
+    public void resetConsumption() {
+        energyConsumption.put("total", 0);
+
+    }
+
+
+    public int getTotalConsumption() {
+        return energyConsumption.getOrDefault("total", 0);
     }
 }
