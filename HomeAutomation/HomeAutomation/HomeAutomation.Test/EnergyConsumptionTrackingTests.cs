6d57a4747888559387ebecda210c590ca3d3a12a
Md. Abid Hasan <emonff89@gmail.com>
2025-03-13 22:33:25 +0600

    #17  EnergyConsumptionTrackingTests

---
+++ HomeAutomation/HomeAutomation/HomeAutomation.Test/EnergyConsumptionTrackingTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation.Test
+{
+    public class EnergyConsumptionTrackingTests
+    {
+        [Fact]
+        public void TestLogEnergyUsage()
+        {
+            var coreSystem = new CoreSystem();
+            var energyTracking = new EnergyConsumptionTracking(coreSystem);
+            coreSystem.SetState("energy", "100 kWh");
+            energyTracking.Update("energy", "100 kWh");
+        }
+    }
+}
