15e54b7d1a4ca4c4fae12042279bbbe2f22c0c52
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2025-03-13 23:14:26 +0600

    #17 Energy consumption manager init test passed

---
+++ HomeAutomation/HomeAutomation/HomeAutomationTest/EnergyConsumptionManagementTest.cs
@@ -0,0 +1,19 @@
+﻿using HomeAutomation.Components;
+
+namespace HomeAutomationTest;
+
+public class EnergyConsumptionManagementTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+    
+    [Test]
+    public void TestEnergyConsumptionManagerInitialize()
+    {
+        EnergyConsumptionManager energyConsumptionManager = new EnergyConsumptionManager(130);
+        
+        Assert.That(energyConsumptionManager.getThreshold(), Is.EqualTo(130));
+    }
+}
