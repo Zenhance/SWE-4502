fe7a5e632e06f7d79371ea0d71fe7713fffaeb0d
sheona001 <sirexcc001@gmail.com>
2025-03-13 14:21:58 +0600

    Issue #1: Core System Framework setup   #10

---
+++ HomeAutomation/HomeAutomation/Core/StateManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace HomeAutomation.Core
+{
+    public class StateManager
+    {
+        private readonly List<IComponent> _components = new List<IComponent>();
+        public EnvironmentalState CurrentState { get; private set; } = new EnvironmentalState();
+
+        public void RegisterComponent(IComponent component)
+        {
+            if (!_components.Contains(component))
+            {
+                _components.Add(component);
+            }
+        }
+
+
+        public void UpdateState(Action<EnvironmentalState> updateAction)
+        {
+            updateAction(CurrentState);
+            NotifyComponents();
+        }
+
+        private void NotifyComponents()
+        {
+            foreach (var component in _components)
+            {
+                component.OnStateChanged(CurrentState);
+            }
+        }
+    }
+}
