f05623551b82ff0e40a875f5b1af36a549a654b6
IQRA109 <127471680+IQRA109@users.noreply.github.com>
2025-03-13 23:52:03 +0600

    central class and IObServer

---
+++ HomeAutomation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+ using System.Collections.Generic;
+ using System.Linq;
+ using System.Text;
+ using System.Threading.Tasks;
+ 
+ namespace HomeAutomation.Core_Model
+ {
+    class centralstate
+    {
+
+    }
+    interface IObServer
+     {
+     }
+ }
