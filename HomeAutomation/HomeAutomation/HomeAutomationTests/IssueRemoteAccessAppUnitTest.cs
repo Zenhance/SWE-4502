6d4c6e78a4b4bc1547e8f4eb052e17599befbf7d
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-13 20:55:35 +0600

    Unit tests #17 : don't need the stringwriter anymore

--- HomeAutomation/HomeAutomation/HomeAutomationTests/IssueRemoteAccessAppUnitTest.cs
+++ HomeAutomation/HomeAutomation/HomeAutomationTests/IssueRemoteAccessAppUnitTest.cs
@@ -1,19 +1,14 @@
 ﻿using HomeAutomation;
 namespace HomeAutomationTests
 {
+
     public class IssueRemoteAccessAppUnitTest
     {
-        private readonly StringWriter stringWriter;
-        public IssueRemoteAccessAppUnitTest()
-        {
-            stringWriter = new StringWriter();
-        }
-        
         [Fact]
         public void ShouldControlSystemSuccessfully()
         {
             var homeEnvironment = new HomeEnvironment();
-            var remoteAccessApp = new RemoteAccess(homeEnvironment,stringWriter);
+            var remoteAccessApp = new RemoteAccess(homeEnvironment);
 
             // Act
             remoteAccessApp.ControlSystem(true);  
@@ -21,6 +16,6 @@ namespace HomeAutomationTests
             // Assert
             Assert.True(homeEnvironment.CurrentState.isMotionDetected);
         }
-
     }
+
 }
