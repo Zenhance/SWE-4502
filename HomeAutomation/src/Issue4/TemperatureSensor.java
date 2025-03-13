5db85b08b32c72587dfb9e3f7cfb25c108239d8e
Hasibul Islam Nirjhar <hasibulislam@iut-dhaka.edu>
2025-03-14 02:04:32 +0600

    Issue #4 temperature sensor added

---
+++ HomeAutomation/src/Issue4/TemperatureSensor.java
@@ -0,0 +1,18 @@
+package Issue4;
+
+import Issue1.StateManager;
+
+public class TemperatureSensor {
+    private final StateManager stateManager;
+    public static final String TEMP_KEY = "temperature";
+
+    public TemperatureSensor(StateManager stateManager) {
+        this.stateManager = stateManager;
+    }
+
+    // Simulate a temperature reading
+    public void readTemperature(double temperature) {
+        System.out.println("Sensor reading: " + temperature + "°C");
+        stateManager.updateState(TEMP_KEY, temperature);
+    }
+}
