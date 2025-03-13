8cd80db10eee4f0c05e55f837ff62d7c5b38b6c9
bhuiyan.minhajul@gmail.com <bhuiyan.minhajul@gmail.com>
2025-03-13 15:58:13 +0600

    Adding Lab 4

---
+++ Lab4_IUTBeans/Lab4_IUTBeans/Condiments/ICondiment.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_IUTBeans
+{
+    public interface ICondiment
+    {
+        string GetCondimentDescription(); 
+        double GetCondimentCost();        
+    }
+
+}
---
+++ Lab4_IUTBeans/Lab4_IUTBeans/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_IUTBeans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get user's beverage choice
+            Beverage beverage = GetBeverageChoice();
+
+            // Keep adding condiments until the user is satisfied
+            bool moreCondiments = true;
+            while (moreCondiments)
+            {
+                Console.WriteLine("Would you like to add a condiment?");
+                Console.WriteLine("1: Milk ($50), 2: Sugar ($25), 3: Whipped Cream ($75), 0: No more condiments");
+
+                int condimentChoice = int.Parse(Console.ReadLine());
+                switch (condimentChoice)
+                {
+                    case 1:
+                        beverage = new CondimentDecorator(beverage, new Milk());
+                        break;
+                    case 2:
+                        beverage = new CondimentDecorator(beverage, new Sugar());
+                        break;
+                    case 3:
+                        beverage = new CondimentDecorator(beverage, new WhippedCream());
+                        break;
+                    case 0:
+                        moreCondiments = false; 
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+
+            // Display the final order
+            Console.WriteLine("Your order: " + beverage.GetDescription() + " $" + beverage.Cost());
+            Console.ReadLine();
+        }
+
+        static Beverage GetBeverageChoice()
+        {
+            Console.WriteLine("Welcome to IUTBeans Coffee Shop!");
+            Console.WriteLine("Please choose a beverage:");
+            Console.WriteLine("1: Espresso ($200), 2: Latte ($300), 3: Cappuccino ($350)");
+
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    return new Espresso();
+                case 2:
+                    return new Latte();
+                case 3:
+                    return new Cappuccino();
+                default:
+                    Console.WriteLine("Invalid choice. Defaulting to Espresso.");
+                    return new Espresso(); 
+            }
+        }
+    }
+}
