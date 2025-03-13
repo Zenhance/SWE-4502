7d1e793cb1e2a5f6d350d20d2c0dd33c6426f0bb
bhuiyan.minhajul@gmail.com <bhuiyan.minhajul@gmail.com>
2025-03-13 14:52:36 +0600

    Adding Lighting

---
+++ HomeAutomation/HomeAutomation/Lighting/LightingController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation
+{
+    public class LightingController : ILightingController
+    {
+        private int _brightness;
+        private bool _isOn;
+
+        public void TurnOn()
+        {
+            _isOn = true;
+            Console.WriteLine("Lights turned ON.");
+        }
+
+        public void TurnOff()
+        {
+            _isOn = false;
+            Console.WriteLine("Lights turned OFF.");
+        }
+
+        public void AdjustBrightness(int level)
+        {
+            if (!_isOn)
+            {
+                Console.WriteLine("Cannot adjust brightness. Lights are OFF.");
+                return;
+            }
+
+            _brightness = Math.Clamp(level, 0, 100);
+            Console.WriteLine($"Brightness set to {_brightness}%.");
+        }
+    }
+}
