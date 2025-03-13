b02fdb84bba6f9fc08c5ad3cf83fd629932333e6
sheona001 <sirexcc001@gmail.com>
2025-03-13 19:40:42 +0600

    #17 : Issue #6: Voice Command System test

---
+++ HomeAutomation/HomeAutomationTest/VoiceCommandProcessorTests.cs
@@ -0,0 +1,32 @@
+﻿using HomeAutomation.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomationTest
+{
+    public class VoiceCommandProcessorTests
+    {
+        [Fact]
+        public void ProcessCommand_ReturnsUpperCase()
+        {
+            var processor = new VoiceCommandProcessor();
+            string result = processor.ProcessCommand("turn on lights");
+
+            Assert.Equal("TURN ON LIGHTS", result);
+            Assert.Single(processor.CommandHistory);
+        }
+
+        [Fact]
+        public void CommandHistory_AccumulatesCommands()
+        {
+            var processor = new VoiceCommandProcessor();
+            processor.ProcessCommand("first");
+            processor.ProcessCommand("second");
+
+            Assert.Equal(2, processor.CommandHistory.Count);
+        }
+    }
+}
