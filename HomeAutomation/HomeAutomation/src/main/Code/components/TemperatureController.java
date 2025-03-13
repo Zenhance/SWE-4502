ec056d52619c3035e664acd202aa07d546c3c58f
irfanAbir1231 <irfahakim@iut-dhaka.edu>
2025-03-13 23:08:26 +0600

    added TemperatureController

---
+++ HomeAutomation/HomeAutomation/src/main/Code/components/TemperatureController.java
@@ -0,0 +1,14 @@
+package components;
+
+import core.EventType;
+import core.HomeSystem;
+import core.HomeSystemObserver;
+
+public interface TemperatureController extends HomeSystemObserver {
+    void setTargetTemperature(String room, double temperature);
+    double getTargetTemperature(String room);
+    double getCurrentTemperature(String room);
+    void updateTemperature(String room, double temperature);
+    boolean isHeatingActive(String room);
+    boolean isCoolingActive(String room);
+}
