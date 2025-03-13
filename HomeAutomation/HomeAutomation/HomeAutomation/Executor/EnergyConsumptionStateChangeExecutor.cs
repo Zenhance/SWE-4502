c8df2ca10f700a68d4ec993e02f7a2de8f2e6e51
Tauhidur Rahman Tauhid <tauhid04023@gmail.com>
2025-03-14 00:00:58 +0600

    #10  EnergyConsumptionStateChangeExecutor

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Executor/EnergyConsumptionStateChangeExecutor.cs
@@ -0,0 +1,18 @@
+﻿using HomeAutomate.Core;
+using HomeAutomate.Models;
+
+namespace HomeAutomate.Executor;
+
+public class EnergyConsumptionStateChangeExecutor : StateChangeExecutor
+{
+    public EnergyConsumptionStateChangeExecutor(EnvironmentState environmentState, Command command)
+    {
+        _environment = environmentState;
+        _command = command;
+    }
+    
+    public override void execute()
+    {
+        _environment.SetEnergyConsumption(_command);
+    }
+}
