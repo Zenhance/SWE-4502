7396bb55ba0be47f42f37f2797fe477f86fcf702
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-13 15:50:45 +0600

    Core Framework #10 : making a central state management system to notify

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Core/EnvironmentalState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation
+{
+    public class EnvironmentalState
+    {
+        public bool isMotionDetected { get; set; }
+        public double Temperature { get; set; }
+
+        public string Location { get; set; }
+    }
+}
