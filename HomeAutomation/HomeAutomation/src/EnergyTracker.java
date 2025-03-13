08d41aba19041e7af38776bc6df861e18e625294
labibahhoque <hoquelabibah@gmail.com>
2025-03-14 00:39:40 +0600

    #14 Modified EnergyTracker

--- HomeAutomation/HomeAutomation/src/EnergyTracker.java
+++ HomeAutomation/HomeAutomation/src/EnergyTracker.java
@@ -1,9 +1,13 @@
 public class EnergyTracker implements Observer {
-    @Override
+    private boolean highPowerUsageDetected=false;
     public void update(EnvironmentState state) {
         Integer powerUsage = (Integer) state.getState("powerUsage");
         if (powerUsage != null && powerUsage > 1000) {
+            highPowerUsageDetected=true;
             System.out.println("High power consumption detected!");
         }
+    }
+    public boolean isPowerHigh(){
+        return highPowerUsageDetected;
     }
 }
