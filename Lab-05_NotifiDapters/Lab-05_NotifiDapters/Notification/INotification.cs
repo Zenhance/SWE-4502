c859ede5f3eeba162a8af567f4cfb4bde5df68ef
bhuiyan.minhajul@gmail.com <bhuiyan.minhajul@gmail.com>
2025-03-13 16:49:41 +0600

    Adding Lab 6

---
+++ Lab-05_NotifiDapters/Lab-05_NotifiDapters/Notification/INotification.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_05_NotifiDapters
+{
+    // Common interface for all notifications
+    public interface INotification
+    {
+        void Send(string to, string message);
+    }
+
+}
---
+++ Lab-05_NotifiDapters/Lab-05_NotifiDapters/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_05_NotifiDapters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create the notification instances
+            INotification emailNotification = new EmailNotification();
+            INotification smsNotification = new SMSNotification();
+
+            // Use the third-party postal service via the adapter
+            EPostalMailService postalService = new EPostalMailService();
+            INotification postalMailNotification = new EPostalMailAdapter(postalService);
+
+            // Send notifications
+            emailNotification.Send("email@example.com", "Hello via Email!");
+            smsNotification.Send("1234567890", "Hello via SMS!");
+            postalMailNotification.Send("postal@example.com", "Hello via Postal Mail!");
+
+            Console.ReadKey();
+        }
+    }
+}
