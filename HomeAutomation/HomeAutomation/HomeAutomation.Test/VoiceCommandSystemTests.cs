ce1e080175f65a516d299d3b42f85a1d96b37f95
Md. Abid Hasan <emonff89@gmail.com>
2025-03-13 22:31:21 +0600

    #17 VoiceCommandSystemTests

---
+++ HomeAutomation/HomeAutomation/HomeAutomation.Test/VoiceCommandSystemTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation.Test
+{
+    public class VoiceCommandSystemTests
+    {
+        [Fact]
+        public void TestTranslateCommand()
+        {
+            var coreSystem = new CoreSystem();
+            var voiceCommandSystem = new VoiceCommandSystem(coreSystem);
+            voiceCommandSystem.TranslateCommand("Turn on lights");
+            Assert.Equal("Turn on lights", coreSystem.GetState("command"));
+        }
+    }
+}
---
+++ HomeAutomation/HomeAutomation/HomeAutomation/VoiceCommandSystem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation
+{
+    public class VoiceCommandSystem : IObserver
+    {
+        private CoreSystem coreSystem;
+
+        public VoiceCommandSystem(CoreSystem coreSystem)
+        {
+            this.coreSystem = coreSystem;
+            coreSystem.RegisterObserver(this);
+        }
+
+        public void Update(string key, string value)
+        {
+            if (key == "command")
+            {
+                ProcessCommand(value);
+            }
+        }
+
+        public void ProcessCommand(string command)
+        {
+            Console.WriteLine($"Processing command: {command}");
+        }
+
+        public void MaintainCommandHistory()
+        {
+            Console.WriteLine("Maintaining command history...");
+        }
+
+        public void TranslateCommand(string command)
+        {
+            coreSystem.SetState("command", command);
+        }
+    }
+}
