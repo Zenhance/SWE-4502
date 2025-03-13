60ca51202a3155c263a154d83613103b8b138f1d
rarefox21 <abrarmubeen@iut-dhaka.edu>
2025-03-13 22:28:20 +0600

    #13 Temperature Done

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Components/TemperatureManager.cs
@@ -0,0 +1,38 @@
+﻿using HomeAutomate.Enum;
+using HomeAutomate.Factory;
+using HomeAutomate.Interfaces;
+using HomeAutomate.Models;
+
+namespace HomeAutomate.Components;
+
+public class TemperatureManager : ISmartHomeComponent
+{
+    private TEMPSTATE _state;
+    private int _temperature;
+    
+    public TemperatureManager()
+    {
+        _state = TEMPSTATE.IDLE;
+        _temperature = 25;
+    }
+
+    public void update(SENSOR sensor, Command command)
+    {
+        float value = command.getValue();
+        if (sensor == SENSOR.TEMPERATURE)
+        {
+            _state = TemperatureFactory.GetState(value);
+            _temperature = TemperatureFactory.GetTemperature(value);
+        }
+    }
+    
+    public TEMPSTATE GetCurrentState()
+    {
+        return _state;
+    }
+    
+    public int GetCurrentTemperatureGoal()
+    {
+        return _temperature;
+    }
+}
