1e9cd44515aaddec071fdadfc940130fa0d36689
IQRA109 <127471680+IQRA109@users.noreply.github.com>
2025-03-13 23:57:50 +0600

    central state manager & #issue10

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+ using System.Collections.Generic;
+ using System.Linq;
+ using System.Text;
+ using System.Threading.Tasks;
+ 
+ namespace HomeAutomation.Core_Model
+ {
+     public  class CentralState
+     {
+         public bool MotionDetected { get; set; }
+         public int LightLevel { get; set; }
+         public bool RoomOccupied { get; set; }
+         public int Temperature { get; set; }
+         public double EnergyUsage { get; set; }
+         public string VoiceCommand { get; set; }
+     }
+    interface IObServer
+     {
+     }
+
+     class CentralStateManager
+     {
+     }
+ }
