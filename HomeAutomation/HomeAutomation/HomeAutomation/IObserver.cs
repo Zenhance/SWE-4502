f3a7417f7c5c8c5904bd1f4a9bc81dd589721a0c
Mohammad Istiaq Prodhan <istiaqprodhan02@gmail.com>
2025-03-14 03:34:16 +0600

    #3 IObserver added

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/IObserver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation
+{
+    public interface IObserver
+    {
+        void Update(string key, string value);
+    }
+}
