cc20acfa00059d6549987f1365c0968c7fba5023
sheona001 <sirexcc001@gmail.com>
2025-03-13 14:55:59 +0600

    #17 : Issue #4: Temperature Control  unit testing

---
+++ HomeAutomation/HomeAutomationTest/TemperatureControllerTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomeAutomation.Core;
+using HomeAutomation.Components;
+
+namespace HomeAutomationTest
+{
+    public class TemperatureControllerTests
+    {
+    }
+}
