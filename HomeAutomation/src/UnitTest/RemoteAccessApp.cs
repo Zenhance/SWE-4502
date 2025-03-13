d7726a4c8c901622c47098680db979d02b8a84a7
Ahmed Sadman Labib <aslabib44@gmail.com>
2025-03-13 17:59:44 +0600

    #17 Add unit tests for RemoteAccessApp class. Ref: #16

---
+++ HomeAutomation/src/UnitTest/RemoteAccessApp.cs
@@ -0,0 +1,22 @@
+﻿using src;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    public class RemoteAccessAppTests
+    {
+        [Fact]
+        public void TestRemoteControl()
+        {
+            var coreComponent = new CoreComponent();
+
+            var remoteAccessApp = new RemoteAccessApp(coreComponent);
+            remoteAccessApp.RemoteControl("Turn off HVAC");
+            Assert.Equal("Turn off HVAC", coreComponent.GetState("notification"));
+        }
+    }
+}
