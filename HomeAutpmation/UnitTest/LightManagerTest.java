17b0e3d5b688cb8a001089ec616e3513def0d5a0
Nafisa Kibria <nafisa21@iut-dhaka.edu>
2025-03-13 22:42:38 +0600

    #17: test functions for LightManagerTest

--- HomeAutpmation/UnitTest/LightManagerTest.java
+++ HomeAutpmation/UnitTest/LightManagerTest.java
@@ -13,7 +13,35 @@ public class LightManagerTest {
         lightManager = new LightManager();
         systemState = new SystemState();
     }
+    @Test
+    public void testDarkStateTurnsOnLights() {
+        systemState.setStateDescription("Dark");
+        lightManager.update(systemState);
+        assertEquals("It's dark, turning on the lights...", lightManager.getLastAction());
+    }
 
+
+    @Test
+    public void testBrightStateTurnsOffLights() {
+        systemState.setStateDescription("Bright");
+        lightManager.update(systemState);
+        assertEquals("It's bright, turning off the lights...", lightManager.getLastAction());
+    }
+
+
+    @Test
+    public void testRoomOccupiedSetsReadingScene() {
+        systemState.setStateDescription("Room Occupied");
+        lightManager.update(systemState);
+        assertEquals("Setting lighting scene to Reading.", lightManager.getLastAction());
+    }
+
+    @Test
+    public void testRoomUnoccupiedTurnsOffLights() {
+        systemState.setStateDescription("Room Unoccupied");
+        lightManager.update(systemState);
+        assertEquals("Turning off the lights...", lightManager.getLastAction());
+    }
 
 
 }
