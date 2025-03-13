197f33384e6dbb4f0e0fcfd12ab29a08a0e52ccc
labibahhoque <hoquelabibah@gmail.com>
2025-03-13 23:53:20 +0600

    Created MotionDetectionTest

--- HomeAutomation/HomeAutomation/src/MotionDetector.java
+++ HomeAutomation/HomeAutomation/src/MotionDetector.java
@@ -1,8 +1,19 @@
-public class MotionDetector implements Observer{
-    public void update(EnvironmentState state){
-        boolean motionDetected=(Boolean) state.getState("motion");
-        if(motionDetected){
-            System.out.println("Motion detected!");
+public class MotionDetector implements Observer {
+    private boolean motionDetected = false;
+
+    @Override
+    public void update(EnvironmentState state) {
+        Boolean motion = (Boolean) state.getState("motion");
+
+        if (motion != null) {
+            motionDetected = motion;
+            if (motionDetected) {
+                System.out.println("Motion detected!");
+            }
         }
+    }
+
+    public boolean isMotionDetected() {
+        return motionDetected;
     }
 }
