7e61a1c6a0e22d8cd7416a624f9ddf3dbf9b93de
Nafisa Kibria <nafisa21@iut-dhaka.edu>
2025-03-13 23:06:05 +0600

    #13: created functions for activating heating,cooling and getting last action

--- HomeAutpmation/src/TemperatureController.java
+++ HomeAutpmation/src/TemperatureController.java
@@ -1,6 +1,27 @@
-public class TemperatureController implements IObserver{
+public class TemperatureController implements IObserver {
+
+    private static final double MIN_TEMP = 18.0;
+    private static final double MAX_TEMP = 26.0;
+    private double lastStateTemp = 0;
+    private String lastAction;
+
     @Override
     public void update(SystemState state) {
 
+
+    }
+
+
+    private void activateHeating() {
+        lastAction = "Temperature is low. Activating heating...";
+    }
+
+
+    private void activateCooling() {
+        lastAction = "Temperature is high. Activating cooling...";
+    }
+
+    public String getLastAction() {
+        return lastAction;
     }
 }
