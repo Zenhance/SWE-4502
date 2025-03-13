4fc4e9bd74cd4c16b8728b570c5d6114f00a89da
rhydita <rhyd.ita77@gmail.com>
2025-03-13 23:58:48 +0600

    #10 light energy temperature state change executor implemented

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Executor/AmbientLightLevelStateChangeExecutor.cs
@@ -0,0 +1,18 @@
+﻿using HomeAutomate.Core;
+using HomeAutomate.Models;
+
+namespace HomeAutomation.Executor;
+
+public class AmbientLightLevelStateChangeExecutor : StateChangeExecutor
+{
+    public AmbientLightLevelStateChangeExecutor(EnvironmentState environmentState, Command command)
+    {
+        _environment = environmentState;
+        _command = command;
+    }
+    
+    public override void execute()
+    {
+        _environment.SetAmbientLightLevel(_command);
+    }
+}
