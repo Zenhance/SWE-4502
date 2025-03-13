308ad683cb7dc974d0b42e7d01528516b74beb08
Nafisa Kibria <nafisa21@iut-dhaka.edu>
2025-03-13 20:42:01 +0600

    #10: Created Sysytemstate constructor

--- HomeAutpmation/src/SystemState.java
+++ HomeAutpmation/src/SystemState.java
@@ -1,2 +1,10 @@
 public class SystemState {
+    private SystemStateType stateType;
+    private String stateDescription;
+
+    public SystemState(SystemStateType stateType, String stateDescription) {
+        this.stateType = stateType;
+        this.stateDescription = stateDescription;
+    }
+
 }
