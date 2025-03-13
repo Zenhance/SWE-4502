baa05c8295c683c8d91f7dc4fea9ae9bf0657612
Nafisa Kibria <nafisa21@iut-dhaka.edu>
2025-03-13 23:15:38 +0600

    #15: created function for getting last action and history

--- HomeAutpmation/src/VoiceSystem.java
+++ HomeAutpmation/src/VoiceSystem.java
@@ -1,6 +1,21 @@
-public class VoiceSystem implements IObserver{
+import java.util.ArrayList;
+import java.util.List;
+
+public class VoiceSystem implements IObserver {
+    private List<String> commandHistory = new ArrayList<>();
+    private String lastAction;
+
     @Override
     public void update(SystemState state) {
 
+    }
+
+
+    public String getLastAction() {
+        return lastAction;
+    }
+
+    public List<String> getCommandHistory() {
+        return commandHistory;
     }
 }
