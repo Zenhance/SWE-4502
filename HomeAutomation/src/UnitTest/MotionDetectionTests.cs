487d7296bdfb38c41ca36fd6eddd8e300ab4000c
Ahmed Sadman Labib <aslabib44@gmail.com>
2025-03-13 17:44:32 +0600

    #17 Add unit tests for motion detection functionality Ref: #11

---
+++ HomeAutomation/src/UnitTest/MotionDetectionTests.cs
@@ -0,0 +1,21 @@
+﻿using src;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    public class MotionDetectionTests
+    {
+        [Fact]
+        public void TestDetectMotion()
+        {
+            var coreComponent = new CoreComponent();
+            var motionDetection = new MotionDetection(coreComponent);
+            motionDetection.DetectMotion("Living Room", "11:00 PM");
+            Assert.Equal("Living Room at 11:00 PM", coreComponent.GetState("motion"));
+        }
+    }
+}
