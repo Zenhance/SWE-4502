4ace3640a771fdc486816b30513f6971dd7020d7
sheona001 <sirexcc001@gmail.com>
2025-03-13 15:49:56 +0600

    #14: Issue #5: Energy Consumption Tracking function implemented

--- HomeAutomation/HomeAutomation/Components/EnergyTracker.cs
+++ HomeAutomation/HomeAutomation/Components/EnergyTracker.cs
@@ -7,11 +7,16 @@ using HomeAutomation.Core;
 
 namespace HomeAutomation.Components
 {
-    public class EnergyTracker
+    public class EnergyTracker : IComponent 
     {
         public double CurrentPowerUsage { get; private set; }
         public double TotalEnergyConsumed { get; private set; }
+        public void OnStateChanged(EnvironmentalState state)
+        {
+           
+            CurrentPowerUsage = state.AmbientLight * 0.7; //dummy calc
+            TotalEnergyConsumed += CurrentPowerUsage;
+        }
 
-      
     }
 }
