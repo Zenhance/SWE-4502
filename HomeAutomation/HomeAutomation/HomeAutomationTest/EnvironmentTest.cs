f19402963623eb20c145eb15f310fcb3476b8495
rakins <shahriar.rakin99@gmail.com>
2025-03-13 22:54:37 +0600

    #17 EnvironmentTest class setting up

---
+++ HomeAutomation/HomeAutomation/HomeAutomationTest/EnvironmentTest.cs
@@ -0,0 +1,35 @@
+﻿using HomeAutomation.Components;
+using HomeAutomation.Core;
+
+namespace HomeAutomationTest;
+
+public class EnvironmentTest
+{
+    private static EnvironmentState _environment;
+    private static MotionDetector _motionDetector;
+    private static LightManager _lightManager;
+    private static TemperatureManager _temperatureManager;
+    private static EnergyConsumptionManager _energyManager;
+    private static VoiceCommandManager _voiceCommandManager;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _environment = new EnvironmentState();
+        
+        _motionDetector = new MotionDetector(0.5f);
+        _environment.AddComponent(_motionDetector);
+
+        _lightManager = new LightManager();
+        _environment.AddComponent(_lightManager);
+        
+        _temperatureManager = new TemperatureManager();
+        _environment.AddComponent(_temperatureManager);
+        
+        _energyManager = new EnergyConsumptionManager(130);
+        _environment.AddComponent(_energyManager);
+        
+        _voiceCommandManager = new VoiceCommandManager(_environment);
+        _environment.AddComponent(_voiceCommandManager);
+    }
+}
