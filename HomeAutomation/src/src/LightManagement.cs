d56c6417a52336b945e3d35148be752e9ceb0485
Ahmed Sadman Labib <aslabib44@gmail.com>
2025-03-13 16:01:25 +0600

    #12 Add LightManagement class for dynamic lighting control and observer integration

---
+++ HomeAutomation/src/src/LightManagement.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src
+{
+    public class LightManagement : IObserver
+    {
+        private CoreComponent coreComponent;
+
+        public LightManagement(CoreComponent coreComponent)
+        {
+            this.coreComponent = coreComponent;
+            coreComponent.RegisterObserver(this);
+        }
+
+        public void Update(string key, string value)
+        {
+            if (key == "light")
+            {
+                AdjustLighting(value);
+            }
+        }
+
+        public void AdjustLighting(string lightLevel)
+        {
+            Console.WriteLine($"Adjusting lighting to: {lightLevel}");
+        }
+
+        public void SetScene(string scene)
+        {
+            coreComponent.SetState("light", scene);
+        }
+
+        public void CheckOccupancy()
+        {
+            Console.WriteLine("Checking room occupancy...");
+        }
+    }
+}
