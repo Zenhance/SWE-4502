ba29d11f4eb7663e1f8e7ab74cbb89ab959ac0a6
Nafisa Kibria <nafisa21@iut-dhaka.edu>
2025-03-13 23:16:29 +0600

    #15: implemented update in voice system

--- HomeAutpmation/src/VoiceSystem.java
+++ HomeAutpmation/src/VoiceSystem.java
@@ -7,6 +7,17 @@ public class VoiceSystem implements IObserver {
 
     @Override
     public void update(SystemState state) {
+        String command = state.getStateDescription();
+
+        if (command.equals("Turn On Lights")) {
+            lastAction = "Processing command: Turn On Lights";
+            commandHistory.add(command);
+        } else if (command.equals("Turn Off Lights")) {
+            lastAction = "Processing command: Turn Off Lights";
+            commandHistory.add(command);
+        } else {
+            lastAction = "Unknown command.";
+        }
 
     }
 
