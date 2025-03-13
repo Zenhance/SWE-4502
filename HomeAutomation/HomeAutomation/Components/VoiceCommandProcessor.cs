04313d3e763a824b9a467097f851cf0001f3a991
sheona001 <sirexcc001@gmail.com>
2025-03-13 19:21:18 +0600

    #15 : Issue #6: Voice Command System

---
+++ HomeAutomation/HomeAutomation/Components/VoiceCommandProcessor.cs
@@ -0,0 +1,43 @@
+﻿using HomeAutomation.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomeAutomation.Core;
+
+
+namespace HomeAutomation.Components
+{
+    public class VoiceCommandProcessor : IComponent
+    {
+        public List<string> CommandHistory { get; private set; } = new List<string>();
+        private VoiceCommandHandler _handlerChain;
+
+        public VoiceCommandProcessor()
+        {
+          
+            var lightHandler = new LightCommandHandler();
+            var temperatureHandler = new TemperatureCommandHandler();
+            var defaultHandler = new DefaultCommandHandler();
+
+            lightHandler.SetNext(temperatureHandler);
+            temperatureHandler.SetNext(defaultHandler);
+
+            _handlerChain = lightHandler;
+        }
+
+        public string ProcessCommand(string command)
+        {
+            CommandHistory.Add(command);
+            return _handlerChain.HandleCommand(command);
+        }
+
+       
+        public void OnStateChanged(EnvironmentalState state)
+        {
+            
+            
+        }
+    }
+}
