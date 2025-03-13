b6f6e861b343e524c861a6b9bd29bcd1942c2e2b
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2025-03-13 22:58:11 +0600

    #17 Light mode test passed

--- HomeAutomation/HomeAutomation/HomeAutomationTest/LightManagementTest.cs
+++ HomeAutomation/HomeAutomation/HomeAutomationTest/LightManagementTest.cs
@@ -1,5 +1,6 @@
 ﻿using HomeAutomation.Components;
 using HomeAutomation.ENUM;
+using HomeAutomation.Models;
 
 namespace HomeAutomationTest;
 
@@ -25,5 +26,37 @@ public class LightManagementTest
         float lightLevel = lightManagement.GetCurrentLightLevel();
         
         Assert.That(lightLevel, Is.EqualTo(1.0f));
+    }
+    
+    [Test]
+    public void TestLightModeUpdateForParty()
+    {
+        LightManager lightManagement = new LightManager();
+        Command command = new Command(0.6f);
+        lightManagement.update(SENSOR.LIGHT, command);
+        LIGHTMODE lightmode = lightManagement.GetCurrentMode();
+        
+        Assert.That(lightmode, Is.EqualTo(LIGHTMODE.PARTY));
+    }
+    
+    [Test]
+    public void TestLightModeUpdateForNight()
+    {
+        LightManager lightManagement = new LightManager();
+        Command command = new Command(0.05f);
+        lightManagement.update(SENSOR.LIGHT, command);
+        LIGHTMODE lightmode = lightManagement.GetCurrentMode();
+        
+        Assert.That(lightmode, Is.EqualTo(LIGHTMODE.NIGHT));
+    }
+
+    [Test]
+    public void TestLightLevelUpdateForMovie()
+    {
+        LightManager lightManagement = new LightManager();
+        Command command = new Command(0.2f);
+        lightManagement.update(SENSOR.LIGHT, command);
+        LIGHTMODE lightmode = lightManagement.GetCurrentMode();
+        Assert.That(lightmode, Is.EqualTo(LIGHTMODE.MOVIE));
     }
 }
