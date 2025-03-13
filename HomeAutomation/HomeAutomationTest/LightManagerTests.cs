512265766bda4491bdad5f81f30a9b7901339cbe
sheona001 <sirexcc001@gmail.com>
2025-03-13 14:07:15 +0600

    Issue #3: Light Management test

---
+++ HomeAutomation/HomeAutomationTest/LightManagerTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomeAutomation.Core;
+using HomeAutomation.Components;
+
+namespace HomeAutomationTest
+{
+    public class LightManagerTests
+    {
+        [Fact]
+        public void OnStateChanged_TurnsLightsOn_WhenAmbientLightLow()
+        {
+            var lightManager = new LightManager();
+            var state = new EnvironmentalState { AmbientLight = 20 };
+
+            lightManager.OnStateChanged(state);
+
+            Assert.True(lightManager.LightsOn);
+        }
+
+        [Fact]
+        public void OnStateChanged_TurnsLightsOff_WhenAmbientLightHigh()
+        {
+            var lightManager = new LightManager();
+            var state = new EnvironmentalState { AmbientLight = 80 };
+
+            lightManager.OnStateChanged(state);
+
+            Assert.False(lightManager.LightsOn);
+        }
+
+        [Fact]
+        public void SetScene_ChangesCurrentScene()
+        {
+            var lightManager = new LightManager();
+            lightManager.SetScene("Movie");
+
+            Assert.Equal("Movie", lightManager.CurrentScene);
+        }
+    }
+}
