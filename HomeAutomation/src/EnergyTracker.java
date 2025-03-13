f03c1c13cea9db81de42fd0bfa7544ab96fb7a67
Faiza Maliat <faiza.maliat20@gmail.com>
2025-03-13 14:20:25 +0600

    #14 : Create EnergyTracker Class

---
+++ HomeAutomation/src/EnergyTracker.java
@@ -0,0 +1,22 @@
+public class EnergyTracker implements IObserver {
+    private HomeState homeState;
+    private int totalUsage;
+
+
+    public EnergyTracker(HomeState homeState) {
+        this.homeState = homeState;
+        this.totalUsage = 0;
+
+    }
+
+    @Override
+    public void update(String stateName, Object value) {
+        if (stateName.equals("power-usage") && value instanceof Integer) {
+            int usage = (Integer) value;
+            totalUsage += usage;
+
+        }
+    }
+
+
+}
