b36269d88982a6b840ad548fdbcdacc02b1e361e
IQRA109 <127471680+IQRA109@users.noreply.github.com>
2025-03-14 00:36:23 +0600

    #issue 1

--- Program.cs
+++ Program.cs
@@ -1,47 +1,20 @@
 ﻿using System;
- using System.Collections.Generic;
- using System.Linq;
- using System.Text;
- using System.Threading.Tasks;
- 
- namespace HomeAutomation.Core_Model
- {
-     public  class CentralState
-     {
-         public bool MotionDetected { get; set; }
-         public int LightLevel { get; set; }
-         public bool RoomOccupied { get; set; }
-         public int Temperature { get; set; }
-         public double EnergyUsage { get; set; }
-         public string VoiceCommand { get; set; }
-     }
-   public interface IObserver
-     {
-         void Update(CentralState state);
-     }
-
-      public class CentralStateManager
-     {
-         private List<IObserver> _observers = new List<IObserver>();
-         private CentralState centralState = new CentralState();
- 
- 
-         public void RegisterObserver(IObserver observer)
-         {
-             _observers.Add(observer);
-         }
- 
-         public void RemoveObserver(IObserver observer)
-         {
-             _observers.Remove(observer);
-         }
-          public void NotifyObservers()
-         {
-             foreach (var observer in _observers)
-             {
-                 observer.Update(centralState);
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace HomeAutomation
+{     // Core/EnvironmentState.cs
+       public class EnvironmentState
+       {                 
+            public Dictionary<string, object> State { get; set; } = new Dictionary<string, object>();
+             public void PrintState()
+             {             
+                    Console.WriteLine("Environment State:");   
+                    foreach (var key in State.Keys)
+                    {
+                        Console.WriteLine($"{key}: {State[key]}");
+                    }
              }
-         }
-     }
- }
- 
+       }
+}
