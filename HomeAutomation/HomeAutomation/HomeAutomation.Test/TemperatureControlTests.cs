f17f5607bb3a7a4811ec04edc869b60e85d383fe
Md. Abid Hasan <emonff89@gmail.com>
2025-03-13 22:29:51 +0600

    #17 TestSetTargetTemperature Added

---
+++ HomeAutomation/HomeAutomation/HomeAutomation.Test/TemperatureControlTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation.Test
+{
+    public class TemperatureControlTests
+    {
+        [Fact]
+        public void TestSetTargetTemperature()
+        {
+            var coreSystem = new CoreSystem();
+            var temperatureControl = new TemperatureControl(coreSystem);
+            temperatureControl.SetTargetTemperature("100°F");
+            Assert.Equal("100°F", coreSystem.GetState("temperature"));
+        }
+    }
+}
---
+++ HomeAutomation/HomeAutomation/HomeAutomation/TemperatureControl.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation
+{
+    public class TemperatureControl : IObserver
+    {
+        private CoreSystem coreSystem;
+
+        public TemperatureControl(CoreSystem coreSystem)
+        {
+            this.coreSystem = coreSystem;
+            coreSystem.RegisterObserver(this);
+        }
+
+        public void Update(string key, string value)
+        {
+            if (key == "temperature")
+            {
+                AdjustTemperature(value);
+            }
+        }
+
+        public void AdjustTemperature(string temperature)
+        {
+            Console.WriteLine($"Adjusting temperature to: {temperature}");
+        }
+
+        public void SetTargetTemperature(string target)
+        {
+            coreSystem.SetState("temperature", target);
+        }
+
+        public void PreventRapidCycling()
+        {
+            Console.WriteLine("Preventing rapid HVAC cycling...");
+        }
+    }
+}
