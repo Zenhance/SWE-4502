b705c151fc04cebe6fa4b5a91fd542fe86f692fc
Ahmed Sadman Labib <aslabib44@gmail.com>
2025-03-13 17:23:27 +0600

    #15 Add VoiceCommandSystem class for processing and managing voice commands

---
+++ HomeAutomation/src/src/VoiceCommandSystem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src
+{
+    public class VoiceCommandSystem : IObserver
+    {
+        private CoreComponent coreComponent;
+
+        public VoiceCommandSystem(CoreComponent coreComponent)
+        {
+            this.coreComponent = coreComponent;
+            coreComponent.RegisterObserver(this);
+        }
+
+        public void Update(string key, string value)
+        {
+            if (key == "command")
+            {
+                ProcessCommand(value);
+            }
+        }
+
+        public void ProcessCommand(string command)
+        {
+            Console.WriteLine($"Processing command: {command}");
+        }
+
+        public void MaintainCommandHistory()
+        {
+            Console.WriteLine("Maintaining command history...");
+        }
+
+        public void TranslateCommand(string command)
+        {
+            coreComponent.SetState("command", command);
+        }
+    }
+}
