e7a5487af8757544c4a6c3618fbefa299ff5ff8a
sheona001 <sirexcc001@gmail.com>
2025-03-13 19:17:47 +0600

    #15 : Issue #6: Voice Command System

---
+++ HomeAutomation/HomeAutomation/Components/TemperatureCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation.Components
+{
+    public class TemperatureCommandHandler : VoiceCommandHandler
+    {
+        public override string HandleCommand(string command)
+        {
+            if (command.ToLower().Contains("temperature"))
+            {
+                // Process temperature command.
+                return "Temperature command processed.";
+            }
+            return base.HandleCommand(command);
+        }
+    }
+}
