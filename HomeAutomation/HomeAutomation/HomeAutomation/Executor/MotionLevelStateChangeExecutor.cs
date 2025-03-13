0b8de0bcaa54eea56913ed8d541dd80af93adfed
rakins <shahriar.rakin99@gmail.com>
2025-03-13 22:08:28 +0600

    #10 MotionLevelStateChangeExecutor class including constructor and execute function done

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Executor/MotionLevelStateChangeExecutor.cs
@@ -0,0 +1,18 @@
+﻿using HomeAutomation.Core;
+using HomeAutomation.Model;
+
+namespace HomeAutomation.Executor;
+
+public class MotionLevelStateChangeExecutor : StateChangeExecutor
+{
+    public MotionLevelStateChangeExecutor(EnvironmentState environmentState, Command command)
+    {
+        _environment = environmentState;
+        _command = command;
+    }
+
+    public override void execute()
+    {
+        _environment.SetMotionLevel(_command);
+    }
+}
