b6afea0a7de7009e0896dd225f41f293dfbc2467
Ishmaam Iftekhar Khan <ishmaam@iut-dhaka.edu>
2025-03-13 20:44:49 +0600

    #10 notify observers done

--- HomeAutomation/HomeAuto/src/main/java/Core/HomeEnvironment.java
+++ HomeAutomation/HomeAuto/src/main/java/Core/HomeEnvironment.java
@@ -1,11 +1,58 @@
 package Core;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class HomeEnvironment {
-
+    private static HomeEnvironment instance;
     private final Map<String, Object> state= new HashMap<>();
     private final Map<String, List<Observer>> observers= new HashMap<>();
+
+    private HomeEnvironment(){}
+
+    public static HomeEnvironment getInstance(){
+        if(instance== null){
+            instance= new HomeEnvironment();
+        }
+        return instance;
+    }
+
+    public void registerObserver(String eventType, Observer observer) {
+        observers.computeIfAbsent(eventType, k -> new ArrayList<>()).add(observer);
+    }
+
+    public void removeObserver(String eventType, Observer observer){
+        List <Observer> observerList= observers.get(eventType);
+                if(observerList!=null){
+                    observerList.remove(observer);
+                }
+    }
+
+    public void setState(String eventType, Object value){
+        state.put(eventType, value);
+        notifyObservers(eventType, value);
+    }
+
+    public Object getState(String eventType){
+        return state.get(eventType);
+    }
+
+    public void notifyObservers(String eventType, Object value){
+        List <Observer> observerList= observers.get(eventType);
+        if(observerList!=null){
+            for(Observer i: observerList){
+                i.update(eventType, value);
+            }
+        }
+    }
+
+
+
+
+
+
+
+
 }
