b9d6d1885a9f45a52cf79ae3b1c308a81bdbe390
Md. Abid Hasan <emonff89@gmail.com>
2025-03-13 22:15:07 +0600

    #17 CoreSytem Unit test Updated

---
+++ HomeAutomation/HomeAutomation/HomeAutomation.Test/CoreSystemTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation.Test
+{
+    public class CoreSystemTests
+    {
+        [Fact]
+        public void TestSetState()
+        {
+            var coreSystem = new CoreSystem();
+            coreSystem.SetState("testKey", "testValue");
+            Assert.Equal("testValue", coreSystem.GetState("testKey"));
+        }
+    }
+}
