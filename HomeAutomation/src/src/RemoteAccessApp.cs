828f36fcb15e1bd97d5c056ad53debd907aa50e1
Ahmed Sadman Labib <aslabib44@gmail.com>
2025-03-13 17:39:23 +0600

    #16 Add RemoteAccessApp class for remote control and notification handling

---
+++ HomeAutomation/src/src/RemoteAccessApp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src
+{
+    public class RemoteAccessApp : IObserver
+    {
+        private CoreComponent coreComponent;
+
+        public RemoteAccessApp(CoreComponent coreComponent)
+        {
+            this.coreComponent = coreComponent;
+            coreComponent.RegisterObserver(this);
+        }
+
+        public void Update(string key, string value)
+        {
+            if (key == "notification")
+            {
+                GenerateNotification(value);
+            }
+        }
+
+        public void GenerateNotification(string message)
+        {
+            Console.WriteLine($"Notification: {message}");
+        }
+
+        public void DisplayState()
+        {
+            Console.WriteLine("Displaying current state...");
+        }
+
+        public void RemoteControl(string command)
+        {
+            coreComponent.SetState("notification", command);
+        }
+    }
+}
