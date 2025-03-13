a97708121186359ecf1705aa4f78c962a888cfd8
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-13 17:01:48 +0600

    Temperature Control #13 : central state fix + hysteresis threshold

--- HomeAutomation/HomeAutomation/HomeAutomation/TemperatureControl/TemperatureControl.cs
+++ HomeAutomation/HomeAutomation/HomeAutomation/TemperatureControl/TemperatureControl.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,26 +12,33 @@ namespace HomeAutomation
         public double TargetTemp;
         public ITemp heat;
         public ITemp cool;
+        public double LastTemp;
 
         public TemperatureControl(double targetTemperature)
         {
             this.TargetTemp = targetTemperature;
             this.heat = new HeatCommand();
             this.cool = new CoolCommand();
+            this.LastTemp = targetTemperature;
         }
-        public void Notify(HomeEnvironment env)
+        public void Notify(EnvironmentalState state)
         {
-            if (env.Temperature < TargetTemp)
+            if (Math.Abs(state.Temperature - LastTemp) > 12.0) // Hysteresis threshold
             {
-                heat.Do();
-            }
-            else if (env.Temperature > TargetTemp)
-            {
-                cool.Do();
-            }
-            else
-            {
-                Console.WriteLine("Temperature is stable.");
+                if (state.Temperature < TargetTemp)
+                {
+                    heat.Do();
+                }
+                else if (state.Temperature > TargetTemp)
+                {
+                    cool.Do();
+                }
+                else
+                {
+                    Console.WriteLine("Temperature is stable.");
+                }
+
+                LastTemp = state.Temperature;
             }
         }
     }
