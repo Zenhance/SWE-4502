42f6e06c4b0ce57a0da1e908d2e5a5df14c9331a
Ahmed Sadman Labib <aslabib44@gmail.com>
2025-03-13 16:10:57 +0600

    #13 Add TemperatureControl class for temperature management and observer integration.

---
+++ HomeAutomation/src/src/TemperatureControl.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src
+{
+    public class TemperatureControl : IObserver
+    {
+        private CoreComponent coreComponent;
+
+        public TemperatureControl(CoreComponent coreComponent)
+        {
+            this.coreComponent = coreComponent;
+            coreComponent.RegisterObserver(this);
+        }
+
+        public void Update(string key, string value)
+        {
+            if (key == "temperature")
+            {
+                AdjustTemperature(value);
+            }
+        }
+
+        public void AdjustTemperature(string temperature)
+        {
+            Console.WriteLine($"Adjusting temperature to: {temperature}");
+        }
+
+        public void SetTargetTemperature(string target)
+        {
+            coreComponent.SetState("temperature", target);
+        }
+
+        public void PreventRapidCycling()
+        {
+            Console.WriteLine("Preventing rapid HVAC cycling...");
+        }
+    }
+}
