c1d3e56c21b18117a58b63c8f6142f4fb2f8dc2d
Nafisa Kibria <nafisa21@iut-dhaka.edu>
2025-03-13 23:36:05 +0600

    #16: created functions for displaying current state, generate notifications and to find last action in RemoteAccess

--- HomeAutpmation/src/RemoteAccess.java
+++ HomeAutpmation/src/RemoteAccess.java
@@ -1,6 +1,24 @@
-public class RemoteAccess implements IObserver{
+public class RemoteAccess implements IObserver {
+
+    private String lastAction;
+    private String currentState;
+
     @Override
     public void update(SystemState state) {
 
     }
+
+    private void displayCurrentState(String state) {
+        currentState = "Current home state: " + state;
+        lastAction = currentState;
+    }
+    private void generateNotification(String message) {
+        lastAction = "Notification: " + message;
+    }
+
+    public String getLastAction() {
+        return lastAction;
+    }
+
+
 }
