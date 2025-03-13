1edea6d1cd31bd6fce0aef14840b029e58409751
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-13 16:55:29 +0600

    Remote Access App #16 : central state fix + stringwrite object for tests

--- HomeAutomation/HomeAutomation/HomeAutomation/Remoteaccess/RemoteAccess.cs
+++ HomeAutomation/HomeAutomation/HomeAutomation/Remoteaccess/RemoteAccess.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,19 +9,21 @@ namespace HomeAutomation
 {
     public class RemoteAccess:IObserver
     {
-        public HomeEnvironment homeenv;
-        public RemoteAccess(HomeEnvironment env)
+        public HomeEnvironment homeenv; private readonly StringWriter stringWriter;
+        public RemoteAccess(HomeEnvironment env,StringWriter sw)
         {
             this.homeenv = env;
+            this.stringWriter = sw;
         }
-        public void Notify(HomeEnvironment env)
+        public void Notify(EnvironmentalState state)
         {
-            Console.WriteLine($"Motion Detected : {env.isMotionDetected}, {env.Temperature}, in {env.Location}");
+            stringWriter.WriteLine($"Motion detected in {state.Location}");
+            Console.WriteLine($"Motion Detected: {state.isMotionDetected}, Temperature: {state.Temperature}, Location: {state.Location}");
         }
 
         public void ControlSystem(bool motionDetected)
         {
-            homeenv.UpdateHomeEnvironment(motionDetected, homeenv.Temperature, homeenv.Location);
+            homeenv.UpdateHomeEnvironment(motionDetected, homeenv.CurrentState.Temperature, homeenv.CurrentState.Location);
         }
     }
 }
