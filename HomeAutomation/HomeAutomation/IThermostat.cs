0fa5faa6c8c9350ece6aa0b4cb1e8d1087a1f137
bhuiyan.minhajul@gmail.com <bhuiyan.minhajul@gmail.com>
2025-03-13 15:01:08 +0600

    Adding IThermostat

---
+++ HomeAutomation/HomeAutomation/IThermostat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation
+{
+    public interface IThermostat
+    {
+        void SetTargetTemperature(double temperature);
+        void UpdateCurrentTemperature(double temperature);
+        void ControlTemperature();
+    }
+}
