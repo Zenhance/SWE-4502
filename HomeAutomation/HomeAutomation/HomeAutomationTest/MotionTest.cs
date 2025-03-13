983659ede618d61d7ce0015f7339f5789f3194f3
rakins <shahriar.rakin99@gmail.com>
2025-03-13 23:35:10 +0600

    #17 SetMotionLevelMultipleNormalTest -- SetMotionLevelSuspiciousCountTest -- SetMotionLevelNormalCountTest --> passed

--- HomeAutomation/HomeAutomation/HomeAutomationTest/MotionTest.cs
+++ HomeAutomation/HomeAutomation/HomeAutomationTest/MotionTest.cs
@@ -47,5 +47,49 @@ public class MotionTest
         Assert.That(motionBehavior, Is.EqualTo(MOTIONBEHAVIOR.SUSPICIOUSBEHAVIOR));
     }
     
+    [Test]
+    public void SetMotionLevelMultipleNormalTest()
+    {
+        MotionDetector motionDetector = new MotionDetector(0.5f);
+        EnvironmentState environmentState = new EnvironmentState();
+        environmentState.AddComponent(motionDetector);
+        environmentState.SetMotionLevel(new Command(0.4f));
+        environmentState.SetMotionLevel(new Command(0.4f));
+        environmentState.SetMotionLevel(new Command(0.6f));
+        
+        MOTIONBEHAVIOR motionBehavior = motionDetector.overall();
+        Assert.That(motionBehavior, Is.EqualTo(MOTIONBEHAVIOR.NORMALBEHAVIOR));
+    }
+
+    [Test]
+    public void SetMotionLevelSuspiciousCountTest()
+    {
+        MotionDetector motionDetector = new MotionDetector(0.5f);
+        EnvironmentState environmentState = new EnvironmentState();
+        environmentState.AddComponent(motionDetector);
+        environmentState.SetMotionLevel(new Command(0.6f));
+        environmentState.SetMotionLevel(new Command(0.4f));
+        environmentState.SetMotionLevel(new Command(0.6f));
+        
+        int suspiciousCount = motionDetector.suspiciousCount();
+        Assert.That(suspiciousCount, Is.EqualTo(2));
+    }
+    
+    
+    [Test]
+    public void SetMotionLevelNormalCountTest()
+    {
+        MotionDetector motionDetector = new MotionDetector(0.5f);
+        EnvironmentState environmentState = new EnvironmentState();
+        environmentState.AddComponent(motionDetector);
+        environmentState.SetMotionLevel(new Command(0.6f));
+        environmentState.SetMotionLevel(new Command(0.4f));
+        environmentState.SetMotionLevel(new Command(0.4f));
+        environmentState.SetMotionLevel(new Command(0.3f));
+        
+        int normalCount = motionDetector.normalCount();
+        Assert.That(normalCount, Is.EqualTo(3));
+    }
+    
     
 }
