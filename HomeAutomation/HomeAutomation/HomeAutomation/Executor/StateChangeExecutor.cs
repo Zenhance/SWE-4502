fb36929dd6fc5a943bfd16cf5f3334fa40edb3cc
rarefox21 <abrarmubeen@iut-dhaka.edu>
2025-03-13 22:57:41 +0600

    #10 StateChangeExecutor done

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Executor/StateChangeExecutor.cs
@@ -0,0 +1,25 @@
+﻿using HomeAutomate.Core;
+using HomeAutomate.Models;
+
+namespace HomeAutomate.Executor;
+
+public abstract class StateChangeExecutor
+{
+    protected EnvironmentState _environment;
+    protected Command _command;
+    
+    public StateChangeExecutor(EnvironmentState environment, Command command)
+    {
+        _environment = environment;
+        _command = command;
+    }
+    
+    public StateChangeExecutor() { }
+    
+    public abstract void execute();
+    
+    public Command getCommand()
+    {
+        return _command;
+    }
+}
