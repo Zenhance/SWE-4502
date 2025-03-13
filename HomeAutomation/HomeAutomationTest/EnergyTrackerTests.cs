6d4f5dc7517ea89d908630b32dc34d067cc2121e
sheona001 <sirexcc001@gmail.com>
2025-03-13 16:18:47 +0600

    #14: Issue #5: Energy Consumption Tracking tests

---
+++ HomeAutomation/HomeAutomationTest/EnergyTrackerTests.cs
@@ -0,0 +1,31 @@
+﻿using HomeAutomation.Components;
+using HomeAutomation.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomationTest
+{
+    public class EnergyTrackerTests
+    {
+        [Fact]
+        public void OnStateChanged_UpdatesPowerUsageAndTotalEnergy()
+        {
+            var tracker = new EnergyTracker();
+            var state = new EnvironmentalState { AmbientLight = 60 };
+
+            tracker.OnStateChanged(state);
+            double expectedUsage = 60 * 0.7;
+
+            Assert.Equal(expectedUsage, tracker.CurrentPowerUsage);
+            Assert.Equal(expectedUsage, tracker.TotalEnergyConsumed);
+
+            // Simulate a second update.
+            tracker.OnStateChanged(state);
+            Assert.Equal(expectedUsage, tracker.CurrentPowerUsage);
+            Assert.Equal(expectedUsage * 2, tracker.TotalEnergyConsumed);
+        }
+    }
+}
