f1441ebe5feedc65da3efa1ea551c19b49079ed6
rhydita <rhyd.ita77@gmail.com>
2025-03-13 23:36:13 +0600

    #13 Temp manager implemented

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Components/TempManager.cs
@@ -0,0 +1,37 @@
+﻿using HomeAutomate.Enum;
+using HomeAutomate.Factory;
+using HomeAutomate.Models;
+
+namespace HomeAutomate.Components;
+
+public class TempManager
+{
+    private TEMPSTATE _state;
+    private int _temperature;
+    
+    public TempManager()
+    {
+        _state = TEMPSTATE.IDLE;
+        _temperature = 25;
+    }
+    
+    public void update(SENSOR sensor, Command command)
+    {
+        float value = command.getValue();
+        if (sensor == SENSOR.TEMPERATURE)
+        {
+            _state = TempFactory.GetState(value);
+            _temperature = TempFactory.GetTemperature(value);
+        }
+    }
+    
+    public TEMPSTATE GetCurrentState()
+    {
+        return _state;
+    }
+    
+    public int GetCurrentTemperatureGoal()
+    {
+        return _temperature;
+    }
+}
