aee616e099f3db9b0333d502ef52c60d651fe176
Ahmed Sadman Labib <aslabib44@gmail.com>
2025-03-13 17:56:26 +0600

    #17 Add unit tests for EnergyConsumptionTracking class. Ref: 14

---
+++ HomeAutomation/src/UnitTest/EnergyConsumptionTrackingTests.cs
@@ -0,0 +1,21 @@
+﻿using src;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    public class EnergyConsumptionTrackingTests
+    {
+        [Fact]
+        public void TestLogEnergyUsage()
+        {
+            var coreComponent = new CoreComponent();
+            var energyTracking = new EnergyConsumptionTracking(coreComponent);
+            coreComponent.SetState("energy", "100 kWh");
+            energyTracking.Update("energy", "100 kWh");
+        }
+    }
+}
