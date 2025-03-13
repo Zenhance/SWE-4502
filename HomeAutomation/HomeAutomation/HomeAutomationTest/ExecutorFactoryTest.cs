9bd94fdf26a9b121b50817ccdf6998f88b68f401
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2025-03-13 23:19:57 +0600

    #17 Execuor factory temp and light object set passed

---
+++ HomeAutomation/HomeAutomation/HomeAutomationTest/ExecutorFactoryTest.cs
@@ -0,0 +1,39 @@
+﻿using HomeAutomation.Core;
+using HomeAutomation.Executor;
+using HomeAutomation.Factory;
+using HomeAutomation.Models;
+
+namespace HomeAutomationTest;
+
+public class ExecutorFactoryTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+    
+    
+    [Test]
+    public void TestExecutorFactoryTemperatureObjectSet()
+    {
+        EnvironmentState environment = new EnvironmentState();
+
+        Command cmd = new Command("temperature value 37 degree");
+        
+        StateChangeExecutor sce = ExecutorFactory.getExecutor(cmd, environment);
+        
+        Assert.That(sce, Is.InstanceOf<TemperatureStateChangeExecutor>());
+    }
+    
+    [Test]
+    public void TestExecutorFactoryAmbientLightLevelObjectSet()
+    {
+        EnvironmentState environment = new EnvironmentState();
+
+        Command cmd = new Command("ambientlightlevel value 0.6 lux");
+        
+        StateChangeExecutor sce = ExecutorFactory.getExecutor(cmd, environment);
+        
+        Assert.That(sce, Is.InstanceOf<AmbientLightLevelStateChangeExecutor>());
+    }
+}
