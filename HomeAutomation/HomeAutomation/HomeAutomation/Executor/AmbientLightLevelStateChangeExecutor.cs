55b6ca7e5a360fd96e65451bc38c8301d8190f43
rakins <shahriar.rakin99@gmail.com>
2025-03-13 22:14:06 +0600

    #10 AmbientLightLevelStateChangeExecutor class done with constructor and execute class

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Executor/AmbientLightLevelStateChangeExecutor.cs
@@ -0,0 +1,18 @@
+﻿using HomeAutomation.Core;
+using HomeAutomation.Model;
+
+namespace HomeAutomation.Executor;
+
+public class AmbientLightLevelStateChangeExecutor : StateChangeExecutor
+{
+    public AmbientLightLevelStateChangeExecutor(EnvironmentState environmentState, Command command)
+    {
+        _environment = environmentState;
+        _command = command;
+    }
+    
+    public override void execute()
+    {
+        _environment.SetAmbientLightLevel(_command);
+    }
+}
