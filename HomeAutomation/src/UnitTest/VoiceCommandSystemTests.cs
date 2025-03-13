7e39c07a209f885ab92e74220ad7af0b837034bc
Ahmed Sadman Labib <aslabib44@gmail.com>
2025-03-13 17:57:45 +0600

    #17 Add unit tests for VoiceCommandSystem class. Ref: #15

---
+++ HomeAutomation/src/UnitTest/VoiceCommandSystemTests.cs
@@ -0,0 +1,22 @@
+﻿using src;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    public class VoiceCommandSystemTests
+    {
+        [Fact]
+        public void TestTranslateCommand()
+        {
+            var coreComponent = new CoreComponent();
+
+            var voiceCommandSystem = new VoiceCommandSystem(coreComponent);
+            voiceCommandSystem.TranslateCommand("Turn on lights");
+            Assert.Equal("Turn on lights", coreComponent.GetState("command"));
+        }
+    }
+}
