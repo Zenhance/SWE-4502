1bd544cd644c183385c91834a795b0f155d0bb6f
navid-kamal <navidkamal568@gmail.com>
2025-03-13 20:52:16 +0600

    feat: add smart home state

---
+++ HomeAutomation/homeautomation/SmartHomeState.java
@@ -0,0 +1,20 @@
+class SmartHomeState implements Subject {
+    private  List<Observer> observers = new ArrayList<>();
+
+    @Override
+    public void registerObserver(Observer o) {
+        observers.add(o);
+    }
+
+    @Override
+    public void removeObserver(Observer o) {
+        observers.remove(o);
+    }
+
+    @Override
+    public void notifyObservers() {
+        for (Observer o : observers) {
+            o.update();
+        }
+    }
+}
