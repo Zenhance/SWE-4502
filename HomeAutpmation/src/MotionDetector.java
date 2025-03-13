549b8a6767c906f48a09344d8eac853bc803f049
Nafisa Kibria <nafisa21@iut-dhaka.edu>
2025-03-13 22:12:06 +0600

    #11: completed MotionDetector class

--- HomeAutpmation/src/MotionDetector.java
+++ HomeAutpmation/src/MotionDetector.java
@@ -4,11 +4,18 @@ import java.util.Map;
 public class MotionDetector implements IObserver {
     private Map<String, Integer> motionCounts = new HashMap<>();
 
+
+
     @Override
     public void update(SystemState state) {
-
+        String location = state.getStateDescription();
+        logMotion(location);
+        analyzeMotionPatterns(location);
     }
 
+    private void logMotion(String location) {
+        System.out.println("Logging motion: " + location);
+    }
 
     private void analyzeMotionPatterns(String location) {
         motionCounts.put(location, motionCounts.getOrDefault(location, 0) + 1);
@@ -19,6 +26,9 @@ public class MotionDetector implements IObserver {
         } else {
             System.out.println("Normal motion activity detected at " + location);
         }
+    }
+    public int getMotionCount(String location) {
+        return motionCounts.getOrDefault(location, 0);
     }
 
 
