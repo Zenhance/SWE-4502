b0b65691149b2c6a955c1c4508c7717db3570a9d
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2025-03-13 23:16:36 +0600

    #17 energy consumtion all test passed

--- HomeAutomation/HomeAutomation/HomeAutomationTest/EnergyConsumptionManagementTest.cs
+++ HomeAutomation/HomeAutomation/HomeAutomationTest/EnergyConsumptionManagementTest.cs
@@ -1,4 +1,6 @@
 ﻿using HomeAutomation.Components;
+using HomeAutomation.ENUM;
+using HomeAutomation.Models;
 
 namespace HomeAutomationTest;
 
@@ -16,4 +18,47 @@ public class EnergyConsumptionManagementTest
         
         Assert.That(energyConsumptionManager.getThreshold(), Is.EqualTo(130));
     }
+    
+    [Test]
+    public void TestEnergyConsumptionUpdate()
+    {
+        EnergyConsumptionManager energyConsumptionManager = new EnergyConsumptionManager(130);
+        
+        energyConsumptionManager.update(SENSOR.ENERGY, new Command(150));
+        PowerConsumptionData powerConsumptionData = energyConsumptionManager.getLastData();
+        
+        Assert.That(powerConsumptionData.getConsumption(), Is.EqualTo(150));
+    }
+
+    [Test]
+    public void TestEnergyConsumptionPatternUsual()
+    {
+        EnergyConsumptionManager energyConsumptionManager = new EnergyConsumptionManager(130);
+        
+        energyConsumptionManager.update(SENSOR.ENERGY, new Command(128));
+        energyConsumptionManager.update(SENSOR.ENERGY, new Command(129));
+        energyConsumptionManager.update(SENSOR.ENERGY, new Command(127));
+        energyConsumptionManager.update(SENSOR.ENERGY, new Command(120));
+        
+        CONSUMPTIONPATTERN consumptionPattern = energyConsumptionManager.getPattern();
+        
+        Assert.That(consumptionPattern, Is.EqualTo(CONSUMPTIONPATTERN.USUAL));
+    }
+    
+    
+    [Test]
+    public void TestEnergyConsumptionPatternUnusual()
+    {
+        EnergyConsumptionManager energyConsumptionManager = new EnergyConsumptionManager(130);
+        
+        energyConsumptionManager.update(SENSOR.ENERGY, new Command(129));
+        energyConsumptionManager.update(SENSOR.ENERGY, new Command(132));
+        energyConsumptionManager.update(SENSOR.ENERGY, new Command(128));
+        energyConsumptionManager.update(SENSOR.ENERGY, new Command(126));
+        
+        CONSUMPTIONPATTERN consumptionPattern = energyConsumptionManager.getPattern();
+        
+        Assert.That(consumptionPattern, Is.EqualTo(CONSUMPTIONPATTERN.UNUSUAL));
+    }
+    
 }
