a4422f6e57ab96c76c45673e5970cf8c8cb25a41
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2025-03-13 22:41:03 +0600

    #17 Normal state and sus state test passed

--- HomeAutomation/HomeAutomation/HomeAutomationTest/MotionTest.cs
+++ HomeAutomation/HomeAutomation/HomeAutomationTest/MotionTest.cs
@@ -22,4 +22,28 @@ public class MotionTest
         MOTIONBEHAVIOR motionBehavior = motionDetector.overall();
         Assert.That(motionBehavior, Is.EqualTo(MOTIONBEHAVIOR.SUSPICIOUS));
     }
+    
+    [Test]
+    public void TestSetMotionLevelNormal()
+    {
+        MotionDetector motionDetector = new MotionDetector(0.5f);
+        EnvironmentState environmentState = new EnvironmentState();
+        environmentState.AddComponent(motionDetector);
+        environmentState.SetMotionLevel(new Command(0.4f));
+        MOTIONBEHAVIOR motionBehavior = motionDetector.overall();
+        Assert.That(motionBehavior, Is.EqualTo(MOTIONBEHAVIOR.NORMAL));
+    }
+
+    [Test]
+    public void TestSetMotionLevelMultipleSuspicious()
+    {
+        MotionDetector motionDetector = new MotionDetector(0.5f);
+        EnvironmentState environmentState = new EnvironmentState();
+        environmentState.AddComponent(motionDetector);
+        environmentState.SetMotionLevel(new Command(0.6f));
+        environmentState.SetMotionLevel(new Command(0.6f));
+        environmentState.SetMotionLevel(new Command(0.4f));
+        MOTIONBEHAVIOR motionBehavior = motionDetector.overall();
+        Assert.That(motionBehavior, Is.EqualTo(MOTIONBEHAVIOR.SUSPICIOUS));
+    }
 }
