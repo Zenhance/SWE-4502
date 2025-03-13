621ebb0b58d618d501c6b06fdb172f24fdb376f0
sheona001 <sirexcc001@gmail.com>
2025-03-13 14:42:38 +0600

    #13: Issue #4: Temperature Control controller creation

---
+++ HomeAutomation/HomeAutomation/Components/TemperatureController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomeAutomation.Core;
+
+namespace HomeAutomation.Components
+{
+    public class TemperatureController : IComponent
+    {
+    }
+}
