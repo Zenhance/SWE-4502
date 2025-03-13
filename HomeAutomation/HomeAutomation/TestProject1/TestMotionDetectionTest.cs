de1ff5a141468b5f0ac8f31ac6109176dca01396
takiafarhin <takiafarhin@iut-dhaka.edu>
2025-03-13 14:01:46 +0600

    #17 Motiondectector test class created  & test case 1

---
+++ HomeAutomation/HomeAutomation/TestProject1/TestMotionDetectionTest.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using HomeAutomation.Components;
+using HomeAutomation.Core_Model;
+
+namespace TestProject1
+{
+    public class TestMotionDetectionTest
+    {
+        [Fact]
+        public void Test_MotionDetection_NoMotion()
+        {
+            MotionDetection motionDetector = new MotionDetection();
+            CentralState state = new CentralState { MotionDetected = false };
+
+            motionDetector.Update(state);
+            Assert.False(state.MotionDetected, "Motion should not be detected initially.");
+        }
+    }
+}
