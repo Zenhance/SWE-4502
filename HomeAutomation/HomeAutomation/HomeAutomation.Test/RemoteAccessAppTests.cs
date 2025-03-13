538166be20acf1bf62817e2af68f5d4803f22cf9
Md. Abid Hasan <emonff89@gmail.com>
2025-03-13 22:24:20 +0600

    #17 RemoteAccessAppTests

---
+++ HomeAutomation/HomeAutomation/HomeAutomation.Test/RemoteAccessAppTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation.Test
+{
+    public class RemoteAccessAppTests
+    {
+        [Fact]
+        public void TestRemoteControl()
+        {
+            var coreSystem = new CoreSystem();
+            var remoteAccessApp = new RemoteAccessApp(coreSystem);
+            remoteAccessApp.RemoteControl("Turn off HVAC");
+            Assert.Equal("Turn off HVAC", coreSystem.GetState("notification"));
+        }
+    }
+}
---
+++ HomeAutomation/HomeAutomation/HomeAutomation/RemoteAccessApp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation
+{
+    public class RemoteAccessApp : IObserver
+    {
+        private CoreSystem coreSystem;
+
+        public RemoteAccessApp(CoreSystem coreSystem)
+        {
+            this.coreSystem = coreSystem;
+            coreSystem.RegisterObserver(this);
+        }
+
+        public void Update(string key, string value)
+        {
+            if (key == "notification")
+            {
+                GenerateNotification(value);
+            }
+        }
+
+        public void GenerateNotification(string message)
+        {
+            Console.WriteLine($"Notification: {message}");
+        }
+
+        public void DisplayState()
+        {
+            Console.WriteLine("Displaying current state...");
+        }
+
+        public void RemoteControl(string command)
+        {
+            coreSystem.SetState("notification", command);
+        }
+    }
+}
