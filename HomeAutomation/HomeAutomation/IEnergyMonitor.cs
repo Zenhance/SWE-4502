057117cd63ea8eeadaf52b2460dea9d426ab39b5
bhuiyan.minhajul@gmail.com <bhuiyan.minhajul@gmail.com>
2025-03-13 15:11:08 +0600

    Adding IEnergyMonitor

---
+++ HomeAutomation/HomeAutomation/IEnergyMonitor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation
+{
+    public interface IEnergyMonitor
+    {
+        void RecordPowerUsage(double watts);
+        double GetTotalConsumption();
+        void AnalyzeUsagePatterns();
+    }
+}
