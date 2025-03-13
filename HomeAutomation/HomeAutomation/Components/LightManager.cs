fc0c1134caafbd03766cc8b69d86aa3852a38b2c
sheona001 <sirexcc001@gmail.com>
2025-03-13 14:04:46 +0600

    Issue #3: Light Management

---
+++ HomeAutomation/HomeAutomation/Components/LightManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomeAutomation.Core;
+
+namespace HomeAutomation.Components
+{
+    public class LightManager : IComponent
+    {
+        public double CurrentAmbientLight { get; private set; }
+        public bool LightsOn { get; private set; }
+        public string CurrentScene { get; private set; } = "Default";
+
+        public void OnStateChanged(EnvironmentalState state)
+        {
+            CurrentAmbientLight = state.AmbientLight;
+            LightsOn = state.AmbientLight < 30;  
+        }
+
+        public void SetScene(string scene)
+        {
+            CurrentScene = scene;
+        }
+    }
+}
