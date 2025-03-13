5a86a05c96d65766583d10f46d686b26a4460046
rarefox21 <abrarmubeen@iut-dhaka.edu>
2025-03-13 23:01:20 +0600

    #10 TemperatureStateChangeExecutor done

---
+++ HomeAutomation/HomeAutomation/HomeAutomation/Executor/TemperatureStateChangeExecutor.cs
@@ -0,0 +1,18 @@
+﻿using HomeAutomate.Core;
+using HomeAutomate.Models;
+
+namespace HomeAutomate.Executor;
+
+public class TemperatureStateChangeExecutor : StateChangeExecutor
+{
+    public TemperatureStateChangeExecutor(EnvironmentState environmentState, Command command)
+    {
+        _environment = environmentState;
+        _command = command;
+    }
+    
+    public override void execute()
+    {
+        _environment.SetTemperature(_command);
+    }
+}
