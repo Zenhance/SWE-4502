a49b2050136eb037a5c7958d757f183fa61718f1
Nafisa Kibria <nafisa21@iut-dhaka.edu>
2025-03-13 22:34:29 +0600

    #12: creaed functions for turning off lights and finding out the last function,also implemented update in LightManager

--- HomeAutpmation/src/LightManager.java
+++ HomeAutpmation/src/LightManager.java
@@ -1,6 +1,30 @@
-public class LightManager implements IObserver{
+public class LightManager implements IObserver {
+
+    private String lastAction;
+
     @Override
     public void update(SystemState state) {
+        String description = state.getStateDescription();
 
+        if (description.equals("Dark")) {
+            lastAction = "It's dark, turning on the lights...";
+        } else if (description.equals("Bright")) {
+            lastAction = "It's bright, turning off the lights...";
+        }
+         else if (description.equals("Room Unoccupied")) {
+            turnOffLights();
+        }
+    }
+
+
+
+
+    private void turnOffLights() {
+        lastAction = "Turning off the lights...";
+    }
+
+
+    public String getLastAction() {
+        return lastAction;
     }
 }
