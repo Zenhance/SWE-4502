0104cb23329cb11fdc32153267deb979dcbbc092
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-13 20:41:41 +0600

    Temperature Control #13 : Allowing hysteresis threshold to be set

--- HomeAutomation/HomeAutomation/HomeAutomation/TemperatureControl/TemperatureControl.cs
+++ HomeAutomation/HomeAutomation/HomeAutomation/TemperatureControl/TemperatureControl.cs
@@ -7,39 +7,43 @@ using System.Threading.Tasks;
 
 namespace HomeAutomation
 {
-    public class TemperatureControl:IObserver
+    public class TemperatureControl : IObserver
     {
         public double TargetTemp;
         public ITemp heat;
         public ITemp cool;
         public double LastTemp;
+        public double HysteresisThreshold;  
 
-        public TemperatureControl(double targetTemperature)
+        public TemperatureControl(double targetTemperature, double hysteresisThreshold = 12.0)
         {
             this.TargetTemp = targetTemperature;
             this.heat = new HeatCommand();
             this.cool = new CoolCommand();
             this.LastTemp = targetTemperature;
+            this.HysteresisThreshold = hysteresisThreshold;
         }
+
         public void Notify(EnvironmentalState state)
         {
-            if (Math.Abs(state.Temperature - LastTemp) > 12.0) // Hysteresis threshold
+            if (Math.Abs(state.Temperature - LastTemp) > HysteresisThreshold)  
             {
                 if (state.Temperature < TargetTemp)
                 {
-                    heat.Do();
+                    heat.Do();  
                 }
                 else if (state.Temperature > TargetTemp)
                 {
-                    cool.Do();
+                    cool.Do();  
                 }
                 else
                 {
                     Console.WriteLine("Temperature is stable.");
                 }
 
-                LastTemp = state.Temperature;
+                LastTemp = state.Temperature;  
             }
         }
     }
+
 }
