76839f63145353c8e85194ce530abac9e5db1da0
Hasibul Islam Nirjhar <hasibulislam@iut-dhaka.edu>
2025-03-14 11:14:03 +0600

    Issue #2 Text Editor with Functionalities

--- TextEditorProject/src/Program.java
+++ TextEditorProject/src/Program.java
@@ -1,2 +1,12 @@
 public class Program {
+    public static void main(String[] args) {
+        TextEditor editor = new TextEditor();
+        editor.setContent("Hello, World!");
+        editor.setCursorPosition(7);
+        editor.addSelection("World");
+        editor.displayState();
+        
+
+
+    }
 }
--- TextEditorProject/src/TextEditor.java
+++ TextEditorProject/src/TextEditor.java
@@ -1,2 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TextEditor {
+    private String content;
+    private int cursorPosition;
+    private List<String> selections;
+
+    public TextEditor() {
+        this.content = "";
+        this.cursorPosition = 0;
+        this.selections = new ArrayList<>();
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setCursorPosition(int position) {
+        if (position < 0) {
+            this.cursorPosition = 0;
+        } else if (position > content.length()) {
+            this.cursorPosition = content.length();
+        } else {
+            this.cursorPosition = position;
+        }
+    }
+
+    public void addSelection(String selection) {
+        selections.add(selection);
+    }
+
+    public void clearSelections() {
+        selections.clear();
+    }
+
+    public void displayState() {
+        System.out.println("Content: " + content);
+        System.out.println("Cursor Position: " + cursorPosition);
+        System.out.println("Selections: " + selections);
+    }
+
 }
