55a4354602fe10dcdcf00f53bcd0cda9f8f2f237
Md. Abid Hasan <emonff89@gmail.com>
2025-03-14 08:04:26 +0600

    CreateD EditorMemento class and basic state management

--- Lab12_textEditor/Lab12_textEditor/EditorMemento.cs
+++ Lab12_textEditor/Lab12_textEditor/EditorMemento.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Lab12_textEditor
 {
-    internal class EditorMemento
+    public class EditorMemento : ISerializable
     {
+        public string _text;
+        public int _cursorPosition;
+        public string _selection;
+        public EditorMemento(string text, int cursorPosition, string selection)
+        {
+            _text = text;
+            _cursorPosition = cursorPosition;
+            _selection = selection;
+        }
+
+        public string GetText()
+        {
+            return _text;
+        }
+        public int GetCursorPosition()
+        {
+            return _cursorPosition;
+        }
+
+        public string GetSelection()
+        {
+            return _selection;
+        }
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+
+            info.AddValue("Text", _text);
+            info.AddValue("CursorPosition", _cursorPosition);
+            info.AddValue("Selection", _selection);
+        }
     }
 }
