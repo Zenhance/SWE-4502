cae5f89f80f609ea5573438e68e3a74ef48cf62f
Nafisa Nawal <nafisanawal@iut-dhaka.edu>
2025-03-14 11:19:02 +0600

    (Issue 2) Added the private variables and constructor of TextEditor class

--- TextEditor/src/textEditor/TextEditor.java
+++ TextEditor/src/textEditor/TextEditor.java
@@ -1,4 +1,14 @@
 package textEditor;
 
 public class TextEditor {
+    private String content;
+    private int cursorPosition;
+    private String selection;
+
+    public TextEditor(String _content, int _cursorPosition, String _selection) {
+        this.content = _content;
+        this.cursorPosition = _cursorPosition;
+        this.selection = _selection;
+    }
+
 }
