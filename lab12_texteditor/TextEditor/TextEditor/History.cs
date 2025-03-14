b0caa3ad2e271c21f2e747acbeb886dba0007122
rakins <shahriar.rakin99@gmail.com>
2025-03-14 12:23:27 +0600

    Issue 4: Implement History Management

--- lab12_texteditor/TextEditor/TextEditor/History.cs
+++ lab12_texteditor/TextEditor/TextEditor/History.cs
@@ -2,5 +2,30 @@
 
 public class History
 {
-    
+    private List<EditorMemento> _mementos;
+    private Editor _editor;
+     
+    public History(Editor editor)
+    {
+        _editor = editor;
+        _mementos = new List<EditorMemento>();
+    }
+     
+    public void Backup()
+    {
+        _mementos.Add(_editor.CreateMemento());
+    }
+     
+    public void Undo()
+    {
+        if (_mementos.Count == 0)
+        {
+            return;
+        }
+        var memento = _mementos.Last();
+        _mementos.Remove(memento);
+        _editor.Restore(memento);
+    }
+     
+ 
 }
