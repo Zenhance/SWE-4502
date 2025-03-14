b1abc7ac867525a09846b99f3a086d09c120a356
nstuli <nusratsiddiquetuli@gmail.com>
2025-03-14 11:49:15 +0600

    Issue #2: Implement Core TextEditor Class

--- DPLab12_Memeto/DPLab09_mediator/src/TextEditor.java
+++ DPLab12_Memeto/DPLab09_mediator/src/TextEditor.java
@@ -9,5 +9,27 @@ public class TextEditor {
         this.selection = "";
     }
 
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setCursorPosition(int position) {
+        this.cursorPosition = position;
+    }
+
+    public void addSelection(String selection) {
+        this.selection = selection;
+    }
+
+    public void clearSelections() {
+        this.selection = "";
+    }
+
+    public void displayState() {
+        System.out.println("Content: " + content);
+        System.out.println("Cursor Position: " + cursorPosition);
+        System.out.println("Selection: " + selection);
+    }
+
 
 }
