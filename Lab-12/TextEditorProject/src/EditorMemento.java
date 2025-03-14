937e876b219df307fe59e689467ec92912729963
HamimSaadAlRaji <hamimsaad.raji@gmail.com>
2025-03-14 14:44:32 +0600

    Issue #3 Implemented Memento Pattern Base

--- Lab-12/TextEditorProject/src/EditorMemento.java
+++ Lab-12/TextEditorProject/src/EditorMemento.java
@@ -1,2 +1,24 @@
+import java.util.List;
+
 public class EditorMemento {
+  private String content;
+  private int cursorPosition;
+  private List<String> selections;
+  public EditorMemento(String content,int cursorPosition,List<String> selections){
+      this.content = content;
+      this.cursorPosition = cursorPosition;
+      this.selections = selections;
+  }
+
+    public int getCursorPosition() {
+        return cursorPosition;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public List<String> getSelections() {
+        return selections;
+    }
 }
