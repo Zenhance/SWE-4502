4ff5dfff4c5a16286ffe480bd7ba7a6edc36ffd9
whiteflags26 <whiteflags26@gmail.com>
2025-03-14 11:36:45 +0600

    feat(text editor): implement memento pattern for content management and state restoration

--- TextEditor/MyTextEditor/EditorMemento.cs
+++ TextEditor/MyTextEditor/EditorMemento.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace MyTextEditor
 {
-    class EditorMemento
+    public class EditorMemento
     {
+        public string Content { get; }
+        public int CursorPosition { get; }
+        public List<string> Selections { get; }
+
+        public EditorMemento(string content, int cursorPosition, List<string> selections)
+        {
+            Content = content;
+            CursorPosition = cursorPosition;
+            Selections = selections;
+        }
     }
 }
--- TextEditor/MyTextEditor/TextEditor.cs
+++ TextEditor/MyTextEditor/TextEditor.cs
@@ -6,81 +6,91 @@ using System.Threading.Tasks;
 
 namespace MyTextEditor
 {
-    using System;
-    using System.Collections.Generic;
 
-    namespace TextEditor
+    public class TextEditor
     {
-        public class TextEditor
+        private string content;
+        private int cursorPosition;
+        private List<string> selections;
+
+        public TextEditor()
         {
-            private string content;
-            private int cursorPosition;
-            private List<string> selections;
+            content = string.Empty;
+            cursorPosition = 0;
+            selections = new List<string>();
+        }
 
-            public TextEditor()
+        public void SetContent(string newContent)
+        {
+            content = newContent;
+
+            if (cursorPosition > content.Length)
             {
-                content = string.Empty;
+                cursorPosition = content.Length;
+            }
+        }
+
+        public void SetCursorPosition(int position)
+        {
+            if (position < 0)
+            {
                 cursorPosition = 0;
-                selections = new List<string>();
             }
-
-            public void SetContent(string newContent)
+            else if (position > content.Length)
             {
-                content = newContent;
-
-                if (cursorPosition > content.Length)
-                {
-                    cursorPosition = content.Length;
-                }
+                cursorPosition = content.Length;
             }
-
-            public void SetCursorPosition(int position)
+            else
             {
-                if (position < 0)
-                {
-                    cursorPosition = 0;
-                }
-                else if (position > content.Length)
-                {
-                    cursorPosition = content.Length;
-                }
-                else
-                {
-                    cursorPosition = position;
-                }
+                cursorPosition = position;
             }
+        }
 
-            public void AddSelection(string selection)
+        public void AddSelection(string selection)
+        {
+            selections.Add(selection);
+        }
+
+        public void ClearSelections()
+        {
+            selections.Clear();
+        }
+
+        public void DisplayState()
+        {
+            Console.WriteLine("=== Text Editor State ===");
+            Console.WriteLine($"Content: {content}");
+            Console.WriteLine($"Cursor Position: {cursorPosition}");
+            Console.WriteLine("Selections:");
+
+            if (selections.Count == 0)
             {
-                selections.Add(selection);
+                Console.WriteLine("empty");
             }
-
-            public void ClearSelections()
+            else
             {
-                selections.Clear();
+                foreach (var selection in selections)
+                {
+                    Console.WriteLine(selection);
+                }
             }
+            Console.WriteLine("=======================");
+        }
 
-            public void DisplayState()
+        public EditorMemento CreateMemento()
+        {
+            return new EditorMemento(content, cursorPosition, new List<string>(selections));
+        }
+
+        public void RestoreFromMemento(EditorMemento memento)
+        {
+            if (memento != null)
             {
-                Console.WriteLine("=== Text Editor State ===");
-                Console.WriteLine($"Content: {content}");
-                Console.WriteLine($"Cursor Position: {cursorPosition}");
-                Console.WriteLine("Selections:");
-
-                if (selections.Count == 0)
-                {
-                    Console.WriteLine("empty");
-                }
-                else
-                {
-                    foreach (var selection in selections)
-                    {
-                        Console.WriteLine(selection);
-                    }
-                }
-                Console.WriteLine("=======================");
+                content = memento.Content;
+                cursorPosition = memento.CursorPosition;
+                selections = new List<string>(memento.Selections);
             }
-
         }
     }
 }
+
