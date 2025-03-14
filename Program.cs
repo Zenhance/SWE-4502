39c77262c8bed5d4318b3a8bd6c33a430809794b
IQRA109 <127471680+IQRA109@users.noreply.github.com>
2025-03-14 14:29:55 +0600

    #issue2 Test in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,44 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-namespace HomeAutomation
-{     // Core/EnvironmentState.cs
-       public class EnvironmentState
-       {                 
-            public Dictionary<string, object> State { get; set; } = new Dictionary<string, object>();
-             public void PrintState()
-             {             
-                    Console.WriteLine("Environment State:");   
-                    foreach (var key in State.Keys)
-                    {
-                        Console.WriteLine($"{key}: {State[key]}");
-                    }
-             }
-       }
+using System.IO;
+using System.Text.Json;
 
-    public class StateChangeNotifier
+namespace TextEditorApp
+{
+    public class Program
     {
-        private static readonly StateChangeNotifier _instance = new StateChangeNotifier();
-        private List<IStateChangeListener> _listeners = new List<IStateChangeListener>();
-
-        private StateChangeNotifier() { }
-
-        public static StateChangeNotifier Instance => _instance;
-
-        public void Subscribe(IStateChangeListener listener)
+        static void main(string[] args)
         {
-            _listeners.Add(listener);
-        }
-
-        public void Notify(EnvironmentState state)
-        {
-            foreach (var listener in _listeners)
-            {
-                listener.OnStateChange(state);
-            }
+            TextEditor editor = new TextEditor();
+            editor.SetContent("Hello World");
+            editor.SetCursorPosition(5);
+            editor.AddSelection("World");
+            editor.DisplayState();
         }
     }
-}
+
 }
