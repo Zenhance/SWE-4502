09b52c8438712c00a8abae34ecdc59398c9c74d7
Md. Abid Hasan <emonff89@gmail.com>
2025-03-14 08:13:02 +0600

    Add History class for state management
    Constructor taking TextEditor instance
      - Backup() method to save current state
      - Undo() method to restore previous state
    - Add proper state management using List<EditorMemento>
    
    Done

--- Lab12_textEditor/Lab12_textEditor/History.cs
+++ Lab12_textEditor/Lab12_textEditor/History.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace Lab12_textEditor
 {
-    internal class History
+    public class History
     {
+        private List<EditorMemento> _mementos;
+        private Editor _editor;
+        private History(Editor editor)
+        {
+            _editor = editor;
+            _mementos =new List<EditorMemento>();
+        }
+        public void Backup()
+        {
+            _mementos.Add(_editor.CreateMemento());
+        }
+        public void Undo()
+        {
+            if (_mementos.Count == 0)
+            {
+                return;
+            }
+            var memento = _mementos.Last();
+            _mementos.Remove(memento);
+            _editor.RestoreFromMemento(memento);
+        }
+       
+
     }
 }
+
