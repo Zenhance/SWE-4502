258a252d134521209a0284c4c28fbeb5d451195c
ramisaferdous <rifahramisa@iut-dhaka.edu>
2025-03-14 14:59:47 +0600

    Implement Core TextEditor Class

---
+++ Lab 12/TextEditor.cs
@@ -0,0 +1,43 @@
+public class Texteditor
+{
+    private string _content;
+    private int _cursorposition;
+    private List<string> _selections;
+
+    public Texteditor()
+    {
+        _content = string.Empty;
+        _cursorPosition = 0;
+        _selections = new List<string>();
+    }
+
+    public void SetContent(string content)
+    {
+        _content = content;
+    }
+    public SetCursorPosition(int position)
+    {
+        if(position < 0 || position > _content.Length)
+        {
+            Console.WriteLine("Invalid cursor position.");
+            return;
+        }
+        _cursorposition=position;
+    }
+    public AddSelection(string selection)
+    {
+        _selections.Add(selection);
+    }
+    public void ClearSelections()
+    {
+        _selections.Clear();
+
+    }
+    public void DisplayState()
+    {
+        Console.WriteLine($"Content: {_content}");
+        Console.WriteLine($"Cursor Position: {_cursorPosition}");
+        Console.WriteLine("Selections: " + string.Join(", ", _selections));
+    }
+    
+}
