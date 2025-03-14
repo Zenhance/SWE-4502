f4e01365843dd8265674fe634c39bbca831b0167
takiafarhin <takiafarhin@iut-dhaka.edu>
2025-03-14 14:07:59 +0600

    Issue #1: Initial Project Setup

---
+++ Lab 12/Lab12/Lab12/EditorMemento.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12
+{
+    [Serializable]
+     public class EditorMemento : ISerializable
+    {
+        protected EditorMemento(SerializationInfo info, StreamingContext context)
+        {
+            
+        }
+    }
+}
---
+++ Lab 12/Lab12/TextEditor_Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TextEditor_Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
