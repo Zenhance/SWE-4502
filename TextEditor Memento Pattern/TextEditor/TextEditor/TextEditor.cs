b013140f607ca7a8a322fe16d0af6e43b3211113
Ahmed Sadman Labib <aslabib44@gmail.com>
2025-03-14 08:34:38 +0600

    Issue 2: Implement Core TextEditor Class

--- TextEditor Memento Pattern/TextEditor/TextEditor/TextEditor.cs
+++ TextEditor Memento Pattern/TextEditor/TextEditor/TextEditor.cs
@@ -6,7 +6,75 @@ using System.Threading.Tasks;
 
 namespace TextEditor
 {
-    internal class TextEditor
+    public class TextEditor
     {
+        private string content;
+        private int cursorPosition;
+        private List<string> selections;
+
+        public TextEditor()
+        {
+            content = string.Empty;
+            cursorPosition = 0;
+            selections = new List<string>();
+        }
+
+        public void SetContent(string newContent)
+        {
+            content = newContent;
+
+            if (cursorPosition > content.Length)
+            {
+                cursorPosition = content.Length;
+            }
+        }
+
+        public void SetCursorPosition(int position)
+        {
+            if (position < 0)
+            {
+                cursorPosition = 0;
+            }
+            else if (position > content.Length)
+            {
+                cursorPosition = content.Length;
+            }
+            else
+            {
+                cursorPosition = position;
+            }
+        }
+
+        public void AddSelection(string selection)
+        {
+            selections.Add(selection);
+        }
+
+        public void ClearSelections()
+        {
+            selections.Clear();
+        }
+
+        public void DisplayState()
+        {
+            Console.WriteLine("---Current State of the TextEditor---");
+            Console.WriteLine($"Content: {content}");
+            Console.WriteLine($"Cursor Position: {cursorPosition}");
+            Console.WriteLine("Selections:");
+
+            if (selections.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                foreach (var selection in selections)
+                {
+                    Console.WriteLine(selection);
+                }
+            }
+            Console.WriteLine("------------------");
+        }
+
     }
 }
