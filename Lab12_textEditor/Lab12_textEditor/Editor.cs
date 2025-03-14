71aa1520ecaa0eac4d834b883cb363d484785d0f
Md. Abid Hasan <emonff89@gmail.com>
2025-03-14 07:59:48 +0600

    Implement basic TextEditor functionality

--- Lab12_textEditor/Lab12_textEditor/Editor.cs
+++ Lab12_textEditor/Lab12_textEditor/Editor.cs
@@ -6,7 +6,65 @@ using System.Threading.Tasks;
 
 namespace Lab12_textEditor
 {
-    internal class Editor
+    public class Editor
     {
+        private string _text;
+        private int _cursorPosition;
+        private string _selection;
+
+
+        public Editor()
+        {
+            _text = string.Empty;
+            _cursorPosition = 0;
+            _selection = string.Empty;
+        }
+        public void Insert(string text)
+        {
+            _text = _text.Insert(_cursorPosition, text);
+            _cursorPosition += text.Length;
+        }
+
+        public void Delete(int length)
+        {
+            if (_cursorPosition+length >_text.Length)
+            {
+                length = _text.Length - _cursorPosition;
+            }
+            _text = _text.Remove(_cursorPosition, length);
+        }
+        public void clearSelection()
+        {
+            _selection = string.Empty;
+        }
+        public void SetCursorPosition(int position)
+        {
+
+            if (position < 0)
+            {
+                position=0;
+            }
+            if (position>_text.Length)
+            {
+                position = _text.Length;
+            }
+            _cursorPosition = position;
+
+        }
+        public string GetText()
+        {
+            return _text;
+        }
+
+        public int GetCursorPosition()
+        {
+            return _cursorPosition;
+        }
+
+        public string GetSelection()
+        {
+            return _selection;
+        }
+
     }
 }
