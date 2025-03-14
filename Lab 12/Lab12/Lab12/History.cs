a7f1ed19ca6c88390137e9da522747207712ac9a
takiafarhin <takiafarhin@iut-dhaka.edu>
2025-03-14 15:06:03 +0600

    Issue #4 - Constructor taking TextEditor instance

---
+++ Lab 12/Lab12/Lab12/History.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12
+{
+    public class History
+    {
+        private List<EditorMemento> _mementos = new List<EditorMemento>();
+        private Editor _editor;
+
+        public History(Editor editor)
+        {
+            _editor = editor;
+        }
+    }
+}
