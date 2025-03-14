a4bc93013fb6fe6c11f3c00eb3f49cf01352fe74
HamimSaadAlRaji <hamimsaad.raji@gmail.com>
2025-03-14 14:27:51 +0600

    Implemented basic TextEditor functionality

--- Lab-12/TextEditorProject/src/TextEditor.java
+++ Lab-12/TextEditorProject/src/TextEditor.java
@@ -1,2 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TextEditor {
+    private String content;
+    private int cursorPosition;
+    private List<String> selections;
+
+    public TextEditor() {
+        this.content = "";
+        this.cursorPosition = 0;
+        this.selections = new ArrayList<>();
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setCursorPosition(int position) {
+
+            this.cursorPosition = position;
+
+    }
+
+    public void addSelection(String selection) {
+        selections.add(selection);
+    }
+
+    public void clearSelections() {
+        selections.clear();
+    }
+
+    public void displayState() {
+        System.out.println("Content: " + content);
+        System.out.println("Cursor Position: " + cursorPosition);
+        System.out.println("Selections: " + selections);
+    }
 }
