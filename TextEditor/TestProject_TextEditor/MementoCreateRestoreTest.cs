f22a15419a3a925c81381540ce286e73bc4980d3
Tauhidur Rahman Tauhid <tauhid04023@gmail.com>
2025-03-14 12:37:43 +0600

    Issue 6 - Created test cases for MementoCreateRestoreTest.cs

--- TextEditor/TestProject_TextEditor/MementoCreateRestoreTest.cs
+++ TextEditor/TestProject_TextEditor/MementoCreateRestoreTest.cs
@@ -1,6 +1,37 @@
-﻿namespace TestProject_TextEditor;
+﻿using TextEditor ;
+namespace TestProject_TextEditor;
 
 public class MementoCreateRestoreTest
 {
-    
+    [Fact]
+    public void CreateMementoTest()
+    {
+        Editor editor = new Editor();
+        editor.Insert("Hello World!");
+        editor.SetCursorPosition(6);
+        editor.Select(6, 6);
+         
+        EditorMemento memento = editor.CreateMemento();
+         
+        Assert.Equal("Hello World!", memento.GetText());
+        Assert.Equal(6, memento.GetCursorPosition());
+        Assert.Equal("World!", memento.GetSelection());
+    }
+ 
+    [Fact]
+    public void RestoreMementoTest()
+    {
+        Editor editor = new Editor();
+        editor.Insert("Hello World!");
+        editor.SetCursorPosition(6);
+        editor.Select(6, 6);
+         
+        EditorMemento memento = editor.CreateMemento();
+         
+        editor.Select(4, 8);
+        Assert.Equal("o World!", editor.GetSelection());
+         
+        editor.Restore(memento);
+        Assert.Equal("World!", editor.GetSelection());
+    }
 }
