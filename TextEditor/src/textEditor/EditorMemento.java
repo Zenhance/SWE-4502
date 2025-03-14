6b0e95ea8d1a377c35cb1843e9ece857f718eac5
Nafisa Nawal <nafisanawal@iut-dhaka.edu>
2025-03-14 11:45:11 +0600

    (Issue 3) Added private fields, constructors and getters in EditorMemento

--- TextEditor/src/textEditor/EditorMemento.java
+++ TextEditor/src/textEditor/EditorMemento.java
@@ -1,4 +1,26 @@
 package textEditor;
 
 public class EditorMemento {
+    private String content;
+    private int cursorPosition;
+    private String selection;
+
+    public EditorMemento(String _content, int _cursorPosition, String _selection){
+        this.content = _content;
+        this.cursorPosition = _cursorPosition;
+        this.selection= _selection;
+
+    }
+
+    public String getContent(){
+        return content;
+    }
+
+    public int getCursorPosition(){
+        return cursorPosition;
+    }
+
+    public String getSelection(){
+        return selection;
+    }
 }
