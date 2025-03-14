8658f24339dee98e15655193cc24fb59eecc3661
IQRA109 <127471680+IQRA109@users.noreply.github.com>
2025-03-14 14:24:22 +0600

    Lab12 #issue1 done

---
+++ HomeAutomation/lab12/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace TextEditorapp
+{
+    public class Program
+    {
+        static void main(string[] args)
+        {
+            Console.WriteLine("The app is started.");
+        }
+    }
+
+}
