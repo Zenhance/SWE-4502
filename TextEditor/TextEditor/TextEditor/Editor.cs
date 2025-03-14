96ccb502c9c98b5862463ec6872c39f40e149bfe
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2025-03-14 14:36:08 +0600

    Issue 2 editor class implemented

--- TextEditor/TextEditor/TextEditor/Editor.cs
+++ TextEditor/TextEditor/TextEditor/Editor.cs
@@ -1,6 +1,62 @@
-﻿namespace TextEditor;
+﻿using System.Net.Sockets;
+
+namespace TextEditor;
 
 public class Editor
 {
+    private string text;
+    private int cursorPosition;
+    private string selection;
     
+    public Editor()
+    {
+        text = string.Empty;
+        cursorPosition = 0;
+        selection = string.Empty;
+    }
+
+    public void insert(string text)
+    {
+        this.text = this.text.Insert(cursorPosition, text);
+        cursorPosition += text.Length;
+    }
+
+    public void delete(int length)
+    {
+        if (cursorPosition + length < this.text.Length)
+        {
+            length = this.text.Length - cursorPosition;
+        }
+        this.text = this.text.Remove(cursorPosition, length);
+    }
+
+    public void Select(int start, int length)
+    {
+        if (start + length > this.text.Length)
+        {
+            length = this.text.Length - start;
+        }
+        
+        selection = this.text.Substring(start, length);
+    }
+
+    public void ClearSelection()
+    {
+        selection = string.Empty;
+    }
+
+    public void SetCursorPosition(int position)
+    {
+        if (position < 0)
+        {
+            position = 0;
+        }
+
+        if (position > this.text.Length)
+        {
+            position = this.text.Length;
+        }
+        
+        cursorPosition = position;
+    }
 }
