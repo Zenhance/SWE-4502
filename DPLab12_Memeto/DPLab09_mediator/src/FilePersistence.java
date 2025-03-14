6eae352ec842137cbc60f1b325a66e1b92e566db
nstuli <nusratsiddiquetuli@gmail.com>
2025-03-14 12:36:52 +0600

    #Issue #5: Add File Persistence

--- DPLab12_Memeto/DPLab09_mediator/src/FilePersistence.java
+++ DPLab12_Memeto/DPLab09_mediator/src/FilePersistence.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FilePersistence {
@@ -6,7 +7,7 @@ public class FilePersistence {
     // Save history (List of EditorMemento) to a file
     public static void saveToFile(String filePath, List<EditorMemento> history) {
         try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filePath))) {
-            out.writeObject(history);  // Write the list of mementos to the file
+            out.writeObject(history.getLast());  // Write the list of mementos to the file
             System.out.println("History saved successfully to " + filePath);
         } catch (IOException e) {
             System.out.println("Error saving file: " + e.getMessage());
@@ -17,7 +18,9 @@ public class FilePersistence {
     public static List<EditorMemento> loadFromFile(String filePath) {
         try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(filePath))) {
             // Read the list of mementos from the file
-            return (List<EditorMemento>) in.readObject();
+            List<EditorMemento> rtrnList = new ArrayList<>();
+            rtrnList.add( (EditorMemento) in.readObject());
+            return rtrnList;
         } catch (IOException e) {
             System.out.println("Error loading file: " + e.getMessage());
             return null;
