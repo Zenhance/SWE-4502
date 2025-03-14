e1084ea66974a61330f664e1af13606f666ccb48
Atik Shahriar Ovi <atikshahriar@iut-dhaka.edu>
2025-03-14 12:29:43 +0600

    Issue #3: Create EditorMemento class

--- TextEditormemento/src/main/java/org/example/EditorMemento.java
+++ TextEditormemento/src/main/java/org/example/EditorMemento.java
@@ -1,5 +1,29 @@
 package org.example;
 
+import java.util.List;
+
 public class EditorMemento
 {
+    private final String content;
+    private final int cursorPosition;
+    private final List<String> selections;
+
+    public EditorMemento(String content, int cursorPosition, List<String> selections) {
+        this.content = content;
+        this.cursorPosition = cursorPosition;
+        this.selections = List.copyOf(selections);
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public int getCursorPosition() {
+        return cursorPosition;
+    }
+
+    public List<String> getSelections() {
+        return selections;
+    }
+
 }
