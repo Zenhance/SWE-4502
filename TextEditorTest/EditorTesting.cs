2187456bfb4398a091ed9dadbf26ef5035cc9ff1
sheona001 <sirexcc001@gmail.com>
2025-03-14 11:29:18 +0600

    ## Issue #6: Create Unit Tests dummy and test setup

---
+++ TextEditorTest/EditorTesting.cs
@@ -0,0 +1,29 @@
+using TextEditorProject.Core;
+using System;
+using System.IO;
+using Xunit;
+
+namespace TextEditorTest
+{
+    public class EditorTesting
+    {
+
+        [Fact]
+        public void SetContent_ShouldUpdateContent()
+        {
+           
+            var editor = new TextEditor();
+            string newContent = "Hello, World!";
+            editor.SetContent(newContent);
+
+         
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                editor.DisplayState();
+                string output = sw.ToString();
+                Assert.Contains(newContent, output);
+            }
+        }
+    }
+}
