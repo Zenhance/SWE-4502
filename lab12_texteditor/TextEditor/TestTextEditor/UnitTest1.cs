1bc0ed8c4b9da76f41dcd104a38968299394446c
rakins <shahriar.rakin99@gmail.com>
2025-03-14 12:04:21 +0600

    Issue 2 --> Implement Core TextEditor Class

---
+++ lab12_texteditor/TextEditor/TestTextEditor/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace TestTextEditor;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
--- lab12_texteditor/TextEditor/TextEditor/Editor.cs
+++ lab12_texteditor/TextEditor/TextEditor/Editor.cs
@@ -2,5 +2,58 @@
 
 public class Editor
 {
+    private string _text;
+    private int _cursorPosition;
+    private string _selection;
+ 
+    public Editor()
+    {
+        _text = string.Empty;
+        _cursorPosition = 0;
+        _selection = string.Empty;
+    }
+     
+    public void Insert(string text)
+    {
+        _text = _text.Insert(_cursorPosition, text);
+        _cursorPosition += text.Length;
+    }
+     
+    public void Delete(int length)
+    {
+        if (_cursorPosition + length > _text.Length)
+        {
+            length = _text.Length - _cursorPosition;
+        }
+        _text = _text.Remove(_cursorPosition, length);
+    }
+     
+    public void Select(int start, int length)
+    {
+        if (start + length > _text.Length)
+        {
+            length = _text.Length - start;
+        }
+        _selection = _text.Substring(start, length);
+    }
+ 
+ 
+    public void ClearSelection()
+    {
+        _selection = string.Empty;
+    }
+     
+    public void SetCursorPosition(int position)
+    {
+        if (position < 0)
+        {
+            position = 0;
+        }
+        if (position > _text.Length)
+        {
+            position = _text.Length;
+        }
+        _cursorPosition = position;
+    }
     
 }
