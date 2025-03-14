95c8a0172e2dae5f6b2dd731c0fef7ae03ebe1bc
IQRA109 <127471680+IQRA109@users.noreply.github.com>
2025-03-14 22:12:06 +0600

    #issue1 done

---
+++ TextEditorProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace TextEditorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Text Editor Started");
+        }
+    }
+
+}
