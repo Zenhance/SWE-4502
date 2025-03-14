83e1b19794bd9abeb4f10d71ffde9e031f52ca02
Nafisa Nawal <nafisanawal@iut-dhaka.edu>
2025-03-14 12:03:40 +0600

    (Issue 4) Created constructor and backup method in History class

--- TextEditor/src/textEditor/History.java
+++ TextEditor/src/textEditor/History.java
@@ -1,4 +1,21 @@
 package textEditor;
 
+import java.util.List;
+
 public class History {
+    public TextEditor editor;
+    public List<EditorMemento> mementos;
+
+    public History(TextEditor _textEditor){
+        this.editor = _textEditor;
+    }
+
+    public void backup(){
+        mementos.add(editor.createMemento());
+        System.out.println("Saved the current state");
+    }
+
+    public void undo(){
+
+    }
 }
