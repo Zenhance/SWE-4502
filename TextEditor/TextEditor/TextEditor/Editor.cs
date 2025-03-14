0f0f43499e1e24a9fcbc367de5dd189c15ab29b7
rhydita <rhyd.ita77@gmail.com>
2025-03-14 15:05:50 +0600

    Issue #2, implemented Editor class

--- TextEditor/TextEditor/TextEditor/Editor.cs
+++ TextEditor/TextEditor/TextEditor/Editor.cs
@@ -2,5 +2,61 @@
 
 public class Editor
 {
+    private string text;
+    private int cursorPosition;
+    private string selection;
+
+    public Editor()
+    {
+        text = string.Empty;
+        cursorPosition = 0;
+        selection = string.Empty;
+    }
+
+    public void Insert(string text)
+    {
+        text = this.text.Insert(cursorPosition, text);
+        cursorPosition += text.Length;
+    }
+
+    public void Delete(int length)
+    {
+        if (cursorPosition + length > text.Length)
+        {
+            length = text.Length - cursorPosition;
+        }
+
+        text = text.Remove(cursorPosition, length);
+    }
     
+    public void Select(int start, int length)
+    {
+        if (start + length > text.Length)
+        {
+            length = text.Length - start;
+        }
+        selection = text.Substring(start, length);
+    }
+    
+    public void ClearSelection()
+    {
+        selection = string.Empty;
+    }
+
+    public void SetCursorPosition(int position)
+    {
+        if (position < 0)
+        {
+            position = 0;
+        }
+
+        if (position > text.Length)
+        {
+            position = text.Length;
+        }
+        cursorPosition = position;
+    }
+
+    
+
 }
