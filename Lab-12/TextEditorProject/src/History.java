8fe345678db78b7309181183e0a24ceef608f102
HamimSaadAlRaji <hamimsaad.raji@gmail.com>
2025-03-14 15:19:27 +0600

    Issue #4 Implemented History

--- Lab-12/TextEditorProject/src/History.java
+++ Lab-12/TextEditorProject/src/History.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class History {
+    private final TextEditor editor;
+    private final List<EditorMemento> mementos;
+
+    public History(TextEditor editor) {
+        this.editor = editor;
+        this.mementos = new ArrayList<>();
+    }
+
+    public void backup() {
+        mementos.add(editor.createMemento());
+    }
+
+    public void undo() {
+        if (mementos.isEmpty()) {
+            System.out.println("No states to undo.");
+            return;
+        }
+        EditorMemento memento = mementos.remove(mementos.size() - 1);
+        editor.restoreFromMemento(memento);
+    }
 }
