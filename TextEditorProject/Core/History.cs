7cca9a707ceaf22f790cd760856a5c7a1cb877ea
sheona001 <sirexcc001@gmail.com>
2025-03-14 11:59:39 +0600

    Issue #4: Implement History Management  backup and undo

--- TextEditorProject/Core/History.cs
+++ TextEditorProject/Core/History.cs
@@ -18,11 +18,30 @@ namespace TextEditorProject.Core
 
         public void Backup()
         {
+            var editor= textEditor.CreateMemento();
+            editorMementos.Add(editor);
+
+            Console.WriteLine("Backup stored successfully.");
+
+
 
         }
 
         public void Undo()
         {
+
+            if(editorMementos.Count > 0)
+            {
+
+                EditorMemento memento = editorMementos[editorMementos.Count-1];
+                editorMementos.RemoveAt(editorMementos.Count-1);
+                textEditor.RestoreFromMemento(memento);
+                Console.WriteLine("Undo operation was successful.");
+            }
+            else
+            {
+                Console.WriteLine("nothing to restore.");
+            }
 
         }
     }
