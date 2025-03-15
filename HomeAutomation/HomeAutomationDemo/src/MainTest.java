9b04d5322256c995f03d662520b2b83dcd1446ab
fmnihal <fattahmahmudnihal@gmail.com>
2025-03-15 13:39:00 +0600

    initial state

---
+++ HomeAutomation/HomeAutomationDemo/src/MainTest.java
@@ -0,0 +1,8 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
---
+++ HomeAutomation/HomeAutomationDemo/test/src/MainTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+
+    @Test
+    public void main() {
+    }
+}
