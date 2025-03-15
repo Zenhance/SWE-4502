9c1841e832579cda96bc93a0e581ee9afc526ff8
Shefayat E- Shams Adib <shefayatadib@iut-dhaka.edu>
2025-03-15 10:57:13 +0600

    Updated Find Weather App

--- FindWeather/WeatherApp/src/App.java
+++ FindWeather/WeatherApp/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to FindWeather App!");
+        System.out.println("Welcome to FindWeather!");
     }
 }
