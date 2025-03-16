3b549b25217b17bf58da8be8e80572f47fb1e45f
bhuiyan.minhajul@gmail.com <bhuiyan.minhajul@gmail.com>
2025-03-16 10:22:38 +0600

    Added Core Framework implementation #10

---
+++ HomeAutomation/HomeAutomation/Core/StateManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation
+{
+    public class StateManager : IStateSubject
+    {
+        private readonly List<IStateObserver> _observers = new List<IStateObserver>();
+        private EnvironmentalState _currentState = new EnvironmentalState();
+
+        public void RegisterObserver(IStateObserver observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void RemoveObserver(IStateObserver observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        public void NotifyObservers()
+        {
+            foreach (var observer in _observers)
+            {
+                observer.Update(_currentState);
+            }
+        }
+
+        public void UpdateState(EnvironmentalState newState)
+        {
+            _currentState = newState;
+            NotifyObservers();
+        }
+    }
+}
