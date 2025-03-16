2cf994c651e7ed89adeea04f80f54a027bc6ff01
bhuiyan.minhajul@gmail.com <bhuiyan.minhajul@gmail.com>
2025-03-16 16:15:55 +0600

    #10 Implement Core Framework

---
+++ HomeAutomation/HomeAutomation/Core/EnvironmentalState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation
+{
+    public class EnvironmentalState
+    {
+        public bool MotionDetected { get; set; }
+        public double Temperature { get; set; }
+        public double AmbientLightLevel { get; set; }
+        public double EnergyUsage { get; set; }
+
+        public override string ToString()
+        {
+            return $"Motion: {MotionDetected}, Temp: {Temperature}°C, Light: {AmbientLightLevel} lux, Power: {EnergyUsage}W";
+        }
+    }
+}
