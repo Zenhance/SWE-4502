5733f8e0aa352fcf32a136a375b0ce29c44ac0d5
sheona001 <sirexcc001@gmail.com>
2025-03-21 05:20:07 +0600

    IssueRepository add, remove

---
+++ taskManager/labfinal_210042111/IssueRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace labfinal_210042111
+{
+    public class IssueRepository
+    {
+
+        private Dictionary<int, Issue> repository = new Dictionary<int, Issue>();
+
+
+        public void addIssue(Issue issue)
+        {
+            repository[issue.Id] = issue;
+        }
+        public void removeIssue(int id)
+        {
+            repository.Remove(id);
+        }
+
+
+
+    }
+}
