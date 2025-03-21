d9cdc4ea2dcd6f9f19a8d14db889fe2912f2f7fa
Kashshaf-Labib <labibkashshaf@gmail.com>
2025-03-21 11:41:20 +0600

    feat(Comment): implement comment class

---
+++ TaskManager/TaskManagerSolution/src/Comment.java
@@ -0,0 +1,50 @@
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class Comment {
+    private String id;
+    private String content;
+    private String author;
+    private LocalDateTime createdAt;
+
+    public Comment() {
+        this.id = UUID.randomUUID().toString();
+        this.createdAt = LocalDateTime.now();
+    }
+
+    private Comment(Comment source) {
+        this.id = source.id;
+        this.content = source.content;
+        this.author = source.author;
+        this.createdAt = source.createdAt;
+    }
+
+
+    public Comment clone() {
+        return new Comment(this);
+    }
+    
+    public String getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
