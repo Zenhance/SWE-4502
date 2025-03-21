d7ec712c12ea303f5aba8d2269710f2c7ed51cb3
bhuiyan.minhajul@gmail.com <bhuiyan.minhajul@gmail.com>
2025-03-21 15:36:17 +0600

    Issue #F05: Redo Command Test

---
+++ TaskManager/TaskManager_Tests/RedoCommand_ShouldReapplyUndoneAction.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager;
+
+namespace TaskManager_Tests
+{
+    [TestClass]
+    public class RedoCommand_ShouldReapplyUndoneAction
+    {
+        [TestMethod]
+        public void RedoCommand_Test_Method()
+        {
+            IssueRepository repo = new IssueRepository();
+            Issue issue = new Issue(1, "Test Issue", "Test Description", Priority.Medium);
+            CreateIssueCommand createIssueCommand = new CreateIssueCommand(repo, issue);
+            CommandManager commandManager = new CommandManager();
+
+            commandManager.ExecuteCommand(createIssueCommand);
+
+            commandManager.Redo();
+
+            var retrievedIssue = repo.GetIssue(1);
+
+        }
+        
+    }
+}
