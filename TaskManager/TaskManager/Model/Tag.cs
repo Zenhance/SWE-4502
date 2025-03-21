dc5fb372a91245638acd6d708752eaa69d4fcc65
rakins <shahriar.rakin99@gmail.com>
2025-03-21 12:27:50 +0600

    Tag class with constructor

--- TaskManager/TaskManager/Model/Tag.cs
+++ TaskManager/TaskManager/Model/Tag.cs
@@ -2,5 +2,14 @@
 
 public class Tag
 {
+    public string name;
+    public int id;
+    public string tagline;
+    
+    public Tag(string name, string tagline)
+    {
+        this.name = name;
+        this.tagline = tagline;
+    }
     
 }
