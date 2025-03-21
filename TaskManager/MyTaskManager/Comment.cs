e6f1059a6abf2af3b5a43ec3c33cd2f0f6ed46e7
whiteflags26 <whiteflags26@gmail.com>
2025-03-21 11:47:58 +0600

    feat(comment): implement Comment class

---
+++ TaskManager/MyTaskManager/Comment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTaskManager
+{
+    public class Comment : ICloneable
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public Comment(int id, string content, string author)
+        {
+            Id = id;
+            Content = content;
+            Author = author;
+            CreatedAt = DateTime.Now;
+        }
+
+        public object Clone()
+        {
+            return new Comment(Id, Content, Author)
+            {
+                CreatedAt = DateTime.Now
+            };
+        }
+    }
+}
--- TaskManager/MyTaskManager/Program.cs
+++ TaskManager/MyTaskManager/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MyTaskManager;
+using System;
+using System.Threading.Tasks;
+
+namespace MyTaskManger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment comment = new Comment(1, "Hello", "John");
+            Comment clone = (Comment)comment.Clone();
+            Console.WriteLine(comment.CreatedAt);
+            Console.WriteLine(clone.CreatedAt);
+        }
+    }
+}
