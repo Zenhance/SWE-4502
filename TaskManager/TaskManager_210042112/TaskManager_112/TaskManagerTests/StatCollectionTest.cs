fd193c9213a028cc8ccc759b7aaa87241b7013ec
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-21 15:12:33 +0600

    #F07 : stats collection test - half done ???

---
+++ TaskManager/TaskManager_210042112/TaskManager_112/TaskManagerTests/StatCollectionTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager_112;
+
+namespace TaskManagerTests
+{
+    public class StatCollectionTest
+    {
+        [Fact]
+        public void StatisticsCollector_ShouldTrackCommandCounts()
+        {
+            var repo = new IssueRepository();
+            var issue = IssueFactory.CreateIssue(1, "Lame", "Description : Dead", Priority.Medium);
+            var createIssue = new CreateIssueCommand(repo, issue);
+            var invoker = new CommandInvoker();
+
+            // Act
+            invoker.ExecuteCommand(createIssue);
+            invoker.ExecuteCommand(new ChangeIssueStatusCommand(issue, Status.InProgress));
+
+
+            // Assert
+            
+        }
+    }
+}
