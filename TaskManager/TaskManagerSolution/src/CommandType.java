fc202d402354148fd597486941486d8b04329e79
Kashshaf-Labib <labibkashshaf@gmail.com>
2025-03-21 11:50:36 +0600

    feat(CommandType): implement CommandType enum

---
+++ TaskManager/TaskManagerSolution/src/CommandType.java
@@ -0,0 +1,3 @@
+public enum CommandType {
+    CREATE, UPDATE, DELETE
+}
--- TaskManager/TaskManagerSolution/src/Issue.java
+++ TaskManager/TaskManagerSolution/src/Issue.java
@@ -37,8 +37,83 @@ public class Issue {
             this.comments.add(comment.clone());
         }
     }
-    
+
     public Issue clone() {
         return new Issue(this);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+        this.lastModifiedDate = LocalDateTime.now();
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+        this.lastModifiedDate = LocalDateTime.now();
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+        this.lastModifiedDate = LocalDateTime.now();
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+        this.lastModifiedDate = LocalDateTime.now();
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public LocalDateTime getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public String getAssignedTo() {
+        return assignedTo;
+    }
+
+    public void setAssignedTo(String assignedTo) {
+        this.assignedTo = assignedTo;
+        this.lastModifiedDate = LocalDateTime.now();
+    }
+
+    public Set<String> getTags() {
+        return tags;
+    }
+
+    public void addTag(String tag) {
+        this.tags.add(tag);
+        this.lastModifiedDate = LocalDateTime.now();
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void addComment(Comment comment) {
+        this.comments.add(comment);
+        this.lastModifiedDate = LocalDateTime.now();
     }
 }
