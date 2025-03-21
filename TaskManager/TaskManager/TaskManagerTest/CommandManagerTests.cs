a600939b550ffc510c8766aadb94c690c02e467f
Mohammad Istiaq Prodhan <istiaqprodhan02@gmail.com>
2025-03-21 14:50:30 +0600

    #F04

--- TaskManager/TaskManager/TaskManagerTest/CommandManagerTests.cs
+++ TaskManager/TaskManager/TaskManagerTest/CommandManagerTests.cs
@@ -1,12 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Xunit;
+using TaskManager.Commands;
+using TaskManager.Models;
+using TaskManager.Repository;
+using TaskManager.Enums;
+using TaskManager.Commands.TaskManager.Commands;
 
-namespace TaskManagerTest
+namespace TaskManager.Tests
 {
-    internal class CommandManagerTests
+    public class CommandManagerTests
     {
+        //#F04
+        [Fact]
+        public void UndoCommand_ShouldRevertLastAction()
+        {
+            var repository = new IssueRepository();
+            var commandManager = new CommandManager();
+            var issue = new Issue
+            {
+                Title = "Test Issue",
+                Description = "Test Description",
+                Priority = Priority.Medium
+            };
+            var createCommand = new CreateIssueCommand(repository, issue);
+
+            commandManager.ExecuteCommand(createCommand);
+            commandManager.Undo();
+            Assert.Empty(repository.GetAll());
+        }
     }
 }
