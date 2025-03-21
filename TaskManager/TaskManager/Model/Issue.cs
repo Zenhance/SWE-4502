c8c63b204b7bcff324576532e114cdf21ed32304
rakins <shahriar.rakin99@gmail.com>
2025-03-21 11:38:15 +0600

    Issue class created

---
+++ TaskManager/TaskManager/Model/Issue.cs
@@ -0,0 +1,13 @@
+﻿using TaskManager.ENUM;
+using TaskManagerOriginal.ENUM;
+
+namespace TaskManagerOriginal.Model;
+
+public class Issue
+{
+    public int id;
+    public string title;
+    public string description;
+    public PRIORITY priority;
+    public STATUS status;
+}
