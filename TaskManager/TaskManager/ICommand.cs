57e9a7ca6323190dda50cffed8dfe6798f19ef04
takiafarhin <takiafarhin@iut-dhaka.edu>
2025-03-21 11:27:14 +0600

    Interface for Command pattern

---
+++ TaskManager/TaskManager/ICommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager
+{
+    public interface ICommand
+    {
+        void Execute();
+        void Undo();
+        string Description { get; }
+    }
+}
