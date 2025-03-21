f0b39b25d31134bd0c9bbd01a8b98488bcd68a77
sheona001 <sirexcc001@gmail.com>
2025-03-21 11:32:56 +0600

    Log Operation

---
+++ taskManager/labfinal_210042111/LogEntry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace labfinal_210042111
+{
+    public class LogEntry
+    {
+        public string Description { get; set; }
+        public DateTime timeStamp { get; set; }
+        public CommandType Type { get; set; }
+
+        public LogEntry(string description, DateTime timeStamp, CommandType type)
+        {
+            Description = description;
+            this.timeStamp = timeStamp;
+            Type = type;
+        }
+
+
+
+        
+    }
+}
