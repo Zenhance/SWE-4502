a28f533ab8e16a184918bfc4abc63d342630cf9c
kmsquare27 <113410233+kmsquare27@users.noreply.github.com>
2025-03-21 12:24:22 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taskManager
+{
+    public enum priority
+    {
+        Low,
+        Medium, 
+        High,
+        Crtitical
+    }
+
+    public enum Status
+    {
+        Open,
+        Inprogress,
+        UnderReview, 
+        Resolved, 
+        Closed
+    }
+
+    public class createIssue 
+    {
+
+    }
+     
+    public class updateIssue
+    {
+
+    }
+    public class changeIssueStatus
+
+
+    {
+
+    }
+    public class addComments
+    {
+
+    }
+
+    }
+    public class triggerNotification
+    {
+
+    }
+'
+    public class triggerNotification
+{
+
+}
+Statistics Collector
+
+
+
+
+
+
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
