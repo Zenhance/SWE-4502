90e25f1e8e34fea57763a081d830256d1d599de5
rakins <shahriar.rakin99@gmail.com>
2025-03-21 12:04:09 +0600

    issue class comment class with constructor

---
+++ TaskManager/TaskManager/Model/Comment.cs
@@ -0,0 +1,16 @@
+﻿namespace TaskManagerOriginal.Model;
+
+public class Comment
+{
+    public int id;
+    public string text;
+    public DateTime createdDate;
+    public User createdBy;
+    
+    public Comment(string text, User createdBy)
+    {
+        this.text = text;
+        this.createdBy = createdBy;
+        this.createdDate = DateTime.Now;
+    }
+}
--- TaskManager/TaskManager/Model/Issue.cs
+++ TaskManager/TaskManager/Model/Issue.cs
@@ -10,4 +10,23 @@ public class Issue
     public string description;
     public PRIORITY priority;
     public STATUS status;
+    public DateTime createdDate;
+    public DateTime lastModifiedDate;
+    public User AssignedTo;
+     
+    public List<Comment> comments;
+    
+    public Issue(string title, string description, PRIORITY priority, User assignedTo, STATUS status, DateTime createdDate, DateTime lastModifiedDate)
+    {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.AssignedTo = assignedTo;
+        this.status = status;
+        this.createdDate = createdDate;
+        this.lastModifiedDate = lastModifiedDate;
+        this.comments = new List<Comment>();
+    }
+    
+    
 }
