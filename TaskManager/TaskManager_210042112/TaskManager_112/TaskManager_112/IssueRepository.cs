d34aeb93014af228d0f239df54aa083cd8fde30f
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-21 12:25:21 +0600

    issuerepo : holds the id and the issue entity

---
+++ TaskManager/TaskManager_210042112/TaskManager_112/TaskManager_112/IssueRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager_112
+{
+    public class IssueRepository
+    {
+        Dictionary<int, Issue> issues = new Dictionary<int, Issue>();
+
+        
+    }
+}
