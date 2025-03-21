677caebb95f17798b33df9f29698dbbfb0ed16fe
Hasibul Islam Nirjhar <hasibulislam@iut-dhaka.edu>
2025-03-21 15:05:55 +0600

    Updated all the test cases

--- IssueTracker-Final/Test/Issue1Test.java
+++ IssueTracker-Final/Test/Issue1Test.java
@@ -37,11 +37,40 @@ Issue issue = new Issue("1", "Test Issue", "This is a test issue", Priority.Medi
 
     public void AddComment_ShouldAddCommentToIssue() {
 
+        IssueRepository repository = new IssueRepository();
+Issue issue = new Issue("1", "Test Issue", "This is a test issue", Priority.Medium, Status.Open, LocalDateTime.now(), LocalDateTime.now(), "Test User", new ArrayList<>(), Arrays.asList("test", "issue"));
+
+        Comment comment = new Comment(1, "Test Comment",  "Test User", LocalDateTime.now());
+
+        CreateIssue createIssue = new CreateIssue(repository, issue);
+
+        createIssue.execute();
+
+        AddComment addComment = new AddComment(repository, issue, comment);
+
+        addComment.execute();
+
+        assertEquals(1, issue.comments.size());
+
     }
 
 
     @Test
     public void ChangeStatus_ShouldUpdateIssueStatus() {
+
+        IssueRepository repository = new IssueRepository();
+
+        Issue issue = new Issue("1", "Test Issue", "This is a test issue", Priority.Medium, Status.Open, LocalDateTime.now(), LocalDateTime.now(), "Test User", new ArrayList<>(), Arrays.asList("test", "issue"));
+
+        CreateIssue createIssue = new CreateIssue(repository, issue);
+
+        createIssue.execute();
+
+        ChangeStatus changeStatus = new ChangeStatus(repository, issue, Status.Closed);
+
+        changeStatus.execute();
+
+        assertEquals(Status.Closed, issue.getStatus());
 
 
     }
