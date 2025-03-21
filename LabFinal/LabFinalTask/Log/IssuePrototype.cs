bf0d477d82468cc0f33638fd54b330d575482837
sakhadib <adibsakhawat@iut-dhaka.edu>
2025-03-21 11:20:04 +0600

    Prototype

---
+++ LabFinal/LabFinalTask/Log/IssuePrototype.cs
@@ -0,0 +1,32 @@
+﻿using System.Data;
+using System.Runtime.InteropServices.JavaScript;
+using LabFinalTask.Model;
+using LabFinalTask.ENUM;
+namespace LabFinalTask.Log;
+
+public class IssuePrototype
+{
+    public int id;
+    public string title;
+    public string description;
+    public PRIORITY priority;
+    public STATUS status;
+    public DateTime createdDate;
+    public DateTime lastModifiedDate;
+    public User AssignedTo;
+    
+    public List<Comment> comments;
+    public List<Tag> tags;
+
+    public IssuePrototype(string title, string desc, PRIORITY priority, STATUS status, DateTime createdDate,
+        DateTime lastModifiedDate, User assignedTo)
+    {
+        this.title = title;
+        this.description = desc;
+        this.priority = priority;
+        this.status = status;
+        this.createdDate = createdDate;
+        this.lastModifiedDate = lastModifiedDate;
+        this.AssignedTo = assignedTo;
+    }
+}
