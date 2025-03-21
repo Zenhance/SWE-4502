d69b7af5a5aa1571e67211402d1b046867cc8c27
sakhadib <adibsakhawat@iut-dhaka.edu>
2025-03-21 14:35:06 +0600

    #F05 : RedoCommand_ShouldReapplyUndoneAction

--- LabFinal/LabFinalTask/Command/CommandManager.cs
+++ LabFinal/LabFinalTask/Command/CommandManager.cs
@@ -3,16 +3,19 @@
 public class CommandManager
 {
     public List<ICommand> commands;
+    public List<ICommand> undoneCommands;
 
     public CommandManager()
     {
         commands = new List<ICommand>();
+        undoneCommands = new List<ICommand>();
     }
     
     public void ExecuteCommand(ICommand command)
     {
         commands.Add(command);
         command.execute();
+        undoneCommands.Clear();
     }
 
     public void UndoCommand()
@@ -22,6 +25,19 @@ public class CommandManager
             ICommand command = commands[commands.Count - 1];
             command.undo();
             commands.Remove(command);
+            undoneCommands.Add(command);
+        }
+    }
+
+
+    public void RedoCommand()
+    {
+        if(undoneCommands.Count > 0)
+        {
+            ICommand command = undoneCommands[undoneCommands.Count - 1];
+            command.execute();
+            undoneCommands.Remove(command);
+            commands.Add(command);
         }
     }
 }
---
+++ LabFinal/LabFinalTest/RedoCommand_ShouldReapplyUndoneAction.cs
@@ -0,0 +1,38 @@
+﻿using LabFinalTask.Command;
+using LabFinalTask.Core;
+using LabFinalTask.ENUM;
+using LabFinalTask.Mediator;
+using LabFinalTask.Model;
+
+namespace LabFinal;
+
+public class RedoCommand_ShouldReapplyUndoneAction
+{
+    [Fact]
+    public void RedoCommandTest()
+    {
+        User shuvro = new User("shuvro");
+        UserMediator userMediator = new UserMediator();
+
+        IssueManager issueManager = new IssueManager(userMediator);
+        CommandManager commandManager = new CommandManager();
+
+        Issue issue1 = new Issue("button round", "round the buttons", PRIORITY.LOW, STATUS.OPEN, shuvro);
+
+        ICommand create_first_issue = new IssueCreateCommand(issueManager, issue1);
+        commandManager.ExecuteCommand(create_first_issue);
+        
+        ICommand changeStatusCommand = new UpdateIssueStatusCommand(issueManager, issue1, STATUS.CLOSED);
+        commandManager.ExecuteCommand(changeStatusCommand);
+        
+        Assert.Equal(STATUS.CLOSED, issue1.status);
+        
+        commandManager.UndoCommand();
+        
+        Assert.Equal(STATUS.OPEN, issue1.status);
+        
+        commandManager.RedoCommand();
+        
+        Assert.Equal(STATUS.CLOSED, issue1.status);
+    }
+}
