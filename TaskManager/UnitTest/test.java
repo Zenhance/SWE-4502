eea244a1bfa20444a39bd02e904776fe2e434d6f
Nafisa Kibria <nafisa21@iut-dhaka.edu>
2025-03-21 14:51:21 +0600

    #F03: Change Status Test

--- TaskManager/UnitTest/test.java
+++ TaskManager/UnitTest/test.java
@@ -6,21 +6,31 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class test {
     private IssueRepository repository;
-    private Command c;
+    private Command c1;
+    private ChangeStatus c2;
     private Issuenotifer notifier;
 
     @BeforeEach
     void setUp() {
         repository = new IssueRepository();
-        c = new CreateIssue(repository, "Bug Fix", "Fix login   issue", Priority.High);
+        c1 = new CreateIssue(repository, "Bug Fix", "Fix login   issue", Priority.High);
         notifier = new Issuenotifer();
+
     }
 
     @Test
     void testCreateIssue() {
-        c.execute();
 
+        c1.execute();
         assertNotNull(repository.getIssue(1), "Issue should be created.");
         assertEquals(1, repository.getIssueCount(), "Issue count should be 1.");
+    }
+
+
+    @Test
+    void ChangeStausTest()
+    {
+
+
     }
 }
