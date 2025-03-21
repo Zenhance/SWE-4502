fe40152670a7b5c9b9844f4ad9e68889202255ea
Ishmaam Iftekhar Khan <ishmaam@iut-dhaka.edu>
2025-03-21 11:12:22 +0600

    #F01 Simple Issue Done

---
+++ TaskManager/210042125_LabFinal/src/Issue.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class Issue {
+
+    private String Id;
+    private String Title;
+    private String Description;
+    private Priority Priority;
+    private Status Status;
+    private Date CreationDate;
+    private Date LastModifiedDate;
+    private String AssignedTo;
+    private String Tags[];
+    private Comment Comments[];
+}
