e5d01cffa863cd53f83aad83a8c7d2e2d9a7fe4b
Shefayat E- Shams Adib <shefayatadib@iut-dhaka.edu>
2025-03-21 14:43:34 +0600

    Issue #F01: CreateIssue_ShouldAddIssueToRepositoryTest passed

--- TaskManager/210042141_TaskManager/UnitTest/src/CreateIssue_ShouldAddIssueToRepository.java
+++ TaskManager/210042141_TaskManager/UnitTest/src/CreateIssue_ShouldAddIssueToRepository.java
@@ -1,7 +1,22 @@
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 public class CreateIssue_ShouldAddIssueToRepository {
 
     @Test
-    public void createIssueTest(){
+    public void CreateIssue_ShouldAddIssueToRepositoryTest() {
+        IssueRepository repository = new IssueRepository();
+        Issue issue = new Issue(1, "Issue 1", "Description", "High", "Open", new Date(), new Date(), "User1", null, null);
+        Command createIssueCommand = new CreateIssueCommand(repository, issue);
+        CommandInvoker invoker = new CommandInvoker();
+        invoker.executeCommand(createIssueCommand);
 
+        assertNotNull(repository.getIssue(1));
+        assertEquals("High", repository.getIssue(1).getPriority());
+        assertEquals("Open", repository.getIssue(1).getStatus());
     }
 }
