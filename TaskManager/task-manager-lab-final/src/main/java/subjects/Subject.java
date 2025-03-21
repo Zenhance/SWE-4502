72381e978c14f3cf9a5d98aa427b715985b4342f
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2025-03-21 14:23:15 +0600

    In Subject: add new methods with parameter

--- TaskManager/task-manager-lab-final/src/main/java/subjects/Subject.java
+++ TaskManager/task-manager-lab-final/src/main/java/subjects/Subject.java
@@ -1,7 +1,11 @@
 package subjects;
 
+import commands.Command;
+import observers.Observer;
+
 public interface Subject {
-    void addObserver();
-    void removeObserver();
-    void notifyObserver();
+    void addObserver(Observer observer);
+    void removeObserver(Observer observer);
+    void notifyObservers(Command command);
+    void notifyObserversUndo(Command command);
 }
