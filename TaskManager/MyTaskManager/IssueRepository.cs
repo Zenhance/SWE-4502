5be5e336c06c89ea672675e329b6c4564bf7fd4b
whiteflags26 <whiteflags26@gmail.com>
2025-03-21 12:02:10 +0600

    feat(issue): implement IssueRepository class

---
+++ TaskManager/MyTaskManager/IssueRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTaskManager
+{
+    public class IssueRepository
+    {
+        public Dictionary<int, Issue> issues;
+        public int nextIssueId;
+        public int nextCommentId;
+
+        public IssueRepository()
+        {
+            issues = new Dictionary<int, Issue>();
+            nextIssueId = 1;
+            nextCommentId = 1;
+        }
+
+        public int GetNextIssueId()
+        {
+            return nextIssueId++;
+        }
+
+        public int GetNextCommentId()
+        {
+            return nextCommentId++;
+        }
+
+        public void AddIssue(Issue issue)
+        {
+            issues[issue.Id] = issue;
+        }
+
+        public Issue GetIssue(int id)
+        {
+            if (issues.ContainsKey(id)) return issues[id];
+            return null;
+        }
+
+        public void UpdateIssue(Issue issue)
+        {
+            if (issues.ContainsKey(issue.Id))
+            {
+                issues[issue.Id] = issue;
+            }
+        }
+
+        public Dictionary<int, Issue> GetAllIssues()
+        {
+            return issues;
+        }
+    }
+}
