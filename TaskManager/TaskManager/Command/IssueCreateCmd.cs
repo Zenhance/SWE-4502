12b0031c3d16fe3cf96150f11e33abd4a9be25a8
rakins <shahriar.rakin99@gmail.com>
2025-03-21 14:06:57 +0600

    issueCreatecmd and issueman created

---
+++ TaskManager/TaskManager/Command/IssueCreateCmd.cs
@@ -0,0 +1,15 @@
+﻿using TaskManagerOriginal.Model;
+
+namespace TaskManagerOriginal.Command;
+
+public class IssueCreateCmd : Icommand
+{
+    IssueMan issueManager;
+    private Issue _issue;
+    public IssueCreateCmd(IssueMan issueManager, Issue issue)
+    {
+        this.issueManager = issueManager;
+        this._issue = issue;
+    }
+    
+}
---
+++ TaskManager/TaskManager/IssueMan.cs
@@ -0,0 +1,6 @@
+﻿namespace TaskManagerOriginal;
+
+public class IssueMan
+{
+    
+}
