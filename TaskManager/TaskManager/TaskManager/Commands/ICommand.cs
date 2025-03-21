c0fcd0bb49d434f21022b552f429fb3f55264d24
Mohammad Istiaq Prodhan <istiaqprodhan02@gmail.com>
2025-03-21 11:40:05 +0600

    ICommand added

---
+++ TaskManager/TaskManager/TaskManager/Commands/ICommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Commands
+{
+        public interface ICommand
+        {
+            void Execute();
+            void Undo();
+            string Description { get; }
+            DateTime Timestamp { get; }
+            CommandType Type { get; }
+        }
+}
