e3b3159a8f7691725c5698bcc74e04f408112486
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-21 15:14:25 +0600

    #F08

---
+++ TaskManager/TaskManager_210042112/TaskManager_112/TaskManagerTests/CommandLoggingTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager_112;
+
+namespace TaskManagerTests
+{
+    public class CommandLoggingTest
+    {
+        [Fact]
+        public void Logger_ShouldStoreCommandHistoryLogs()
+        {
+            var repo = new IssueRepository();
+            var issue = IssueFactory.CreateIssue(1, "TT", "...", Priority.Medium);
+            var createIssueCommand = new CreateIssueCommand(repo, issue);
+            var invoker = new CommandInvoker();
+            var logger = new CommandLogger();
+
+            // Act
+            invoker.ExecuteCommand(createIssueCommand);
+            logger.AddLogs("issue created but at what cost");
+            
+
+        }
+    }
+}
