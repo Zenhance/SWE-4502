d7d3784ceffa6a8f79fd313016245e87b5c3eaaa
Ayonspoiler <ayon4052@gmail.com>
2025-03-21 12:31:59 +0600

    Redo Command Implemented with Test

---
+++ TaskManager/Lab_final_210042166/Unit_Test/Redo_Command_Test/RedoCommandTest.java
@@ -0,0 +1,25 @@
+package Lab_final_210042166.Unit_Test.Redo_Command_Test;
+import Lab_final_210042166.Task_Management_System.Create_Issue.CreateIssueCommand;
+import Lab_final_210042166.Task_Management_System.Enum.Priority;
+import Lab_final_210042166.Task_Management_System.Interface.Command;
+import Lab_final_210042166.Task_Management_System.Issue.Issue;
+import Lab_final_210042166.Task_Management_System.Issue_Repository.IssueRepository;
+import Lab_final_210042166.Task_Management_System.Manage_Command.CommandManager;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class RedoCommandTest {
+    @Test
+    public void RedoCommand_ShouldReapplyUndoneAction() {
+        IssueRepository repository = IssueRepository.getInstance();
+        Issue issue = new Issue("1", "Test Issue", "Description", Priority.MEDIUM, "User1");
+        Command createCommand = new CreateIssueCommand(issue, repository);
+        CommandManager manager = new CommandManager();
+
+        manager.executeCommand(createCommand);
+        manager.undo();
+        manager.redo();
+
+        assertNotNull(repository.getIssue("1"));
+    }
+
+}
