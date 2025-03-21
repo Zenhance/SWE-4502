67cc2dcf9e876d5db10a3d3998e094c43cc5c0b4
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2025-03-21 14:54:30 +0600

    Modify Comment: add getter setters

--- TaskManager/task-manager-lab-final/src/main/java/models/Comment.java
+++ TaskManager/task-manager-lab-final/src/main/java/models/Comment.java
@@ -1,10 +1,36 @@
 package models;
 
 import java.security.Timestamp;
+import java.time.LocalDateTime;
 
 public class Comment {
     private String id;
     private String content;
     private String author;
-    private Timestamp createdAt;
+    private LocalDateTime createdAt;
+
+    public String getId(){
+        return id;
+    }
+    public String getContent(){
+        return content;
+    }
+    public String getAuthor(){
+        return author;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setId(String id){
+        this.id = id;
+    }
+    public  void setContent(String content){
+        this.content = content;
+    }
+    public  void setAuthor(String author){
+        this.author = author;
+    }
+    public void setCreatedAt(LocalDateTime createdAt){
+        this.createdAt = createdAt;
+    }
 }
