84fb7b22e458f76f5b69ed33f1f5f33444aa701b
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-21 12:00:18 +0600

    base issue class

---
+++ TaskManager/TaskManager_210042112/TaskManager_112/TaskManager_112/Issue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager_112
+{
+    public class Issue
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Priority { get; set; }
+        public string Status { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+        public string AssignedTo { get; set; }
+        public List<string> Tags { get; set; } //cause tags can be multiple
+        public List<string> Comments { get; set; } //cause comments can be multiple
+        public Issue(int id,string title, string description, List<string> tags)
+        {
+            ID = id;
+            Title = title;
+            Description = description;
+            CreationDate = DateTime.Now;
+            LastModifiedDate = DateTime.Now;
+            Tags = tags.ToList();    
+            Comments = new List<string>();
+
+        }
+
+    }
+}
