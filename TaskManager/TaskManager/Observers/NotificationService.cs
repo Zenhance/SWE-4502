b9c75fafd0a8de2c2d469eb7587650c19f5ac894
Tauhidur Rahman Tauhid <tauhid04023@gmail.com>
2025-03-21 14:31:40 +0600

    NotificationService.cs created for trigger notification

---
+++ TaskManager/TaskManager/Observers/NotificationService.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManager.Observers;
+
+public class NotificationService : IObserver
+{
+    public void Update(string message)
+    {
+      Console.WriteLine($"Notification: {message}");  
+    }
+    
+}
