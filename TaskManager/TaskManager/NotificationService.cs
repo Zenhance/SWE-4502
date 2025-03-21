52286a3b68b6aded65cdd8fe514b7cff35cec0c0
bhuiyan.minhajul@gmail.com <bhuiyan.minhajul@gmail.com>
2025-03-21 16:00:18 +0600

    Issue #F06: Observer Notification Test

---
+++ TaskManager/TaskManager/NotificationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager
+{
+    public class NotificationService
+    {
+        public void Notify(string message)
+        {
+            Console.WriteLine("Notification sent: " + message);
+        }
+    }
+}
---
+++ TaskManager/TaskManager_Tests/Observer_ShouldBeNotifiedOfCommands.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using TaskManager;
+
+namespace TaskManager_Tests
+{
+    [TestClass]
+    public class Observer_ShouldBeNotifiedOfCommands
+    {
+        [TestMethod]
+        public void Observer_ShouldBeNotified_Test_Method()
+        {
+            IssueRepository repo = new IssueRepository();
+            Issue issue = new Issue(1, "Test Issue", "Test Description", Priority.Medium);
+            CreateIssueCommand createIssueCommand = new CreateIssueCommand(repo, issue);
+            CommandManager commandManager = new CommandManager();
+            NotificationService notificationService = new NotificationService();
+
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                commandManager.ExecuteCommand(createIssueCommand);
+                notificationService.Notify("Issue created: " + issue.Title);
+
+                string output = sw.ToString();
+                StringAssert.Contains(output, "Notification sent: Issue created: Test Issue");
+            }
+        }
+    }
+}
