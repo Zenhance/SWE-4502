8dbfa25d35059e31e02c2d9c417193f2db01f5ff
rakins <shahriar.rakin99@gmail.com>
2025-03-21 14:46:49 +0600

    IssueDescriptionUpdateCmd class with constructor created

---
+++ TaskManager/TaskManager/Command/IssueDescriptionUpdateCmd.cs
@@ -0,0 +1,19 @@
+﻿using TaskManagerOriginal.Model;
+
+namespace TaskManagerOriginal.Command;
+
+public class IssueDescriptionUpdateCmd
+{
+    private IssueMan _issueManager;
+    private Issue _issue;
+    private string _description;
+    private string _oldDescription;
+    
+    public IssueDescriptionUpdateCmd(IssueMan issueManager, Issue issue, string description)
+    {
+        this._issueManager = issueManager;
+        this._issue = issue;
+        this._description = description;
+        this._oldDescription = issue.description;
+    }
+}
