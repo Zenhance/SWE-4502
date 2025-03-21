19d37c57930fe2aaa6c9373ccf89b4adee00df80
IQRA109 <127471680+IQRA109@users.noreply.github.com>
2025-03-21 13:57:39 +0600

    Add files via upload

---
+++ TaskManager/Create IssueCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class CreateIssueCommand : ICommand
+{
+   
+        private IssueRepository _repository;
+        private Issue _issue;
+
+        public CreateIssueCommand(IssueRepository repository, Issue issue)
+        {
+            _repository = repository;
+            _issue = issue;
+        }
+
+        public void Execute() => _repository.AddIssue(_issue);
+        public void Undo() => _repository.GetIssue(_issue.Id)?.Comments.Clear();
+    
+}
