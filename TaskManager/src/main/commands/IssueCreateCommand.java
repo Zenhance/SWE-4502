ed76c778dcf6cbe106ebfb89eef28af4ada78162
Hasin Mahtab <112191602+hasin023@users.noreply.github.com>
2025-03-21 14:37:08 +0600

    issue repository and createissuecommand

--- TaskManager/src/main/commands/IssueCreateCommand.java
+++ TaskManager/src/main/commands/IssueCreateCommand.java
@@ -1,30 +1,32 @@
 package main.commands;
 
 import main.core.Issue;
+import main.features.IssueRepository;
 
 public class IssueCreateCommand extends Command{
     private Issue issue;
     private String title;
     private String description;
     private String assignedTo;
-    private CommandHistory commandHistory;
+    private IssueRepository issueRepository;
 
     public IssueCreateCommand(String _title, String _description, String _assignedTo) {
         super();
         this.title = _title;
         this.description = _description;
         this.assignedTo = _assignedTo;
-        this.commandHistory = new CommandHistory();
+        this.issueRepository = new IssueRepository();
     }
 
     @Override
     public void execute() {
         issue = new Issue(title, description, assignedTo);
-        commandHistory.PushToMainStack(issue);
+        issueRepository.AddIssueToRepository(issue);
     }
 
     @Override
     public void undo() {
-        
+        String issueId = issue.GetId();
+        issueRepository.RemoveIssueFromRepository(issueId);
     }
 }
--- TaskManager/src/main/features/IssueRepository.java
+++ TaskManager/src/main/features/IssueRepository.java
@@ -1,5 +1,35 @@
 package main.features;
 
+import main.core.Issue;
+import java.util.HashMap;
+
 public class IssueRepository {
-    
+    HashMap<String, Issue> issueHashMap;
+
+    public IssueRepository() {
+        this.issueHashMap = new HashMap<String, Issue>();
+    }
+
+    public void AddIssueToRepository(Issue i) {
+        String issueId = i.GetId();
+        issueHashMap.put(issueId, i);
+    }
+
+    public Issue GetIssueFromRepository(String issueId) {
+        issueHashMap.get(issueId);
+    }
+
+    public void RemoveIssueFromRepository(String issueId) {
+        issueHashMap.remove(issueId);
+    }
+
+    public void EmptyIssuesFromRepository() {
+        issueHashMap.clear();
+    }
+
+    public void ListAllIssuesInRepository() {
+        for (String issueId : issueHashMap.values()) {
+            System.out.println("Issue ->" + issueId);
+        }
+    }
 }
