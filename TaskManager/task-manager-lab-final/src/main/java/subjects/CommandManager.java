2dcad7d4d66671d0531e197668afa5e14fe55dfe
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2025-03-21 12:11:53 +0600

    Add a Concrete Subject Class

---
+++ TaskManager/task-manager-lab-final/src/main/java/subjects/CommandManager.java
@@ -0,0 +1,19 @@
+package subjects;
+
+public class CommandManager implements Subject{
+    @Override
+    public void addObserver() {
+
+    }
+
+    @Override
+    public void removeObserver() {
+
+    }
+
+    @Override
+    public void notifyObserver() {
+
+    }
+
+}
