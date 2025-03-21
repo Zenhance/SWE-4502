fb98bbc2e652a918edca8eba533efe7a3f482932
rakins <shahriar.rakin99@gmail.com>
2025-03-21 14:51:41 +0600

    IssuePriorityUpdate class done with constructor and functions

--- TaskManager/TaskManager/Command/IssuePriorityUpdate.cs
+++ TaskManager/TaskManager/Command/IssuePriorityUpdate.cs
@@ -1,6 +1,31 @@
-﻿namespace TaskManagerOriginal.Command;
+﻿using TaskManager.ENUM;
+using TaskManagerOriginal.Model;
+
+namespace TaskManagerOriginal.Command;
 
 public class IssuePriorityUpdate
 {
+    IssueMan _issueManager;
+    Issue _issue;
+    private PRIORITY _priority;
+    private PRIORITY _oldPriority;
+    
+    public IssuePriorityUpdate(IssueMan issueManager, Issue issue, PRIORITY priority)
+    {
+        this._issueManager = issueManager;
+        this._issue = issue;
+        this._priority = priority;
+        this._oldPriority = issue.priority;
+    }
+    
+    public void Execute()
+    {
+        this._issueManager.UpdateIssuePriority(this._issue, this._priority);
+    }
+    
+    public void Undo()
+    {
+        this._issueManager.UpdateIssuePriority(this._issue, this._oldPriority);
+    }
     
 }
