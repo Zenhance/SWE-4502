4965c38e9a1687819c0a29b685149473a9be28bc
Kashshaf-Labib <labibkashshaf@gmail.com>
2025-03-21 12:21:10 +0600

    feat(CreateIssueCommand): implement functionalities of create issue command (issue: #F01)

--- TaskManager/TaskManagerSolution/src/CreateIssueCommand.java
+++ TaskManager/TaskManagerSolution/src/CreateIssueCommand.java
@@ -1,12 +1,31 @@
 public class CreateIssueCommand extends Command{
+    private final IssueRepository repository;
+
+    private final Issue issue;
+
+    private boolean isExecuted= false;
+
+    public CreateIssueCommand(IssueRepository repository, Issue issue) {
+        super("Created issue: " + issue.getTitle(), CommandType.CREATE);
+        this.repository = repository;
+        this.issue = issue;
+    }
 
     @Override
     public void execute() {
-
+        repository.addIssue(issue);
+        isExecuted = true;
     }
 
     @Override
     public void undo() {
+        if (isExecuted) {
+            repository.removeIssue(issue.getId());
+            isExecuted = false;
+        }
+    }
 
+    public Issue getIssue() {
+        return issue;
     }
 }
