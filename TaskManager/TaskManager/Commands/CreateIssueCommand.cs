93329ff8dc8fb7dcb0a24611cc09e55060ef04f2
Tauhidur Rahman Tauhid <tauhid04023@gmail.com>
2025-03-21 11:56:40 +0600

    CreateIssueCommand.cs created

---
+++ TaskManager/TaskManager/Commands/CreateIssueCommand.cs
@@ -0,0 +1,29 @@
+﻿using TaskManager.Models;
+using TaskManager.Repositories;
+
+namespace TaskManager.Commands;
+
+public class CreateIssueCommand : Command 
+{
+    private readonly IssueRepository _repository;
+    private readonly Issue _issue;
+
+    public CreateIssueCommand(IssueRepository repository, Issue issue)
+    {
+        _repository = repository;
+        _issue = issue;
+        Description=$"Created issue with title: {issue.Title}";
+        Timestamp=DateTime.Now;
+        Type=CommandType.Create;
+    }
+
+    public override void Execute()
+    {
+        _repository.AddIssue(_issue);
+    }
+
+    public override void Undo()
+    {
+        
+    }
+}
