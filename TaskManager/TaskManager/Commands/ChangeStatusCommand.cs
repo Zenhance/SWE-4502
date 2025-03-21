6e17e8fc34baff27f6ce68105e9f50bc49f5086d
Tauhidur Rahman Tauhid <tauhid04023@gmail.com>
2025-03-21 14:08:49 +0600

    ChangeStatusCommand.cs and CommandManager.cs created

---
+++ TaskManager/TaskManager/Commands/ChangeStatusCommand.cs
@@ -0,0 +1,45 @@
+﻿using TaskManager.Models;
+using TaskManager.Repositories;
+namespace TaskManager.Commands;
+
+public class ChangeStatusCommand:Command
+{
+    private readonly IssueRepository _repository;
+    private readonly int _issueId;
+    private readonly Status _newStatus;
+    private Status _previousStatus;
+
+    public ChangeStatusCommand(IssueRepository repository, int issueId, Status newStatus)
+    {
+        _repository = repository;
+        _issueId = issueId;
+        _newStatus = newStatus;
+        Description=$"Changed status of issue with ID: {issueId} to {newStatus}";
+        Timestamp=DateTime.Now;
+        Type=CommandType.Update;
+    }
+
+    public override void Undo()
+    {
+       var issue = _repository.GetIssue(_issueId);
+       if (issue != null)
+       {
+           issue.Status = _previousStatus;
+           _repository.UpdateIssue(issue);
+       }
+        
+    }
+
+    public override void Execute()
+    {
+       var issue = _repository.GetIssue(_issueId);
+       if (issue != null)
+       {    
+           _previousStatus = issue.Status;
+           issue.Status = _newStatus;
+           _repository.UpdateIssue(issue);
+       }
+    }
+    
+    
+}
---
+++ TaskManager/TaskManager/Commands/CommandManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+namespace TaskManager.Commands;
+
+public class CommandManager
+{
+    private readonly Stack<Command> _undoStack = new Stack<Command>();
+    private readonly Stack<Command> _redoStack = new Stack<Command>();
+
+    public void ExecuteCommand(Command command)
+    {     command.Execute();
+        _undoStack.Push(command);
+        _redoStack.Clear();
+    }
+    
+    public void Redo()
+    {
+        if (_redoStack.Count > 0)
+        {
+            var command = _redoStack.Pop();
+            command.Execute();
+            _undoStack.Push(command);
+        }
+    }
+}
