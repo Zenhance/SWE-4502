6a09b4b599110ab03e17cb8cb24cf121685a12bf
AbuNomanHaiderKanon <abunomanhaiderkanon@gmail.com>
2025-03-21 13:56:44 +0600

    added dummy test as asked

---
+++ src/test/java/models/Priority_dummy_test.java
@@ -0,0 +1,16 @@
+package models;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+class Priority_dummy_test {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
