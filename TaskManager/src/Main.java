75fa53c575c5f0168064011599317624a7bd43dc
RIFAF RAHMAN <127125526+MathaThikNai@users.noreply.github.com>
2025-03-21 15:21:59 +0600

    .

--- TaskManager/src/Main.java
+++ TaskManager/src/Main.java
@@ -7,23 +7,23 @@ public class Main {
         StatisticsCollector statistics = new StatisticsCollector();
 
 
-        Issue issue1 = new Issue("210042118", "Fix Bug", "Fix the login bug", Priority.HIGH, "Alice", Arrays.asList("bug", "login"));
+        Issue issue1 = new Issue("1", "Fix Bug", "Fix the login bug", Priority.HIGH, "Alice", Arrays.asList("bug", "login"));
         Command createCommand = new Create_Issue(repository, issue1);
         createCommand.execute();
         logger.log("Create", "Created issue with ID 210042118");
         statistics.incrementCommandCount("Create");
 
 
-        Issue updatedIssue = new Issue("210042118", "Fix Bug", "Fix the login bug and improve performance", Priority.CRITICAL, "Alice", Arrays.asList("bug", "login"));
+        Issue updatedIssue = new Issue("1", "Fix Bug", "Fix the login bug and improve performance", Priority.CRITICAL, "Alice", Arrays.asList("bug", "login"));
         Command updateCommand = new Update_Issue(repository, "1", updatedIssue);
         updateCommand.execute();
-        logger.log("Update", "Updated issue with ID 210042118");
+        logger.log("Update", "Updated issue with ID 1");
         statistics.incrementCommandCount("Update");
 
 
         Command changeStatusCommand = new Change_Issue_Status(repository, "1", Status.IN_PROGRESS);
         changeStatusCommand.execute();
-        logger.log("Update", "Changed status of issue with ID 210042118 to IN_PROGRESS");
+        logger.log("Update", "Changed status of issue with ID 1 to IN_PROGRESS");
         statistics.incrementCommandCount("Update");
 
 
