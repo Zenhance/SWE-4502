57faf89d70cbac1428624922b58e2c4e2ded480b
Shefayat E- Shams Adib <shefayatadib@iut-dhaka.edu>
2025-03-21 14:27:37 +0600

    Command Invoker updated

--- TaskManager/210042141_TaskManager/src/CommandInvoker.java
+++ TaskManager/210042141_TaskManager/src/CommandInvoker.java
@@ -1,12 +1,28 @@
+import java.util.Stack;
+
 public class CommandInvoker {
+    private Stack<Command> commandHistory = new Stack<>();
+    private Stack<Command> redoHistory = new Stack<>();
 
-
-    public void undo(){
-
+    public void executeCommand(Command command) {
+        command.execute();
+        commandHistory.push(command);
+        redoHistory.clear();
     }
 
-    public void redo(){
-
+    public void undo() {
+        if (!commandHistory.isEmpty()) {
+            Command command = commandHistory.pop();
+            command.undo();
+            redoHistory.push(command);
+        }
     }
 
+    public void redo() {
+        if (!redoHistory.isEmpty()) {
+            Command command = redoHistory.pop();
+            command.execute();
+            commandHistory.push(command);
+        }
+    }
 }
