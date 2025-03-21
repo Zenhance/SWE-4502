2e027df7aed3b6371fa2a42eaeec394eee79a301
Nahiyan3 <Nahiyan49@gmail.com>
2025-03-21 14:20:34 +0600

    Class structure of AddCommentCommand

--- Lab Final/TaskManager/src/AddCommentCommand.java
+++ Lab Final/TaskManager/src/AddCommentCommand.java
@@ -1,1 +1,31 @@
-public class 
+public class AddCommentCommand implements Command{
+    private IssueService issueService;
+    private int issueId;
+    private String content;
+    private String author;
+    private Comment addedComment;
+
+    public AddCommentCommand(IssueService issueService, int issueId, String content, String author) {
+        this.issueService = issueService;
+        this.issueId = issueId;
+        this.content = content;
+        this.author = author;
+    }
+
+    @Override
+    public void execute() {
+        
+
+    }
+
+    @Override
+    public void undo() {
+
+    }
+
+    public Comment getAddedComment() {
+        return addedComment;
+    }
+}
+
+
