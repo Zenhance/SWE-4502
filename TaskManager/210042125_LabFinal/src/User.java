c692c55cc67944829b62ed20666d1579213828e0
Ishmaam Iftekhar Khan <ishmaam@iut-dhaka.edu>
2025-03-21 12:24:19 +0600

    Update User.java

--- TaskManager/210042125_LabFinal/src/User.java
+++ TaskManager/210042125_LabFinal/src/User.java
@@ -1,6 +1,10 @@
 public class User {
     private String name;
 
+    public void subscribe(){
+
+    }
+
     public void receiveNotification(String s){
         System.out.println("Issue changes: "+s);
     }
