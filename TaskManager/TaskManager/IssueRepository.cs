52097c858bd91533c09a4f065d01a1c7dd25f72d
sakibhossain323 <sakibhossain323@gmail.com>
2025-03-21 12:12:00 +0600

    #F01 : implement issue repository

---
+++ TaskManager/TaskManager/IssueRepository.cs
@@ -0,0 +1,22 @@
+﻿using TaskManager.Domain;
+
+namespace TaskManager;
+
+public class IssueRepository
+{
+    private int _id = 0;
+    private HashSet<Issue> _issues = new HashSet<Issue>();
+
+    public Issue Create(Issue issue)
+    {
+        _id++;
+        issue.Id = _id;
+        _issues.Add(issue);
+        return issue;
+    }
+
+    public Issue? GetById(int id)
+    {
+        return _issues.Where(i => i.Id == id).First();
+    }
+}
