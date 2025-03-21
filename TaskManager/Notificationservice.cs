88f4ec4c4b66c51870527b03878893698f5a34be
IQRA109 <127471680+IQRA109@users.noreply.github.com>
2025-03-21 14:08:27 +0600

    NotificationService class created

---
+++ TaskManager/Notificationservice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class NotificationService
+{
+    private List<IObserver> observers = new();
+
+    public void Subscribe(IObserver observer) => observers.Add(observer);
+    public void Unsubscribe(IObserver observer) => observers.Remove(observer);
+    public void Notify(string message)
+    {
+        foreach (var observer in observers)
+            observer.Update(message);
+    }
+}
