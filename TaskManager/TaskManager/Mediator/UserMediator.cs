048e3e6181bcbd4bbac496188d3ff3f244df65ff
rarefox21 <abrarmubeen@iut-dhaka.edu>
2025-03-21 14:54:00 +0600

    User Mediator Created

---
+++ TaskManager/TaskManager/Mediator/UserMediator.cs
@@ -0,0 +1,37 @@
+﻿//User Mediator
+using TaskManager.Model;
+
+namespace TaskManager.Mediator;
+
+public class UserMediator
+{
+    public List<User> users;
+    
+    public UserMediator()
+    {
+        users = new List<User>();
+    }
+    
+    public void AddUser(User user)
+    {
+        if(users.Contains(user))
+        {
+            return;
+        }
+        
+        users.Add(user);
+    }
+    
+    public void RemoveUser(User user)
+    {
+        users.Remove(user);
+    }
+    
+    public void NotifyUsers(string message)
+    {
+        foreach (User user in users)
+        {
+            user.ReceiveEmail(message);
+        }
+    }
+}
