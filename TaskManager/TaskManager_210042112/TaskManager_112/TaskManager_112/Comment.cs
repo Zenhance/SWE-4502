1ab5331c415948e64680acf488141ae943de3b43
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-21 12:20:08 +0600

    comment class

---
+++ TaskManager/TaskManager_210042112/TaskManager_112/TaskManager_112/Comment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager_112
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public Comment(int id, string content, string author)
+        {
+            Id = id;
+            Content = content;
+            Author = author;
+            CreatedAt = DateTime.Now;
+
+        }
+    }
+}
