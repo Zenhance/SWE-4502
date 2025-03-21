f3521fa627072927b03838c220639750fba0724b
sheona001 <sirexcc001@gmail.com>
2025-03-21 14:42:05 +0600

    created email observer

---
+++ taskManager/labfinal_210042111/EmailObserver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace labfinal_210042111
+{
+    public class ConsoleObserver:IObserver
+    {
+        public List<string> notifications = new List<string>();
+
+        public void Update(string message)
+        {
+            Console.WriteLine("Observer Received: " + message);
+            notifications.Add(message);
+        }
+
+
+    }
+}
