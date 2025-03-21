676e92b3aa56a7db8c81f8b45f48056fe0d22d20
Nafisa Kibria <nafisa21@iut-dhaka.edu>
2025-03-21 14:14:56 +0600

    Updated all the files to create a test case for CreateIssue

--- TaskManager/src/CreateIssue.java
+++ TaskManager/src/CreateIssue.java
@@ -1,13 +1,23 @@
-public class CreateIssue implements Command{
-    private Receiver r;
-    IssueRepository r1;
-    public CreateIssue(Receiver r)
-    {
-        this.r=r;
-
+class CreateIssue implements Command {
+    public IssueRepository repository;
+    private final Issue issue;
+    public CreateIssue(IssueRepository r, String title, String desc, Priority
+            priority) {
+        this.repository = r;
+        this.issue = new Issue(r.getIssueCount() + 1, title, desc, priority);
     }
-    @Override
+
     public void execute() {
-        r.CreateIssue(r1);
+        repository.addIssue(issue);
+        System.out.println("Created: " + issue);
+    }
+
+    public void undo() {
+        repository.removeIssue(issue.getId());
+        System.out.println("Undo Create: Removed issue " + issue.getId());
+    }
+
+    public String getDescription() {
+        return "Create Issue: " + issue.getId();
     }
 }
--- TaskManager/src/Issue.java
+++ TaskManager/src/Issue.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class Issue implements  Receiver {
-    public int Id=0;
+public class Issue  {
+    public int Id;
     public List<Comment> commentList =new ArrayList<>();
     public String title;
     public String description;
@@ -19,47 +19,76 @@ public class Issue implements  Receiver {
 
 
 
-
-
-
-    @Override
-    public void CreateIssue(IssueRepository r) {
-
-       r.hm1.put(Id,new Issue());
-       Id++;
-       System.out.println("Issue " + Id +"has been created");
+    public Issue(int id, String title, String description, Priority priority) {
+        this.Id = id;
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.status = Status.Open;
+        this.CreationDate = new Date();
+        this.LastModifiedDate = CreationDate;
     }
 
-    @Override
-    public void UpdateIssue(String description) {
-        this.description=description;
 
+    public void UpdateIssue(String title, String description, Priority p) {
+        this.title = title;
+        this.description = description;
+        this.priority = p;
+        this.LastModifiedDate = new Date();
+    }
+    public void changeStatus(Status status) {
+        this.status = status;
+        this.LastModifiedDate = new Date();
+    }
+    public void addComment(Comment comment) {
+        commentList.add(comment);
+        this.LastModifiedDate = new Date();
     }
 
-    @Override
-    public void ChangeStatus(Priority p) {
-        this.priority=p;
+    public Status getStatus()
+    { return status; }
 
-    }
 
-    @Override
-    public void Addcomments(Comment c) {
-        commentList.add(c);
 
-    }
 
-    @Override
-    public void LogOperation() {
+//    @Override
+//    public void CreateIssue(IssueRepository r) {
+//
+//       r.hm1.put(Id,new Issue());
+//       Id++;
+//       System.out.println("Issue " + Id +"has been created");
+//    }
+//
+//
+//
+//    @Override
+//    public void ChangeStatus(Priority p) {
+//        this.priority=p;
+//
+//    }
+//
+//    @Override
+//    public void Addcomments(Comment c) {
+//        commentList.add(c);
+//
+//    }
+//
+//    @Override
+//    public void LogOperation() {
+//
+//    }
+//
+//    @Override
+//    public void TriggerNotification() {
+//
+//    }
+//
+//    @Override
+//    public void StatisticController() {
+//
+//    }
 
-    }
-
-    @Override
-    public void TriggerNotification() {
-
-    }
-
-    @Override
-    public void StatisticController() {
-
+    public Integer getId() {
+        return Id;
     }
 }
