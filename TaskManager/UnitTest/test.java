5c50d9074b283fd8e19960f4c9ecbd719582363a
Nafisa Kibria <nafisa21@iut-dhaka.edu>
2025-03-21 14:15:21 +0600

    #F01: Create Issue Test

--- TaskManager/UnitTest/test.java
+++ TaskManager/UnitTest/test.java
@@ -1,9 +1,26 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class test {
-    @Test
-    public void test()
-    {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+public class test {
+    private IssueRepository repository;
+    private Command c;
+    private Issuenotifer notifier;
+
+    @BeforeEach
+    void setUp() {
+        repository = new IssueRepository();
+        c = new CreateIssue(repository, "Bug Fix", "Fix login   issue", Priority.High);
+        notifier = new Issuenotifer();
+    }
+
+    @Test
+    void testCreateIssue() {
+        c.execute();
+
+        assertNotNull(repository.getIssue(1), "Issue should be created.");
+        assertEquals(1, repository.getIssueCount(), "Issue count should be 1.");
     }
 }
