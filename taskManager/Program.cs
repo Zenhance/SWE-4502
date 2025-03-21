3530189e18db5c9f98c52e69026c83ba1b2b5699
kmsquare27 <113410233+kmsquare27@users.noreply.github.com>
2025-03-21 15:34:52 +0600

    Add files via upload

---
+++ taskManager/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taskManager
+{
+
+    public enum type
+    {
+        Create,
+        Update,
+        Delete
+    }
+   
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
+
+
---
+++ taskManager/issue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taskManager
+{
+    public enum priority
+    {
+        Low,
+        Medium,
+        High,
+        Crtitical
+    }
+
+    public enum status
+    {
+        Open,
+        Inprogress,
+        UnderReview,
+        Resolved,
+        Closed
+    }
+    public class issue 
+    {
+        public int id;
+        public string title;
+        public string description;
+        public enum priority { Low, Medium, High, Crtitical }
+        public enum status { Open, Inprogress, UnderReview, Resolved, Closed }
+        public DateTime creation_date;
+        public DateTime last_modified_date;
+        public string assignedto { get; set; }
+        List<string> tags = new List<string> { };
+        List<object> Icomments = new List<object> { };
+
+        public issue(string title, string description, Enum priority)
+        {
+            this.title = title;
+            this.description = description;
+        }
+
+        
+    }
+}
