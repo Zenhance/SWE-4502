208325cf9020fc498b0c5a977d65e69e6b46de33
mdhralif <hasiburrahman999.alif@gmail.com>
2025-03-21 13:46:57 +0600

    AbstractCommand Class Created.

---
+++ TaskManager/LabFinal/src/command/AbstractCommand.java
@@ -0,0 +1,21 @@
+package command;
+import java.time.LocalDateTime;
+
+public abstract class AbstractCommand implements Command {
+    private final LocalDateTime timestamp;
+    private final CommandType type;
+
+    public AbstractCommand(CommandType type) {
+        this.timestamp = LocalDateTime.now();
+        this.type = type;
+    }
+    @Override
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+    @Override
+    public CommandType getType() {
+        return type;
+    }
+
+}
