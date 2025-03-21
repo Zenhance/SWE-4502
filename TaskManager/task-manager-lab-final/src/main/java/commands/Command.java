00a9bab4ef0190808f4ec4337b7b8eb47ba441f0
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2025-03-21 13:51:05 +0600

    Modify Command Class: Add attribute and getters

--- TaskManager/task-manager-lab-final/src/main/java/commands/Command.java
+++ TaskManager/task-manager-lab-final/src/main/java/commands/Command.java
@@ -2,16 +2,32 @@ package commands;
 
 import models.CommandType;
 
+import java.security.Timestamp;
+
 public abstract class Command {
     String commandEntry;
+    String description;
     CommandType commandType;
-    abstract void execute();
+    protected Timestamp timestamp;
+    abstract public void execute();
 
-    abstract void undo();
+    abstract public void undo();
 
-    Command(String issue, CommandType type){
+    public Command(String issue, CommandType type){
         this.commandEntry = issue;
         this.commandType = type;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public CommandType getType() {
+        return commandType;
     }
     
 }
