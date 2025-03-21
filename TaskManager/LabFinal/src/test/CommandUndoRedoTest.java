6cc8446ab2512dde990492adad4ae87952b39f83
mdhralif <hasiburrahman999.alif@gmail.com>
2025-03-21 15:14:08 +0600

    #F04: Undo Command Testing.

---
+++ TaskManager/LabFinal/src/test/CommandUndoRedoTest.java
@@ -0,0 +1,34 @@
+package test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import taskmanager.TaskManager;
+import model.Issue;
+import model.Priority;
+import model.Status;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CommandUndoRedoTest {
+    private TaskManager taskManager;
+    @BeforeEach
+    void setUp()
+    {
+        taskManager = new TaskManager();
+    }
+
+    //#F04:
+    @Test
+    void undoCommand_ShouldRevertLastAction() {
+
+        String issueId = taskManager.createIssue("Undo Command Testing", "Pleaseeeeeeee NO!", Priority.High);
+
+        taskManager.changeStatus(issueId, Status.InProgress);
+        Issue afterCommand = taskManager.getIssue(issueId);
+        boolean undoResult = taskManager.undo();
+
+        assertTrue(undoResult);
+        Issue afterUndo = taskManager.getIssue(issueId);
+        assertEquals(Status.InProgress, afterCommand.getStatus());
+        assertEquals(Status.Open, afterUndo.getStatus());
+    }
+
+}
