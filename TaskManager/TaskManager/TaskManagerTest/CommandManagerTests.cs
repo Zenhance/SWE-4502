80d399263fd926bf86498f280bcdc4c5d6df2545
Mohammad Istiaq Prodhan <istiaqprodhan02@gmail.com>
2025-03-21 14:55:26 +0600

    #F05

--- TaskManager/TaskManager/TaskManagerTest/CommandManagerTests.cs
+++ TaskManager/TaskManager/TaskManagerTest/CommandManagerTests.cs
@@ -28,5 +28,28 @@ namespace TaskManager.Tests
             commandManager.Undo();
             Assert.Empty(repository.GetAll());
         }
+        //#F05
+        [Fact]
+        public void RedoCommand_ShouldReapplyUndoneAction()
+        {
+            var repository = new IssueRepository();
+            var commandManager = new CommandManager();
+            var issue = new Issue
+            {
+                Title = "Test Issue",
+                Description = "Test Description",
+                Priority = Priority.Medium
+            };
+            var createCommand = new CreateIssueCommand(repository, issue);
+
+            commandManager.ExecuteCommand(createCommand);
+            commandManager.Undo();
+            commandManager.Redo();
+
+            var retrievedIssue = repository.GetById(issue.Id);
+            Assert.Equal("Test Issue", retrievedIssue.Title);
+            Assert.Equal("Test Description", retrievedIssue.Description);
+            Assert.Equal(Priority.Medium, retrievedIssue.Priority);
+        }
     }
 }
