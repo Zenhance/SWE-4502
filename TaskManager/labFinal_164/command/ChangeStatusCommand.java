08694f1781d54ed78b1229b8181bc3f0b9e243a6
Ekramul Esham <112435061+EshaAam@users.noreply.github.com>
2025-03-21 15:25:23 +0600

    add comment tes

--- TaskManager/labFinal_164/command/ChangeStatusCommand.java
+++ TaskManager/labFinal_164/command/ChangeStatusCommand.java
@@ -1,4 +1,22 @@
 package labFinal_164.command;
 
+import labFinal_164.model.Status;
+import labFinal_164.repository.IssueRepository;
+
 public class ChangeStatusCommand {
+    private IssueRepository repository;
+    private String issueId;
+    private Status oldStatus;
+    private Status newStatus;
+    private String description;
+
+    public ChangeStatusCommand(IssueRepository repository, String issueId, Status newStatus) {
+        this.repository = repository;
+        this.issueId = issueId;
+        this.newStatus = newStatus;
+        this.oldStatus = repository.getIssue(issueId).getStatus();
+        this.description = "Change status of " + issueId + " from " + oldStatus + " to " + newStatus;
+    }
+
+
 }
