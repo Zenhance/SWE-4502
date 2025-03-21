98d35ae034357a8bc7f011c37ad88f0f1b1a2b28
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-21 14:56:15 +0600

    #F03 : change status test : passed...?

---
+++ TaskManager/TaskManager_210042112/TaskManager_112/TaskManagerTests/ChangeStatusTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager_112;
+
+namespace TaskManagerTests
+{
+    public class ChangeStatusTest
+    {
+        [Fact]
+        public void ChangeStatus_ShouldUpdateIssueStatus()
+        {
+            var repo = new IssueRepository();
+            var issue = IssueFactory.CreateIssue(12, "Blah blah", "Description is unnecessary", Priority.Critical);
+
+            var createissuecom = new CreateIssueCommand(repo, issue);
+
+            var invoke = new CommandInvoker();
+            invoke.ExecuteCommand(createissuecom);
+
+            //Act
+            var changestatus = new ChangeIssueStatusCommand(issue, Status.InProgress);
+            invoke.ExecuteCommand(changestatus);
+
+            //Assert
+            Assert.Equal(Status.InProgress, issue.Status);
+
+        }
+
+    }
+}
