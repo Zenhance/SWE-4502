e2376b318b4e006bb3e213b890fdba8ec6b9b72a
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-21 15:04:17 +0600

    #F04: undo command test : failed -_-

---
+++ TaskManager/TaskManager_210042112/TaskManager_112/TaskManagerTests/UndoCommandTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager_112;
+
+namespace TaskManagerTests
+{
+    public class UndoCommandTest
+    {
+        [Fact]
+        public void UndoCommand_ShouldRevertLastAction()
+        {
+            var repo = new IssueRepository();
+            var iss = IssueFactory.CreateIssue(12, "LOL","Papercut", Priority.Low);
+
+            var createissue = new CreateIssueCommand(repo, iss);
+            var invoke_the_joker = new CommandInvoker();
+            invoke_the_joker.ExecuteCommand(createissue);
+
+            var changestatus = new ChangeIssueStatusCommand(iss, Status.Resolved);
+            invoke_the_joker.ExecuteCommand(changestatus);
+
+            //Act
+            invoke_the_joker.Undo();
+
+            //Assert
+            Assert.Equal(Status.UnderReview,iss.Status);
+
+        }
+
+    }
+}
