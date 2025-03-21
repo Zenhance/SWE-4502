8d30040faf933ea37eec22df4e8fb337d1040d7d
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-21 13:48:25 +0600

    Issue : factory for creating issue

---
+++ TaskManager/TaskManager_210042112/TaskManager_112/TaskManager_112/IssueFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager_112
+{
+    public static class IssueFactory
+    {
+        public static Issue CreateIssue(int id, string title, string description, Priority priority)
+        {
+            return new Issue(id, title, description, priority);
+        }
+    }
+}
