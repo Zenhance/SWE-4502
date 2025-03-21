ab4285db2ec3f321c269c7eeaa9b6caf65a75b9f
bhuiyan.minhajul@gmail.com <bhuiyan.minhajul@gmail.com>
2025-03-21 14:30:01 +0600

    Adding CommandManager

---
+++ TaskManager/TaskManager/Command/CommandManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Command
+{
+    class CommandManager
+    {
+        private Stack<ICommand> _undoStack = new Stack<ICommand>();
+        private Stack<ICommand> _redoStack = new Stack<ICommand>();
+
+        public void ExecuteCommand(ICommand command)
+        {
+            command.Execute();
+            _undoStack.Push(command);
+            _redoStack.Clear(); 
+        }
+
+
+
+    }
+}
