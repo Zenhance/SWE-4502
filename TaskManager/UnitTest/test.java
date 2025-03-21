db51363092bddcbba5edeafc65bfbb991ac0b2d6
Nafisa Kibria <nafisa21@iut-dhaka.edu>
2025-03-21 11:10:35 +0600

    #1 : created Issue class and setup for unit test

---
+++ TaskManager/UnitTest/test.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class test {
+    @Test
+    public void test()
+    {
+
+    }
+}
---
+++ TaskManager/src/Issue.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class Issue {
+    public int Id;
+    public String title;
+    public String description;
+    public int priority;
+    public String status;
+    public Date CreationDate;
+    public Date LastModifiedDate;
+    public String Assignedto;
+    public String Tags[];
+
+
+}
