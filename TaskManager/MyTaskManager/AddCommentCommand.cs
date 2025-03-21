a2592aa8d1824814586498e825776a520ae65667
whiteflags26 <whiteflags26@gmail.com>
2025-03-21 14:12:41 +0600

    Closed #F02

---
+++ TaskManager/MyTaskManager/AddCommentCommand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTaskManager
+{
+    public class AddCommentCommand : Command
+    {
+        public IssueRepository repository;
+        public int issueId;
+        public Comment comment;
+
+        public AddCommentCommand(IssueRepository repository, int issueId, string content, string author)
+        {
+            this.repository = repository;
+            this.issueId = issueId;
+            int commentId = repository.GetNextCommentId();
+            this.comment = new Comment(commentId, content, author);
+            this.Type = CommandType.Create;
+            this.Description = $"Added comment to issue id {issueId}";
+        }
+
+        public override void Execute()
+        {
+            Issue issue = repository.GetIssue(issueId);
+            if (issue != null)
+            {
+                issue.Comments.Add(comment);
+                issue.LastModifiedDate = DateTime.Now;
+                repository.UpdateIssue(issue);
+            }
+        }
+
+        public override void Undo()
+        {
+            Issue issue = repository.GetIssue(issueId);
+            if (issue != null)
+            {
+                foreach (var TempComment in issue.Comments)
+                {
+                    if (TempComment.Id == comment.Id) issue.Comments.Remove(TempComment);
+                }
+                
+                issue.LastModifiedDate = DateTime.Now;
+                repository.UpdateIssue(issue);
+            }
+        }
+    }
+}
--- TaskManager/MyTaskManagerTest/MyTaskManagerTest.cs
+++ TaskManager/MyTaskManagerTest/MyTaskManagerTest.cs
@@ -21,5 +21,21 @@ namespace MyTaskManagerTest
             Assert.Equal(Priority.Medium, retrievedIssue.Priority);
             Assert.Equal(Status.Open, retrievedIssue.Status);
         }
+
+        [Fact]
+        public void AddComment_ShouldAddCommentToIssue()
+        {
+            var issueRepository = new IssueRepository();
+            var createCommand = new CreateIssueCommand(issueRepository, "Test Issue", "This is a test issue", Priority.Medium);
+            createCommand.Execute();
+            var retrievedIssue = issueRepository.GetIssue(createCommand.issue.Id);
+
+            var addCommentToIssueCommand = new AddCommentCommand(issueRepository, createCommand.issue.Id, "Test comment", "Test author");
+            addCommentToIssueCommand.Execute();
+
+            Assert.Single(retrievedIssue.Comments);
+            Assert.Equal("Test comment", retrievedIssue.Comments[0].Content);
+            Assert.Equal("Test author", retrievedIssue.Comments[0].Author);
+        }
     }
 }
