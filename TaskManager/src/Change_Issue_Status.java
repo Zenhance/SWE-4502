e07b303864d43e00d30192c93c5c8ad15dca449e
RIFAF RAHMAN <127125526+MathaThikNai@users.noreply.github.com>
2025-03-21 14:00:15 +0600

    14

--- TaskManager/src/Change_Issue_Status.java
+++ TaskManager/src/Change_Issue_Status.java
@@ -2,23 +2,23 @@
 public class Change_Issue_Status implements Command {
     private IssueRepository repository;
     private String issueId;
-    private Status oldStatus;
+    private Status OldStatus;
     private Status newStatus;
 
     public Change_Issue_Status(IssueRepository repository, String issueId, Status newStatus) {
         this.repository = repository;
         this.issueId = issueId;
         this.newStatus = newStatus;
-        this.oldStatus = repository.getIssue(issueId).getStatus();
+        this.OldStatus = repository.getIssue(issueId).getStatus();
     }
-
-
-
     @Override
     public void undo() {
         Issue issue = repository.getIssue(issueId);
-        issue.setStatus(oldStatus);
+        issue.setStatus(OldStatus);
     }
+
+
+
 
     @Override
     public void execute() {
