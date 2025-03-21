a21d0ccba1571e563cbfa2ddbe35abcc28c16111
ramisaferdous <rifahramisa@iut-dhaka.edu>
2025-03-21 15:05:16 +0600

    #F01: Create Issue Test

--- TaskManager/Test/src/CreateIssueTest.java
+++ TaskManager/Test/src/CreateIssueTest.java
@@ -1,10 +1,31 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CreateIssueTest {
-    @Test
-    public void CreateIssue_ShouldAddIssueToRepository(){
-        Issue issue= new Issue("First Issue","Completed first issue");
+    private IssueRepository repo;
+    private Issue issue;
 
-        assertEquals("The issue has been created successfully", issue.createIssue(13,"First Issue","Completed first issue"),"Succesfull");
+    @BeforeEach
+    void setup() {
+        repo = IssueRepository.getInstance();
+        issue = new Issue("bug fixing", "A Ui bug was found", Priority.MEDIUM);
+        repo.createIssue(issue);
+    }
+
+    @Test
+    void testcreateIssue() {
+        assertNotNull(repo.getIssue(issue.getId()));
+    }
+
+    @Test
+    void testGetIssue() {
+        Issue retrievedIssue = repo.getIssue(issue.getId());
+        assertEquals(issue.getId(), retrievedIssue.getId());
+    }
+
+    @Test
+    void testRemoveIssue() {
+        repo.removeIssue(issue.getId());
+        assertNull(repo.getIssue(issue.getId()));
     }
 }
