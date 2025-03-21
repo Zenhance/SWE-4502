802f43ae6adb1505f372feb84c9747feb4e045a0
Hasibul Islam Nirjhar <hasibulislam@iut-dhaka.edu>
2025-03-21 12:23:09 +0600

    Comment added

--- IssueTracker-Final/src/Comment.java
+++ IssueTracker-Final/src/Comment.java
@@ -1,4 +1,30 @@
+import java.time.LocalDateTime;
+
 public class Comment {
+
+    public int Id;
+
+    public String Content;
+
+    public String Author;
+
+    public LocalDateTime CreatedAt;
+
+
+    public Comment (int ID, String Content, String Author, LocalDateTime CreatedAt){
+
+        this.Id=ID;
+        this.Content=Content;
+
+        this.Author=Author;
+
+        this.CreatedAt=CreatedAt;
+
+    }
+
+
+
+
 
 
 
