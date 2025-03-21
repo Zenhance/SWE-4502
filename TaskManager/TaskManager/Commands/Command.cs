cd1c6d2f2ccb6b291c9a07c6124610c21d963e48
Tauhidur Rahman Tauhid <tauhid04023@gmail.com>
2025-03-21 11:42:39 +0600

    Command.cs created

---
+++ TaskManager/TaskManager/Commands/Command.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace TaskManager.Commands;
+
+public abstract class Command
+{
+    public abstract void Execute();
+    public abstract void Undo();
+    public string Description{ get; set; }
+    public DateTime Timestamp { get; set; }
+    public CommandType Type { get; set; }
+}
+
+public enum CommandType
+{
+    Create,
+    Update,
+    Delete
+}
