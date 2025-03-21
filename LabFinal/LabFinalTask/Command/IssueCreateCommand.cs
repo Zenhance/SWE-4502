4a9dd73c84610ebf33e8b5dafbc001c03641e1ca
sakhadib <adibsakhawat@iut-dhaka.edu>
2025-03-21 12:02:56 +0600

    IssueCreateCommand.cs

--- LabFinal/LabFinalTask/Command/IssueCreateCommand.cs
+++ LabFinal/LabFinalTask/Command/IssueCreateCommand.cs
@@ -10,19 +10,21 @@ public class IssueCreateCommand : ICommand
     private Issue _issue;
     private IssuePrototype _issuePrototype;
     
-    public IssueCreateCommand(IssueManager issueManager)
+    public IssueCreateCommand(IssueManager issueManager, Issue issue)
     {
         _issueManager = issueManager;
+        _issue = issue;
     }
 
     public void execute()
     {
-        
+        _issuePrototype = _issue.Log();
+        _issueManager.AddIssue(_issue);
     }
 
     public void undo()
     {
-        
+        _issueManager.RemoveIssue(_issue);
     }
 
 }
