314696b650656010d5e46fc81bc7acd452de8530
sakhadib <adibsakhawat@iut-dhaka.edu>
2025-03-21 12:26:59 +0600

    Commands

---
+++ LabFinal/LabFinalTask/Command/IssueDescriptionUpdateCommand.cs
@@ -0,0 +1,31 @@
+﻿using LabFinalTask.Core;
+using LabFinalTask.Model;
+
+namespace LabFinalTask.Command;
+
+public class IssueDescriptionUpdateCommand : ICommand
+{
+    private IssueManager _issueManager;
+    private Issue _issue;
+    private string _oldDescription;
+    private string _newDescription;
+    
+    public IssueDescriptionUpdateCommand(IssueManager issueManager, Issue issue, string newDescription)
+    {
+        _issueManager = issueManager;
+        _issue = issue;
+        _oldDescription = issue.description;
+        _newDescription = newDescription;
+    }
+
+    public void execute()
+    {
+        _issueManager.UpdateIssueDescription(_issue, _newDescription);
+    }
+
+    public void undo()
+    {
+        _issueManager.UpdateIssueDescription(_issue, _oldDescription);
+    }
+    
+}
---
+++ LabFinal/LabFinalTask/Command/IssueTitleUpdateCommand.cs
@@ -0,0 +1,31 @@
+﻿using LabFinalTask.Core;
+using LabFinalTask.Model;
+
+namespace LabFinalTask.Command;
+
+public class IssueTitleUpdateCommand : ICommand
+{
+    private IssueManager _issueManager;
+    private Issue _issue;
+    private string _oldTitle;
+    private string _newTitle;
+    
+    public IssueTitleUpdateCommand(IssueManager issueManager, Issue issue, string newTitle)
+    {
+        _issueManager = issueManager;
+        _issue = issue;
+        _oldTitle = issue.title;
+        _newTitle = newTitle;
+    }
+
+    public void execute()
+    {
+        _issueManager.UpdateIssueTitle(_issue, _newTitle);
+    }
+
+    public void undo()
+    {
+        _issueManager.UpdateIssueTitle(_issue, _oldTitle);
+    }
+    
+}
