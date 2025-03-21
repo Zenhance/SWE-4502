bc466a8a7813a71010cd74b2ee8e2882945363fc
Atik Shahriar Ovi <atikshahriar@iut-dhaka.edu>
2025-03-21 15:22:33 +0600

    Update Issue

--- TaskManager/src/main/java/org.example/Issue.java
+++ TaskManager/src/main/java/org.example/Issue.java
@@ -8,16 +8,22 @@ public class Issue
 {
 
 
-    public String IssueID,Description,Status,Title,Priority;
+    public String IssueID,Description,Title,Priority;
     public Date CreationDate;
     public Date LastModifiedDate;
     public String Tags[];
     public List<Comment> commentList =new ArrayList<>();
+    public Status status;
 
-
-
-
-
-
-
+    public Issue(String issueID, String description, String title, String priority, Date creationDate, Date lastModifiedDate, String[] tags, List<Comment> commentList, Status status) {
+        IssueID = issueID;
+        Description = description;
+        Title = title;
+        Priority = priority;
+        CreationDate = creationDate;
+        LastModifiedDate = lastModifiedDate;
+        Tags = tags;
+        this.commentList = commentList;
+        this.status = status;
+    }
 }
