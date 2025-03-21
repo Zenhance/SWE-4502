06c7c2513f2141f5285509f64b8b074a4f7b7d31
Nafisa Kibria <nafisa21@iut-dhaka.edu>
2025-03-21 13:55:16 +0600

    Updated IssueRepository

--- TaskManager/src/IssueRepository.java
+++ TaskManager/src/IssueRepository.java
@@ -1,6 +1,20 @@
 import java.util.HashMap;
+import java.util.Map;
 
 public class IssueRepository {
-    HashMap<Integer, Issue> hm1 = new HashMap<>();
+    private final Map<Integer, Issue> issues = new HashMap<>();
+    private int nextId = 1;
+    public void addIssue(Issue issue) {
+        issues.put(issue.getId(), issue);
+    }
+    public Issue getIssue(int id) {
+        return issues.get(id);
+    }
+    public void removeIssue(int id) {
+        issues.remove(id);
+    }
+    public int getIssueCount() {
+        return issues.size();
+    }
 
 }
