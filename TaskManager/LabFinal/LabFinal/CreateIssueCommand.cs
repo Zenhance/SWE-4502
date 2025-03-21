80db8b8aecf8807fa708963fe29399d02665c0ab
rhydita <rhyd.ita77@gmail.com>
2025-03-21 14:21:38 +0600

    issue repository class created

---
+++ TaskManager/LabFinal/LabFinal/CreateIssueCommand.cs
@@ -0,0 +1,28 @@
+﻿namespace LabFinal
+{
+    public class CreateIssueCommand : ICommand
+    {
+        private readonly IssueRepository _repository;
+        private readonly Issue _issue;
+        private CommandLog _log;
+
+        public CreateIssueCommand(IssueRepository repository, Issue issue)
+        {
+            _repository = repository;
+            _issue = issue;
+            _log = new CommandLog(CommandType.Create, $"Created Issue with ID {_issue.Id}");
+        }
+
+        public void Execute()
+        {
+            _repository.AddIssue(_issue);
+        }
+
+        public void Undo()
+        {
+            _repository.RemoveIssue(_issue.Id);
+        }
+
+        public CommandLog GetLog() => _log;
+    }
+}
---
+++ TaskManager/LabFinal/LabFinal/IssueRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace LabFinal
+{
+    public class IssueRepository
+    {
+        private readonly Dictionary<int, Issue> _issues = new Dictionary<int, Issue>();
+
+        public void AddIssue(Issue issue)
+        {
+            _issues[issue.Id] = issue;
+        }
+
+        public void RemoveIssue(int issueId)
+        {
+            _issues.Remove(issueId);
+        }
+
+        public Issue GetIssue(int issueId)
+        {
+            return _issues[issueId];
+        }
+    }
+}
