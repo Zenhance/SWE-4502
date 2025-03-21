e5a96ebbbaec5d3690db158036a57b873ae68bb7
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-21 15:10:45 +0600

    #F06: Observer notification test : couldn't do it

---
+++ TaskManager/TaskManager_210042112/TaskManager_112/TaskManagerTests/ObserverNotificationTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagerTests
+{
+    public class ObserverNotificationTest
+    {
+        [Fact]
+        public void Observer_ShouldBeNotifiedOfCommands()
+        { }
+    }
+}
