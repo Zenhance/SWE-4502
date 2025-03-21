dfb4fa113e194849e5a382dabee172f1772a2759
sakhadib <adibsakhawat@iut-dhaka.edu>
2025-03-21 12:29:59 +0600

    Issue Priority and Status

---
+++ LabFinal/LabFinalTask/Command/IssuePriorityUpdateCommand.cs
@@ -0,0 +1,32 @@
+﻿using LabFinalTask.Core;
+using LabFinalTask.ENUM;
+using LabFinalTask.Model;
+
+namespace LabFinalTask.Command;
+
+public class IssuePriorityUpdateCommand : ICommand
+{
+    private IssueManager _issueManager;
+    private Issue _issue;
+    private PRIORITY _oldPriority;
+    private PRIORITY _newPriority;
+    
+    public IssuePriorityUpdateCommand(IssueManager issueManager, Issue issue, PRIORITY newPriority)
+    {
+        _issueManager = issueManager;
+        _issue = issue;
+        _oldPriority = issue.priority;
+        _newPriority = newPriority;
+    }
+
+    public void execute()
+    {
+        _issueManager.UpdateIssuePriority(_issue, _newPriority);
+    }
+
+    public void undo()
+    {
+        _issueManager.UpdateIssuePriority(_issue, _oldPriority);
+    }
+    
+}
---
+++ LabFinal/LabFinalTask/Command/UpdateIssueStatusCommand.cs
@@ -0,0 +1,32 @@
+﻿using LabFinalTask.Core;
+using LabFinalTask.ENUM;
+using LabFinalTask.Model;
+
+namespace LabFinalTask.Command;
+
+public class UpdateIssueStatusCommand : ICommand
+{
+    private IssueManager _issueManager;
+    private Issue _issue;
+    private STATUS _oldStatus;
+    private STATUS _newStatus;
+    
+    public UpdateIssueStatusCommand(IssueManager issueManager, Issue issue, STATUS newStatus)
+    {
+        _issueManager = issueManager;
+        _issue = issue;
+        _oldStatus = issue.status;
+        _newStatus = newStatus;
+    }
+
+    public void execute()
+    {
+        _issueManager.UpdateIssueStatus(_issue, _newStatus);
+    }
+
+    public void undo()
+    {
+        _issueManager.UpdateIssueStatus(_issue, _oldStatus);
+    }
+    
+}
