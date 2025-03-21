5a5ad4284efdc0076afb25b64848e73f80724b4c
Atik Shahriar Ovi <atikshahriar@iut-dhaka.edu>
2025-03-21 14:33:27 +0600

    change folder path

---
+++ TaskManager/src/main/java/org.example/Comment.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.Date;
+
+public class Comment
+{
+
+    public String Id,Content;
+    public Date CreatedAt;
+    //public User Author;
+
+}
---
+++ TaskManager/src/test/java/org.example/TestCodes.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestCodes {
+    @Test
+    public void testThings()
+    {
+        Square sq = new Square();
+        assertEquals(5, sq.area());  // Example test case
+
+
+    }
+}
