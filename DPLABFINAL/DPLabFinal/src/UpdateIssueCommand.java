f5860ac05ff254b18ef2b373f704313bc636729f
nstuli <nusratsiddiquetuli@gmail.com>
2025-03-21 12:14:44 +0600

    update issue command

--- DPLABFINAL/DPLabFinal/src/UpdateIssueCommand.java
+++ DPLABFINAL/DPLabFinal/src/UpdateIssueCommand.java
@@ -4,6 +4,24 @@ public class UpdateIssueCommand implements Command{
     private String newTitle;
     private String newDescription;
 
-    
+    public UpdateIssueCommand(IssueRepository repository, Issue issue, String newTitle, String newDescription) {
+        this.repository = repository;
+        this.issue = issue;
+        this.newTitle = newTitle;
+        this.newDescription = newDescription;
+    }
+
+    @Override
+    public void execute()
+    {
+        issue.setTitle(newTitle);
+        issue.setDescription((newDescription));
+    }
+
+    @Override
+    public void undo()
+    {
+        System.out.println("undo the update here!!");
+    }
 
 }
