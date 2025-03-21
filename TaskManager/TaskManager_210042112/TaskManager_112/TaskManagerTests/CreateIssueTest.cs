73f70a800ed9961c4c09d59cebd6fa2644ef5cb2
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-21 15:20:54 +0600

    #F10 : observer removal test

--- TaskManager/TaskManager_210042112/TaskManager_112/TaskManagerTests/CreateIssueTest.cs
+++ TaskManager/TaskManager_210042112/TaskManager_112/TaskManagerTests/CreateIssueTest.cs
@@ -7,18 +7,18 @@ namespace TaskManagerTests
         [Fact]
         public void CreateIssue_ShouldAddIssueToRepository()
         {
-            var repository = new IssueRepository();
-            var issue = IssueFactory.CreateIssue(1, "First Issue", "Description", Priority.Medium);
-            var createIssueCommand = new CreateIssueCommand(repository, issue);
+            var repo = new IssueRepository();
+            var issue = IssueFactory.CreateIssue(1, "Trying", "Commiting is hard", Priority.Medium);
+            var createIssueCommand = new CreateIssueCommand(repo, issue);
             var invoker = new CommandInvoker();
 
             // Act
             invoker.ExecuteCommand(createIssueCommand);
 
             // Assert
-            var retrievedIssue = repository.GetIssue(1);
+            var retrievedIssue = repo.GetIssue(1);
             Assert.NotNull(retrievedIssue);
-            Assert.Equal("First Issue", retrievedIssue.Title);
+            Assert.Equal("Trying", retrievedIssue.Title);
             Assert.Equal(Priority.Medium, retrievedIssue.Priority);
             Assert.Equal(Status.Open, retrievedIssue.Status);
         }
---
+++ TaskManager/TaskManager_210042112/TaskManager_112/TaskManagerTests/ObserverRemovalTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager_112;
+
+namespace TaskManagerTests
+{
+    public class ObserverRemovalTest
+    {
+        [Fact]
+        public void RemoveObserver_ShouldStopNotifications()
+        {
+            var repo = new IssueRepository();
+            var issue = IssueFactory.CreateIssue(122, "Last?", "LLL", Priority.Low);
+            var createIssue = new CreateIssueCommand(repo, issue);
+            var invoker = new CommandInvoker();
+            var issueManager = new IssueManager();
+            var observer1 = new IssueObserver("namisa");
+            issueManager.Attach(observer1);
+            issueManager.Detach(observer1);
+
+            // Act
+            invoker.ExecuteCommand(createIssue);
+            issueManager.Notify("Issue Created: " + issue.Title);
+            
+        }
+    }
+}
