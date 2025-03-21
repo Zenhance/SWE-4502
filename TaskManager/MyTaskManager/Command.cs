1da6571807e1e6d8e9ca417d07fa30149d778f90
whiteflags26 <whiteflags26@gmail.com>
2025-03-21 12:06:49 +0600

    feat(command): implement abstract Command class

---
+++ TaskManager/MyTaskManager/Command.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTaskManager
+{
+    public abstract class Command
+    {
+        public string Description { get; set; }
+        public DateTime Timestamp { get; set; }
+        public CommandType Type { get; set; }
+
+        public Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+
+        public abstract void Execute();
+        public abstract void Undo();
+    }
+}
