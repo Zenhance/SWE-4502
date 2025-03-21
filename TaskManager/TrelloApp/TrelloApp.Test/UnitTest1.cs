e5ebad59f173c73750ef5ee83ce316550516e54d
Ahmed Sadman Labib <aslabib44@gmail.com>
2025-03-21 13:53:18 +0600

    Verify that the issue exists in repository with correct properties and default status

--- TaskManager/TrelloApp/TrelloApp.Test/UnitTest1.cs
+++ TaskManager/TrelloApp/TrelloApp.Test/UnitTest1.cs
@@ -1,3 +1,5 @@
+using static TrelloApp.Issue;
+
 namespace TrelloApp.Test
 {
     public class UnitTest1
@@ -5,7 +7,22 @@ namespace TrelloApp.Test
         [Fact]
         public void CreateIssue_ShouldAddIssueToRepository()
         {
-            Issue issue = new issue(1, "Change Password", "Dummy desc", );
+            int id = 1;
+            Issue issue = new Issue(id, "Change Password", "Dummy description", Issue.priority.Low, 
+                Issue.status.Open, 21);
+
+            IssueRepository repository = new IssueRepository(issue);
+
+            repository.createIssue(issue);
+
+            Assert.True(repository.issues.ContainsKey(id)); 
+            var addedIssue = repository.issues[id];
+
+            Assert.Equal(issue.title, addedIssue.title); 
+            Assert.Equal(issue.description, addedIssue.description); 
+            Assert.Equal(Issue.priority.Low, addedIssue.priorityType); 
+            Assert.Equal(Issue.status.Open, addedIssue.statusType); 
+            Assert.Equal(issue.creationDate, addedIssue.creationDate);
         }
     }
 }
