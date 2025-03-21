8d17f583b86cc66fc32d16207267e85e02f75ed4
Atik Shahriar Ovi <atikshahriar@iut-dhaka.edu>
2025-03-21 15:23:13 +0600

    Add IssueRepository class

---
+++ TaskManager/src/main/java/org.example/IssueRepository.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+public class IssueRepository {
+    private Map<String, Issue> issues = new HashMap<>();
+
+    public void addIssue(Issue issue) {
+        issues.put(issue.getId(), issue);
+    }
+
+    public Issue getIssue(String id) {
+        return issues.get(id);
+    }
+
+    public void updateIssue(Issue issue) {
+        issues.put(issue.getId(), issue);
+    }
+
+    public void deleteIssue(String id) {
+        issues.remove(id);
+    }
+}
