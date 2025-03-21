1e23b06f16f8886575f774b3e883c45d844bbe28
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2025-03-21 12:28:00 +0600

    Add model: CommandLog

--- TaskManager/task-manager-lab-final/src/main/java/models/CommandLog.java
+++ TaskManager/task-manager-lab-final/src/main/java/models/CommandLog.java
@@ -1,2 +1,15 @@
-package models;public class CommandLog {
+package models;
+
+import java.security.Timestamp;
+
+public class CommandLog {
+    private String description;
+    private Timestamp timestamp;
+    private CommandType type;
+
+    public CommandLog(String description, Timestamp timestamp, CommandType type){
+        this.description = description;
+        this.timestamp = timestamp;
+        this.type = type;
+    }
 }
