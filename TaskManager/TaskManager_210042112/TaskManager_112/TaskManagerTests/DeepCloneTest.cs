e4bea739886bab3d9ef7600c24b4e095adef0bd4
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-21 15:15:59 +0600

    #F13 : deep clone test passed

---
+++ TaskManager/TaskManager_210042112/TaskManager_112/TaskManagerTests/DeepCloneTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager_112;
+
+namespace TaskManagerTests
+{
+    public class DeepCloneTest
+    {
+        [Fact]
+        public void Issue_Clone_ShouldCreateCompleteDeepCopy()
+        {
+            var issue = IssueFactory.CreateIssue(1, "TTT", "DDD", Priority.Critical);
+
+            // Act
+            var clonedIssue = new Issue(issue.ID, issue.Title, issue.Description, issue.Priority);
+
+            // Assert
+            Assert.NotSame(issue, clonedIssue);
+            Assert.Equal(issue.Title, clonedIssue.Title);
+        }
+    }
+}
