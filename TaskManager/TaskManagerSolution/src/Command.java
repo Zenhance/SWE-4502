0a124053cc117e68593c2bcd3ce293802a2914db
Kashshaf-Labib <labibkashshaf@gmail.com>
2025-03-21 11:55:56 +0600

    feat(Command Class): implement Command class (issue #F04, #F05)

--- TaskManager/TaskManagerSolution/src/Command.java
+++ TaskManager/TaskManagerSolution/src/Command.java
@@ -1,3 +1,29 @@
+import java.time.LocalDateTime;
+
 public abstract class Command {
+    private String description;
+    private LocalDateTime timestamp;
+    private CommandType type;
+
+    public Command(String description, CommandType type) {
+        this.description = description;
+        this.timestamp = LocalDateTime.now();
+        this.type = type;
+    }
+
+    public abstract void execute();
+    public abstract void undo();
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public CommandType getType() {
+        return type;
+    }
 
 }
