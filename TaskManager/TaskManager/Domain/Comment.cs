21d29fb331b72bf6d11b932c9a0781b9095deac8
sakibhossain323 <sakibhossain323@gmail.com>
2025-03-21 11:29:57 +0600

    #F01 : create domain model

---
+++ TaskManager/TaskManager/Domain/Comment.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskManager.Domain;
+
+public class Comment
+{
+    public int Id { get; set; }
+    public string? Content { get; set; }
+    public string? Author { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
---
+++ TaskManager/TaskManager/Domain/Issue.cs
@@ -0,0 +1,33 @@
+﻿namespace TaskManager.Domain;
+
+
+public enum IssuePriority
+{
+    Low,
+    Medium,
+    High,
+    Critical
+}
+
+public enum IssueStatus
+{
+    Open,
+    InProgress,
+    UnderReview,
+    Resolved,
+    Closed
+}
+public class Issue
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public IssuePriority Priority { get; set; }
+    public IssueStatus Status { get; set; } = IssueStatus.Open;
+    public DateTime CreationDate { get; set; }
+    public DateTime LastModifiedDate { get; set; }
+    public string? AssigendTo { get; set; }
+    public HashSet<string> Tags { get; set; } = new();
+
+
+}
