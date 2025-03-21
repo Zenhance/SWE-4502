e295fdbd03f5767597489f9bb45a05b0d5446709
nstuli <nusratsiddiquetuli@gmail.com>
2025-03-21 13:39:34 +0600

    command log , email observer , notification system

--- DPLABFINAL/DPLabFinal/src/NotificationService.java
+++ DPLABFINAL/DPLabFinal/src/NotificationService.java
@@ -1,7 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
-
 public class NotificationService {
     private List<Observer> observers = new ArrayList<>();
-    public void
+    public void registerObserver(Observer observer) {
+        observers.add(observer);
+    }
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+    public void notifyObservers(String message) {
+        for (Observer observer : observers) {
+            observer.update(message);
+        }
+    }
 }
