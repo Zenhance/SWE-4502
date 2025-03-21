65d77466e679d2542137f0fca2ffa2c1bad04760
Kashshaf-Labib <labibkashshaf@gmail.com>
2025-03-21 12:05:56 +0600

    feat(Command Manager): implement command manager to do redo, undo operation (issue: #F11, #F12)

---
+++ TaskManager/TaskManagerSolution/src/CommandManager.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class CommandManager {
+    private final Stack<Command> undoStack= new Stack<>();
+    private final Stack<Command> redoStack = new Stack<>();
+
+    private final List<Command> commandHistory = new ArrayList<>();
+
+    public void executeCommand(Command command) {
+        command.execute();
+        undoStack.push(command);
+        commandHistory.add(command);
+        redoStack.clear();
+    }
+
+    public void undo() {
+        if (!undoStack.isEmpty()) {
+            Command command = undoStack.pop();
+            command.undo();
+            redoStack.push(command);
+        }
+    }
+
+    public void redo() {
+        if (!redoStack.isEmpty()) {
+            Command command = redoStack.pop();
+            command.execute();
+            undoStack.push(command);
+        }
+    }
+
+    public List<Command> getCommandHistory() {
+        return new ArrayList<>(commandHistory);
+    }
+}
