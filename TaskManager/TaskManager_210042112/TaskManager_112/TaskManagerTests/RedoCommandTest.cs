653c3e17979f1fecc2666268ecd6276a4eded0e9
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-03-21 15:08:00 +0600

    #F05 : redo command test passed(copied from the undo test just added the redo function) + #F04 : undo command passed

--- TaskManager/TaskManager_210042112/TaskManager_112/TaskManagerTests/RedoCommandTest.cs
+++ TaskManager/TaskManager_210042112/TaskManager_112/TaskManagerTests/RedoCommandTest.cs
@@ -7,13 +7,13 @@ using TaskManager_112;
 
 namespace TaskManagerTests
 {
-    public class UndoCommandTest
+    public class RedoCommandTest
     {
         [Fact]
-        public void UndoCommand_ShouldRevertLastAction()
+        public void RedoCommand_ShouldReapplyUndoneAction()
         {
             var repo = new IssueRepository();
-            var iss = IssueFactory.CreateIssue(12, "LOL","Papercut", Priority.Low);
+            var iss = IssueFactory.CreateIssue(12, "Ugh", "Papercut2", Priority.High);
 
             var createissue = new CreateIssueCommand(repo, iss);
             var invoke_the_joker = new CommandInvoker();
@@ -22,13 +22,14 @@ namespace TaskManagerTests
             var changestatus = new ChangeIssueStatusCommand(iss, Status.Resolved);
             invoke_the_joker.ExecuteCommand(changestatus);
 
-            //Act
+            
             invoke_the_joker.Undo();
 
+            //Act
+            invoke_the_joker.Redo();
+
             //Assert
-            Assert.Equal(Status.Open,iss.Status);
-
+            Assert.Equal(Status.Resolved, iss.Status);
         }
-
     }
 }
