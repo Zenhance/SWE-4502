16f056c7460792694192f033f2ff252aa98d0517
Tauhidur Rahman Tauhid <tauhid04023@gmail.com>
2025-03-21 14:51:11 +0600

    Created StatisticsCollector.cs

---
+++ TaskManager/TaskManager/Services/StatisticsCollector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using TaskManager.Commands;
+namespace TaskManager.Services;
+
+public class StatisticsCollector
+{
+    private readonly Dictionary<CommandType, int> _commandcounts=new Dictionary<CommandType, int>();
+
+    public void TrackCommand(Command command)
+    {
+        if (_commandcounts.ContainsKey(command.Type))
+        {
+            _commandcounts[command.Type]++;
+        }
+        else
+        {
+            _commandcounts[command.Type] = 1;
+        }
+    }
+
+    public int GetCommandCount(CommandType type)
+    {
+        return _commandcounts.ContainsKey(type) ? _commandcounts[type] : 0;
+    }
+}
