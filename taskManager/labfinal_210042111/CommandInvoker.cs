0b9a1dbab9c2843bd745ca6c8d12140547637e72
sheona001 <sirexcc001@gmail.com>
2025-03-21 14:13:22 +0600

    Issue #F01: Create Issue Test

---
+++ taskManager/labfinal_210042111/CommandInvoker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace labfinal_210042111
+{
+    public class CommandInvoker
+    {
+        private Stack<ICommand> commandStack = new Stack<ICommand>();
+        private Stack<ICommand> undoStack = new Stack<ICommand>();
+
+        public void executeCommand(ICommand command)
+        {
+            command.Execute();
+            commandStack.Push(command);
+            undoStack.Clear();
+        }
+        public void undo()
+        {
+            if (!commandStack.Any())
+            {
+                ICommand command = commandStack.Pop();
+                command.undo();
+                undoStack.Push(command);
+            }
+        }
+        public void redo()
+        {
+            if (!undoStack.Any())
+            {
+                ICommand command = undoStack.Pop();
+                command.Execute();
+                commandStack.Push(command);
+            }
+
+        }
+    }
+}
--- taskManager/labfinaltest/UnitTest1.cs
+++ taskManager/labfinaltest/UnitTest1.cs
@@ -6,19 +6,24 @@ namespace labfinaltest
 {
     public class UnitTest1
     {
+      //Issue #F01: Create Issue Test
         [Fact]
-        public void Test1()
+        public void CreateIssueTest()
         {
             
-          //invoker
+            CommandInvoker invoker = new CommandInvoker();
           
 
             Issue issue = new Issue(1, "FIRST", "FIRST Description", Priority.medium, "Alice");
             IssueRepository repository=new IssueRepository(1, issue);
             CreateIssueCommand createCommand = new CreateIssueCommand(repository, issue);
-            //invoker.executeCommand(createCommand);
+            invoker.executeCommand(createCommand);
             Issue retrieved = repository.getIssue(1);
-            //assert
+            
+            Assert.NotNull(retrieved);
+            Assert.Equal("FIRST", retrieved.getTitle());
+           
+            Assert.Equal(Status.open, retrieved.getStatus());
 
 
         }
