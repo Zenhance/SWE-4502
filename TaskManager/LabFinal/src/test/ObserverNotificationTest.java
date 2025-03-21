22951b9e5b845e3d3f4f8d339cfbab25a4b6f1ea
mdhralif <hasiburrahman999.alif@gmail.com>
2025-03-21 15:36:19 +0600

    #F10: Observer Removal Testing.

--- TaskManager/LabFinal/src/test/ObserverNotificationTest.java
+++ TaskManager/LabFinal/src/test/ObserverNotificationTest.java
@@ -30,5 +30,29 @@ public class ObserverNotificationTest
         Assertions.assertEquals(NotificationType.ISSUE_CREATED, testObserver.getNotifications().get(0).getType());
     }
 
+    //#F09:
+    @Test
+    void multipleObservers_ShouldAllBeNotified() {
+        TestObserver observer2 = new TestObserver();
+        taskManager.registerObserver(observer2);
+        taskManager.createIssue("Multiple Observer Test", "Testing", Priority.Medium);
+
+        Assertions.assertEquals(1, testObserver.getNotificationCount());
+        Assertions.assertEquals(1, observer2.getNotificationCount());
+    }
+
+    //#F10:
+    @Test
+    void removeObserver_ShouldStopNotifications() {
+        taskManager.createIssue("Before Remove", "Test", Priority.Low);
+        taskManager.removeObserver(testObserver);
+        taskManager.createIssue("After Remove", "Should not notify", Priority.High);
+
+        Assertions.assertEquals(1, testObserver.getNotificationCount());
+        Assertions.assertTrue(testObserver.getNotifications().get(0).getMessage().contains("Before Remove"));
+    }
+
+
+
 
 }
