4cc74ff44e9703d0ba422861bb1e6dce8bcc5a78
sheona001 <sirexcc001@gmail.com>
2025-03-21 15:00:58 +0600

    status

---
+++ taskManager/labfinal_210042111/CommandLogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace labfinal_210042111
+{
+    public class CommandLogger:IObserver
+    {
+        private List<string> logHistory = new List<string>();
+      
+        public void Update(string message)
+        {
+            logHistory.Add(message);
+        }
+        public List<string> getLogHistory()
+        {
+            return logHistory;
+        }
+    }
+}
--- taskManager/labfinal_210042111/StatisticsCollector.cs
+++ taskManager/labfinal_210042111/StatisticsCollector.cs
@@ -7,5 +7,28 @@ namespace labfinal_210042111
 {
     public class StatisticsCollector
     {
+        public int CreateCommandCount { get; private set; }
+        public int UpdateCommandCount { get; private set; }
+        public int DeleteCommandCount { get; private set; }
+        public int IssuesCreated { get; private set; }
+
+        public void Increment(CommandType type)
+        {
+            switch (type)
+            {
+                case CommandType.create:
+                    CreateCommandCount++;
+                    break;
+                case CommandType.update:
+                    UpdateCommandCount++;
+                    break;
+                case CommandType.delete:
+                    DeleteCommandCount++;
+                    break;
+            }
+        }
+
+        public void IncrementIssueCreated() => IssuesCreated++;
+
     }
 }
