100a6448eb8360d90beb9bd165692208ce254f70
bhuiyan.minhajul@gmail.com <bhuiyan.minhajul@gmail.com>
2025-03-21 14:31:59 +0600

    Fixing CommandManager

--- TaskManager/TaskManager/Command/CommandManager.cs
+++ TaskManager/TaskManager/Command/CommandManager.cs
@@ -8,14 +8,14 @@ namespace TaskManager.Command
 {
     class CommandManager
     {
-        private Stack<ICommand> _undoStack = new Stack<ICommand>();
-        private Stack<ICommand> _redoStack = new Stack<ICommand>();
+        private Stack<ICommand> undoStack = new Stack<ICommand>();
+        private Stack<ICommand> redoStack = new Stack<ICommand>();
 
         public void ExecuteCommand(ICommand command)
         {
             command.Execute();
-            _undoStack.Push(command);
-            _redoStack.Clear(); 
+            undoStack.Push(command);
+            redoStack.Clear(); 
         }
 
 
