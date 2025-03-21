9c03b495b68fbd74b1fecb9c231b73572a5db76b
sakhadib <adibsakhawat@iut-dhaka.edu>
2025-03-21 13:46:58 +0600

    CreateCommentCommand.cs

---
+++ LabFinal/LabFinalTask/Command/CreateCommentCommand.cs
@@ -0,0 +1,29 @@
+﻿using LabFinalTask.Core;
+using LabFinalTask.Model;
+
+namespace LabFinalTask.Command;
+
+public class CreateCommentCommand : ICommand
+{
+    private IssueManager _issueManager;
+    private Issue _issue;
+    private Comment _comment;
+    
+    public CreateCommentCommand(IssueManager issueManager, Issue issue, Comment comment)
+    {
+        _issueManager = issueManager;
+        _issue = issue;
+        _comment = comment;
+    }
+
+    public void execute()
+    {
+        _issueManager.AddIssueComment(_issue, _comment);
+    }
+
+    public void undo()
+    {
+        _issueManager.AddIssueComment(_issue, _comment);
+    }
+
+}
