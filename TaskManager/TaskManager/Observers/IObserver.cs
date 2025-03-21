76c7d15bccad341a12ce58f9e9e08aa4d967d9c7
Tauhidur Rahman Tauhid <tauhid04023@gmail.com>
2025-03-21 14:21:24 +0600

    Logger.cs created for Log Operation and for notifications interface  IObserver.cs created

---
+++ TaskManager/TaskManager/Observers/IObserver.cs
@@ -0,0 +1,6 @@
+﻿namespace TaskManager.Observers;
+
+public interface IObserver
+{
+    void Update(string message);
+}
---
+++ TaskManager/TaskManager/Services/Logger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using TaskManager.Commands;
+namespace TaskManager.Services;
+
+public class Logger
+{
+    private readonly List<string> _logs = new List<string>();
+
+    public void LogCommand(Command command)
+    {
+        var logEntry=$"{command.Timestamp}: {command.Description}";
+        _logs.Add(logEntry);
+    }
+    public IEnumerable<string> GetLogs()
+    {
+        return _logs;
+    }
+}
