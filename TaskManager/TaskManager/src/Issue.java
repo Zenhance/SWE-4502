6c633d3163c44dd02c9fa9983d88b0b6f470e2fb
labibahhoque <hoquelabibah@gmail.com>
2025-03-21 14:24:50 +0600

    Modified Issue

--- TaskManager/TaskManager/src/Issue.java
+++ TaskManager/TaskManager/src/Issue.java
@@ -1,6 +1,7 @@
 import java.util.Date;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Issue{
     public int Id;
@@ -12,38 +13,4 @@ public class Issue{
     public Date LastModifiedDate;
     public String AssignedTo;
     List<String>Tags=new ArrayList<>();
-    List<Comment>comments=new ArrayList<>();
-
-    public Issue create(){
-        return new Issue();
-    }
-
-    public void update(String property){
-        this.property=property;
-    }
-    public void changeStatus(){
-        switch(status){
-            case Open:
-                status=Open;
-                break;
-            case InProgress:
-                status=InProgress;
-                break;
-            case UnderReview:
-                status=UnderReview;
-                break;
-            case Resolved:
-                status=Resolved;
-                break;
-            case Closed:
-                status=Closed;
-                break;
-            default:
-                status=Open;
-                break;
-        }
-    }
-    public void addComment(Comment comment){
-        comments.add(comment);
-    }
 }
